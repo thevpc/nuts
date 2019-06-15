740c4575d101d3041b9b90d23a611891b310fc3e
vpc <vpc@localhost>
2019-06-15 19:08:19 +0100

    v0.5.6.0 : Fixed commandline aprse issue

--- nuts/src/main/java/net/vpc/app/nuts/NutsCommandLimited.java
+++ nuts/src/main/java/net/vpc/app/nuts/NutsCommandLimited.java
@@ -285,6 +285,16 @@ class NutsCommandLimited implements NutsCommandLine {
         return next(NutsArgumentType.ANY, names);
     }
 
+    private boolean isPrefixed(String[] nameSeqArray) {
+        for (int i = 0; i < nameSeqArray.length - 1; i++) {
+            NutsArgument x = get(i);
+            if (x == null || !x.getString().equals(nameSeqArray[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     @Override
     public NutsArgument next(NutsArgumentType expectValue, String... names) {
         if (expectValue == null) {
@@ -307,11 +317,8 @@ class NutsCommandLimited implements NutsCommandLine {
                     }
                 }
             }
-            for (int i = 0; i < nameSeqArray.length - 1; i++) {
-                NutsArgument x = get(i);
-                if (x == null || !x.getString().equals(nameSeqArray[i])) {
-                    return null;
-                }
+            if(!isPrefixed(nameSeqArray)){
+                continue;
             }
             String name = nameSeqArray[nameSeqArray.length - 1];
             NutsArgument p = get(nameSeqArray.length - 1);
