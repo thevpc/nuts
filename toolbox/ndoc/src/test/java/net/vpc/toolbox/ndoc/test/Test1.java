bca09cd8675addfa961f15d824d5abefa8d53452
thevpc <taha.bensalah@gmail.com>
2020-09-23 23:21:52 +0100

    WIP: repository preparation

---
+++ toolbox/ndoc/src/test/java/net/vpc/toolbox/ndoc/test/Test1.java
@@ -0,0 +1,77 @@
+/**
+ * ====================================================================
+ *            Nuts : Network Updatable Things Service
+ *                  (universal package manager)
+ * <br>
+ * is a new Open Source Package Manager to help install packages
+ * and libraries for runtime execution. Nuts is the ultimate companion for
+ * maven (and other build managers) as it helps installing all package
+ * dependencies at runtime. Nuts is not tied to java and is a good choice
+ * to share shell scripts and other 'things' . Its based on an extensible
+ * architecture to help supporting a large range of sub managers / repositories.
+ * <br>
+ * Copyright (C) 2016-2020 thevpc
+ * <br>
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * <br>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <br>
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * ====================================================================
+ */
+package net.vpc.toolbox.ndoc.test;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author vpc
+ */
+public class Test1 {
+
+    @Test
+    public void test1() {
+//        String s = " values are :\n"
+//                + "<ul>\n"
+//                + "<li>'local' : corresponds to a local updatable repository.\n"
+//                + "\n"
+//                + "will be named\n"
+//                + " 'local'</li>\n"
+//                + "<li>'m2', '.m2', 'maven-local' : corresponds the local maven folder\n"
+//                + "repository. will be named 'local'</li>\n"
+//                + "<li>'maven-central': corresponds the remote maven central repository.\n"
+//                + "will be named 'local'</li>\n"
+//                + "<li>'maven-git', 'vpc-public-maven': corresponds the remote maven\n"
+//                + "vpc-public-maven git folder repository. will be named 'local'</li>\n"
+//                + "<li>'maven-git', 'vpc-public-nuts': corresponds the remote nuts\n"
+//                + "vpc-public-nuts git folder repository. will be named 'local'</li>\n"
+//                + "<li>name=uri-or-path : corresponds the given uri. will be named name.\n"
+//                + "Here are some examples:\n"
+//                + "<ul>\n"
+//                + "<li>myremote=http://192.168.6.3/folder</li>\n"
+//                + "<li>myremote=/folder/subfolder</li>\n"
+//                + "<li>myremote=c:/folder/subfolder</li>\n"
+//                + "</ul>\n"
+//                + "</li>\n"
+//                + "<li>uri-or-path : corresponds the given uri. will be named uri's last\n"
+//                + "path component name. Here are some examples:\n"
+//                + "<ul>\n"
+//                + "<li>http://192.168.6.3/folder : will be named 'folder'</li>\n"
+//                + "<li>myremote=/folder/subfolder : will be named 'folder'</li>\n"
+//                + "<li>myremote=c:/folder/subfolder : will be named 'folder'</li>\n"
+//                + "</ul>\n"
+//                + "</li>\n"
+//                + "</ul>";
+//        JDDocElement u = JPDoc._JDDocElement(s);
+//        System.out.println(u);
+    }
+    
+}
