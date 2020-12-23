2d9300e6d566177212e24b51eaf48118e04deaf6
thevpc <taha.bensalah@gmail.com>
2020-12-23 14:07:09 +0100

    ADDED: website

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -85,7 +85,7 @@ module.exports = {
       {
         docs: {
           // It is recommended to set document id as docs home page (`docs/` path).
-          homePageId: 'get-started/installation',
+          homePageId: 'intro/introduction',
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
