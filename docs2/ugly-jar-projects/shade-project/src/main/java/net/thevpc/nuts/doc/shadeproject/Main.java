c22da74d45b217fe29140d2d170e50cf26207561
thevpc <taha.bensalah@gmail.com>
2021-11-29 14:28:21 +0100

    [FIXED] added example of ugly fat jar projects

---
+++ docs2/ugly-jar-projects/shade-project/src/main/java/net/thevpc/nuts/doc/shadeproject/Main.java
@@ -0,0 +1,16 @@
+package net.thevpc.nuts.doc.shadeproject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author vpc
+ */
+public class Main {
+    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        LOG.debug("A simple app with dependencies using maven-shade-plugin");
+    }
+}
---
+++ docs2/ugly-jar-projects/springboot-project/src/main/java/net/thevpc/nuts/doc/springbootproject/Main.java
@@ -0,0 +1,16 @@
+package net.thevpc.nuts.doc.springbootproject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author vpc
+ */
+public class Main {
+    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        LOG.debug("A simple app with dependencies using spring-boot-maven-plugin");
+    }
+}
