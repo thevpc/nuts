3840a7510b261f17a3f23d827ba282995788f237
thevpc <taha.bensalah@gmail.com>
2023-08-16 14:52:23 +0100

    [ADDED] added http codes

--- core/nuts/src/main/java/net/thevpc/nuts/web/NHttpCode.java
+++ core/nuts/src/main/java/net/thevpc/nuts/web/NHttpCode.java
@@ -5,15 +5,31 @@ import java.util.Map;
 import java.util.Objects;
 
 public class NHttpCode {
+    private static final Map<Integer, NHttpCode> cache = new HashMap<>();
     public static final NHttpCode OK = of(200);
+    public static final NHttpCode CREATED = of(201);
+    public static final NHttpCode ACCEPTED = of(202);
+    public static final NHttpCode NON_AUTHORITATIVE_INFORMATION = of(203);
+    public static final NHttpCode NO_CONTENT = of(204);
+    public static final NHttpCode RESET_CONTENT = of(205);
     public static final NHttpCode BAD_REQUEST = of(400);
     public static final NHttpCode UNAUTHORIZED = of(401);
+    public static final NHttpCode PAYMENT_REQUIRED = of(402);
     public static final NHttpCode FORBIDDEN = of(403);
     public static final NHttpCode NOT_FOUND = of(404);
     public static final NHttpCode METHOD_NOT_ALLOWED = of(405);
+    public static final NHttpCode METHOD_NOT_ACCEPTABLE = of(406);
+    public static final NHttpCode PROXY_AUTHENTICATION_REQUIRED = of(407);
+    public static final NHttpCode REQUEST_TIMEOUT = of(408);
+    public static final NHttpCode CONFLICT = of(409);
+    public static final NHttpCode GONE = of(410);
+    public static final NHttpCode LENGTH_REQUIRED = of(411);
+    public static final NHttpCode PRECONDITION_FAILED = of(412);
+    public static final NHttpCode CONTENT_TOO_LARGE = of(413);
+    public static final NHttpCode URI_TOO_LONG = of(414);
+    public static final NHttpCode UNSUPPORTED_MEDIA_TYPE = of(415);
     public static final NHttpCode INTERNAL_SERVER_ERROR = of(500);
     private int code;
-    private static final Map<Integer, NHttpCode> cache = new HashMap<>();
 
     public static NHttpCode of(int code) {
         synchronized (NHttpCode.cache) {
