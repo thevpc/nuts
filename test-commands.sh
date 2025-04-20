ae732f5c87561ed927aed42d96f512a266abdc3e
thevpc <taha.bensalah@gmail.com>
2025-04-20 16:48:49 +0100

    [ADDED] add bundle creation script

---
+++ test-commands.sh
@@ -0,0 +1,9 @@
+cd ~/programs/nuts-bundles/
+nuts-dev -ZyS --!switch
+nuts-dev bundle --yes nserver --lib=nsh
+nuts-dev bundle --yes kifkif
+nuts-dev bundle --yes pnote
+nuts-dev bundle --yes nsite
+nuts-dev bundle --yes nops
+nuts-dev bundle --yes njob
+nuts-dev bundle --yes net.thevpc.ndoc
