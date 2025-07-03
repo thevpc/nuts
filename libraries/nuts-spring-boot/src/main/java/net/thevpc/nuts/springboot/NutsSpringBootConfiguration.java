774acda342f49af74299d8ac39b4be9157530909
thevpc <taha.bensalah@gmail.com>
2025-07-03 18:56:44 +0100

    [CHANGED] now spring supports annotation Nuts applications

--- libraries/nuts-spring-boot/src/main/java/net/thevpc/nuts/springboot/NutsSpringBootConfiguration.java
+++ libraries/nuts-spring-boot/src/main/java/net/thevpc/nuts/springboot/NutsSpringBootConfiguration.java
@@ -12,6 +12,7 @@ import net.thevpc.nuts.format.NFormats;
 import net.thevpc.nuts.io.NIO;
 import net.thevpc.nuts.io.NPrintStream;
 import net.thevpc.nuts.io.NTerminal;
+import net.thevpc.nuts.time.NClock;
 import net.thevpc.nuts.time.NProgressMonitors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
@@ -34,57 +35,57 @@ public class NutsSpringBootConfiguration {
     private Environment env;
 
     @Bean
-    public NTerminal nTerminal(ApplicationArguments applicationArguments) {
+    public NTerminal nTerminal(@Autowired ApplicationArguments applicationArguments) {
         return nutsSession(applicationArguments).getTerminal();
     }
 
     @Bean
-    public NPrintStream nOut(ApplicationArguments applicationArguments) {
+    public NPrintStream nOut(@Autowired ApplicationArguments applicationArguments) {
         return nutsSession(applicationArguments).out();
     }
 
 
     @Bean
-    public NSession nutsSession(ApplicationArguments applicationArguments) {
+    public NSession nutsSession(@Autowired ApplicationArguments applicationArguments) {
         return nutsWorkspace(applicationArguments).currentSession();
     }
 
 
     @Bean
-    public NTerminal nutsTerminal(ApplicationArguments applicationArguments) {
+    public NTerminal nutsTerminal(@Autowired ApplicationArguments applicationArguments) {
         return nutsSession(applicationArguments).getTerminal();
     }
 
     @Bean
-    public NExprs nutsNExprs(ApplicationArguments applicationArguments) {
+    public NExprs nutsNExprs(@Autowired ApplicationArguments applicationArguments) {
         return nutsSession(applicationArguments).callWith(() -> {
             return NExprs.of();
         });
     }
 
     @Bean
-    public NIdFilters nutsIdFilters(ApplicationArguments applicationArguments) {
+    public NIdFilters nutsIdFilters(@Autowired ApplicationArguments applicationArguments) {
         return nutsSession(applicationArguments).callWith(() -> {
             return NIdFilters.of();
         });
     }
 
     @Bean
-    public NDependencyFilters nutsDependencyFilters(ApplicationArguments applicationArguments) {
+    public NDependencyFilters nutsDependencyFilters(@Autowired ApplicationArguments applicationArguments) {
         return nutsSession(applicationArguments).callWith(() -> {
             return NDependencyFilters.of();
         });
     }
 
     @Bean
-    public NDefinitionFilters nutsDefinitionFilters(ApplicationArguments applicationArguments) {
+    public NDefinitionFilters nutsDefinitionFilters(@Autowired ApplicationArguments applicationArguments) {
         return nutsSession(applicationArguments).callWith(() -> {
             return NDefinitionFilters.of();
         });
     }
 
     @Bean
-    public NLibPaths nutsLibPaths(ApplicationArguments applicationArguments) {
+    public NLibPaths nutsLibPaths(@Autowired ApplicationArguments applicationArguments) {
         return nutsSession(applicationArguments).callWith(() -> {
             return NLibPaths.of();
         });
@@ -92,35 +93,35 @@ public class NutsSpringBootConfiguration {
 
 
     @Bean
-    public NProgressMonitors nutsProgressMonitors(ApplicationArguments applicationArguments) {
+    public NProgressMonitors nutsProgressMonitors(@Autowired ApplicationArguments applicationArguments) {
         return nutsSession(applicationArguments).callWith(() -> {
             return NProgressMonitors.of();
         });
     }
 
     @Bean
-    public NIO nutsIO(ApplicationArguments applicationArguments) {
+    public NIO nutsIO(@Autowired ApplicationArguments applicationArguments) {
         return nutsSession(applicationArguments).callWith(() -> {
             return NIO.of();
         });
     }
 
     @Bean
-    public NFormats nutsFormats(ApplicationArguments applicationArguments) {
+    public NFormats nutsFormats(@Autowired ApplicationArguments applicationArguments) {
         return nutsSession(applicationArguments).callWith(() -> {
             return NFormats.of();
         });
     }
 
     @Bean
-    public NExtensions nutsExtensions(ApplicationArguments applicationArguments) {
+    public NExtensions nutsExtensions(@Autowired ApplicationArguments applicationArguments) {
         return nutsSession(applicationArguments).callWith(() -> {
             return NExtensions.of();
         });
     }
 
     @Bean
-    public NScheduler nutsScheduler(ApplicationArguments applicationArguments) {
+    public NScheduler nutsScheduler(@Autowired ApplicationArguments applicationArguments) {
         return nutsSession(applicationArguments).callWith(() -> {
             return NScheduler.of();
         });
@@ -133,14 +134,14 @@ public class NutsSpringBootConfiguration {
         });
     }
 
-
-    @Bean
-    public NApplication nutsApplication() {
+    private Object resolveValidSpringBootApplication(NWorkspace workspace, ApplicationArguments applicationArguments) {
         Map<String, Object> bootApps = new HashMap<>();
         for (Map.Entry<String, Object> e : sac.getBeansWithAnnotation(SpringBootApplication.class).entrySet()) {
             Object o = e.getValue();
             if (o instanceof NApplication) {
-                return (NApplication) o;
+                return o;
+            } else if (NApplications.isAnnotatedApplicationClass(o.getClass())) {
+                return o;
             } else {
                 bootApps.put(e.getKey(), o);
             }
@@ -154,9 +155,28 @@ public class NutsSpringBootConfiguration {
         }
     }
 
+    @Bean
+    public NApplication nutsApplication(@Autowired NWorkspace workspace, @Autowired ApplicationArguments applicationArguments) {
+        NClock now = NClock.now();
+        NApplication validApp = null;
+        Object validAppBean = resolveValidSpringBootApplication(workspace, applicationArguments);
+        if (validAppBean instanceof NApplication) {
+            validApp = (NApplication) validAppBean;
+        } else {
+            validApp = NApplications.createApplicationInstanceFromAnnotatedInstance(validAppBean);
+        }
+//        Object finalValidAppBean = validAppBean;
+//        workspace.runWith(() -> {
+//            NApp a = NApp.of();
+//            a.setArguments(applicationArguments.getSourceArgs());
+//            a.prepare(new NAppInitInfo(applicationArguments.getSourceArgs(), NApplications.unproxyType(finalValidAppBean.getClass()), now));
+//        });
+        return validApp;
+    }
+
 
     @Bean
-    public NWorkspace nutsWorkspace(ApplicationArguments applicationArguments) {
+    public NWorkspace nutsWorkspace(@Autowired ApplicationArguments applicationArguments) {
         return Nuts.openWorkspace(
                 NBootArguments.of(resolveNutsArgs())
                         .setAppArgs(applicationArguments.getSourceArgs())
@@ -164,14 +184,12 @@ public class NutsSpringBootConfiguration {
     }
 
     @Bean
-    public CommandLineRunner nutsCommandLineRunner(ApplicationArguments applicationArguments) {
-        return args -> NApplications.runApplication(
-                new NMainArgs()
-                        .setApplicationInstance(nutsApplication())
-                        .setNutsArgs(resolveNutsArgs())
-                        .setArgs(applicationArguments.getSourceArgs())
-                        .setHandleMode(NApplicationHandleMode.PROPAGATE))
-                ;
+    public CommandLineRunner nutsCommandLineRunner(@Autowired NWorkspace workspace, @Autowired ApplicationArguments applicationArguments) {
+        return args -> NApp.builder(applicationArguments.getSourceArgs())
+                .instance(nutsApplication(workspace, applicationArguments))
+                .setNutsArgs(resolveNutsArgs())
+                .propagateErrors()
+                .run();
     }
 
     private String[] resolveNutsArgs() {
