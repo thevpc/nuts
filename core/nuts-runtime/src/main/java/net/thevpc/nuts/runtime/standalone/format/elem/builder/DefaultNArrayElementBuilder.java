035d8db97e3764a8ab7e9967a8fd342579c45bed
thevpc <taha.bensalah@gmail.com>
2025-07-23 09:49:40 +0100

    [ADD] added children()

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/format/elem/builder/DefaultNArrayElementBuilder.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/format/elem/builder/DefaultNArrayElementBuilder.java
@@ -690,4 +690,8 @@ public class DefaultNArrayElementBuilder extends AbstractNElementBuilder impleme
         return this;
     }
 
+    @Override
+    public List<NElement> children() {
+        return new ArrayList<>(this.values);
+    }
 }
