e372ea386a3c1199f731ce65f1107b076fa620b5
thevpc <taha.bensalah@gmail.com>
2025-08-06 15:52:48 +0100

    Update NElementMapperNLiteral.java : fixed ntextup parsing

---
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/elem/mapper/NElementMapperNLiteral.java
@@ -0,0 +1,30 @@
+package net.thevpc.nuts.runtime.standalone.elem.mapper;
+
+import net.thevpc.nuts.elem.NElement;
+import net.thevpc.nuts.elem.NElementFactoryContext;
+import net.thevpc.nuts.elem.NElementMapper;
+import net.thevpc.nuts.util.NLiteral;
+
+import java.lang.reflect.Type;
+
+public class NElementMapperNLiteral implements NElementMapper<NLiteral> {
+
+    @Override
+    public Object destruct(NLiteral src, Type typeOfSrc, NElementFactoryContext context) {
+        return context.defaultDestruct(
+                src.asObject().orNull(), null
+        );
+    }
+
+    @Override
+    public NElement createElement(NLiteral o, Type typeOfSrc, NElementFactoryContext context) {
+        return context.defaultCreateElement(o.asObject().orNull(), null);
+    }
+
+    @Override
+    public NLiteral createObject(NElement o, Type typeOfResult, NElementFactoryContext context) {
+        Object any = context.defaultCreateObject(o, Object.class);
+        return NLiteral.of(any);
+    }
+
+}
