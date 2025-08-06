d7174f4f7ffec01a8b4dc5f3fe7499e9a30a011f
thevpc <taha.bensalah@gmail.com>
2025-08-06 15:52:48 +0100

    Update NElementMapperNObjectElement.java : fixed ntextup parsing

---
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/elem/mapper/NElementMapperNObjectElement.java
@@ -0,0 +1,20 @@
+package net.thevpc.nuts.runtime.standalone.elem.mapper;
+
+import net.thevpc.nuts.elem.*;
+
+import java.lang.reflect.Type;
+
+public class NElementMapperNObjectElement extends NElementMapperNElement {
+
+    public NElementMapperNObjectElement() {
+    }
+
+    @Override
+    public NObjectElement createObject(NElement o, Type typeOfResult, NElementFactoryContext context) {
+        o = super.createObject(o, typeOfResult, context);
+        if (o.isAnyObject()) {
+            return o.asObject().get();
+        }
+        return NElement.ofObjectBuilder().set("value", o).build();
+    }
+}
