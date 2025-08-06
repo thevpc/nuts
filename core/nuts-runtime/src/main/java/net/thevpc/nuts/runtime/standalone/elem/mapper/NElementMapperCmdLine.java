5d35dfd0e7538dc70030bf5aec7a56e1bc537282
thevpc <taha.bensalah@gmail.com>
2025-08-06 15:52:48 +0100

    Update NElementMapperCmdLine.java : fixed ntextup parsing

---
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/elem/mapper/NElementMapperCmdLine.java
@@ -0,0 +1,27 @@
+package net.thevpc.nuts.runtime.standalone.elem.mapper;
+
+import net.thevpc.nuts.cmdline.NCmdLine;
+import net.thevpc.nuts.elem.NElement;
+import net.thevpc.nuts.elem.NElementFactoryContext;
+import net.thevpc.nuts.elem.NElementMapper;
+
+import java.lang.reflect.Type;
+
+public class NElementMapperCmdLine implements NElementMapper<NCmdLine> {
+
+    @Override
+    public Object destruct(NCmdLine src, Type typeOfSrc, NElementFactoryContext context) {
+        return src.toStringArray();
+    }
+
+    @Override
+    public NElement createElement(NCmdLine o, Type typeOfSrc, NElementFactoryContext context) {
+        return context.defaultCreateElement(destruct(o, null, context), null);
+    }
+
+    @Override
+    public NCmdLine createObject(NElement o, Type to, NElementFactoryContext context) {
+        String[] i = context.defaultCreateObject(o, String[].class);
+        return NCmdLine.of(i);
+    }
+}
