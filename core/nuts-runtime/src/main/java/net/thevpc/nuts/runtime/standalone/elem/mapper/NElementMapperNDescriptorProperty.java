d5dbd34398f0f5b458316274957cf9b11163eea3
thevpc <taha.bensalah@gmail.com>
2025-08-06 15:52:48 +0100

    Update NElementMapperNDescriptorProperty.java : fixed ntextup parsing

---
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/elem/mapper/NElementMapperNDescriptorProperty.java
@@ -0,0 +1,33 @@
+package net.thevpc.nuts.runtime.standalone.elem.mapper;
+
+import net.thevpc.nuts.*;
+import net.thevpc.nuts.elem.NElement;
+import net.thevpc.nuts.elem.NElementFactoryContext;
+import net.thevpc.nuts.elem.NElementMapper;
+import net.thevpc.nuts.runtime.standalone.DefaultNDescriptorPropertyBuilder;
+
+import java.lang.reflect.Type;
+
+public class NElementMapperNDescriptorProperty implements NElementMapper<NDescriptorProperty> {
+
+    @Override
+    public Object destruct(NDescriptorProperty src, Type typeOfSrc, NElementFactoryContext context) {
+        return context.defaultDestruct(
+                new DefaultNDescriptorPropertyBuilder(src), null
+        );
+    }
+
+    @Override
+    public NElement createElement(NDescriptorProperty o, Type typeOfSrc, NElementFactoryContext context) {
+        return context.defaultCreateElement(
+                new DefaultNDescriptorPropertyBuilder(o), null
+        );
+    }
+
+    @Override
+    public NDescriptorProperty createObject(NElement o, Type typeOfResult, NElementFactoryContext context) {
+        DefaultNDescriptorPropertyBuilder builder = (DefaultNDescriptorPropertyBuilder) context.defaultCreateObject(o, DefaultNDescriptorPropertyBuilder.class);
+        return new DefaultNDescriptorPropertyBuilder(builder).build();
+    }
+
+}
