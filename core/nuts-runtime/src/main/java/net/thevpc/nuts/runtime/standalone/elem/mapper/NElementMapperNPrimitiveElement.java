32d4923e61649c77ba3eb05c6cfc2ee76ffcd744
thevpc <taha.bensalah@gmail.com>
2025-08-06 15:52:48 +0100

    Update NElementMapperNPrimitiveElement.java : fixed ntextup parsing

---
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/elem/mapper/NElementMapperNPrimitiveElement.java
@@ -0,0 +1,29 @@
+package net.thevpc.nuts.runtime.standalone.elem.mapper;
+
+import net.thevpc.nuts.elem.*;
+
+import java.lang.reflect.Type;
+
+public class NElementMapperNPrimitiveElement implements NElementMapper<NPrimitiveElement> {
+
+    public NElementMapperNPrimitiveElement() {
+    }
+
+    @Override
+    public Object destruct(NPrimitiveElement src, Type typeOfSrc, NElementFactoryContext context) {
+        return src.asLiteral().asObject().orNull();
+    }
+
+    @Override
+    public NElement createElement(NPrimitiveElement src, Type typeOfSrc, NElementFactoryContext context) {
+        return src;
+    }
+
+    @Override
+    public NPrimitiveElement createObject(NElement o, Type typeOfResult, NElementFactoryContext context) {
+        if (o.type().isAnyPrimitive()) {
+            return o.asPrimitive().get();
+        }
+        return NElement.ofString(o.toString());
+    }
+}
