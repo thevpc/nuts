62ddd347735c3a86413fcb32ce2b396e025e1933
thevpc <taha.bensalah@gmail.com>
2025-08-06 15:52:49 +0100

    Update NElementMapperString.java : fixed ntextup parsing

---
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/elem/mapper/NElementMapperString.java
@@ -0,0 +1,23 @@
+package net.thevpc.nuts.runtime.standalone.elem.mapper;
+
+import net.thevpc.nuts.elem.*;
+
+import java.lang.reflect.Type;
+
+public class NElementMapperString implements NElementMapper<String> {
+
+    @Override
+    public Object destruct(String src, Type typeOfSrc, NElementFactoryContext context) {
+        return src;
+    }
+
+    @Override
+    public NElement createElement(String o, Type typeOfSrc, NElementFactoryContext context) {
+        return NElement.ofString(String.valueOf(o));
+    }
+
+    @Override
+    public String createObject(NElement o, Type to, NElementFactoryContext context) {
+        return o.asStringValue().get();
+    }
+}
