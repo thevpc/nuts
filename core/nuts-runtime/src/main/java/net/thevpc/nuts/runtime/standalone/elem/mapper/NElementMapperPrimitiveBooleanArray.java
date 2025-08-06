1c62a60ab38e326e1d7d8f63e698b9707d31b9eb
thevpc <taha.bensalah@gmail.com>
2025-08-06 15:52:48 +0100

    Update NElementMapperPrimitiveBooleanArray.java : fixed ntextup parsing

---
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/elem/mapper/NElementMapperPrimitiveBooleanArray.java
@@ -0,0 +1,35 @@
+package net.thevpc.nuts.runtime.standalone.elem.mapper;
+
+import net.thevpc.nuts.elem.NArrayElement;
+import net.thevpc.nuts.elem.NElement;
+import net.thevpc.nuts.elem.NElementFactoryContext;
+import net.thevpc.nuts.elem.NElementMapper;
+import net.thevpc.nuts.runtime.standalone.elem.DefaultNElementFactoryService;
+
+import java.lang.reflect.Type;
+
+public class NElementMapperPrimitiveBooleanArray implements NElementMapper<boolean[]> {
+
+    public NElementMapperPrimitiveBooleanArray() {
+    }
+
+    @Override
+    public Object destruct(boolean[] src, Type typeOfSrc, NElementFactoryContext context) {
+        return DefaultNElementFactoryService._destructArray1(src, context);
+    }
+
+    @Override
+    public NElement createElement(boolean[] src, Type typeOfSrc, NElementFactoryContext context) {
+        return DefaultNElementFactoryService._createArray1(src, context);
+    }
+
+    @Override
+    public boolean[] createObject(NElement o, Type typeOfResult, NElementFactoryContext context) {
+        NArrayElement earr = o.asArray().get();
+        boolean[] arr = new boolean[earr.size()];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (boolean) context.createObject(earr.get(i).get(), boolean.class);
+        }
+        return arr;
+    }
+}
