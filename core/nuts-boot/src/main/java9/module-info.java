6a94c1b6452f2364bf562cf6276b5520520863e2
thevpc <taha.bensalah@gmail.com>
2025-08-20 01:49:48 +0100

    Update module-info.java : fixed to support j9+ module system

--- core/nuts-boot/src/main/java9/module-info.java
+++ core/nuts-boot/src/main/java9/module-info.java
@@ -1,12 +1,13 @@
 /**
  * documentation
  */
-module net.thevpc.nuts {
+module net.thevpc.nuts.boot {
     requires java.base;
     requires java.logging;
     requires java.xml;
     requires java.desktop;
-    //exports net.thevpc.nuts;
+    exports net.thevpc.nuts.boot;
+    exports net.thevpc.nuts.core;
     //exports net.thevpc.nuts.spi;
     //uses net.thevpc.nuts.spi.NutsBootWorkspaceFactory;
     //uses net.thevpc.nuts.spi.NutsComponent;
