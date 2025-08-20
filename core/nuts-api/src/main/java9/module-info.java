cbe1c8f78f15e51c9c0f5e7d4fe262b3094a02aa
thevpc <taha.bensalah@gmail.com>
2025-08-20 01:49:48 +0100

    Update module-info.java : fixed to support j9+ module system

--- core/nuts-api/src/main/java9/module-info.java
+++ core/nuts-api/src/main/java9/module-info.java
@@ -6,8 +6,26 @@ module net.thevpc.nuts {
     requires java.logging;
     requires java.xml;
     requires java.desktop;
+    requires net.thevpc.nuts.boot;
+
     exports net.thevpc.nuts;
+    exports net.thevpc.nuts.cmdline;
+    exports net.thevpc.nuts.concurrent;
+    exports net.thevpc.nuts.elem;
+    exports net.thevpc.nuts.expr;
+    exports net.thevpc.nuts.ext;
+    exports net.thevpc.nuts.format;
+    exports net.thevpc.nuts.io;
+    exports net.thevpc.nuts.log;
+    exports net.thevpc.nuts.math;
+    exports net.thevpc.nuts.reflect;
+    exports net.thevpc.nuts.security;
     exports net.thevpc.nuts.spi;
-    uses net.thevpc.nuts.spi.NutsBootWorkspaceFactory;
-    uses net.thevpc.nuts.spi.NutsComponent;
+    exports net.thevpc.nuts.text;
+    exports net.thevpc.nuts.time;
+    exports net.thevpc.nuts.util;
+    exports net.thevpc.nuts.web;
+
+    uses net.thevpc.nuts.boot.NBootWorkspaceFactory;
+    uses net.thevpc.nuts.spi.NComponent;
 }
