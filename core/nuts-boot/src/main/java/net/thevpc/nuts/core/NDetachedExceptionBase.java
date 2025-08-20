b7322b2ba81fb45eda0f738aaef7a88cda28f805
thevpc <taha.bensalah@gmail.com>
2025-08-20 01:49:59 +0100

    Update NDetachedExceptionBase.java : fixed to support j9+ module system

---
+++ core/nuts-boot/src/main/java/net/thevpc/nuts/core/NDetachedExceptionBase.java
@@ -0,0 +1,8 @@
+package net.thevpc.nuts.core;
+
+/**
+ * Detached Exceptions are Nuts Exception that
+ * are not bound to any NWorkspace/NSession
+ */
+public interface NDetachedExceptionBase extends NAnyExceptionBase{
+}
