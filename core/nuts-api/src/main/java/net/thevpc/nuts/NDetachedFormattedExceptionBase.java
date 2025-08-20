39e0c19feb56d59e2f8e7682d49dd2adf2371b6c
thevpc <taha.bensalah@gmail.com>
2025-08-20 01:49:47 +0100

    Update NDetachedFormattedExceptionBase.java : fixed to support j9+ module system

--- core/nuts-api/src/main/java/net/thevpc/nuts/NDetachedFormattedExceptionBase.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/NDetachedFormattedExceptionBase.java
@@ -1,8 +1,10 @@
 package net.thevpc.nuts;
 
+import net.thevpc.nuts.core.NDetachedExceptionBase;
+
 /**
  * Detached Exceptions are Nuts Exception that
  * are not bound to any NWorkspace/NSession
  */
-public interface NDetachedFormattedExceptionBase extends NAnyFormattedExceptionBase,NDetachedExceptionBase{
+public interface NDetachedFormattedExceptionBase extends NAnyFormattedExceptionBase, NDetachedExceptionBase {
 }
