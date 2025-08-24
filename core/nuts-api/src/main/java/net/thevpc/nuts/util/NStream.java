2959f83e4b1efd7956e06dd9606a469f00f6f484
thevpc <taha.bensalah@gmail.com>
2025-08-24 16:14:10 +0100

    Update NStream.java : added NTextArt support

--- core/nuts-api/src/main/java/net/thevpc/nuts/util/NStream.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/util/NStream.java
@@ -3,31 +3,28 @@
  * Nuts : Network Updatable Things Service
  * (universal package manager)
  * <br>
- * is a new Open Source Package Manager to help install packages
- * and libraries for runtime execution. Nuts is the ultimate companion for
- * maven (and other build managers) as it helps installing all package
- * dependencies at runtime. Nuts is not tied to java and is a good choice
- * to share shell scripts and other 'things' . Its based on an extensible
- * architecture to help supporting a large range of sub managers / repositories.
+ * is a new Open Source Package Manager to help install packages and libraries
+ * for runtime execution. Nuts is the ultimate companion for maven (and other
+ * build managers) as it helps installing all package dependencies at runtime.
+ * Nuts is not tied to java and is a good choice to share shell scripts and
+ * other 'things' . Its based on an extensible architecture to help supporting a
+ * large range of sub managers / repositories.
  *
  * <br>
  * <p>
- * Copyright [2020] [thevpc]
- * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE Version 3 (the "License");
- * you may  not use this file except in compliance with the License. You may obtain
- * a copy of the License at https://www.gnu.org/licenses/lgpl-3.0.en.html
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
- * either express or implied. See the License for the specific language
+ * Copyright [2020] [thevpc] Licensed under the GNU LESSER GENERAL PUBLIC
+ * LICENSE Version 3 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * https://www.gnu.org/licenses/lgpl-3.0.en.html Unless required by applicable
+ * law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
- * <br>
- * ====================================================================
+ * <br> ====================================================================
  */
 package net.thevpc.nuts.util;
 
 import net.thevpc.nuts.*;
-import net.thevpc.nuts.elem.NElementDescribable;
 import net.thevpc.nuts.elem.NElementRedescribable;
 import net.thevpc.nuts.reserved.rpi.NCollectionsRPI;
 
@@ -45,6 +42,7 @@ import java.util.stream.*;
  * @since 0.5.4
  */
 public interface NStream<T> extends Iterable<T>, NElementRedescribable<NStream<T>>, AutoCloseable {
+
     static <T> NStream<T> ofArray(T... str) {
         return NCollectionsRPI.of().arrayToStream(str);
     }
@@ -78,13 +76,13 @@ public interface NStream<T> extends Iterable<T>, NElementRedescribable<NStream<T
     }
 
     /**
-     * return result as a  java.util.List .
+     * return result as a java.util.List .
      * <p>
      * consumes the result and returns a list Calling this method twice will
      * result in unexpected behavior (may return an empty list as the result is
      * already consumed or throw an Exception)
      *
-     * @return result as a  java.util.List
+     * @return result as a java.util.List
      */
     List<T> toList();
 
@@ -120,15 +118,14 @@ public interface NStream<T> extends Iterable<T>, NElementRedescribable<NStream<T
     NOptional<T> findSingleton();
 
     /**
-     * return result as a  java.util.stream.Stream .
+     * return result as a java.util.stream.Stream .
      * <p>
      * Calling this method twice will result in unexpected behavior (may return
      * 0 as the result is already consumed or throw an Exception)
      *
-     * @return result as a  java.util.stream.Stream
+     * @return result as a java.util.stream.Stream
      */
     Stream<T> stream();
-
 
     NStream<T> skip(long n);
 
@@ -144,17 +141,21 @@ public interface NStream<T> extends Iterable<T>, NElementRedescribable<NStream<T
     long count();
 
     /**
-     * return NutsStream a stream consisting of the results of applying the given function to the elements of this stream.
+     * return NutsStream a stream consisting of the results of applying the
+     * given function to the elements of this stream.
      *
-     * @param <R>    to type
+     * @param <R> to type
      * @param mapper mapper
-     * @return NutsStream a stream consisting of the results of applying the given function to the elements of this stream.
+     * @return NutsStream a stream consisting of the results of applying the
+     * given function to the elements of this stream.
      */
     <R> NStream<R> map(Function<? super T, ? extends R> mapper);
 
     <R> NStream<R> mapUnsafe(UnsafeFunction<? super T, ? extends R> mapper);
 
     <R> NStream<R> mapUnsafe(UnsafeFunction<? super T, ? extends R> mapper, Function<Exception, ? extends R> onError);
+
+    <V> NStream<V> instanceOf(Class<V> type);
 
     NStream<T> sorted();
 
@@ -207,13 +208,13 @@ public interface NStream<T> extends Iterable<T>, NElementRedescribable<NStream<T
     <A> A[] toArray(IntFunction<A[]> generator);
 
     <K, U> Map<K, U> toMap(Function<? super T, ? extends K> keyMapper,
-                           Function<? super T, ? extends U> valueMapper);
+            Function<? super T, ? extends U> valueMapper);
 
     <K, U> Map<K, U> toOrderedMap(Function<? super T, ? extends K> keyMapper,
-                                  Function<? super T, ? extends U> valueMapper);
+            Function<? super T, ? extends U> valueMapper);
 
     <K, U> Map<K, U> toSortedMap(Function<? super T, ? extends K> keyMapper,
-                                 Function<? super T, ? extends U> valueMapper);
+            Function<? super T, ? extends U> valueMapper);
 
     <R> NStream<R> flatMapIter(Function<? super T, ? extends Iterator<? extends R>> mapper);
 
@@ -250,8 +251,8 @@ public interface NStream<T> extends Iterable<T>, NElementRedescribable<NStream<T
     NIterator<T> iterator();
 
     <R> R collect(Supplier<R> supplier,
-                  BiConsumer<R, ? super T> accumulator,
-                  BiConsumer<R, R> combiner);
+            BiConsumer<R, ? super T> accumulator,
+            BiConsumer<R, R> combiner);
 
     <R, A> R collect(Collector<? super T, A, R> collector);
 
