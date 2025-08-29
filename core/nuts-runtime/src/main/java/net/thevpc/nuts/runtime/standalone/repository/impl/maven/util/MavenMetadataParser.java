9bf1b205a9e05c047aadb25496ed587eb285cb39
thevpc <taha.bensalah@gmail.com>
2025-08-29 04:26:19 +0100

    Update MavenMetadataParser.java : [CHANGED] removed NLogOp and made NLog extensible to support slf4J and other backends

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/repository/impl/maven/util/MavenMetadataParser.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/repository/impl/maven/util/MavenMetadataParser.java
@@ -177,8 +177,7 @@ public class MavenMetadataParser {
 
             info.setLastUpdated(lastUpdated.toString().trim().isEmpty() ? null : new SimpleDateFormat("yyyyMMddHHmmss").parse(lastUpdated.toString().trim()));
         } catch (Exception ex) {
-            LOG.with().level(Level.SEVERE).error(ex)
-                    .log(NMsg.ofJ("failed to parse date {0} : {1}", lastUpdated, ex));
+            LOG.log(NMsg.ofJ("failed to parse date {0} : {1}", lastUpdated, ex).asFinestFail(ex));
         }
         for (String version : versions) {
             info.getVersions().add(version.trim());
