7d964997bdebddebd5bfac1179baafc2d605edc4
thevpc <taha.bensalah@gmail.com>
2025-08-29 21:13:09 +0100

    Update code.js : [CHANGED] requireWithDefault => requireDefaults

---
+++ documentation/website/src/resources/assets/js/code.js
@@ -0,0 +1,6 @@
+Prism.plugins.NormalizeWhitespace.setDefaults({
+    'remove-trailing': true,
+    'remove-indent': true,  // ✅ removes leading spaces from every line
+    'left-trim': true,
+    'right-trim': true
+});
