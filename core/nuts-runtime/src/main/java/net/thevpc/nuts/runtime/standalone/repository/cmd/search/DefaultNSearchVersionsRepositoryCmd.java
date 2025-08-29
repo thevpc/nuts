f31c572144852791b1fc10f7f6d73223446a8343
thevpc <taha.bensalah@gmail.com>
2025-08-29 04:26:19 +0100

    Update DefaultNSearchVersionsRepositoryCmd.java : [CHANGED] removed NLogOp and made NLog extensible to support slf4J and other backends

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/repository/cmd/search/DefaultNSearchVersionsRepositoryCmd.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/repository/cmd/search/DefaultNSearchVersionsRepositoryCmd.java
@@ -14,8 +14,8 @@ import net.thevpc.nuts.NConstants;
 import net.thevpc.nuts.elem.NDescribables;
 import net.thevpc.nuts.format.NPositionType;
 import net.thevpc.nuts.log.NLog;
-import net.thevpc.nuts.log.NLogOp;
-import net.thevpc.nuts.log.NLogVerb;
+
+import net.thevpc.nuts.log.NMsgIntent;
 import net.thevpc.nuts.runtime.standalone.definition.NDefinitionHelper;
 import net.thevpc.nuts.util.*;
 import net.thevpc.nuts.runtime.standalone.id.util.CoreNIdUtils;
@@ -31,10 +31,6 @@ public class DefaultNSearchVersionsRepositoryCmd extends AbstractNSearchVersions
 
     public DefaultNSearchVersionsRepositoryCmd(NRepository repo) {
         super(repo);
-    }
-
-    protected NLogOp _LOGOP() {
-        return _LOG().with();
     }
 
     protected NLog _LOG() {
@@ -57,8 +53,9 @@ public class DefaultNSearchVersionsRepositoryCmd extends AbstractNSearchVersions
                     try {
                         d = xrepo.getIndexStore().searchVersions(id);
                     } catch (NException ex) {
-                        _LOGOP().level(Level.FINEST).verb(NLogVerb.FAIL)
-                                .log(NMsg.ofC("error finding version with Indexer for %s : %s", getRepo().getName(), ex));
+                        _LOG()
+                                .log(NMsg.ofC("error finding version with Indexer for %s : %s", getRepo().getName(), ex)
+                                        .withLevel(Level.FINEST).withIntent(NMsgIntent.FAIL));
                     }
                     if (d != null && filter != null) {
                         resultList.add(
@@ -77,12 +74,13 @@ public class DefaultNSearchVersionsRepositoryCmd extends AbstractNSearchVersions
             result = NIteratorUtils.coalesce(resultList);
             return this;
         } catch (RuntimeException ex) {
-            _LOGOP().level(Level.FINEST).verb(NLogVerb.FAIL)
+            _LOG()
                     .log(NMsg.ofC("[%s] %s %s %s",
                             NStringUtils.formatAlign(getFetchMode().toString(), 7, NPositionType.FIRST),
                             NStringUtils.formatAlign(getRepo().getName(), 20, NPositionType.FIRST),
                             NStringUtils.formatAlign("Fetch versions for", 24, NPositionType.FIRST),
-                            id));
+                            id)
+                            .withLevel(Level.FINEST).withIntent(NMsgIntent.FAIL));
             throw ex;
         }
     }
