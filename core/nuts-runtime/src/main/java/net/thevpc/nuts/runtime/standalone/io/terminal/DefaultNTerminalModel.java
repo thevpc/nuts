81f70e77625a675e0eafef7b814b0b7d60320480
thevpc <taha.bensalah@gmail.com>
2025-08-29 04:26:18 +0100

    Update DefaultNTerminalModel.java : [CHANGED] removed NLogOp and made NLog extensible to support slf4J and other backends

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/io/terminal/DefaultNTerminalModel.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/io/terminal/DefaultNTerminalModel.java
@@ -1,17 +1,13 @@
 package net.thevpc.nuts.runtime.standalone.io.terminal;
 
 import net.thevpc.nuts.log.NLog;
-import net.thevpc.nuts.log.NLogOp;
+
 import net.thevpc.nuts.util.NUnused;
 
 @NUnused
 public class DefaultNTerminalModel {
 
     public DefaultNTerminalModel() {
-    }
-
-    protected NLogOp _LOGOP() {
-        return _LOG().with();
     }
 
     protected NLog _LOG() {
