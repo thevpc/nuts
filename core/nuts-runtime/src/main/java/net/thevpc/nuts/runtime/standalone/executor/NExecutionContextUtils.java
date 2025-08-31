ad49f624bbc81cb0f2106fc78657869c71d00610
thevpc <taha.bensalah@gmail.com>
2025-08-31 22:23:48 +0100

    Update NExecutionContextUtils.java : update doc

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/executor/NExecutionContextUtils.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/executor/NExecutionContextUtils.java
@@ -7,8 +7,24 @@ import net.thevpc.nuts.NStoreType;
 import net.thevpc.nuts.io.NPath;
 import net.thevpc.nuts.runtime.standalone.xtra.expr.StringPlaceHolderParser;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class NExecutionContextUtils {
-    public static StringPlaceHolderParser.PlaceHolderProvider<NDefinition> DEFINITION_PLACEHOLDER=new StringPlaceHolderParser.PlaceHolderProvider<NDefinition>() {
+    public static Map<String, String> defaultEnv(NDefinition definition) {
+        Map<String, String> m = new HashMap<>();
+        m.put("NUTS_ID", definition.getId().toString());
+        m.put("NUTS_ARTIFACT_ID", definition.getId().getArtifactId());
+        m.put("NUTS_GROUP_ID", definition.getId().getGroupId());
+        m.put("NUTS_VERSION", definition.getId().getVersion().toString());
+        for (NStoreType value : NStoreType.values()) {
+            NPath v = NPath.ofIdStore(definition.getId(), value);
+            m.put("NUTS_ID_" + value.name(), v.toString());
+        }
+        return m;
+    }
+
+    public static StringPlaceHolderParser.PlaceHolderProvider<NDefinition> DEFINITION_PLACEHOLDER = new StringPlaceHolderParser.PlaceHolderProvider<NDefinition>() {
         @Override
         public String get(String key, NDefinition definition) {
             switch (key) {
@@ -16,35 +32,35 @@ public class NExecutionContextUtils {
                     return (definition.getId().toString());
                 }
                 case "NUTS_ID_BIN": {
-                    NPath v = NWorkspace.of().getStoreLocation(definition.getId(), NStoreType.BIN);
+                    NPath v = NPath.ofIdStore(definition.getId(), NStoreType.BIN);
                     return (v.toString());
                 }
                 case "NUTS_ID_CONF": {
-                    NPath v = NWorkspace.of().getStoreLocation(definition.getId(), NStoreType.CONF);
+                    NPath v = NPath.ofIdStore(definition.getId(), NStoreType.CONF);
                     return (v.toString());
                 }
                 case "NUTS_ID_LOG": {
-                    NPath v = NWorkspace.of().getStoreLocation(definition.getId(), NStoreType.LOG);
+                    NPath v = NPath.ofIdStore(definition.getId(), NStoreType.LOG);
                     return (v.toString());
                 }
                 case "NUTS_ID_CACHE": {
-                    NPath v = NWorkspace.of().getStoreLocation(definition.getId(), NStoreType.CACHE);
+                    NPath v = NPath.ofIdStore(definition.getId(), NStoreType.CACHE);
                     return (v.toString());
                 }
                 case "NUTS_ID_LIB": {
-                    NPath v = NWorkspace.of().getStoreLocation(definition.getId(), NStoreType.LIB);
+                    NPath v = NPath.ofIdStore(definition.getId(), NStoreType.LIB);
                     return (v.toString());
                 }
                 case "NUTS_ID_RUN": {
-                    NPath v = NWorkspace.of().getStoreLocation(definition.getId(), NStoreType.RUN);
+                    NPath v = NPath.ofIdStore(definition.getId(), NStoreType.RUN);
                     return (v.toString());
                 }
                 case "NUTS_ID_TEMP": {
-                    NPath v = NWorkspace.of().getStoreLocation(definition.getId(), NStoreType.TEMP);
+                    NPath v = NPath.ofIdStore(definition.getId(), NStoreType.TEMP);
                     return (v.toString());
                 }
                 case "NUTS_ID_VAR": {
-                    NPath v = NWorkspace.of().getStoreLocation(definition.getId(), NStoreType.VAR);
+                    NPath v = NPath.ofIdStore(definition.getId(), NStoreType.VAR);
                     return (v.toString());
                 }
             }
@@ -52,7 +68,7 @@ public class NExecutionContextUtils {
         }
     };
 
-    public static StringPlaceHolderParser.PlaceHolderProvider<NExecutionContext> EXECUTION_CONTEXT_PLACEHOLDER=new StringPlaceHolderParser.PlaceHolderProvider<NExecutionContext>() {
+    public static StringPlaceHolderParser.PlaceHolderProvider<NExecutionContext> EXECUTION_CONTEXT_PLACEHOLDER = new StringPlaceHolderParser.PlaceHolderProvider<NExecutionContext>() {
         @Override
         public String get(String key, NExecutionContext executionContext) {
             return DEFINITION_PLACEHOLDER.get(key, executionContext.getDefinition());
