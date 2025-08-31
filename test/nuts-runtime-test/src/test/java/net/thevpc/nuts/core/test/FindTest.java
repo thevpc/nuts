b1f997b759fa1c065c0eb5c0daba7281b3218a9a
thevpc <taha.bensalah@gmail.com>
2025-08-31 22:23:49 +0100

    Update FindTest.java : update doc

--- test/nuts-runtime-test/src/test/java/net/thevpc/nuts/core/test/FindTest.java
+++ test/nuts-runtime-test/src/test/java/net/thevpc/nuts/core/test/FindTest.java
@@ -19,7 +19,7 @@ import org.junit.jupiter.api.*;
 public class FindTest {
     @BeforeAll
     public static void init() {
-        TestUtils.openNewMinTestWorkspace();
+        TestUtils.openNewMinTestWorkspace("--stacktrace");
     }
 
     @Test
