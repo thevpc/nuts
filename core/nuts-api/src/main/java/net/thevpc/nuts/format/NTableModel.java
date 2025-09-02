20f945959b6d0392ba990f1dfec4e5e450974e0e
thevpc <taha.bensalah@gmail.com>
2025-09-02 09:10:16 +0100

    Update NTableModel.java : added NTextArtTableRenderer

--- core/nuts-api/src/main/java/net/thevpc/nuts/format/NTableModel.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/format/NTableModel.java
@@ -12,18 +12,21 @@
  * <br>
  * <p>
  * Copyright [2020] [thevpc]
- * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE Version 3 (the "License"); 
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE Version 3 (the "License");
  * you may  not use this file except in compliance with the License. You may obtain
  * a copy of the License at https://www.gnu.org/licenses/lgpl-3.0.en.html
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an 
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
- * either express or implied. See the License for the specific language 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  * <br>
  * ====================================================================
  */
 package net.thevpc.nuts.format;
+
+import net.thevpc.nuts.text.NText;
+import net.thevpc.nuts.util.NMsg;
 
 /**
  * @author thevpc
@@ -40,7 +43,7 @@ public interface NTableModel {
 
     int getRowsCount();
 
-    Object getCellValue(int row, int column);
+    NText getCellValue(int row, int column);
 
     default int getCellColSpan(int row, int column) {
         return 1;
@@ -50,8 +53,8 @@ public interface NTableModel {
         return 1;
     }
 
-    default Object getHeaderValue(int column) {
-        return "column " + (column + 1);
+    default NText getHeaderValue(int column) {
+        return NText.of(NMsg.ofC("column %s", (column + 1)));
     }
 
     default int getHeaderColSpan(int column) {
