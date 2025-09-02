7a8142484c22fd013c50029a1ed70d82330d10bf
thevpc <taha.bensalah@gmail.com>
2025-09-02 09:10:17 +0100

    Update AbstractNText.java : added NTextArtTableRenderer

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/text/parser/AbstractNText.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/text/parser/AbstractNText.java
@@ -1,11 +1,13 @@
 package net.thevpc.nuts.runtime.standalone.text.parser;
 
 import net.thevpc.nuts.runtime.standalone.text.NTextNodeWriterStringer;
-import net.thevpc.nuts.text.NText;
-import net.thevpc.nuts.text.NTextBuilder;
+import net.thevpc.nuts.text.*;
 import net.thevpc.nuts.util.NBlankable;
 
 import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public abstract class AbstractNText implements NText {
 
@@ -26,8 +28,8 @@ public abstract class AbstractNText implements NText {
     }
 
     @Override
-    public int textLength() {
-        return immutable().textLength();
+    public int length() {
+        return immutable().length();
     }
 
     @Override
@@ -45,4 +47,80 @@ public abstract class AbstractNText implements NText {
         return NTextBuilder.of().append(this);
     }
 
+    @Override
+    public boolean isPrimitive() {
+        return this instanceof NPrimitiveText;
+    }
+
+    @Override
+    public boolean isNormalized() {
+        return this instanceof NNormalizedText;
+    }
+
+    @Override
+    public List<NText> split(char c) {
+        return split(String.valueOf(c), false);
+    }
+
+    @Override
+    public List<NText> split(char c, boolean returnSeparator) {
+        return split(String.valueOf(c), returnSeparator);
+    }
+
+    @Override
+    public List<NText> split(String separator) {
+        return split(separator, false);
+    }
+
+    @Override
+    public NPrimitiveText[] toCharArray() {
+        return toCharList().toArray(new NPrimitiveText[0]);
+    }
+
+    @Override
+    public NText repeat(int times) {
+        if (times <= 0) {
+            return NText.ofBlank();
+        }
+        if (times == 1) {
+            return this;
+        }
+        NTextBuilder b = NTextBuilder.of();
+        for (int i = 0; i < times; i++) {
+            b.append(this);
+        }
+        return b.build();
+    }
+
+    @Override
+    public NText concat(NText other) {
+        if (other == null) {
+            return this;
+        }
+        return NText.ofList(this, other).simplify();
+    }
+
+    @Override
+    public NText concat(NText... others) {
+        List<NText> aa = new ArrayList<>();
+        aa.add(this);
+        if (others != null) {
+            aa.addAll(Arrays.asList(others));
+        }
+        return NText.ofList(aa).simplify();
+    }
+
+    public NNormalizedText normalize() {
+        return NTexts.of().normalize(this);
+    }
+
+    @Override
+    public NNormalizedText normalize(NTextTransformConfig config) {
+        return NTexts.of().normalize(this, config);
+    }
+
+    @Override
+    public NNormalizedText normalize(NTextTransformer transformer, NTextTransformConfig config) {
+        return NTexts.of().normalize(this, transformer, config);
+    }
 }
