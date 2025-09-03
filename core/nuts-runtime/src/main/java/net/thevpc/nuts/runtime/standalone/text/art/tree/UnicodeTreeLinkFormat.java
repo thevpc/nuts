895f2cc9d7d5856bfc596a0b7353c30eb4e6b9f0
thevpc <taha.bensalah@gmail.com>
2025-09-03 03:36:59 +0100

    Update UnicodeTreeLinkFormat.java : [UPDATED] update Table rendering

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/text/art/tree/UnicodeTreeLinkFormat.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/text/art/tree/UnicodeTreeLinkFormat.java
@@ -1,7 +1,7 @@
 /**
  * ====================================================================
- *            Nuts : Network Updatable Things Service
- *                  (universal package manager)
+ * Nuts : Network Updatable Things Service
+ * (universal package manager)
  * <br>
  * is a new Open Source Package Manager to help install packages and libraries
  * for runtime execution. Nuts is the ultimate companion for maven (and other
@@ -10,15 +10,15 @@
  * other 'things' . It's based on an extensible architecture to help supporting a
  * large range of sub managers / repositories.
  * <br>
- *
- * Copyright [2020] [thevpc]  
- * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE Version 3 (the "License"); 
+ * <p>
+ * Copyright [2020] [thevpc]
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE Version 3 (the "License");
  * you may  not use this file except in compliance with the License. You may obtain
  * a copy of the License at https://www.gnu.org/licenses/lgpl-3.0.en.html
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an 
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
- * either express or implied. See the License for the specific language 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  * <br> ====================================================================
  */
@@ -26,46 +26,42 @@ package net.thevpc.nuts.runtime.standalone.text.art.tree;
 
 import net.thevpc.nuts.format.NPositionType;
 import net.thevpc.nuts.format.NTreeLinkFormat;
+import net.thevpc.nuts.text.NText;
 
 /**
- *
  * @author thevpc
  */
 class UnicodeTreeLinkFormat implements NTreeLinkFormat {
 
     @Override
-    public String formatMain(NPositionType type) {
+    public NText formatMain(NPositionType type) {
         switch (type) {
             case FIRST: {
-                return "";
+                return NText.ofBlank();
             }
             case CENTER: {
-                return "├── ";
+                return NText.ofPlain("├── ");
             }
             case LAST: {
-                return "└── ";
+                return NText.ofPlain("└── ");
             }
         }
-        return "";
+        return NText.ofBlank();
     }
 
     @Override
-    public String formatChild(NPositionType type) {
-        String p = "";
+    public NText formatChild(NPositionType type) {
         switch (type) {
             case FIRST: {
-                p = "";
-                break;
+                return NText.ofBlank();
             }
             case CENTER: {
-                p = "│   ";
-                break;
+                return NText.ofPlain("│   ");
             }
             case LAST: {
-                p = "    ";
-                break;
+                return NText.ofPlain("    ");
             }
         }
-        return p;
+        return NText.ofBlank();
     }
 }
