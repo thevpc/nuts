777a205bc3aabd5d7bba03ebf9559207b5418bb7
thevpc <taha.bensalah@gmail.com>
2025-09-03 03:36:59 +0100

    Update NElementTreeModel.java : [UPDATED] update Table rendering

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/format/tree/NElementTreeModel.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/format/tree/NElementTreeModel.java
@@ -1,34 +1,34 @@
-package net.thevpc.nuts.runtime.standalone.format.tree;
-
-import net.thevpc.nuts.format.NTreeModel;
-import net.thevpc.nuts.runtime.standalone.text.art.tree.XNode;
-import net.thevpc.nuts.runtime.standalone.text.art.tree.XNodeFormatter;
-import net.thevpc.nuts.text.NText;
-
-import java.util.List;
-import java.util.Map;
-
-class NElementTreeModel implements NTreeModel {
-
-    private final XNode root;
-
-    public NElementTreeModel(NText rootName, Object destructredObject, XNodeFormatter format) {
-        this.root = new XNode(null, destructredObject, 
-                ((destructredObject instanceof List) || (destructredObject instanceof Map))?rootName:null, format);
-    }
-
-    public NElementTreeModel(XNode node) {
-        this.root = node;
-    }
-
-    @Override
-    public Object getRoot() {
-        return root;
-    }
-
-    @Override
-    public List getChildren(Object o) {
-        return ((XNode) o).getChildren();
-    }
-
-}
+//package net.thevpc.nuts.runtime.standalone.format.tree;
+//
+//import net.thevpc.nuts.format.NTreeModel;
+//import net.thevpc.nuts.runtime.standalone.text.art.tree.XNode;
+//import net.thevpc.nuts.runtime.standalone.text.art.tree.XNodeFormatter;
+//import net.thevpc.nuts.text.NText;
+//
+//import java.util.List;
+//import java.util.Map;
+//
+//class NElementTreeModel implements NTreeModel {
+//
+//    private final XNode root;
+//
+//    public NElementTreeModel(NText rootName, Object destructredObject, XNodeFormatter format) {
+//        this.root = new XNode(null, destructredObject,
+//                ((destructredObject instanceof List) || (destructredObject instanceof Map))?rootName:null, format);
+//    }
+//
+//    public NElementTreeModel(XNode node) {
+//        this.root = node;
+//    }
+//
+//    @Override
+//    public Object getRoot() {
+//        return root;
+//    }
+//
+//    @Override
+//    public List getChildren(Object o) {
+//        return ((XNode) o).getChildren();
+//    }
+//
+//}
