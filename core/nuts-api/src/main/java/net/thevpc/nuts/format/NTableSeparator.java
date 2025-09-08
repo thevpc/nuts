9b1e97c9f74d38cdc27b41d2f93fc4270638d82d
thevpc <taha.bensalah@gmail.com>
2025-09-08 17:32:15 +0100

    [ADDED] added better support for Text Tables

--- core/nuts-api/src/main/java/net/thevpc/nuts/format/NTableSeparator.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/format/NTableSeparator.java
@@ -35,21 +35,21 @@ import net.thevpc.nuts.util.NNameFormat;
  * @app.category Format
  */
 public enum NTableSeparator implements NEnum {
-    FIRST_ROW_START,
-    FIRST_ROW_LINE,
-    FIRST_ROW_SEP,
-    FIRST_ROW_END,
-    ROW_START,
-    ROW_SEP,
-    ROW_END,
-    MIDDLE_ROW_START,
-    MIDDLE_ROW_LINE,
-    MIDDLE_ROW_SEP,
-    MIDDLE_ROW_END,
-    LAST_ROW_START,
-    LAST_ROW_LINE,
-    LAST_ROW_SEP,
-    LAST_ROW_END;
+    FIRST_ROW_START, // "╭"
+    FIRST_ROW_LINE, // "─"
+    FIRST_ROW_SEP, // "┬"
+    FIRST_ROW_END, // "╮"
+    ROW_START, // "│"
+    ROW_SEP, // "│"
+    ROW_END, // "│"
+    MIDDLE_ROW_START, // "├"
+    MIDDLE_ROW_LINE, // "─"
+    MIDDLE_ROW_SEP, // "┼"
+    MIDDLE_ROW_END, // "┤"
+    LAST_ROW_START, // "╰"
+    LAST_ROW_LINE, // "─"
+    LAST_ROW_SEP, //  "┴"
+    LAST_ROW_END; // "╯"
 
     /**
      * lower-cased identifier for the enum entry
