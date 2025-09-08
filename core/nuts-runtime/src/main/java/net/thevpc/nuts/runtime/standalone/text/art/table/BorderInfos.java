4c1c9dfaf7f6ccfb4fef448dbc68359f467509d5
thevpc <taha.bensalah@gmail.com>
2025-09-08 17:43:06 +0100

    [UPDATE] reimplemented Table format

---
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/text/art/table/BorderInfos.java
@@ -0,0 +1,13 @@
+package net.thevpc.nuts.runtime.standalone.text.art.table;
+
+class BorderInfos {
+    boolean lastRow;
+    boolean lastColumn;
+    boolean firstRow;
+    boolean firstColumn;
+    boolean previousColumnHasTopBorder;
+    boolean previousRowHasLeftBorder;
+    boolean previousRowIsSpanned;
+    boolean hasTopBorder;
+    boolean hasLeftBorder;
+}
