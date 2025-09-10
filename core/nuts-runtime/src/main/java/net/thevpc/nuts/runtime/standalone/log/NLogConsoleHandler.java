50886f3a93a0397d867dedb55cb9358384344144
thevpc <taha.bensalah@gmail.com>
2025-09-10 05:13:18 +0100

    Update NLogConsoleHandler.java : fixed logging

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/log/NLogConsoleHandler.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/log/NLogConsoleHandler.java
@@ -1,63 +1,53 @@
 package net.thevpc.nuts.runtime.standalone.log;
 
 
+import net.thevpc.nuts.NErr;
+import net.thevpc.nuts.NExceptions;
 import net.thevpc.nuts.NWorkspace;
+import net.thevpc.nuts.log.NLog;
 import net.thevpc.nuts.log.NLogConfig;
 import net.thevpc.nuts.io.NPrintStream;
 import net.thevpc.nuts.NSession;
+import net.thevpc.nuts.log.NLogSPI;
+import net.thevpc.nuts.runtime.standalone.workspace.NWorkspaceExt;
+import net.thevpc.nuts.util.NMsg;
+import net.thevpc.nuts.util.NStringUtils;
 
 import java.io.PrintStream;
+import java.time.Instant;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.logging.Filter;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
 import java.util.logging.StreamHandler;
 
-public class NLogConsoleHandler extends StreamHandler {
+public class NLogConsoleHandler implements NLogSPI {
+    private DefaultNLogModel model;
+    private boolean suspendTerminalMode = false;
+    private final LinkedList<Rec> suspendedTerminalRecords = new LinkedList<>();
+    private int suspendedMax = 100;
+    private static class Rec{
+        Instant instant;
+        NMsg msg;
 
-    private NPrintStream out;
-
-    public NLogConsoleHandler(NPrintStream out, boolean closeable) {
-        setFormatter(new NLogRichFormatter(false));
-        setOutputStream(out, closeable);
-    }
-
-    public synchronized void setOutputStream(NPrintStream out, boolean closable) throws SecurityException {
-        flush();
-        this.out = out;
-        if (out == null) {
-            super.setOutputStream(new PrintStream(System.err) {
-                @Override
-                public void close() {
-                    //
-                }
-            });
-        }else {
-            if (closable) {
-                super.setOutputStream(out.asPrintStream());
-            } else {
-                super.setOutputStream(new PrintStream(out.asPrintStream()) {
-                    @Override
-                    public void close() {
-                        //
-                    }
-                });
-            }
+        public Rec(Instant instant, NMsg msg) {
+            this.instant = instant;
+            this.msg = msg;
         }
     }
 
-    public NPrintStream out() {
-        return out;
+    public NLogConsoleHandler(DefaultNLogModel model) {
+        this.model = model;
     }
 
     @Override
-    public boolean isLoggable(LogRecord record) {
-        if (!super.isLoggable(record)) {
+    public boolean isLoggable(Level level) {
+        int levelValue = this.model.getTermLevel() == null ? Level.INFO.intValue() : this.model.getTermLevel().intValue();
+        if (!(level.intValue() >= levelValue && levelValue != Level.OFF.intValue())) {
             return false;
         }
-        NSession session = NLogUtils.resolveSession(record);
-        if (session.isBot()) {
-            return false;
-        }
+        NSession session = NSession.of();
         NLogConfig logConfig = NWorkspace.of().getBootOptions().getLogConfig().orElseGet(NLogConfig::new);
         Level sessionLogLevel = session.getLogTermLevel();
         if (sessionLogLevel == null) {
@@ -69,28 +59,52 @@ public class NLogConsoleHandler extends StreamHandler {
             }
         }
         final int sessionLogLevelValue = sessionLogLevel.intValue();
-        Level recLogLevel = record.getLevel();
-        if (recLogLevel.intValue() < sessionLogLevelValue || sessionLogLevelValue == Level.OFF.intValue()) {
+        if (!(level.intValue() >= sessionLogLevelValue && sessionLogLevelValue != Level.OFF.intValue())) {
             return false;
-        }
-        Filter sessionLogFilter = session.getLogTermFilter();
-        if (sessionLogFilter == null && logConfig != null) {
-            sessionLogFilter = logConfig.getLogTermFilter();
-        }
-        if (sessionLogFilter != null) {
-            if (!sessionLogFilter.isLoggable(record)) {
-                return false;
-            }
         }
         return true;
     }
 
     @Override
-    public synchronized void publish(LogRecord record) {
-        if (!isLoggable(record)) {
+    public synchronized void log(NMsg message) {
+        if (!isLoggable(message.getLevel())) {
             return;
         }
-        out.resetLine();
-        super.publish(record);
+        if (suspendTerminalMode) {
+            synchronized (suspendedTerminalRecords) {
+                suspendedTerminalRecords.add(new Rec(Instant.now(), message));
+                if (suspendedTerminalRecords.size() > suspendedMax) {
+                    Rec r = suspendedTerminalRecords.removeFirst();
+                    log0(r);
+                }
+            }
+        } else {
+            log0(new Rec(Instant.now(), message));
+        }
+
+    }
+
+    public void suspendTerminal() {
+        suspendTerminalMode = true;
+    }
+
+    public void resumeTerminal() {
+        suspendTerminalMode = false;
+        DefaultNLogModel logModel = NWorkspaceExt.of().getModel().logModel;
+        synchronized (suspendedTerminalRecords) {
+            for (Iterator<Rec> iterator = suspendedTerminalRecords.iterator(); iterator.hasNext(); ) {
+                Rec r = iterator.next();
+                iterator.remove();
+                log0(r);
+            }
+        }
+    }
+
+    private void log0(Rec rec) {
+        NErr.resetLine();
+        NErr.println(NMsg.ofC("%s %s %s %s", rec.instant, rec.msg.getLevel(), rec.msg.getIntent(), rec.msg));
+        if (rec.msg.getThrowable() != null) {
+            NErr.println(NStringUtils.stacktrace(rec.msg.getThrowable()));
+        }
     }
 }
