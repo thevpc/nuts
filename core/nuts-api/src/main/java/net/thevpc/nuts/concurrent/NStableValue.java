70f46e41d0e12263a1de2a01ec5ac9386068b397
thevpc <taha.bensalah@gmail.com>
2025-09-15 01:24:33 +0100

    Update NStableValue.java : add NCachedValue

---
+++ core/nuts-api/src/main/java/net/thevpc/nuts/concurrent/NStableValue.java
@@ -0,0 +1,21 @@
+package net.thevpc.nuts.concurrent;
+
+import java.util.function.Supplier;
+
+public interface NStableValue<T> {
+
+    static <T> NStableValue<T> of(Supplier<T> supplier) {
+        return NConcurrentFactory.of().createStableValue(supplier);
+    }
+
+    T get();
+
+    boolean isEvaluated();
+
+    boolean isValid();
+
+    boolean isError();
+
+    boolean setIfAbsent(T value);
+    boolean computeAndSetIfAbsent(Supplier<T> value);
+}
