e0b804a20dca0825b5071314cfba41e0db42c758
thevpc <taha.bensalah@gmail.com>
2025-09-17 13:31:19 +0100

    [UPDATE] added NRateLimitedValue

---
+++ core/nuts-api/src/main/java/net/thevpc/nuts/concurrent/NRateLimitStrategy.java
@@ -0,0 +1,22 @@
+package net.thevpc.nuts.concurrent;
+
+public interface NRateLimitStrategy {
+    /**
+     * Try to consume tokens.
+     * @param count number of tokens
+     * @return true if tokens were available and consumed
+     */
+    boolean tryConsume(int count);
+
+    /**
+     * How long to wait (ms) until enough tokens are available.
+     * @param count number of tokens to consume
+     * @return milliseconds to wait
+     */
+    long nextAvailableMillis(int count);
+
+    /**
+     * Serialize current state for persistence
+     */
+    NRateLimitStrategyModel toModel();
+}
