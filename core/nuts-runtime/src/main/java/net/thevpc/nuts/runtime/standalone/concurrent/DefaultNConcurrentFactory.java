7f2c1085bad843f2a47051db28bd21dc9bfad960
thevpc <taha.bensalah@gmail.com>
2025-09-17 13:31:01 +0100

    [UPDATE] added NRateLimitedValue

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/concurrent/DefaultNConcurrentFactory.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/concurrent/DefaultNConcurrentFactory.java
@@ -1,9 +1,7 @@
 package net.thevpc.nuts.runtime.standalone.concurrent;
 
 import net.thevpc.nuts.NConstants;
-import net.thevpc.nuts.concurrent.NCachedValue;
-import net.thevpc.nuts.concurrent.NConcurrentFactory;
-import net.thevpc.nuts.concurrent.NStableValue;
+import net.thevpc.nuts.concurrent.*;
 import net.thevpc.nuts.spi.NComponentScope;
 import net.thevpc.nuts.spi.NScopeType;
 import net.thevpc.nuts.spi.NSupportLevelContext;
@@ -12,6 +10,8 @@ import java.util.function.Supplier;
 
 @NComponentScope(NScopeType.WORKSPACE)
 public class DefaultNConcurrentFactory implements NConcurrentFactory {
+    private NRateLimitedValueStore memoryStore = new NRateLimitedValueStoreMemory();
+    private NRateLimitedValueFactory memoryFactory = new NRateLimitedValueFactoryImpl(memoryStore);
 
     @Override
     public int getSupportLevel(NSupportLevelContext context) {
@@ -19,12 +19,22 @@ public class DefaultNConcurrentFactory implements NConcurrentFactory {
     }
 
     @Override
-    public <T> NCachedValue<T> createCachedValue(Supplier<T> supplier) {
+    public <T> NRateLimitedValueFactory rateLimitedValueFactory() {
+        return memoryFactory;
+    }
+
+    @Override
+    public <T> NRateLimitedValueFactory rateLimitedValueFactory(NRateLimitedValueStore store) {
+        return store == null ? memoryFactory : new NRateLimitedValueFactoryImpl(store);
+    }
+
+    @Override
+    public <T> NCachedValue<T> cachedValue(Supplier<T> supplier) {
         return NCachedValueImpl.of(supplier);
     }
 
     @Override
-    public <T> NStableValue<T> createStableValue(Supplier<T> supplier) {
+    public <T> NStableValue<T> stableValue(Supplier<T> supplier) {
         return NStableValueImpl.of(supplier);
     }
 }
