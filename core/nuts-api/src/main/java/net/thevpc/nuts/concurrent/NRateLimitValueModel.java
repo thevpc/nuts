a8e2ad383b10aa9d6d7bd968f27b018d8d69d067
thevpc <taha.bensalah@gmail.com>
2025-09-18 01:17:09 +0100

    Update NRateLimitValueModel.java : updated rate limiting

---
+++ core/nuts-api/src/main/java/net/thevpc/nuts/concurrent/NRateLimitValueModel.java
@@ -0,0 +1,50 @@
+package net.thevpc.nuts.concurrent;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Objects;
+
+public class NRateLimitValueModel implements Serializable {
+    private String id;
+    private long lastAccess;
+    private NRateLimitRuleModel[] rules;
+
+    public NRateLimitValueModel(String id, long lastAccess, NRateLimitRuleModel[] rules) {
+        this.id = id == null ? "" : id;
+        this.lastAccess = lastAccess;
+        this.rules = rules;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public long getLastAccess() {
+        return lastAccess;
+    }
+
+    public NRateLimitRuleModel[] getRules() {
+        return rules;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        NRateLimitValueModel that = (NRateLimitValueModel) o;
+        return Objects.equals(id, that.id) && Objects.equals(lastAccess, that.lastAccess) && Objects.deepEquals(rules, that.rules);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, lastAccess, Arrays.hashCode(rules));
+    }
+
+    @Override
+    public String toString() {
+        return "NLimitedValueData{" +
+                "uuid='" + id + '\'' +
+                ", lastAccess=" + lastAccess +
+                ", rules=" + Arrays.toString(rules) +
+                '}';
+    }
+}
