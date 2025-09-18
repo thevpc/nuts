aa91b53d2d8ba58ef19a763cde853d7cb2bb7632
thevpc <taha.bensalah@gmail.com>
2025-09-18 01:17:09 +0100

    Update NRateLimitValueResult.java : updated rate limiting

---
+++ core/nuts-api/src/main/java/net/thevpc/nuts/concurrent/NRateLimitValueResult.java
@@ -0,0 +1,26 @@
+package net.thevpc.nuts.concurrent;
+
+import net.thevpc.nuts.util.NCallable;
+import net.thevpc.nuts.util.NMsg;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public interface NRateLimitValueResult {
+    boolean success();
+
+    //throw error if not success
+    NRateLimitValueResult orElseError();
+
+    NRateLimitValueResult orElseThrow(Function<NMsg, RuntimeException> other);
+
+    NRateLimitValueResult onSuccess(Runnable r);
+
+    <T> NRateLimitValueResult onSuccessCall(NCallable<T> r);
+
+    NRateLimitValueResult orElse(Consumer<NMsg> r);
+
+    <T> NRateLimitValueResult orElseCall(Function<NMsg, T> r);
+
+    <T> T get();
+}
