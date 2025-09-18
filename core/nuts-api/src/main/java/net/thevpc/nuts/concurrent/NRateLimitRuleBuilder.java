ea38b9432e19a5bed6fdcff103f159a9600ec41f
thevpc <taha.bensalah@gmail.com>
2025-09-18 01:17:08 +0100

    Update NRateLimitRuleBuilder.java : updated rate limiting

---
+++ core/nuts-api/src/main/java/net/thevpc/nuts/concurrent/NRateLimitRuleBuilder.java
@@ -0,0 +1,50 @@
+package net.thevpc.nuts.concurrent;
+
+import java.time.Duration;
+import java.time.Instant;
+
+public interface NRateLimitRuleBuilder {
+    NRateLimitRuleBuilder withCapacity(int capacity);
+
+    NRateLimitRuleBuilder withStartDate(Instant startDate);
+
+    NRateLimitRuleBuilder withStrategy(NRateLimitDefaultStrategy strategy);
+
+    NRateLimitRuleBuilder withStrategy(String strategy);
+
+    NRateLimitRuleBuilder withDuration(Duration duration);
+
+    default NRateLimitRuleBuilder per(Duration duration) {
+        return withDuration(duration);
+    }
+
+    default NRateLimitRuleBuilder withLimit(String limitId) {
+        return end().withLimit(limitId);
+    }
+
+    default NRateLimitRuleBuilder withLimit(String limitId, int max) {
+        return end().withLimit(limitId, max);
+    }
+
+    default NRateLimitRuleBuilder withLimit(String limitId, int max, String strategy) {
+        return end().withLimit(limitId).withCapacity(max).withStrategy(strategy);
+    }
+
+    default NRateLimitRuleBuilder withLimit(String limitId, int capacity, Duration duration) {
+        return end().withLimit(limitId).withCapacity(capacity).withDuration(duration);
+    }
+
+    default NRateLimitRuleBuilder withLimit(int capacity) {
+        return end().withLimit(null).withCapacity(capacity);
+    }
+
+    default NRateLimitRuleBuilder withLimit(int capacity, Duration duration) {
+        return end().withLimit(null).withCapacity(capacity).withDuration(duration);
+    }
+
+    default NRateLimitValue build() {
+        return end().build();
+    }
+
+    NRateLimitValueBuilder end();
+}
