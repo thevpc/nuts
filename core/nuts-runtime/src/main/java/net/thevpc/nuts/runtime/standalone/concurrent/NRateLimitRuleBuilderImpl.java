82c3a2f7beb9c7c05fc08d6227109910418937ed
thevpc <taha.bensalah@gmail.com>
2025-09-18 01:17:09 +0100

    Update NRateLimitRuleBuilderImpl.java : updated rate limiting

---
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/concurrent/NRateLimitRuleBuilderImpl.java
@@ -0,0 +1,85 @@
+package net.thevpc.nuts.runtime.standalone.concurrent;
+
+import net.thevpc.nuts.concurrent.NRateLimitDefaultStrategy;
+import net.thevpc.nuts.concurrent.NRateLimitValueBuilder;
+import net.thevpc.nuts.concurrent.NRateLimitRuleBuilder;
+import net.thevpc.nuts.util.NStringUtils;
+
+import java.time.Duration;
+import java.time.Instant;
+
+class NRateLimitRuleBuilderImpl implements NRateLimitRuleBuilder {
+    private final NRateLimitValueBuilderImpl defaultNLimitedValueBuilder;
+    private String id;
+    private String strategy;
+    private int max;
+    private Duration duration;
+    private Instant startDate;
+    public NRateLimitRuleBuilderImpl(String id, NRateLimitValueBuilderImpl defaultNLimitedValueBuilder) {
+        this.id = id;
+        this.defaultNLimitedValueBuilder = defaultNLimitedValueBuilder;
+    }
+
+    public NRateLimitRuleBuilderImpl setId(String id) {
+        this.id = id;
+        return this;
+    }
+
+    public Instant getStartDate() {
+        return startDate;
+    }
+
+    public String getStrategy() {
+        return strategy;
+    }
+
+    @Override
+    public NRateLimitRuleBuilder withCapacity(int capacity) {
+        this.max= capacity;
+        return this;
+    }
+
+    @Override
+    public NRateLimitRuleBuilder withDuration(Duration duration) {
+        this.duration=duration;
+        return this;
+    }
+
+    @Override
+    public NRateLimitRuleBuilder withStrategy(String strategy) {
+        this.strategy= NStringUtils.trimToNull(strategy);
+        return this;
+    }
+
+    @Override
+    public NRateLimitRuleBuilder withStartDate(Instant startDate) {
+        return this;
+    }
+
+    @Override
+    public NRateLimitRuleBuilder withStrategy(NRateLimitDefaultStrategy strategy) {
+        this.strategy=strategy==null?null:strategy.id();
+        return this;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getMax() {
+        return max;
+    }
+
+    public Duration getDuration() {
+        return duration;
+    }
+
+    @Override
+    public NRateLimitValueBuilder end() {
+        defaultNLimitedValueBuilder.rules.add(this);
+        if (this == defaultNLimitedValueBuilder.lastRule) {
+            defaultNLimitedValueBuilder.lastRule = null;
+        }
+        return defaultNLimitedValueBuilder;
+    }
+}
