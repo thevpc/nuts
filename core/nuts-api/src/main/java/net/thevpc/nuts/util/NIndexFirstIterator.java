51fdf89eaef5580bcb19aaf52f0a21616a831973
thevpc <taha.bensalah@gmail.com>
2025-09-25 01:16:38 +0100

    Update NIndexFirstIterator.java : update doc

--- core/nuts-api/src/main/java/net/thevpc/nuts/util/NIndexFirstIterator.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/util/NIndexFirstIterator.java
@@ -6,7 +6,7 @@
 package net.thevpc.nuts.util;
 
 import net.thevpc.nuts.*;
-import net.thevpc.nuts.elem.NDescribables;
+import net.thevpc.nuts.elem.NElementDescribables;
 import net.thevpc.nuts.elem.NElement;
 import net.thevpc.nuts.log.NLog;
 
@@ -33,8 +33,8 @@ public class NIndexFirstIterator<T> extends NIteratorBase<T> {
         return NElement
                 .ofObjectBuilder()
                 .name("IndexFirst")
-                .set("index", NDescribables.describeResolveOrDestruct(index))
-                .set("nonIndex", NDescribables.describeResolveOrDestruct(other))
+                .set("index", NElementDescribables.describeResolveOrDestruct(index))
+                .set("nonIndex", NElementDescribables.describeResolveOrDestruct(other))
                 .build()
                 ;
     }
