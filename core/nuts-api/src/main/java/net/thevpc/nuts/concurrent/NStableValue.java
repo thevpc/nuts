74011ef5744ee2e7fdb65a9624759ee62227e315
thevpc <taha.bensalah@gmail.com>
2025-09-25 01:16:37 +0100

    Update NStableValue.java : update doc

--- core/nuts-api/src/main/java/net/thevpc/nuts/concurrent/NStableValue.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/concurrent/NStableValue.java
@@ -1,8 +1,10 @@
 package net.thevpc.nuts.concurrent;
 
+import net.thevpc.nuts.elem.NElementDescribable;
+
 import java.util.function.Supplier;
 
-public interface NStableValue<T> {
+public interface NStableValue<T> extends Supplier<T>, NElementDescribable {
 
     static <T> NStableValue<T> of(Supplier<T> supplier) {
         return NConcurrentFactory.of().stableValue(supplier);
