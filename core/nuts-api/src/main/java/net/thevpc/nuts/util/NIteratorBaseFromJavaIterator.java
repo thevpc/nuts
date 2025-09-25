d2130e46cfd6f3e25504dfb87158fca76f4b19a3
thevpc <taha.bensalah@gmail.com>
2025-09-25 01:16:39 +0100

    Update NIteratorBaseFromJavaIterator.java : update doc

--- core/nuts-api/src/main/java/net/thevpc/nuts/util/NIteratorBaseFromJavaIterator.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/util/NIteratorBaseFromJavaIterator.java
@@ -36,7 +36,7 @@ public class NIteratorBaseFromJavaIterator<T> extends NIteratorBase<T> {
 
     @Override
     public NElement describe() {
-        NObjectElement b = NDescribables.describeResolveOr(base, () -> NElement.ofObjectBuilder().build())
+        NObjectElement b = NElementDescribables.describeResolveOr(base, () -> NElement.ofObjectBuilder().build())
                 .asObject().get();
         NElement a = description.get();
         if (b.isEmpty()) {
