7ff004aeaebe85c06e3811eecb88a2ea290fec1d
thevpc <taha.bensalah@gmail.com>
2025-09-25 01:16:39 +0100

    Update NIteratorSorted.java : update doc

--- core/nuts-api/src/main/java/net/thevpc/nuts/util/NIteratorSorted.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/util/NIteratorSorted.java
@@ -1,6 +1,6 @@
 package net.thevpc.nuts.util;
 
-import net.thevpc.nuts.elem.NDescribables;
+import net.thevpc.nuts.elem.NElementDescribables;
 import net.thevpc.nuts.elem.NElement;
 
 import java.util.Iterator;
@@ -22,11 +22,11 @@ class NIteratorSorted<T> extends NIteratorBase<T> {
 
     @Override
     public NElement describe() {
-        return NDescribables.describeResolveOrDestructAsObject(base)
+        return NElementDescribables.describeResolveOrDestructAsObject(base)
                 .builder()
                 .set("sort",
                         NElement.ofObjectBuilder()
-                                .set("comparator", NDescribables.describeResolveOrDestruct(c))
+                                .set("comparator", NElementDescribables.describeResolveOrDestruct(c))
                                 .set("distinct", removeDuplicates)
                                 .build()
                         )
