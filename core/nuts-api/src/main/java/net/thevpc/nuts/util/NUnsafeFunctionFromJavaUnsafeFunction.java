641ca5b9611f48319531838cc5349429a331ee22
thevpc <taha.bensalah@gmail.com>
2025-09-25 01:16:39 +0100

    Update NUnsafeFunctionFromJavaUnsafeFunction.java : update doc

--- core/nuts-api/src/main/java/net/thevpc/nuts/util/NUnsafeFunctionFromJavaUnsafeFunction.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/util/NUnsafeFunctionFromJavaUnsafeFunction.java
@@ -1,6 +1,6 @@
 package net.thevpc.nuts.util;
 
-import net.thevpc.nuts.elem.NDescribables;
+import net.thevpc.nuts.elem.NElementDescribables;
 import net.thevpc.nuts.elem.NElement;
 
 import java.util.function.Supplier;
@@ -31,6 +31,6 @@ public class NUnsafeFunctionFromJavaUnsafeFunction<T, V> implements NUnsafeFunct
 
     @Override
     public NElement describe() {
-        return NDescribables.safeDescribe(description, NDescribables.ofLateToString(this));
+        return NElementDescribables.safeDescribe(description, NElementDescribables.ofLateToString(this));
     }
 }
