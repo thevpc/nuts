ea14f30cd4c0352b1e742f8ef7257a1d17e6d66a
thevpc <taha.bensalah@gmail.com>
2025-09-25 01:16:38 +0100

    Update NConvertedIterator.java : update doc

--- core/nuts-api/src/main/java/net/thevpc/nuts/util/NConvertedIterator.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/util/NConvertedIterator.java
@@ -25,7 +25,7 @@
  */
 package net.thevpc.nuts.util;
 
-import net.thevpc.nuts.elem.NDescribables;
+import net.thevpc.nuts.elem.NElementDescribables;
 import net.thevpc.nuts.elem.NElement;
 
 import java.util.Iterator;
@@ -51,8 +51,8 @@ public class NConvertedIterator<F, T> extends NIteratorBase<T> {
     public NElement describe() {
         return NElement.ofObjectBuilder()
                 .name("Map")
-                .set("mapper", NDescribables.describeResolveOrDestruct(converter))
-                .set("base", NDescribables.describeResolveOrDestruct(base))
+                .set("mapper", NElementDescribables.describeResolveOrDestruct(converter))
+                .set("base", NElementDescribables.describeResolveOrDestruct(base))
                 .build();
     }
 
