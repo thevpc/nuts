08be2f22d279e6dc588b1fe94050791c7cc1ab3a
thevpc <taha.bensalah@gmail.com>
2025-09-25 01:16:39 +0100

    Update NPushBackIterator.java : update doc

--- core/nuts-api/src/main/java/net/thevpc/nuts/util/NPushBackIterator.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/util/NPushBackIterator.java
@@ -25,7 +25,7 @@
 */
 package net.thevpc.nuts.util;
 
-import net.thevpc.nuts.elem.NDescribables;
+import net.thevpc.nuts.elem.NElementDescribables;
 import net.thevpc.nuts.elem.NElement;
 
 import java.util.Iterator;
@@ -46,7 +46,7 @@ public class NPushBackIterator<T> extends NIteratorBase<T> {
 
     @Override
     public NElement describe() {
-        return NDescribables.describeResolveOrDestructAsObject(base)
+        return NElementDescribables.describeResolveOrDestructAsObject(base)
                 .builder()
                 .set("pushBack",true)
                 .build()
