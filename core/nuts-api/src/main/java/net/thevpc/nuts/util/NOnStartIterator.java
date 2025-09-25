df9780c51af3fd670dabab73deaef8bffd0a08c9
thevpc <taha.bensalah@gmail.com>
2025-09-25 01:16:39 +0100

    Update NOnStartIterator.java : update doc

--- core/nuts-api/src/main/java/net/thevpc/nuts/util/NOnStartIterator.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/util/NOnStartIterator.java
@@ -1,6 +1,6 @@
 package net.thevpc.nuts.util;
 
-import net.thevpc.nuts.elem.NDescribables;
+import net.thevpc.nuts.elem.NElementDescribables;
 import net.thevpc.nuts.elem.NElement;
 
 import java.util.Iterator;
@@ -18,9 +18,9 @@ class NOnStartIterator<T> extends NIteratorBase<T> {
 
     @Override
     public NElement describe() {
-        return NDescribables.describeResolveOrDestructAsObject(base)
+        return NElementDescribables.describeResolveOrDestructAsObject(base)
                 .builder()
-                .set("onStart", NDescribables.describeResolveOrToString(r))
+                .set("onStart", NElementDescribables.describeResolveOrToString(r))
                 .build()
                 ;
     }
