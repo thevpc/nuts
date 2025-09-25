3391eb887094df347db626b8d5be66e8798554de
thevpc <taha.bensalah@gmail.com>
2025-09-25 19:00:44 +0100

    [FIXED] added log

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/workspace/cmd/recom/SimpleRecommendationConnector.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/workspace/cmd/recom/SimpleRecommendationConnector.java
@@ -1,25 +1,18 @@
 package net.thevpc.nuts.runtime.standalone.workspace.cmd.recom;
 
-import java.io.IOException;
-
+import net.thevpc.nuts.NException;
 import net.thevpc.nuts.NSession;
-import net.thevpc.nuts.elem.NElementParser;
 import net.thevpc.nuts.elem.NElementWriter;
-import net.thevpc.nuts.elem.NElements;
 import net.thevpc.nuts.NIllegalArgumentException;
-import net.thevpc.nuts.runtime.standalone.io.util.CoreIOUtils;
-import net.thevpc.nuts.runtime.standalone.xtra.web.DefaultNWebCli;
+import net.thevpc.nuts.format.NContentType;
 import net.thevpc.nuts.util.NMsg;
 
-import java.io.OutputStream;
 import java.io.UncheckedIOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLConnection;
-import java.nio.charset.StandardCharsets;
 import java.util.Locale;
 
 import net.thevpc.nuts.io.NIOException;
+import net.thevpc.nuts.web.NWebCli;
+import net.thevpc.nuts.web.NWebRequest;
 
 public class SimpleRecommendationConnector extends AbstractRecommendationConnector {
     public SimpleRecommendationConnector() {
@@ -29,32 +22,24 @@ public class SimpleRecommendationConnector extends AbstractRecommendationConnect
     @Override
     public <T> T post(String url, RequestQueryInfo ri, Class<T> resultType) {
         validateRequest(ri);
-        NSession session = NSession.of();
         try {
-            URL url2 = CoreIOUtils.urlOf(ri.server + url);
-            URLConnection con = url2.openConnection();
-            DefaultNWebCli.prepareGlobalConnection(con);
-            HttpURLConnection http = (HttpURLConnection) con;
-            http.setRequestMethod("POST");
-            http.setDoOutput(true);
-            http.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-            http.setRequestProperty("Accept", "*/*");
-            String loc = session.getLocale().orDefault();
+            NWebCli cli = NWebCli.of();
+            cli.setConnectTimeout(500);
+            cli.setReadTimeout(500);
+            NWebRequest post = cli.POST(ri.server + url)
+                    .setContentType("application/json; charset=UTF-8")
+                    .setHeader("Accept", "*/*");
+            String loc = NSession.of().getLocale().orDefault();
             if (loc == null) {
                 loc = Locale.getDefault().toString();
             }
-            http.setRequestProperty("Accept-Language", loc);
+            post.setHeader("Accept-Language", loc);
             String out = NElementWriter.ofJson().toString(ri.q);
-            int length = out.length();
-            http.setFixedLengthStreamingMode(length);
-            http.setConnectTimeout(1000);
-            http.setReadTimeout(1000);
-            http.connect();
-            try (OutputStream os = http.getOutputStream()) {
-                os.write(out.getBytes(StandardCharsets.UTF_8));
-            }
-            return NElementParser.ofJson().parse(http.getInputStream(), resultType);
-        } catch (IOException | UncheckedIOException ex) {
+            post.setRequestBody(out.getBytes());
+            return post.run().getContentAs(resultType, NContentType.JSON);
+        } catch (NException ex) {
+            throw ex;
+        } catch (UncheckedIOException ex) {
             throw new NIOException(NMsg.ofC("recommendations are not available : %s", ex.toString()), ex);
         } catch (Exception ex) {
             throw new NIllegalArgumentException(NMsg.ofC("unexpected error : %s", ex.toString()), ex);
