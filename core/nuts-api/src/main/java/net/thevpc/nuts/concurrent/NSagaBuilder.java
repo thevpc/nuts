c897c79d543bd9737e903a78344ab3136479b511
thevpc <taha.bensalah@gmail.com>
2025-10-01 22:35:14 +0100

    Update NSagaBuilder.java : [NEW VERSION] v0.8.7

---
+++ core/nuts-api/src/main/java/net/thevpc/nuts/concurrent/NSagaBuilder.java
@@ -0,0 +1,32 @@
+package net.thevpc.nuts.concurrent;
+
+/**
+ * @since 0.8.7
+ */
+public interface NSagaBuilder {
+    Suite<NSagaBuilder> start();
+    <T> NSaga<T> build();
+
+    interface Suite<P>  {
+        Suite<P> then(String name, NSagaStep step);
+        If<Suite<P>> thenIf(String name, NSagaCondition condition);
+        While<Suite<P>> thenWhile(String name, NSagaCondition condition);
+
+        P end();
+    }
+    interface While<P>  {
+        While<P> then(String name, NSagaStep step);
+        If<While<P>> thenIf(String name, NSagaCondition condition);
+        While<While<P>> thenWhile(String name, NSagaCondition condition);
+        P end();
+    }
+
+    interface If<P>  {
+        If<P> then(String name, NSagaStep step);
+        If<If<P>> thenIf(String name, NSagaCondition condition);
+        While<If<P>> thenWhile(String name, NSagaCondition condition);
+        If<P> elseIf(String name, NSagaCondition condition);
+        If<P> otherwise();
+        P end();
+    }
+}
