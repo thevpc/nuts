15e7d576e5f9abeaeac59193bd6f0882159a7334
thevpc <taha.bensalah@gmail.com>
2025-10-01 22:35:14 +0100

    Update NStableValueFactory.java : [NEW VERSION] v0.8.7

---
+++ core/nuts-api/src/main/java/net/thevpc/nuts/concurrent/NStableValueFactory.java
@@ -0,0 +1,29 @@
+package net.thevpc.nuts.concurrent;
+
+import net.thevpc.nuts.reflect.NBeanContainer;
+
+import java.util.function.Supplier;
+
+/**
+ * @since 0.8.6
+ */
+public interface NStableValueFactory {
+    static NStableValueFactory of() {
+        return NConcurrent.of().stableValueFactory();
+    }
+
+    static NStableValueFactory of(NStableValueStore store) {
+        return NConcurrent.of().stableValueFactory(store);
+    }
+
+    NStableValueFactory withStore(NStableValueStore store);
+
+    NStableValueFactory withBeanContainer(NBeanContainer beanContainer);
+
+    NBeanContainer getBeanContainer();
+
+    NStableValueStore getStore();
+
+    <T> NStableValue<T> of(Supplier<T> supplier);
+    <T> NStableValue<T> of(String id, Supplier<T> supplier);
+}
