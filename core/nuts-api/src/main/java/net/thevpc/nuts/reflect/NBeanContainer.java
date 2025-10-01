7786ccd84c03b8cab6280b4950ba08b751e151ee
thevpc <taha.bensalah@gmail.com>
2025-10-01 22:35:14 +0100

    Update NBeanContainer.java : [NEW VERSION] v0.8.7

---
+++ core/nuts-api/src/main/java/net/thevpc/nuts/reflect/NBeanContainer.java
@@ -0,0 +1,33 @@
+package net.thevpc.nuts.reflect;
+
+import net.thevpc.nuts.NScopedValue;
+import net.thevpc.nuts.elem.NElement;
+import net.thevpc.nuts.util.NOptional;
+
+public interface NBeanContainer {
+    static NScopedValue<NBeanContainer> current() {
+        return NReflect.of().scopedBeanContainer();
+    }
+
+    <T> NOptional<T> get(NBeanRef ref);
+
+    default <T> NOptional<T> get(String ref) {
+        return get(NBeanRef.of(ref));
+    }
+
+    default <T> NOptional<T> get(String ref, NElement variant) {
+        return get(NBeanRef.of(ref, variant));
+    }
+
+    default <T> T of(NBeanRef ref) {
+        return this.<T>get(ref).get();
+    }
+
+    default <T> T of(String ref) {
+        return this.<T>of(NBeanRef.of(ref));
+    }
+
+    default <T> T of(String ref, NElement variant) {
+        return this.<T>of(NBeanRef.of(ref, variant));
+    }
+}
