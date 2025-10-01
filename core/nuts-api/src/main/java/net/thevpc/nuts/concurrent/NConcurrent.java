893e1342def27cf1377fd1f7d9b99de2cc78b771
thevpc <taha.bensalah@gmail.com>
2025-10-01 22:35:13 +0100

    Update NConcurrent.java : [NEW VERSION] v0.8.7

---
+++ core/nuts-api/src/main/java/net/thevpc/nuts/concurrent/NConcurrent.java
@@ -0,0 +1,136 @@
+package net.thevpc.nuts.concurrent;
+
+import net.thevpc.nuts.ext.NExtensions;
+import net.thevpc.nuts.spi.NComponent;
+import net.thevpc.nuts.util.NCallable;
+
+import java.util.concurrent.ExecutorService;
+import java.util.function.Supplier;
+
+/**
+ * @since 0.8.7
+ */
+public interface NConcurrent extends NComponent {
+    static NConcurrent of() {
+        return NExtensions.of(NConcurrent.class);
+    }
+    ExecutorService executorService();
+
+    <T> NCachedValue<T> cachedValue(Supplier<T> supplier);
+
+    <T> NCachedValue<T> cachedValue(String id, Supplier<T> supplier);
+
+
+    NConcurrent setCachedValueFactory(NCachedValueFactory cachedValueFactory);
+
+    NCachedValueFactory memoryCachedValueFactory();
+
+    NCachedValueFactory defaultCachedValueFactory();
+
+    NCachedValueFactory cachedValueFactory();
+
+    NCachedValueFactory cachedValueFactory(NCachedValueStore store);
+
+
+    <T> NStableValue<T> stableValue(Supplier<T> supplier);
+
+    <T> NStableValue<T> stableValue(String id, Supplier<T> supplier);
+
+    NConcurrent setStableValueFactory(NStableValueFactory StableValueFactory);
+
+    NStableValueFactory memoryStableValueFactory();
+
+    NStableValueFactory defaultStableValueFactory();
+
+    NStableValueFactory stableValueFactory();
+
+    NStableValueFactory stableValueFactory(NStableValueStore store);
+
+    NRateLimitValueFactory defaultRateLimitValueFactory();
+
+    NRateLimitValueFactory memoryRateLimitValueFactory();
+
+    NConcurrent setRateLimitValueFactory(NRateLimitValueFactory factory);
+
+    NRateLimitValueFactory rateLimitValueFactory();
+
+    NRateLimitValueFactory rateLimitValueFactory(NRateLimitValueStore store);
+
+    /**
+     * @since 0.8.7
+     */
+    <T> NRetryCall<T> retryCall(NCallable<T> callable);
+
+    /**
+     * @since 0.8.7
+     */
+    <T> NRetryCall<T> retryCall(String id, NCallable<T> callable);
+
+
+    /**
+     * @since 0.8.7
+     */
+    NConcurrent setRetryCallFactory(NRetryCallFactory retryCallFactory);
+
+    /**
+     * @since 0.8.7
+     */
+    NRetryCallFactory memoryRetryCallFactory();
+
+    /**
+     * @since 0.8.7
+     */
+    NRetryCallFactory defaultRetryCallFactory();
+
+    /**
+     * @since 0.8.7
+     */
+    NRetryCallFactory retryCallFactory();
+
+    /**
+     * @since 0.8.7
+     */
+    <T> NCircuitBreakerCall<T> circuitBreakerCall(NCallable<T> callable);
+
+    /**
+     * @since 0.8.7
+     */
+    <T> NCircuitBreakerCall<T> circuitBreakerCall(String id, NCallable<T> callable);
+
+
+    /**
+     * @since 0.8.7
+     */
+    NConcurrent setCircuitBreakerCallFactory(NCircuitBreakerCallFactory circuitBreakerCallFactory);
+
+    /**
+     * @since 0.8.7
+     */
+    NCircuitBreakerCallFactory memoryCircuitBreakerCallFactory();
+
+    /**
+     * @since 0.8.7
+     */
+    NCircuitBreakerCallFactory defaultCircuitBreakerCallFactory();
+
+    /**
+     * @since 0.8.7
+     */
+    NCircuitBreakerCallFactory circuitBreakerCallFactory();
+
+
+    /**
+     * @since 0.8.7
+     */
+    NSagaFactory defaultSagaFactory();
+    /**
+     * @since 0.8.7
+     */
+    NSagaFactory sagaFactory();
+    /**
+     * @since 0.8.7
+     */
+    NSagaFactory memorySagaFactory();
+
+    NSagaBuilder saga();
+}
