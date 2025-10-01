7729483250a1dbfe476106335a4160b11aaad1fd
thevpc <taha.bensalah@gmail.com>
2025-10-01 22:35:14 +0100

    Update NSagaNodeType.java : [NEW VERSION] v0.8.7

---
+++ core/nuts-api/src/main/java/net/thevpc/nuts/concurrent/NSagaNodeType.java
@@ -0,0 +1,37 @@
+package net.thevpc.nuts.concurrent;
+
+import net.thevpc.nuts.util.NEnum;
+import net.thevpc.nuts.util.NEnumUtils;
+import net.thevpc.nuts.util.NNameFormat;
+import net.thevpc.nuts.util.NOptional;
+
+import java.io.Serializable;
+
+/**
+ * @since 0.8.7
+ */
+public enum NSagaNodeType implements NEnum {
+    STEP,
+    IF,
+    WHILE,
+    SUITE,
+    UNDO;
+
+    /**
+     * lower-cased identifier for the enum entry
+     */
+    private final String id;
+
+    NSagaNodeType() {
+        this.id = NNameFormat.ID_NAME.format(name());
+    }
+
+    public static NOptional<NSagaNodeType> parse(String value) {
+        return NEnumUtils.parseEnum(value, NSagaNodeType.class);
+    }
+
+    @Override
+    public String id() {
+        return id;
+    }
+}
