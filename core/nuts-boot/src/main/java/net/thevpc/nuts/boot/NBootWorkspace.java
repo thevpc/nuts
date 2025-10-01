d280055f622bc07430ef6d3515932234405bdfc5
thevpc <taha.bensalah@gmail.com>
2025-10-01 22:35:15 +0100

    Update NBootWorkspace.java : [NEW VERSION] v0.8.7

--- core/nuts-boot/src/main/java/net/thevpc/nuts/boot/NBootWorkspace.java
+++ core/nuts-boot/src/main/java/net/thevpc/nuts/boot/NBootWorkspace.java
@@ -7,7 +7,7 @@ import net.thevpc.nuts.boot.reserved.util.NBootUtils;
 import java.util.Arrays;
 
 public interface NBootWorkspace {
-    String NUTS_BOOT_VERSION = "0.8.6";
+    String NUTS_BOOT_VERSION = "0.8.7";
 
     static NBootWorkspace of(String[] args) {
         return of(NBootArguments.of(args));
