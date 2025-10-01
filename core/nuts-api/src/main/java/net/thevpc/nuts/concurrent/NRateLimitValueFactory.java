c2d47d8d06b7d4ba087496aecdcbd3b7d9611cb8
thevpc <taha.bensalah@gmail.com>
2025-10-01 22:35:13 +0100

    Update NRateLimitValueFactory.java : [NEW VERSION] v0.8.7

--- core/nuts-api/src/main/java/net/thevpc/nuts/concurrent/NRateLimitValueFactory.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/concurrent/NRateLimitValueFactory.java
@@ -1,17 +1,27 @@
 package net.thevpc.nuts.concurrent;
 
+import net.thevpc.nuts.reflect.NBeanContainer;
+
 import java.util.function.Function;
 
 public interface NRateLimitValueFactory {
     static NRateLimitValueFactory of() {
-        return NConcurrentFactory.of().rateLimitValueFactory();
+        return NConcurrent.of().rateLimitValueFactory();
     }
 
     static NRateLimitValueFactory of(NRateLimitValueStore store) {
-        return NConcurrentFactory.of().rateLimitValueFactory(store);
+        return NConcurrent.of().rateLimitValueFactory(store);
     }
 
     NRateLimitValueBuilder valueBuilder(String id);
+
+    NRateLimitValueFactory withStore(NRateLimitValueStore store);
+
+    NRateLimitValueFactory withBeanContainer(NBeanContainer beanContainer);
+
+    NBeanContainer getBeanContainer();
+
+    NRateLimitValueStore getStore();
 
     NRateLimitValueFactory defineStrategy(String name, Function<NRateLimitRuleModel, NRateLimitRule> definition);
 }
