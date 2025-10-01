8947aa03b838a8a63c7cfc2aebcdbb91237de5a0
thevpc <taha.bensalah@gmail.com>
2025-10-01 22:35:14 +0100

    Update NRetryCallFactory.java : [NEW VERSION] v0.8.7

---
+++ core/nuts-api/src/main/java/net/thevpc/nuts/concurrent/NRetryCallFactory.java
@@ -0,0 +1,22 @@
+package net.thevpc.nuts.concurrent;
+
+import net.thevpc.nuts.reflect.NBeanContainer;
+import net.thevpc.nuts.util.NCallable;
+
+/**
+ * @since 0.8.7
+ */
+public interface NRetryCallFactory {
+    NBeanContainer getBeanContainer();
+
+    NRetryCallFactory withBeanContainer(NBeanContainer container);
+
+    NRetryCallStore getStore();
+
+    NRetryCallFactory withStore(NRetryCallStore store);
+
+    <T> NRetryCall<T> of(NCallable<T> callable);
+
+    <T> NRetryCall<T> of(String id, NCallable<T> callable);
+
+}
