886388bb700c1abb4b26771191694d16bb18f8e8
thevpc <taha.bensalah@gmail.com>
2025-10-02 18:45:13 +0100

    Rename NSagaCallContext.java → NSagaContext.java : wip-load-balancer

--- core/nuts-api/src/main/java/net/thevpc/nuts/concurrent/NSagaContext.java
+++
@@ -1,9 +0,0 @@
-package net.thevpc.nuts.concurrent;
-
-/**
- * @since 0.8.7
- */
-public interface NSagaContext {
-    <T> T getVar(String name);
-    NSagaContext setVar(String name, Object value);
-}
