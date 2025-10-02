e3b71ed99fc83d6225d8565581672be3f1265075
thevpc <taha.bensalah@gmail.com>
2025-10-02 18:45:17 +0100

    Update NSagaCallNodeType.java : wip-load-balancer

---
+++ core/nuts-api/src/main/java/net/thevpc/nuts/concurrent/NSagaCallNodeType.java
@@ -0,0 +1,35 @@
+package net.thevpc.nuts.concurrent;
+
+import net.thevpc.nuts.util.NEnum;
+import net.thevpc.nuts.util.NEnumUtils;
+import net.thevpc.nuts.util.NNameFormat;
+import net.thevpc.nuts.util.NOptional;
+
+/**
+ * @since 0.8.7
+ */
+public enum NSagaCallNodeType implements NEnum {
+    STEP,
+    IF,
+    WHILE,
+    SUITE,
+    UNDO;
+
+    /**
+     * lower-cased identifier for the enum entry
+     */
+    private final String id;
+
+    NSagaCallNodeType() {
+        this.id = NNameFormat.ID_NAME.format(name());
+    }
+
+    public static NOptional<NSagaCallNodeType> parse(String value) {
+        return NEnumUtils.parseEnum(value, NSagaCallNodeType.class);
+    }
+
+    @Override
+    public String id() {
+        return id;
+    }
+}
