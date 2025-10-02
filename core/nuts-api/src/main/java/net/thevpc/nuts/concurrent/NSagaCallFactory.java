a503d051f2dd375b75c9625bf77571af60ccebf2
thevpc <taha.bensalah@gmail.com>
2025-10-02 18:45:17 +0100

    Update NSagaCallFactory.java : wip-load-balancer

---
+++ core/nuts-api/src/main/java/net/thevpc/nuts/concurrent/NSagaCallFactory.java
@@ -0,0 +1,19 @@
+package net.thevpc.nuts.concurrent;
+
+import net.thevpc.nuts.reflect.NBeanContainer;
+
+/**
+ * @since 0.8.7
+ */
+public interface NSagaCallFactory {
+    NBeanContainer getBeanContainer();
+
+    NSagaCallFactory withBeanContainer(NBeanContainer container);
+
+    NSagaCallStore getStore();
+
+    NSagaCallFactory withStore(NSagaCallStore store);
+
+    NSagaBuilder of();
+
+}
