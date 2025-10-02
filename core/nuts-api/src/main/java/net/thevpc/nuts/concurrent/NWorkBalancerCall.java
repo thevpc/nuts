cdc8da565ed2a8696db07afd07513654ee90bfde
thevpc <taha.bensalah@gmail.com>
2025-10-03 00:20:18 +0100

    Update NWorkBalancerCall.java : update load balancer

--- core/nuts-api/src/main/java/net/thevpc/nuts/concurrent/NWorkBalancerCall.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/concurrent/NWorkBalancerCall.java
@@ -2,5 +2,29 @@ package net.thevpc.nuts.concurrent;
 
 import net.thevpc.nuts.util.NCallable;
 
+import java.util.List;
+
+/**
+ * Represents a synchronous load-balanced call.
+ * Executes work on multiple workers according to the chosen strategy.
+ *
+ * @param <T> the type returned by the call
+ * @since 0.8.7
+ */
 public interface NWorkBalancerCall<T> extends NCallable<T> {
+
+    /**
+     * Returns all currently running jobs for this call.
+     */
+    List<NWorkBalancerRunningJob<T>> runningJobs();
+
+    /**
+     * Returns true if any jobs are currently running for this call.
+     */
+    boolean hasRunningJobs();
+
+    /**
+     * Returns the number of currently active jobs.
+     */
+    int runningJobsCount();
 }
