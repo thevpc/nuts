a5381516845140a022cf370dc9c78a2f73d9a522
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:24:07 +0100

    Update RawOutputStream.java : bump nuts 0.8.7

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/text/RawOutputStream.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/text/RawOutputStream.java
@@ -1,6 +1,6 @@
 package net.thevpc.nuts.runtime.standalone.text;
 
-import net.thevpc.nuts.NUnsupportedEnumException;
+import net.thevpc.nuts.util.NUnsupportedEnumException;
 import net.thevpc.nuts.runtime.standalone.io.outputstream.BaseTransparentFilterOutputStream;
 import net.thevpc.nuts.runtime.standalone.io.terminal.NTerminalModeOp;
 
