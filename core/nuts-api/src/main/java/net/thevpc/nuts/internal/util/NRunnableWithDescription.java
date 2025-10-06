834c8fa232ed4d3dcad11ccfaf3886e374f4be79
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:24:33 +0100

    Update NRunnableWithDescription.java : bump nuts 0.8.7

---
+++ core/nuts-api/src/main/java/net/thevpc/nuts/internal/util/NRunnableWithDescription.java
@@ -0,0 +1,26 @@
+package net.thevpc.nuts.internal.util;
+
+import net.thevpc.nuts.elem.NElement;
+import net.thevpc.nuts.concurrent.NRunnable;
+
+import java.util.function.Supplier;
+
+public class NRunnableWithDescription implements NRunnable {
+    private final NRunnable base;
+    private final Supplier<NElement> nfo;
+
+    public NRunnableWithDescription(NRunnable base, Supplier<NElement> nfo) {
+        this.base = base;
+        this.nfo = nfo;
+    }
+
+    @Override
+    public NElement describe() {
+        return nfo.get();
+    }
+
+    @Override
+    public void run() {
+        base.run();
+    }
+}
