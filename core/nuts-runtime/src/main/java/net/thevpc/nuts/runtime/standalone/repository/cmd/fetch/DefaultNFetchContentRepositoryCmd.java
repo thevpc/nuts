7fd8b455ac07f2b693bdaf38592931c49efb6eb4
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:24:04 +0100

    Update DefaultNFetchContentRepositoryCmd.java : bump nuts 0.8.7

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/repository/cmd/fetch/DefaultNFetchContentRepositoryCmd.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/repository/cmd/fetch/DefaultNFetchContentRepositoryCmd.java
@@ -24,10 +24,13 @@
  */
 package net.thevpc.nuts.runtime.standalone.repository.cmd.fetch;
 
-import net.thevpc.nuts.*;
-import net.thevpc.nuts.NConstants;
+import net.thevpc.nuts.core.NConstants;
+import net.thevpc.nuts.artifact.NDescriptor;
+import net.thevpc.nuts.artifact.NId;
+import net.thevpc.nuts.artifact.NNotFoundException;
 import net.thevpc.nuts.io.NPath;
 import net.thevpc.nuts.log.NMsgIntent;
+import net.thevpc.nuts.core.NRepository;
 import net.thevpc.nuts.runtime.standalone.log.NLogUtils;
 import net.thevpc.nuts.runtime.standalone.repository.impl.NRepositoryExt;
 import net.thevpc.nuts.runtime.standalone.util.CoreStringUtils;
