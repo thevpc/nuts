d5e7a51d6d9a91078a1478a203eb2385f4bc6f26
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:24:01 +0100

    Update DefaultSearchFormatJson.java : bump nuts 0.8.7

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/format/json/DefaultSearchFormatJson.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/format/json/DefaultSearchFormatJson.java
@@ -8,7 +8,7 @@ package net.thevpc.nuts.runtime.standalone.format.json;
 import net.thevpc.nuts.cmdline.NArg;
 import net.thevpc.nuts.cmdline.NCmdLine;
 import net.thevpc.nuts.elem.NElementWriter;
-import net.thevpc.nuts.format.NContentType;
+import net.thevpc.nuts.text.NContentType;
 import net.thevpc.nuts.io.NPrintStream;
 import net.thevpc.nuts.runtime.standalone.format.NFetchDisplayOptions;
 import net.thevpc.nuts.runtime.standalone.format.DefaultSearchFormatBase;
