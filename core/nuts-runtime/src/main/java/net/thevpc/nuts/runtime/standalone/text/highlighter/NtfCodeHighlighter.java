6dbbf33bdc152ea01c1d29bdcc6e12a24bcb238f
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:24:08 +0100

    Update NtfCodeHighlighter.java : bump nuts 0.8.7

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/text/highlighter/NtfCodeHighlighter.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/text/highlighter/NtfCodeHighlighter.java
@@ -1,8 +1,8 @@
 package net.thevpc.nuts.runtime.standalone.text.highlighter;
 
 import net.thevpc.nuts.spi.NCodeHighlighter;
-import net.thevpc.nuts.NConstants;
-import net.thevpc.nuts.NWorkspace;
+import net.thevpc.nuts.core.NConstants;
+import net.thevpc.nuts.core.NWorkspace;
 import net.thevpc.nuts.text.NText;
 import net.thevpc.nuts.text.NTexts;
 import net.thevpc.nuts.spi.NSupportLevelContext;
