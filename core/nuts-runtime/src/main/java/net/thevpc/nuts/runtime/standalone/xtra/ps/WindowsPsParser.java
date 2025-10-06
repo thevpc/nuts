6ba7d3fefa564006b621caa2a7ce9a72813690d4
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:24:17 +0100

    Update WindowsPsParser.java : bump nuts 0.8.7

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/xtra/ps/WindowsPsParser.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/xtra/ps/WindowsPsParser.java
@@ -1,6 +1,6 @@
 package net.thevpc.nuts.runtime.standalone.xtra.ps;
 
-import net.thevpc.nuts.NShellFamily;
+import net.thevpc.nuts.platform.NShellFamily;
 import net.thevpc.nuts.cmdline.NCmdLine;
 import net.thevpc.nuts.cmdline.NCmdLines;
 import net.thevpc.nuts.io.NIOException;
