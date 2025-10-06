74d03bcc7198cf7714d1c6b5a1366ee200765cad
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:24:17 +0100

    Update ScriptBuilder.java : bump nuts 0.8.7

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/xtra/shell/ScriptBuilder.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/xtra/shell/ScriptBuilder.java
@@ -1,7 +1,7 @@
 package net.thevpc.nuts.runtime.standalone.xtra.shell;
 
-import net.thevpc.nuts.NId;
-import net.thevpc.nuts.NShellFamily;
+import net.thevpc.nuts.artifact.NId;
+import net.thevpc.nuts.platform.NShellFamily;
 import net.thevpc.nuts.runtime.standalone.workspace.cmd.settings.util.PathInfo;
 import net.thevpc.nuts.runtime.standalone.workspace.cmd.settings.ndi.NdiScriptOptions;
 import net.thevpc.nuts.runtime.standalone.workspace.cmd.settings.ndi.base.BaseSystemNdi;
