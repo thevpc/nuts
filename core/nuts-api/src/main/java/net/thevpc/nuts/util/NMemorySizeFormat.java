08a85f37137f5da63ee246c09e8fd84839e622d4
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:23:51 +0100

    Update NMemorySizeFormat.java : bump nuts 0.8.7

--- core/nuts-api/src/main/java/net/thevpc/nuts/util/NMemorySizeFormat.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/util/NMemorySizeFormat.java
@@ -1,8 +1,7 @@
 package net.thevpc.nuts.util;
 
-import net.thevpc.nuts.format.NPositionType;
+import net.thevpc.nuts.text.NPositionType;
 import net.thevpc.nuts.io.NMemoryPrintStream;
-import net.thevpc.nuts.io.NPlainPrintStream;
 import net.thevpc.nuts.io.NPrintStream;
 import net.thevpc.nuts.io.NTerminalMode;
 import net.thevpc.nuts.text.NTextStyle;
