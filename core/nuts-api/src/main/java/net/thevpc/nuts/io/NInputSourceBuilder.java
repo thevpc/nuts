a598b622feaec920c7a9503ea5d189a618efe00b
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:23:44 +0100

    Update NInputSourceBuilder.java : bump nuts 0.8.7

--- core/nuts-api/src/main/java/net/thevpc/nuts/io/NInputSourceBuilder.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/io/NInputSourceBuilder.java
@@ -1,6 +1,6 @@
 package net.thevpc.nuts.io;
 
-import net.thevpc.nuts.reserved.rpi.NIORPI;
+import net.thevpc.nuts.internal.rpi.NIORPI;
 import net.thevpc.nuts.util.NMsg;
 import net.thevpc.nuts.time.NProgressListener;
 
