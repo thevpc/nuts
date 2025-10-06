e5357de6fbf98eeb6503e341a9a36b3199841d8d
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:24:29 +0100

    Update NUnsafeRunnableWithDescription.java : bump nuts 0.8.7

---
+++ core/nuts-api/src/main/java/net/thevpc/nuts/concurrent/NUnsafeRunnableWithDescription.java
@@ -0,0 +1,26 @@
+package net.thevpc.nuts.concurrent;
+
+import net.thevpc.nuts.elem.NElement;
+import net.thevpc.nuts.util.NImmutable;
+
+import java.util.function.Supplier;
+
+public class NUnsafeRunnableWithDescription implements NUnsafeRunnable, NImmutable {
+    private final NUnsafeRunnable base;
+    private final Supplier<NElement> nfo;
+
+    public NUnsafeRunnableWithDescription(NUnsafeRunnable base, Supplier<NElement> nfo) {
+        this.base = base;
+        this.nfo = nfo;
+    }
+
+    @Override
+    public NElement describe() {
+        return nfo.get();
+    }
+
+    @Override
+    public void run() throws Exception {
+        base.run();
+    }
+}
