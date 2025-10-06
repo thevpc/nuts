a42c438fd30d24c644d4ad956fb1a429f468b4c4
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:24:36 +0100

    Update NReadOnlyException.java : bump nuts 0.8.7

---
+++ core/nuts-api/src/main/java/net/thevpc/nuts/util/NReadOnlyException.java
@@ -0,0 +1,57 @@
+/**
+ * ====================================================================
+ * Nuts : Network Updatable Things Service
+ * (universal package manager)
+ * <br>
+ * is a new Open Source Package Manager to help install packages and libraries
+ * for runtime execution. Nuts is the ultimate companion for maven (and other
+ * build managers) as it helps installing all package dependencies at runtime.
+ * Nuts is not tied to java and is a good choice to share shell scripts and
+ * other 'things' . It's based on an extensible architecture to help supporting a
+ * large range of sub managers / repositories.
+ * <br>
+ * <p>
+ * Copyright [2020] [thevpc]
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE Version 3 (the "License"); 
+ * you may  not use this file except in compliance with the License. You may obtain
+ * a copy of the License at https://www.gnu.org/licenses/lgpl-3.0.en.html
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an 
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language 
+ * governing permissions and limitations under the License.
+ * <br>
+ * ====================================================================
+ */
+package net.thevpc.nuts.util;
+
+
+import net.thevpc.nuts.core.NSession;
+import net.thevpc.nuts.core.NWorkspace;
+import net.thevpc.nuts.text.NText;
+import net.thevpc.nuts.text.NTextStyle;
+
+/**
+ * Created by vpc on 1/15/17.
+ *
+ * @app.category Exceptions
+ * @since 0.5.4
+ */
+public class NReadOnlyException extends NException {
+
+    /**
+     * Constructs a new NutsReadOnlyException exception
+     *
+     * @param location location
+     */
+    public NReadOnlyException(String location) {
+        super(NMsg.ofC("cannot update readonly workspace %s", NText.ofStyled(location, NTextStyle.path())));
+    }
+
+    /**
+     * Constructs a new NutsReadOnlyException exception
+     */
+    public NReadOnlyException() {
+        this(NSession.get().isNotPresent()? null : String.valueOf(NWorkspace.of().getWorkspaceLocation()));
+    }
+}
