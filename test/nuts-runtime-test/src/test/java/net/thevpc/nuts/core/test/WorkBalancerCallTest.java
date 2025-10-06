9f698e83393f075353f1c9263e83d461508ff35e
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:24:20 +0100

    Update WorkBalancerCallTest.java : bump nuts 0.8.7

--- test/nuts-runtime-test/src/test/java/net/thevpc/nuts/core/test/WorkBalancerCallTest.java
+++ test/nuts-runtime-test/src/test/java/net/thevpc/nuts/core/test/WorkBalancerCallTest.java
@@ -1,12 +1,16 @@
 package net.thevpc.nuts.core.test;
 
-import net.thevpc.nuts.NBootOptionsBuilder;
-import net.thevpc.nuts.NExceptions;
-import net.thevpc.nuts.NOut;
+import net.thevpc.nuts.core.NBootOptionsBuilder;
+import net.thevpc.nuts.util.NExceptions;
+import net.thevpc.nuts.io.NOut;
 import net.thevpc.nuts.concurrent.*;
 import net.thevpc.nuts.core.test.utils.TestUtils;
+import net.thevpc.nuts.concurrent.NCallable;
+import net.thevpc.nuts.util.NMsg;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.TimeUnit;
 
 public class WorkBalancerCallTest {
     @BeforeAll
@@ -17,34 +21,45 @@ public class WorkBalancerCallTest {
 
     @Test
     public void test1() {
-        NWorkBalancerCallFactory factory = NWorkBalancerCallFactory.of();
-        NWorkBalancerCall<String> call = factory.<String>ofBuilder("example")
-                .addWorker("worker1",()->{
-                    NOut.println("call worker1");
-                    try {
-                        Thread.sleep(2000);
-                    } catch (InterruptedException e) {
-                        throw NExceptions.ofUncheckedException(e);
-                    }
-                    return "hello 1";
-                })
+        NWorkBalancerFactory factory = NWorkBalancerFactory.of();
+        NWorkBalancer<String> workBalancer = factory.<String>ofBuilder("example")
+                .addWorker("worker1")
+                .withWeight(1)
                 .then()
-                .addWorker("worker2",()->{
-                    NOut.println("call worker2");
-                    try {
-                        Thread.sleep(2000);
-                    } catch (InterruptedException e) {
-                        throw NExceptions.ofUncheckedException(e);
-                    }
-                    return "hello 1";
-                })
+                .addWorker("worker2")
+                .withWeight(2)
                 .then()
-                .setStrategy("round-robin")
+                .setStrategy(NWorkBalancerDefaultStrategy.ROUND_ROBIN)
                 .build();
 
-        for (int i = 0; i < 100; i++) {
-            call.call();
+        NCallable<String> callable = workBalancer.of("hello", new NWorkBalancerJob<String>() {
+            @Override
+            public String call(NWorkBalancerJobContext context) {
+                synchronized (WorkBalancerCallTest.class) {
+                    NOut.println(NMsg.ofC("call worker:%s jobName:%s jobId:%s", context.getWorkerName(), context.getJobName(), context.getJobId()));
+                }
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    throw NExceptions.ofUncheckedException(e);
+                }
+                return "hello from " + context.getWorkerName();
+            }
+        });
+        for (int i = 0; i < 50; i++) {
+            NConcurrent.of().executorService().submit(callable);
         }
+        NConcurrent.of().executorService().shutdown();
+        NOut.println(NMsg.ofC("runningJobsCount %s",workBalancer.getRunningJobsCount()));
+        NOut.println(NMsg.ofC("workerLoads %s",workBalancer.getWorkerLoads()));
+        try {
+            NConcurrent.of().executorService().awaitTermination(Long.MAX_VALUE, TimeUnit.HOURS);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        NOut.println("-------------------------------------------------------------");
+        NOut.println(NMsg.ofC("runningJobsCount %s",workBalancer.getRunningJobsCount()));
+        NOut.println(NMsg.ofC("workerLoads %s",workBalancer.getWorkerLoads()));
     }
 
 }
