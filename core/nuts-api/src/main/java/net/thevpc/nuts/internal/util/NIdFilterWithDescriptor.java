0942152d7cd6b5196f500b93483983ec59348c4c
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:24:32 +0100

    Update NIdFilterWithDescriptor.java : bump nuts 0.8.7

---
+++ core/nuts-api/src/main/java/net/thevpc/nuts/internal/util/NIdFilterWithDescriptor.java
@@ -0,0 +1,36 @@
+package net.thevpc.nuts.internal.util;
+
+import net.thevpc.nuts.artifact.NIdFilter;
+import net.thevpc.nuts.elem.NElementDescribables;
+import net.thevpc.nuts.elem.NElement;
+import net.thevpc.nuts.spi.base.NIdFilterDelegate;
+import net.thevpc.nuts.util.NFilter;
+
+import java.util.function.Supplier;
+
+public class NIdFilterWithDescriptor extends NIdFilterDelegate {
+    private NIdFilter base;
+    private Supplier<NElement> description;
+
+    public NIdFilterWithDescriptor(NIdFilter base, Supplier<NElement> description) {
+        super();
+        this.base = base;
+        this.description = description;
+    }
+
+    @Override
+    public NIdFilter baseNIdFilter() {
+        return base;
+    }
+
+    @Override
+    public NFilter redescribe(Supplier<NElement> description) {
+        this.description = description;
+        return this;
+    }
+
+    @Override
+    public NElement describe() {
+        return NElementDescribables.safeDescribeOfBase(description, base);
+    }
+}
