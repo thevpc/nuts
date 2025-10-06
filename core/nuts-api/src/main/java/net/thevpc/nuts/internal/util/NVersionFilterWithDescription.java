11ffb2434cd06c4b54e2e00be9fe03f904307054
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:24:33 +0100

    Update NVersionFilterWithDescription.java : bump nuts 0.8.7

---
+++ core/nuts-api/src/main/java/net/thevpc/nuts/internal/util/NVersionFilterWithDescription.java
@@ -0,0 +1,36 @@
+package net.thevpc.nuts.internal.util;
+
+import net.thevpc.nuts.artifact.NVersionFilter;
+import net.thevpc.nuts.elem.NElementDescribables;
+import net.thevpc.nuts.elem.NElement;
+import net.thevpc.nuts.spi.base.NVersionFilterDelegate;
+import net.thevpc.nuts.util.NFilter;
+
+import java.util.function.Supplier;
+
+public class NVersionFilterWithDescription extends NVersionFilterDelegate {
+    private NVersionFilter baseVersionFilter;
+    private Supplier<NElement> description;
+
+    public NVersionFilterWithDescription(NVersionFilter baseVersionFilter, Supplier<NElement> description) {
+        super();
+        this.baseVersionFilter = baseVersionFilter;
+        this.description = description;
+    }
+
+    @Override
+    public NVersionFilter baseVersionFilter() {
+        return baseVersionFilter;
+    }
+
+    @Override
+    public NFilter redescribe(Supplier<NElement> description) {
+        this.description=description;
+        return this;
+    }
+
+    @Override
+    public NElement describe() {
+        return NElementDescribables.safeDescribeOfBase(description, baseVersionFilter);
+    }
+}
