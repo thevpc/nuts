0009e84ad667d4194ef47fb56eb0e208b6ecd520
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:24:32 +0100

    Update NReservedOptionalThrowable.java : bump nuts 0.8.7

---
+++ core/nuts-api/src/main/java/net/thevpc/nuts/internal/optional/NReservedOptionalThrowable.java
@@ -0,0 +1,106 @@
+package net.thevpc.nuts.internal.optional;
+
+import net.thevpc.nuts.util.*;
+
+import java.util.function.Supplier;
+
+public abstract class NReservedOptionalThrowable<T> extends NReservedOptionalImpl<T> implements Cloneable {
+    private static boolean DEBUG;
+
+    static {
+        String property = System.getProperty("nuts.optional.debug");
+        DEBUG = Boolean.parseBoolean(property);
+    }
+
+    private Throwable rootStack = DEBUG ? new Throwable() : null;
+    private Supplier<NOptional<T>> defaultValue;
+
+    public NReservedOptionalThrowable() {
+    }
+
+    public T orDefault() {
+        if (defaultValue == null) {
+            return null;
+        }
+        NOptional<T> o = defaultValue.get();
+        if (o == null) {
+            return null;
+        }
+        return o.orDefault();
+    }
+
+    @Override
+    public NOptional<T> orDefaultOptional() {
+        if (defaultValue == null) {
+            return null;
+        }
+        NOptional<T> o = defaultValue.get();
+        if (o == null) {
+            return NOptional.ofEmpty(getMessage());
+        }
+        return o.orDefaultOptional();
+    }
+
+    protected NMsg prepareMessage(NMsg m) {
+        if (DEBUG) {
+            return NMsg.ofC("%s.\n    call stack:\n%s\n    root stack:\n%s", m,
+                    NStringUtils.stacktrace(new Throwable()),
+                    NStringUtils.stacktrace(rootStack)
+            );
+        }
+        if (m == null) {
+            m = NMsg.ofMissingValue();
+        }
+        return m;
+    }
+
+    @Override
+    public boolean isNull() {
+        return false;
+    }
+
+    @Override
+    public NOptional<T> withDefault(Supplier<T> value) {
+        NReservedOptionalThrowable<T> c = (NReservedOptionalThrowable<T>) clone();
+        c.defaultValue = value == null ? null : () -> NOptional.of(value.get());
+        return c;
+    }
+
+    @Override
+    public NOptional<T> withDefaultOptional(Supplier<NOptional<T>> value) {
+        NReservedOptionalThrowable<T> c = (NReservedOptionalThrowable<T>) clone();
+        c.defaultValue = value == null ? null : () -> {
+            NOptional<T> i = value.get();
+            if (i == null) {
+                return NOptional.ofEmpty(getMessage());
+            }
+            return this;
+        };
+        return c;
+    }
+
+    @Override
+    public NOptional<T> withDefault(T value) {
+        NReservedOptionalThrowable<T> c = (NReservedOptionalThrowable<T>) clone();
+        c.defaultValue = value == null ? null : () -> NOptional.of(value);
+        return c;
+    }
+
+    @Override
+    public NOptional<T> withoutDefault() {
+        NReservedOptionalThrowable<T> c = (NReservedOptionalThrowable<T>) clone();
+        c.defaultValue = null;
+        return c;
+    }
+
+    @Override
+    protected NOptional<T> clone() {
+        try {
+            return (NOptional<T>) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+}
