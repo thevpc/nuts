44333b38fedcdba420e5bd4470a5e91f15f99dc2
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:24:06 +0100

    Update InstallLogItemTable.java : bump nuts 0.8.7

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/repository/impl/main/InstallLogItemTable.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/repository/impl/main/InstallLogItemTable.java
@@ -1,6 +1,6 @@
 package net.thevpc.nuts.runtime.standalone.repository.impl.main;
 
-import net.thevpc.nuts.NWorkspace;
+import net.thevpc.nuts.core.NWorkspace;
 import net.thevpc.nuts.runtime.standalone.workspace.NWorkspaceExt;
 import net.thevpc.nuts.runtime.standalone.xtra.nanodb.NanoDB;
 import net.thevpc.nuts.runtime.standalone.xtra.nanodb.NanoDBTableStore;
