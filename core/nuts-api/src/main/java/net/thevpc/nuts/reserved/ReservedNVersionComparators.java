a30f75072e799a8f0c61918dfbfa31fe5bc0614f
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:23:43 +0100

    Rename ReservedNVersionComparators.java → ReservedNVersionComparators.java : bump nuts 0.8.7

--- core/nuts-api/src/main/java/net/thevpc/nuts/reserved/ReservedNVersionComparators.java
+++
@@ -1,8 +0,0 @@
-package net.thevpc.nuts.reserved;
-
-import net.thevpc.nuts.NScopedValue;
-import net.thevpc.nuts.NVersionComparator;
-
-public class ReservedNVersionComparators {
-    public static NScopedValue<NVersionComparator> SCOPED_VERSION_COMPARATOR = new NScopedValue<>(NVersionComparator::ofMaven);
-}
