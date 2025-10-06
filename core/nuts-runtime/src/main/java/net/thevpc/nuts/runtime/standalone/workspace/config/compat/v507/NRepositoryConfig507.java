db88f114c610bcb6c815a84d273cd4b057cbdfb4
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:24:15 +0100

    Update NRepositoryConfig507.java : bump nuts 0.8.7

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/workspace/config/compat/v507/NRepositoryConfig507.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/workspace/config/compat/v507/NRepositoryConfig507.java
@@ -25,10 +25,11 @@
  */
 package net.thevpc.nuts.runtime.standalone.workspace.config.compat.v507;
 
-import net.thevpc.nuts.*;
-import net.thevpc.nuts.NStoreStrategy;
-import net.thevpc.nuts.NStoreType;
-import net.thevpc.nuts.NUserConfig;
+import net.thevpc.nuts.core.NConfigItem;
+import net.thevpc.nuts.core.NStoreStrategy;
+import net.thevpc.nuts.platform.NStoreType;
+import net.thevpc.nuts.core.NRepositoryRef;
+import net.thevpc.nuts.security.NUserConfig;
 
 import java.util.List;
 import java.util.Map;
