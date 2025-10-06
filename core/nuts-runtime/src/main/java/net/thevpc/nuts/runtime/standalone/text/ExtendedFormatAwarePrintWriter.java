52c9ee690af8556dec08adb6b98dc5cc53bfc2f1
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:24:07 +0100

    Update ExtendedFormatAwarePrintWriter.java : bump nuts 0.8.7

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/text/ExtendedFormatAwarePrintWriter.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/text/ExtendedFormatAwarePrintWriter.java
@@ -1,10 +1,11 @@
 package net.thevpc.nuts.runtime.standalone.text;
 
-import net.thevpc.nuts.*;
+import net.thevpc.nuts.core.NWorkspace;
 import net.thevpc.nuts.runtime.standalone.io.terminal.NTerminalModeOp;
 import net.thevpc.nuts.spi.NSystemTerminalBase;
 import net.thevpc.nuts.text.NText;
 import net.thevpc.nuts.util.NMsg;
+import net.thevpc.nuts.util.NUnsupportedEnumException;
 
 import java.io.OutputStream;
 import java.io.PrintWriter;
