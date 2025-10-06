ea759d0853846e8864517e87e6a6270c56937d10
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:24:05 +0100

    Update AbstractRepositoryFilter.java : bump nuts 0.8.7

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/repository/filter/AbstractRepositoryFilter.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/repository/filter/AbstractRepositoryFilter.java
@@ -24,7 +24,7 @@
  */
 package net.thevpc.nuts.runtime.standalone.repository.filter;
 
-import net.thevpc.nuts.NRepositoryFilter;
+import net.thevpc.nuts.core.NRepositoryFilter;
 import net.thevpc.nuts.elem.NElement;
 import net.thevpc.nuts.spi.base.AbstractNFilter;
 import net.thevpc.nuts.util.NFilter;
