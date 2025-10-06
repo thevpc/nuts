6e0b25fc7474d33b2017c1ea011a91bab726087b
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:23:47 +0100

    Update NRepositorySelectorList.java : bump nuts 0.8.7

--- core/nuts-api/src/main/java/net/thevpc/nuts/spi/NRepositorySelectorList.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/spi/NRepositorySelectorList.java
@@ -26,7 +26,7 @@
  */
 package net.thevpc.nuts.spi;
 
-import net.thevpc.nuts.NAddRepositoryOptions;
+import net.thevpc.nuts.core.NAddRepositoryOptions;
 import net.thevpc.nuts.util.NBlankable;
 import net.thevpc.nuts.util.NMsg;
 import net.thevpc.nuts.util.NOptional;
