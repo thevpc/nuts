204667d09f4b03470eb71c266322211140a36cc8
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:24:20 +0100

    Update OptionalTest.java : bump nuts 0.8.7

--- test/nuts-runtime-test/src/test/java/net/thevpc/nuts/core/test/OptionalTest.java
+++ test/nuts-runtime-test/src/test/java/net/thevpc/nuts/core/test/OptionalTest.java
@@ -6,8 +6,8 @@
 package net.thevpc.nuts.core.test;
 
 import net.thevpc.nuts.core.test.utils.TestUtils;
-import net.thevpc.nuts.NDetachedEmptyOptionalException;
-import net.thevpc.nuts.NDetachedErrorOptionalException;
+import net.thevpc.nuts.util.NDetachedEmptyOptionalException;
+import net.thevpc.nuts.util.NDetachedErrorOptionalException;
 import net.thevpc.nuts.util.NMsg;
 import net.thevpc.nuts.util.NOptional;
 import org.junit.jupiter.api.Assertions;
