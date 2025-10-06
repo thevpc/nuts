ee3741eb6410611e28adad91bf952b997751b84f
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:24:13 +0100

    Update NdiUtils.java : bump nuts 0.8.7

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/workspace/cmd/settings/ndi/util/NdiUtils.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/workspace/cmd/settings/ndi/util/NdiUtils.java
@@ -27,7 +27,7 @@ package net.thevpc.nuts.runtime.standalone.workspace.cmd.settings.ndi.util;
 
 
 import net.thevpc.nuts.io.NIOException;
-import net.thevpc.nuts.NIllegalArgumentException;
+import net.thevpc.nuts.util.NIllegalArgumentException;
 import net.thevpc.nuts.runtime.standalone.xtra.web.DefaultNWebCli;
 import net.thevpc.nuts.util.NMsg;
 import net.thevpc.nuts.runtime.standalone.workspace.cmd.settings.ndi.unix.AnyNixNdi;
