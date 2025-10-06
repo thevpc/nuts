acd27d636a673e634a3c4a42b6b8eb8bc5d3769b
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:24:20 +0100

    Update DescriptorTest.java : bump nuts 0.8.7

--- test/nuts-runtime-test/src/test/java/net/thevpc/nuts/core/test/DescriptorTest.java
+++ test/nuts-runtime-test/src/test/java/net/thevpc/nuts/core/test/DescriptorTest.java
@@ -5,7 +5,10 @@
  */
 package net.thevpc.nuts.core.test;
 
-import net.thevpc.nuts.*;
+import net.thevpc.nuts.artifact.NDefinition;
+import net.thevpc.nuts.artifact.NDependency;
+import net.thevpc.nuts.command.NFetchCmd;
+import net.thevpc.nuts.command.NSearchCmd;
 import net.thevpc.nuts.core.test.utils.TestUtils;
 import org.junit.jupiter.api.*;
 
