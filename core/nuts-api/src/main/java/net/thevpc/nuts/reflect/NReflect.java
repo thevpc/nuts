ae9dc9c0fa79c9fbb83410efab9590e11be5358e
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:23:46 +0100

    Update NReflect.java : bump nuts 0.8.7

--- core/nuts-api/src/main/java/net/thevpc/nuts/reflect/NReflect.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/reflect/NReflect.java
@@ -1,6 +1,6 @@
 package net.thevpc.nuts.reflect;
 
-import net.thevpc.nuts.NScopedValue;
+import net.thevpc.nuts.concurrent.NScopedValue;
 import net.thevpc.nuts.ext.NExtensions;
 import net.thevpc.nuts.spi.NComponent;
 
