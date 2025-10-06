386662c4ddab4e4e43ca958994fe1ffbf2a82ca5
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:24:13 +0100

    Update WinCmdShellWriter.java : bump nuts 0.8.7

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/workspace/cmd/settings/shell/WinCmdShellWriter.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/workspace/cmd/settings/shell/WinCmdShellWriter.java
@@ -1,6 +1,6 @@
 package net.thevpc.nuts.runtime.standalone.workspace.cmd.settings.shell;
 
-import net.thevpc.nuts.NShellFamily;
+import net.thevpc.nuts.platform.NShellFamily;
 import net.thevpc.nuts.util.NStringUtils;
 
 public class WinCmdShellWriter extends AbstractShellWriter {
