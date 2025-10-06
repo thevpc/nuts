5ee70705c264b792419d91bfeb03de4746dedc7d
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:23:44 +0100

    Update NMemoryPrintStream.java : bump nuts 0.8.7

--- core/nuts-api/src/main/java/net/thevpc/nuts/io/NMemoryPrintStream.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/io/NMemoryPrintStream.java
@@ -26,7 +26,7 @@
  */
 package net.thevpc.nuts.io;
 
-import net.thevpc.nuts.reserved.rpi.NIORPI;
+import net.thevpc.nuts.internal.rpi.NIORPI;
 
 public interface NMemoryPrintStream extends NPrintStream {
     static NMemoryPrintStream of() {
