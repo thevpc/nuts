8908c10f820064d3f6c4cd7ba96869ff95a502f9
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:24:28 +0100

    Update NNotExecutableException.java : bump nuts 0.8.7

---
+++ core/nuts-api/src/main/java/net/thevpc/nuts/command/NNotExecutableException.java
@@ -0,0 +1,63 @@
+/**
+ * ====================================================================
+ * Nuts : Network Updatable Things Service
+ * (universal package manager)
+ * <br>
+ * is a new Open Source Package Manager to help install packages
+ * and libraries for runtime execution. Nuts is the ultimate companion for
+ * maven (and other build managers) as it helps installing all package
+ * dependencies at runtime. Nuts is not tied to java and is a good choice
+ * to share shell scripts and other 'things' . Its based on an extensible
+ * architecture to help supporting a large range of sub managers / repositories.
+ *
+ * <br>
+ * <p>
+ * Copyright [2020] [thevpc]
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE Version 3 (the "License");
+ * you may  not use this file except in compliance with the License. You may obtain
+ * a copy of the License at https://www.gnu.org/licenses/lgpl-3.0.en.html
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ * <br>
+ * ====================================================================
+ */
+package net.thevpc.nuts.command;
+
+import net.thevpc.nuts.artifact.NId;
+import net.thevpc.nuts.util.NMsg;
+
+/**
+ * Exception thrown when a non-executable nuts id is requested to run.
+ *
+ * @app.category Exceptions
+ * @since 0.5.4
+ */
+public class NNotExecutableException extends NExecutionException {
+
+    /**
+     * artifact id
+     */
+    private final NId id;
+
+    /**
+     * Constructs a new NNotExecutableException exception
+     *
+     * @param id artifact id
+     */
+    public NNotExecutableException(NId id) {
+        super(NMsg.ofC("not executable %s", (id == null ? "<null>" : id)), NExecutionException.ERROR_255);
+        this.id = id;
+    }
+
+    /**
+     * artifact id
+     *
+     * @return artifact id
+     */
+    public NId getId() {
+        return id;
+    }
+}
