1ada520b1b90469f2e2c3ade8a2e91c70715f27e
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:24:28 +0100

    Update NInstallEvent.java : bump nuts 0.8.7

---
+++ core/nuts-api/src/main/java/net/thevpc/nuts/command/NInstallEvent.java
@@ -0,0 +1,62 @@
+/**
+ * ====================================================================
+ * Nuts : Network Updatable Things Service
+ * (universal package manager)
+ * <br>
+ * is a new Open Source Package Manager to help install packages
+ * and libraries for runtime execution. Nuts is the ultimate companion for
+ * maven (and other build managers) as it helps installing all package
+ * dependencies at runtime. Nuts is not tied to java and is a good choice
+ * to share shell scripts and other 'things' . Its based on an extensible
+ * architecture to help supporting a large range of sub managers / repositories.
+ *
+ * <br>
+ * <p>
+ * Copyright [2020] [thevpc]
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE Version 3 (the "License");
+ * you may  not use this file except in compliance with the License. You may obtain
+ * a copy of the License at https://www.gnu.org/licenses/lgpl-3.0.en.html
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ * <br>
+ * ====================================================================
+ */
+package net.thevpc.nuts.command;
+
+import net.thevpc.nuts.artifact.NDefinition;
+import net.thevpc.nuts.core.NSessionProvider;
+import net.thevpc.nuts.core.NWorkspace;
+
+/**
+ * Event describing installation of an artifact
+ *
+ * @author thevpc
+ * @app.category Events
+ * @since 0.5.6
+ */
+public interface NInstallEvent extends NSessionProvider {
+
+    /**
+     * return artifact definition
+     *
+     * @return artifact definition
+     */
+    NDefinition getDefinition();
+
+    /**
+     * vcurrent workspace
+     *
+     * @return current workspace
+     */
+    NWorkspace getWorkspace();
+
+    /**
+     * return true if installation was forced
+     *
+     * @return true if installation was forced
+     */
+    boolean isForce();
+}
