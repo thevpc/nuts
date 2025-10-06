f5170de5fd5c6f2b968daa8d7b77d99bf4d527aa
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:23:57 +0100

    Update NDependencyFilterTrue.java : bump nuts 0.8.7

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/dependency/filter/NDependencyFilterTrue.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/dependency/filter/NDependencyFilterTrue.java
@@ -1,6 +1,8 @@
 package net.thevpc.nuts.runtime.standalone.dependency.filter;
 
-import net.thevpc.nuts.*;
+import net.thevpc.nuts.artifact.NDependency;
+import net.thevpc.nuts.artifact.NDependencyFilter;
+import net.thevpc.nuts.artifact.NId;
 import net.thevpc.nuts.util.NFilterOp;
 
 public final class NDependencyFilterTrue extends AbstractDependencyFilter{
