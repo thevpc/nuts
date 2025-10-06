ebfc02aa957cf3d5bbbd2d577c1dc0bbfd0f32be
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:23:56 +0100

    Update DefaultNDependencies.java : bump nuts 0.8.7

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/dependency/DefaultNDependencies.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/dependency/DefaultNDependencies.java
@@ -1,6 +1,6 @@
 package net.thevpc.nuts.runtime.standalone.dependency;
 
-import net.thevpc.nuts.*;
+import net.thevpc.nuts.artifact.*;
 import net.thevpc.nuts.elem.NElementDescribables;
 import net.thevpc.nuts.elem.NElement;
 import net.thevpc.nuts.util.NIterator;
