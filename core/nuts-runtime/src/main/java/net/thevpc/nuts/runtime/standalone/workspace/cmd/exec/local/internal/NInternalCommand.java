777ca3fa36dd880d64f50b857127f74086131fbd
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:24:11 +0100

    Update NInternalCommand.java : bump nuts 0.8.7

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/workspace/cmd/exec/local/internal/NInternalCommand.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/workspace/cmd/exec/local/internal/NInternalCommand.java
@@ -1,6 +1,6 @@
 package net.thevpc.nuts.runtime.standalone.workspace.cmd.exec.local.internal;
 
-import net.thevpc.nuts.NExecCmd;
+import net.thevpc.nuts.command.NExecCmd;
 import net.thevpc.nuts.spi.NComponent;
 
 public interface NInternalCommand extends NComponent {
