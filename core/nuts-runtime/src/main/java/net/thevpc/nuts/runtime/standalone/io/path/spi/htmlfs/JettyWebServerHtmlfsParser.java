1c8784084fbd048c9f3f0aa1c46cde9054431ba5
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:24:03 +0100

    Update JettyWebServerHtmlfsParser.java : bump nuts 0.8.7

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/io/path/spi/htmlfs/JettyWebServerHtmlfsParser.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/io/path/spi/htmlfs/JettyWebServerHtmlfsParser.java
@@ -1,6 +1,6 @@
 package net.thevpc.nuts.runtime.standalone.io.path.spi.htmlfs;
 
-import net.thevpc.nuts.NCallableSupport;
+import net.thevpc.nuts.concurrent.NCallableSupport;
 import net.thevpc.nuts.runtime.standalone.util.XmlEscaper;
 
 import java.io.BufferedReader;
