8de8877e7447cb9d2eb4131b2d54009b792fe88e
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:24:12 +0100

    Update AbstractSystemNdi.java : bump nuts 0.8.7

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/workspace/cmd/settings/ndi/base/AbstractSystemNdi.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/workspace/cmd/settings/ndi/base/AbstractSystemNdi.java
@@ -1,6 +1,5 @@
 package net.thevpc.nuts.runtime.standalone.workspace.cmd.settings.ndi.base;
 
-import net.thevpc.nuts.NWorkspace;
 import net.thevpc.nuts.runtime.standalone.workspace.cmd.settings.ndi.SystemNdi;
 
 public abstract class AbstractSystemNdi implements SystemNdi {
