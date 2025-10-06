207051564f920c67c3ad6ae182a890829dac7af8
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:24:16 +0100

    Update DefaultNUncompress.java : bump nuts 0.8.7

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/xtra/compress/DefaultNUncompress.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/xtra/compress/DefaultNUncompress.java
@@ -5,8 +5,7 @@
  */
 package net.thevpc.nuts.runtime.standalone.xtra.compress;
 
-import net.thevpc.nuts.*;
-import net.thevpc.nuts.NConstants;
+import net.thevpc.nuts.core.NConstants;
 import net.thevpc.nuts.util.NBlankable;
 import net.thevpc.nuts.ext.NExtensions;
 import net.thevpc.nuts.io.*;
