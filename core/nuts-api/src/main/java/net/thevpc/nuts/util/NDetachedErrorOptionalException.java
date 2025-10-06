ba8fb08d5aa596f712396b333e6f39934a84ec2b
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:24:36 +0100

    Update NDetachedErrorOptionalException.java : bump nuts 0.8.7

---
+++ core/nuts-api/src/main/java/net/thevpc/nuts/util/NDetachedErrorOptionalException.java
@@ -0,0 +1,14 @@
+package net.thevpc.nuts.util;
+
+public class NDetachedErrorOptionalException extends RuntimeException implements NDetachedFormattedExceptionBase {
+    private NMsg formattedMessage;
+
+    public NDetachedErrorOptionalException(NMsg formattedMessage,Throwable cause) {
+        super(formattedMessage == null ? "error optional" : formattedMessage.toString(),cause);
+        this.formattedMessage = formattedMessage == null ? NMsg.ofC("error") : formattedMessage;
+    }
+
+    public NMsg getFormattedMessage() {
+        return formattedMessage;
+    }
+}
