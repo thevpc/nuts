0284dbdd965f4b5d0973c89f7cdce77abaabf1fe
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:24:14 +0100

    Update DefaultNUninstallInternalExecutable.java : bump nuts 0.8.7

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/workspace/cmd/uninstall/DefaultNUninstallInternalExecutable.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/workspace/cmd/uninstall/DefaultNUninstallInternalExecutable.java
@@ -5,7 +5,9 @@
  */
 package net.thevpc.nuts.runtime.standalone.workspace.cmd.uninstall;
 
-import net.thevpc.nuts.*;
+import net.thevpc.nuts.command.NExecCmd;
+import net.thevpc.nuts.command.NExecutionException;
+import net.thevpc.nuts.command.NUninstallCmd;
 import net.thevpc.nuts.runtime.standalone.app.util.NAppUtils;
 import net.thevpc.nuts.runtime.standalone.util.ExtraApiUtils;
 import net.thevpc.nuts.runtime.standalone.workspace.cmd.exec.local.internal.DefaultInternalNExecutableCommand;
