fe628d2688ab83e9c1d4bbaa621e6c5bef38372f
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:23:45 +0100

    Rename NConnexionStringBuilderFactory.java → NConnexionStringBuilderFactory.java : bump nuts 0.8.7

--- core/nuts-api/src/main/java/net/thevpc/nuts/util/NConnexionStringBuilderFactory.java
+++
@@ -1,9 +0,0 @@
-package net.thevpc.nuts.util;
-
-import net.thevpc.nuts.spi.NComponent;
-
-public interface NConnexionStringBuilderFactory extends NComponent {
-    NConnexionStringBuilder create();
-
-    NOptional<NConnexionStringBuilder> create(String expression);
-}
