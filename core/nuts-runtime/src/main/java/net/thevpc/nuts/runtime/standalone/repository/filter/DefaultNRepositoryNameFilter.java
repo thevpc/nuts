ba73d73fffbccfeb165bd994dfe7b8b0ba973e04
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:24:05 +0100

    Update DefaultNRepositoryNameFilter.java : bump nuts 0.8.7

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/repository/filter/DefaultNRepositoryNameFilter.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/repository/filter/DefaultNRepositoryNameFilter.java
@@ -1,6 +1,8 @@
 package net.thevpc.nuts.runtime.standalone.repository.filter;
 
-import net.thevpc.nuts.*;
+import net.thevpc.nuts.core.NRepository;
+import net.thevpc.nuts.core.NRepositoryFilter;
+import net.thevpc.nuts.core.NRepositoryFilters;
 import net.thevpc.nuts.runtime.standalone.xtra.glob.GlobUtils;
 import net.thevpc.nuts.util.NBlankable;
 import net.thevpc.nuts.util.NFilterOp;
