dd48797862accbc474363eec3dbaf0d5af9a6fbe
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:23:56 +0100

    Update NDefinitionFilterParser.java : bump nuts 0.8.7

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/definition/filter/NDefinitionFilterParser.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/definition/filter/NDefinitionFilterParser.java
@@ -1,7 +1,7 @@
 package net.thevpc.nuts.runtime.standalone.definition.filter;
 
-import net.thevpc.nuts.NDefinitionFilter;
-import net.thevpc.nuts.NDefinitionFilters;
+import net.thevpc.nuts.artifact.NDefinitionFilter;
+import net.thevpc.nuts.artifact.NDefinitionFilters;
 import net.thevpc.nuts.runtime.standalone.util.filters.NTypedFiltersParser;
 
 public class NDefinitionFilterParser extends NTypedFiltersParser<NDefinitionFilter> {
