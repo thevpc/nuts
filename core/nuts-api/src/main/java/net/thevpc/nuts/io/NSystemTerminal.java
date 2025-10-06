17767c47130cefef625408711289e6c5277c6c40
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:23:45 +0100

    Update NSystemTerminal.java : bump nuts 0.8.7

--- core/nuts-api/src/main/java/net/thevpc/nuts/io/NSystemTerminal.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/io/NSystemTerminal.java
@@ -26,7 +26,7 @@
  */
 package net.thevpc.nuts.io;
 
-import net.thevpc.nuts.reserved.rpi.NIORPI;
+import net.thevpc.nuts.internal.rpi.NIORPI;
 import net.thevpc.nuts.util.NMsg;
 import net.thevpc.nuts.spi.NSystemTerminalBase;
 
