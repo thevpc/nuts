dcf8e9f6b028a5c9cd52031540ead015a2d2d8fc
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:24:29 +0100

    Update DefaultCallableSupport.java : bump nuts 0.8.7

---
+++ core/nuts-api/src/main/java/net/thevpc/nuts/concurrent/DefaultCallableSupport.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.thevpc.nuts.concurrent;
+
+import net.thevpc.nuts.util.NExceptions;
+import net.thevpc.nuts.util.NMsg;
+import net.thevpc.nuts.util.NOptional;
+
+import java.util.function.Supplier;
+
+/**
+ * Default implementation of NutsSupported
+ *
+ * @param <T> value type
+ * @author thevpc
+ */
+public class DefaultCallableSupport<T> implements CallableSupport<T> {
+    private final Supplier<T> value;
+    private final int supportLevel;
+    private final Supplier<NMsg> emptyMessage;
+
+    public DefaultCallableSupport(Supplier<T> value, int supportLevel, Supplier<NMsg> emptyMessage) {
+        this.value = value;
+        if (this.value == null && supportLevel > 0) {
+            throw new IllegalArgumentException("null callable requires invalid support");
+        } else if (this.value != null && supportLevel <= 0) {
+            throw new IllegalArgumentException("non null callable requires valid support");
+        }
+        this.supportLevel = supportLevel;
+        this.emptyMessage = emptyMessage == null ? () -> NMsg.ofMissingValue() : emptyMessage;
+    }
+
+    public T call() {
+        if (isValid()) {
+            return value.get();
+        } else {
+            NMsg m = emptyMessage.get();
+            if (m == null) {
+                m = NMsg.ofMissingValue();
+            }
+            throw NExceptions.ofSafeNoSuchElementException(m);
+        }
+    }
+
+    public int getSupportLevel() {
+        return supportLevel;
+    }
+
+    @Override
+    public NOptional<T> toOptional() {
+        if (isValid()) {
+            return NOptional.ofCallable(() -> value.get());
+        }
+        NMsg m = emptyMessage.get();
+        if (m == null) {
+            m = NMsg.ofMissingValue();
+        }
+        return NOptional.ofEmpty(m);
+    }
+}
