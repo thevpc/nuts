6cc467b9714eec6e14e46fecdbc0abe70df0ed45
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:24:28 +0100

    Update NPrepareCmd.java : bump nuts 0.8.7

---
+++ core/nuts-api/src/main/java/net/thevpc/nuts/command/NPrepareCmd.java
@@ -0,0 +1,76 @@
+/**
+ * ====================================================================
+ * Nuts : Network Updatable Things Service
+ * (universal package manager)
+ * <br>
+ * is a new Open Source Package Manager to help install packages
+ * and libraries for runtime execution. Nuts is the ultimate companion for
+ * maven (and other build managers) as it helps installing all package
+ * dependencies at runtime. Nuts is not tied to java and is a good choice
+ * to share shell scripts and other 'things' . Its based on an extensible
+ * architecture to help supporting a large range of sub managers / repositories.
+ *
+ * <br>
+ * <p>
+ * Copyright [2020] [thevpc]
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE Version 3 (the "License");
+ * you may  not use this file except in compliance with the License. You may obtain
+ * a copy of the License at https://www.gnu.org/licenses/lgpl-3.0.en.html
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ * <br>
+ * ====================================================================
+ */
+package net.thevpc.nuts.command;
+
+import net.thevpc.nuts.artifact.NId;
+import net.thevpc.nuts.core.NWorkspaceCmd;
+import net.thevpc.nuts.cmdline.NCmdLineConfigurable;
+import net.thevpc.nuts.ext.NExtensions;
+
+import java.util.List;
+
+/**
+ * @author thevpc
+ * @app.category Config
+ * @since 0.5.5
+ */
+public interface NPrepareCmd extends NWorkspaceCmd {
+    static NPrepareCmd of() {
+        return NExtensions.of(NPrepareCmd.class);
+    }
+
+    NPrepareCmd setTargetServer(String remoteServer);
+
+    NPrepareCmd setUserName(String remoteUser);
+
+    NPrepareCmd setVersion(String version);
+
+    NPrepareCmd setIds(List<NId> id);
+
+    NPrepareCmd addIds(List<NId> id);
+    
+    /**
+     * configure the current command with the given arguments. This is an
+     * override of the {@link NCmdLineConfigurable#configure(boolean, String...) }
+     * to help return a more specific return type;
+     *
+     * @param skipUnsupported when true, all unsupported options are skipped
+     * @param args            argument to configure with
+     * @return {@code this} instance
+     */
+    @Override
+    NPrepareCmd configure(boolean skipUnsupported, String... args);
+
+    /**
+     * execute the command and return this instance
+     *
+     * @return {@code this} instance
+     */
+    @Override
+    NPrepareCmd run();
+
+}
