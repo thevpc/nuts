e22601c35663ed897888ad8307965fc940ddab06
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:24:01 +0100

    Update DefaultSearchFormatXml.java : bump nuts 0.8.7

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/format/xml/DefaultSearchFormatXml.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/format/xml/DefaultSearchFormatXml.java
@@ -11,7 +11,7 @@ import java.io.PrintWriter;
 import net.thevpc.nuts.cmdline.NArg;
 import net.thevpc.nuts.cmdline.NCmdLine;
 import net.thevpc.nuts.elem.NElements;
-import net.thevpc.nuts.format.NContentType;
+import net.thevpc.nuts.text.NContentType;
 import net.thevpc.nuts.io.NPrintStream;
 import net.thevpc.nuts.runtime.standalone.format.NFetchDisplayOptions;
 import net.thevpc.nuts.runtime.standalone.format.DefaultSearchFormatBase;
