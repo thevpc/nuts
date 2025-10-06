2631f05ae008b3f51268f9993d34eb51a25a13e1
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:24:33 +0100

    Update NConnexionStringBuilder.java : bump nuts 0.8.7

---
+++ core/nuts-api/src/main/java/net/thevpc/nuts/net/NConnexionStringBuilder.java
@@ -0,0 +1,68 @@
+package net.thevpc.nuts.net;
+
+import net.thevpc.nuts.ext.NExtensions;
+import net.thevpc.nuts.spi.NComponent;
+import net.thevpc.nuts.util.NOptional;
+
+import java.util.List;
+import java.util.Map;
+
+public interface NConnexionStringBuilder extends NComponent {
+    static NOptional<NConnexionStringBuilder> get(String expression) {
+        return NExtensions.of(NConnexionStringBuilderFactory.class)
+                .create(expression);
+    }
+
+    static NConnexionStringBuilder of(String expression) {
+        return get(expression).get();
+    }
+
+    static NConnexionStringBuilder of() {
+        return NExtensions.of(NConnexionStringBuilderFactory.class)
+                .create();
+    }
+
+    String getUserName();
+
+    NConnexionStringBuilder setUserName(String userName);
+
+    String getPassword();
+
+    NConnexionStringBuilder setPassword(String password);
+
+    String getHost();
+
+    NConnexionStringBuilder setHost(String host);
+
+    NConnexionStringBuilder getRoot();
+
+    NConnexionStringBuilder getParent();
+
+    String getPort();
+
+    NConnexionStringBuilder setPort(String port);
+
+    NConnexionStringBuilder setQueryMap(Map<String, List<String>> queryMap);
+
+    NOptional<Map<String, List<String>>> getQueryMap();
+
+    String getPath();
+
+    NConnexionStringBuilder setPath(String path);
+
+    String getProtocol();
+
+    NConnexionStringBuilder setProtocol(String protocol);
+
+    String getQueryString();
+
+    NConnexionStringBuilder setQueryString(String queryString);
+
+    NConnexionStringBuilder copy();
+
+    List<String> getNames();
+
+    NConnexionStringBuilder resolve(String child);
+
+    NConnexionString build();
+}
