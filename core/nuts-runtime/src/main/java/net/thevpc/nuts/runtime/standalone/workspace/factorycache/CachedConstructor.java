0c0667e9895188d1d9f0617ce7d6b0d67b956c7c
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:24:16 +0100

    Update CachedConstructor.java : bump nuts 0.8.7

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/workspace/factorycache/CachedConstructor.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/workspace/factorycache/CachedConstructor.java
@@ -1,8 +1,6 @@
 package net.thevpc.nuts.runtime.standalone.workspace.factorycache;
 
-import net.thevpc.nuts.NSession;
-
-import java.lang.reflect.Constructor;
+import net.thevpc.nuts.core.NSession;
 
 public interface CachedConstructor<T> {
     T newInstance(Object[] args,NSession session);
