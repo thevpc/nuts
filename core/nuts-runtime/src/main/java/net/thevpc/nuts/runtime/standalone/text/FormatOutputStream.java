85f0154363aa07d91f407e8b566af23172d19ed2
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:05:21 +0100

    Update FormatOutputStream.java : change support to score

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/text/FormatOutputStream.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/text/FormatOutputStream.java
@@ -1,7 +1,7 @@
 package net.thevpc.nuts.runtime.standalone.text;
 
 import net.thevpc.nuts.util.NIllegalArgumentException;
-import net.thevpc.nuts.util.NMsg;
+import net.thevpc.nuts.text.NMsg;
 import net.thevpc.nuts.util.NUnsupportedEnumException;
 import net.thevpc.nuts.runtime.standalone.io.terminal.NTerminalModeOp;
 import net.thevpc.nuts.runtime.standalone.io.terminal.NTerminalModeOpUtils;
