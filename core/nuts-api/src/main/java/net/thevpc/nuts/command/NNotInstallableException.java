0705229d5768078e65d6e07dd285853660e5565f
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:04:58 +0100

    Update NNotInstallableException.java : change support to score

--- core/nuts-api/src/main/java/net/thevpc/nuts/command/NNotInstallableException.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/command/NNotInstallableException.java
@@ -27,7 +27,7 @@
 package net.thevpc.nuts.command;
 
 import net.thevpc.nuts.artifact.NId;
-import net.thevpc.nuts.util.NMsg;
+import net.thevpc.nuts.text.NMsg;
 
 /**
  * This exception is thrown when an artifact fails to be installed.
