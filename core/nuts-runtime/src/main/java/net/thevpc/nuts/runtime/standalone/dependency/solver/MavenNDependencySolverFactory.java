a6d19c76f4fe2b182fbe315f3cae50b371a73f3c
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:05:11 +0100

    Update MavenNDependencySolverFactory.java : change support to score

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/dependency/solver/MavenNDependencySolverFactory.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/dependency/solver/MavenNDependencySolverFactory.java
@@ -1,7 +1,7 @@
 package net.thevpc.nuts.runtime.standalone.dependency.solver;
 
 import net.thevpc.nuts.runtime.standalone.dependency.solver.maven.MavenNDependencySolver;
-import net.thevpc.nuts.spi.NSupportLevelContext;
+import net.thevpc.nuts.spi.NScorableContext;
 import net.thevpc.nuts.spi.NDependencySolver;
 import net.thevpc.nuts.spi.NDependencySolverFactory;
 
@@ -11,7 +11,7 @@ public class MavenNDependencySolverFactory implements NDependencySolverFactory {
     }
 
     @Override
-    public int getSupportLevel(NSupportLevelContext context) {
+    public int getScore(NScorableContext context) {
         return 1;
     }
 
