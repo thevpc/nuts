c238c2ebad3b9631ddf18e4d4d7705e1cb56e9da
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:05:10 +0100

    Update AbstractNLock.java : change support to score

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/concurrent/AbstractNLock.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/concurrent/AbstractNLock.java
@@ -1,7 +1,7 @@
 package net.thevpc.nuts.runtime.standalone.concurrent;
 
 import net.thevpc.nuts.concurrent.NLock;
-import net.thevpc.nuts.util.NMsg;
+import net.thevpc.nuts.text.NMsg;
 import net.thevpc.nuts.util.NOptional;
 
 import java.util.concurrent.Callable;
