05b9dec974c24f42e381da6849a400d1bd50555a
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:05:01 +0100

    Update NPath.java : change support to score

--- core/nuts-api/src/main/java/net/thevpc/nuts/io/NPath.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/io/NPath.java
@@ -26,12 +26,13 @@
 package net.thevpc.nuts.io;
 
 import net.thevpc.nuts.artifact.NId;
-import net.thevpc.nuts.boot.reserved.util.NBootPlatformHome;
+import net.thevpc.nuts.boot.internal.util.NBootPlatformHome;
 import net.thevpc.nuts.core.NLocationKey;
 import net.thevpc.nuts.core.NWorkspace;
 import net.thevpc.nuts.net.NConnexionString;
 import net.thevpc.nuts.platform.NStoreType;
 import net.thevpc.nuts.core.NRepository;
+import net.thevpc.nuts.text.NMsg;
 import net.thevpc.nuts.text.NText;
 import net.thevpc.nuts.util.*;
 import net.thevpc.nuts.text.NTreeVisitor;
