f35ce7a393abe805ff78cdfec53a35cc6f156c4a
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:05:21 +0100

    Update TableNTextArtRendererFactory.java : change support to score

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/text/art/table/TableNTextArtRendererFactory.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/text/art/table/TableNTextArtRendererFactory.java
@@ -1,11 +1,10 @@
 package net.thevpc.nuts.runtime.standalone.text.art.table;
 
-import net.thevpc.nuts.core.NConstants;
 import net.thevpc.nuts.io.NInputSource;
 import net.thevpc.nuts.io.NPath;
 import net.thevpc.nuts.runtime.standalone.text.art.NTextArtImpl;
 import net.thevpc.nuts.runtime.standalone.text.art.img.PixelNTextArtImageRenderer;
-import net.thevpc.nuts.spi.NSupportLevelContext;
+import net.thevpc.nuts.spi.NScorableContext;
 import net.thevpc.nuts.text.NTextArtImageRenderer;
 import net.thevpc.nuts.text.NTextArtRenderer;
 import net.thevpc.nuts.text.NTextArtRendererFactory;
@@ -88,8 +87,8 @@ public class TableNTextArtRendererFactory implements NTextArtRendererFactory {
     }
 
     @Override
-    public int getSupportLevel(NSupportLevelContext context) {
-        return NConstants.Support.DEFAULT_SUPPORT;
+    public int getScore(NScorableContext context) {
+        return DEFAULT_SCORE;
     }
 
     public NOptional<NTextArtRenderer> getRenderer(String renderName) {
