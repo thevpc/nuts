bbb9b2211e2a83ac778cf810e2411d0d8814d284
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:05:04 +0100

    Update NProgressMonitor.java : change support to score

--- core/nuts-api/src/main/java/net/thevpc/nuts/time/NProgressMonitor.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/time/NProgressMonitor.java
@@ -1,7 +1,7 @@
 package net.thevpc.nuts.time;
 
 import net.thevpc.nuts.concurrent.NCallable;
-import net.thevpc.nuts.util.NMsg;
+import net.thevpc.nuts.text.NMsg;
 import net.thevpc.nuts.util.NOptional;
 
 /**
