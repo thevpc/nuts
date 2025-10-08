2a7bc2be43623363296fbe53ea4077150d062b4d
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:05:25 +0100

    Update NdiScriptOptions.java : change support to score

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/workspace/cmd/settings/ndi/NdiScriptOptions.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/workspace/cmd/settings/ndi/NdiScriptOptions.java
@@ -17,7 +17,7 @@ import net.thevpc.nuts.io.NPath;
 import net.thevpc.nuts.util.NAssert;
 import net.thevpc.nuts.util.NIllegalArgumentException;
 import net.thevpc.nuts.util.NLiteral;
-import net.thevpc.nuts.util.NMsg;
+import net.thevpc.nuts.text.NMsg;
 
 import java.io.IOException;
 import java.nio.file.Files;
