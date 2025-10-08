eb83bd1aa846a8560797dae660c525f15ca7aea6
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:05:07 +0100

    Update NBootOptionsInfo.java : change support to score

--- core/nuts-boot/src/main/java/net/thevpc/nuts/boot/NBootOptionsInfo.java
+++ core/nuts-boot/src/main/java/net/thevpc/nuts/boot/NBootOptionsInfo.java
@@ -24,7 +24,7 @@
  */
 package net.thevpc.nuts.boot;
 
-import net.thevpc.nuts.boot.reserved.util.NBootUtils;
+import net.thevpc.nuts.boot.internal.util.NBootUtils;
 
 import java.io.InputStream;
 import java.io.PrintStream;
