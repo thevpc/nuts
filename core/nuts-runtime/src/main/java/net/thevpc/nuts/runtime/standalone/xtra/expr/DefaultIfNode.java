0a00ee45e2cd398e0860c7419221a2d072465a8b
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:05:26 +0100

    Update DefaultIfNode.java : change support to score

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/xtra/expr/DefaultIfNode.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/xtra/expr/DefaultIfNode.java
@@ -1,7 +1,7 @@
 package net.thevpc.nuts.runtime.standalone.xtra.expr;
 
 import net.thevpc.nuts.expr.*;
-import net.thevpc.nuts.util.NMsg;
+import net.thevpc.nuts.text.NMsg;
 import net.thevpc.nuts.util.NOptional;
 
 import java.util.Collections;
