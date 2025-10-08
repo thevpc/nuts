4c79bee5f42a1274d2405a1a0d3dd6f717ced13e
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:05:04 +0100

    Update NAssertException.java : change support to score

--- core/nuts-api/src/main/java/net/thevpc/nuts/util/NAssertException.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/util/NAssertException.java
@@ -1,5 +1,7 @@
 package net.thevpc.nuts.util;
 
+import net.thevpc.nuts.text.NMsg;
+
 public class NAssertException extends NIllegalArgumentException {
 
     public NAssertException(NMsg formattedMessage, Throwable cause) {
