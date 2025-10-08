9ab5064aa63fe3dd7ed2782e096dedb5ee48b3df
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:05:02 +0100

    Update DefaultNConnexionStringBuilder.java : change support to score

--- core/nuts-api/src/main/java/net/thevpc/nuts/net/DefaultNConnexionStringBuilder.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/net/DefaultNConnexionStringBuilder.java
@@ -1,7 +1,6 @@
 package net.thevpc.nuts.net;
 
-import net.thevpc.nuts.core.NConstants;
-import net.thevpc.nuts.spi.NSupportLevelContext;
+import net.thevpc.nuts.spi.NScorableContext;
 import net.thevpc.nuts.util.*;
 
 import java.io.UnsupportedEncodingException;
@@ -388,7 +387,7 @@ public class DefaultNConnexionStringBuilder implements Cloneable, NConnexionStri
     }
 
     @Override
-    public int getSupportLevel(NSupportLevelContext context) {
-        return NConstants.Support.DEFAULT_SUPPORT;
+    public int getScore(NScorableContext context) {
+        return DEFAULT_SCORE;
     }
 }
