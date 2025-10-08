8b6ca8ea77ee3865032d4f905ce464f2aef09f46
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:05:26 +0100

    Update DefaultNExprsCommonOps.java : change support to score

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/xtra/expr/DefaultNExprsCommonOps.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/xtra/expr/DefaultNExprsCommonOps.java
@@ -1,5 +1,6 @@
 package net.thevpc.nuts.runtime.standalone.xtra.expr;
 
+import net.thevpc.nuts.text.NMsg;
 import net.thevpc.nuts.util.NIllegalArgumentException;
 import net.thevpc.nuts.expr.NExprCommonOp;
 import net.thevpc.nuts.expr.NExprOpType;
