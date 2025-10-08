cba1f97641144cb67b50f686b091976c5f4b513e
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:05:27 +0100

    Update PrintStreamProgressHandler.java : change support to score

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/xtra/mon/PrintStreamProgressHandler.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/xtra/mon/PrintStreamProgressHandler.java
@@ -1,7 +1,7 @@
 package net.thevpc.nuts.runtime.standalone.xtra.mon;
 
-import net.thevpc.nuts.util.NMsg;
-import net.thevpc.nuts.util.NMsgTemplate;
+import net.thevpc.nuts.text.NMsg;
+import net.thevpc.nuts.text.NMsgTemplate;
 import net.thevpc.nuts.time.NProgressHandlerEvent;
 import net.thevpc.nuts.time.NProgressHandler;
 
