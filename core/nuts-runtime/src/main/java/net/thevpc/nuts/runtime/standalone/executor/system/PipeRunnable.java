2d19a1b440a226f89d1a672e216ad8597be1ab0b
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:05:13 +0100

    Update PipeRunnable.java : change support to score

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/executor/system/PipeRunnable.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/executor/system/PipeRunnable.java
@@ -27,7 +27,7 @@ package net.thevpc.nuts.runtime.standalone.executor.system;
 import net.thevpc.nuts.log.NLog;
 import net.thevpc.nuts.log.NMsgIntent;
 
-import net.thevpc.nuts.util.NMsg;
+import net.thevpc.nuts.text.NMsg;
 import net.thevpc.nuts.io.NNonBlockingInputStream;
 import net.thevpc.nuts.runtime.standalone.io.util.StopMonitor;
 
