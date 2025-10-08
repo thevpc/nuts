ab1a63283a558c395b40f87454cb21d22a4e30fb
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:05:16 +0100

    Update NPrintStreamSystem.java : change support to score

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/io/printstream/NPrintStreamSystem.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/io/printstream/NPrintStreamSystem.java
@@ -9,7 +9,7 @@ import net.thevpc.nuts.io.NTerminalMode;
 import net.thevpc.nuts.spi.NSystemTerminalBase;
 import net.thevpc.nuts.text.NTerminalCmd;
 import net.thevpc.nuts.util.NIllegalArgumentException;
-import net.thevpc.nuts.util.NMsg;
+import net.thevpc.nuts.text.NMsg;
 
 import java.io.OutputStream;
 import java.io.PrintStream;
