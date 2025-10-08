829570f34a83e97b80de9ec7597f9a78c67c1522
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:05:09 +0100

    Update module-info.java : change support to score

--- core/nuts-boot/src/main/java9/module-info.java
+++ core/nuts-boot/src/main/java9/module-info.java
@@ -7,7 +7,7 @@ module net.thevpc.nuts.boot {
     requires java.xml;
     requires java.desktop;
     exports net.thevpc.nuts.boot;
-    exports net.thevpc.nuts.core;
+    exports net.thevpc.nuts.boot.core;
     //exports net.thevpc.nuts.spi;
     //uses net.thevpc.nuts.spi.NutsBootWorkspaceFactory;
     //uses net.thevpc.nuts.spi.NutsComponent;
