e361bf77568e65cec899e1e03996663cf0011a66
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:05:19 +0100

    Update DefaultNRepositoryModel.java : change support to score

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/repository/config/DefaultNRepositoryModel.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/repository/config/DefaultNRepositoryModel.java
@@ -20,7 +20,7 @@ import net.thevpc.nuts.runtime.standalone.workspace.NWorkspaceUtils;
 import net.thevpc.nuts.log.NLog;
 
 import net.thevpc.nuts.util.NBlankable;
-import net.thevpc.nuts.util.NMsg;
+import net.thevpc.nuts.text.NMsg;
 import net.thevpc.nuts.util.NOptional;
 
 import java.nio.file.Path;
