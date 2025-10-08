5f934b1a717464d6c7f3d860ec3ac784bf5ac16f
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:05:14 +0100

    Update CommandForIdNInstallerComponent.java : change support to score

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/installer/CommandForIdNInstallerComponent.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/installer/CommandForIdNInstallerComponent.java
@@ -40,7 +40,7 @@ import net.thevpc.nuts.runtime.standalone.executor.NExecutionContextUtils;
 import net.thevpc.nuts.runtime.standalone.workspace.NWorkspaceUtils;
 import net.thevpc.nuts.runtime.standalone.xtra.expr.StringPlaceHolderParser;
 import net.thevpc.nuts.spi.NInstallerComponent;
-import net.thevpc.nuts.spi.NSupportLevelContext;
+import net.thevpc.nuts.spi.NScorableContext;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -127,8 +127,8 @@ public class CommandForIdNInstallerComponent implements NInstallerComponent {
     }
 
     @Override
-    public int getSupportLevel(NSupportLevelContext criteria) {
-        return NConstants.Support.DEFAULT_SUPPORT;
+    public int getScore(NScorableContext criteria) {
+        return DEFAULT_SCORE;
     }
 
 
