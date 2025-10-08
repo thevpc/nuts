37d851bc3e6e7f83df46932a458151292d61726f
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:05:05 +0100

    Update NEmptyOptionalException.java : change support to score

--- core/nuts-api/src/main/java/net/thevpc/nuts/util/NEmptyOptionalException.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/util/NEmptyOptionalException.java
@@ -1,5 +1,7 @@
 package net.thevpc.nuts.util;
 
+import net.thevpc.nuts.text.NMsg;
+
 public class NEmptyOptionalException extends NException {
 
     public NEmptyOptionalException(NMsg formattedMessage) {
