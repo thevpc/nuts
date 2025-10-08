817c3b035bd6103d69046b918c4518182f2c79f6
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:05:21 +0100

    Update NTextArtImpl.java : change support to score

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/text/art/NTextArtImpl.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/text/art/NTextArtImpl.java
@@ -26,14 +26,13 @@ package net.thevpc.nuts.runtime.standalone.text.art;
 import java.util.*;
 import java.util.stream.Collectors;
 
-import net.thevpc.nuts.core.NConstants;
 import net.thevpc.nuts.ext.NExtensions;
 import net.thevpc.nuts.io.NPath;
 import net.thevpc.nuts.spi.NComponentScope;
 import net.thevpc.nuts.spi.NScopeType;
-import net.thevpc.nuts.spi.NSupportLevelContext;
+import net.thevpc.nuts.spi.NScorableContext;
 import net.thevpc.nuts.text.*;
-import net.thevpc.nuts.util.NMsg;
+import net.thevpc.nuts.text.NMsg;
 import net.thevpc.nuts.util.NOptional;
 import net.thevpc.nuts.util.NStream;
 
@@ -178,8 +177,8 @@ public class NTextArtImpl implements NTextArt {
     }
 
     @Override
-    public int getSupportLevel(NSupportLevelContext context) {
-        return NConstants.Support.DEFAULT_SUPPORT;
+    public int getScore(NScorableContext context) {
+        return DEFAULT_SCORE;
     }
 
 }
