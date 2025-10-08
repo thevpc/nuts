a88e10a10797dbe3738d68dff0ca22326f2f9651
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:05:19 +0100

    Update DefaultNSearchRepositoryCmd.java : change support to score

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/repository/cmd/search/DefaultNSearchRepositoryCmd.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/repository/cmd/search/DefaultNSearchRepositoryCmd.java
@@ -5,15 +5,16 @@
  */
 package net.thevpc.nuts.runtime.standalone.repository.cmd.search;
 
+import net.thevpc.nuts.artifact.NArtifactNotFoundException;
 import net.thevpc.nuts.core.NConstants;
 import net.thevpc.nuts.artifact.NId;
-import net.thevpc.nuts.artifact.NNotFoundException;
 import net.thevpc.nuts.command.NFetchMode;
 import net.thevpc.nuts.concurrent.NRunnable;
 import net.thevpc.nuts.core.NSession;
 import net.thevpc.nuts.elem.NElementDescribables;
 import net.thevpc.nuts.core.NIndexerNotAccessibleException;
 import net.thevpc.nuts.core.NRepository;
+import net.thevpc.nuts.text.NMsg;
 import net.thevpc.nuts.text.NPositionType;
 import net.thevpc.nuts.log.NMsgIntent;
 import net.thevpc.nuts.util.NIndexFirstIterator;
@@ -86,7 +87,7 @@ public class DefaultNSearchRepositoryCmd extends AbstractNSearchRepositoryCmd {
                     .onStart(startRunnable)
                     .onFinish(endRunnable)
                     .build();
-        } catch (NNotFoundException | SecurityException ex) {
+        } catch (NArtifactNotFoundException | SecurityException ex) {
             _LOG()
                     .log(NMsg.ofJ("{0} search packages", NStringUtils.formatAlign(getRepo().getName(), 20, NPositionType.FIRST))
                             .withLevel(Level.FINEST).withIntent(NMsgIntent.FAIL));
