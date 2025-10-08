1f4f501de4109ec5e5f44b43705ef7889b97b87a
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:39:30 +0100

    Update NBootMsgParam.java : change support to score

---
+++ core/nuts-boot/src/main/java/net/thevpc/nuts/boot/internal/util/NBootMsgParam.java
@@ -0,0 +1,25 @@
+package net.thevpc.nuts.boot.internal.util;
+
+import java.util.function.Supplier;
+
+public class NBootMsgParam {
+    private String name;
+    private Supplier<?> value;
+
+    public static NBootMsgParam of(String name, Supplier<?> value) {
+        return new NBootMsgParam(name,value);
+    }
+
+    public NBootMsgParam(String name, Supplier<?> value) {
+        this.name = NBootUtils.requireNonBlank(name, "name");
+        this.value = NBootUtils.requireNonNull(value, "value");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Supplier<?> getValue() {
+        return value;
+    }
+}
