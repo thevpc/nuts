3af6f747130b245942f1f722698f7243925bbb80
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:05:22 +0100

    Update DefaultUnitFormat.java : change support to score

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/text/util/DefaultUnitFormat.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/text/util/DefaultUnitFormat.java
@@ -1,7 +1,7 @@
 package net.thevpc.nuts.runtime.standalone.text.util;
 
 import net.thevpc.nuts.util.NIllegalArgumentException;
-import net.thevpc.nuts.util.NMsg;
+import net.thevpc.nuts.text.NMsg;
 import net.thevpc.nuts.text.NText;
 import net.thevpc.nuts.text.NTextBuilder;
 import net.thevpc.nuts.text.NTextStyle;
