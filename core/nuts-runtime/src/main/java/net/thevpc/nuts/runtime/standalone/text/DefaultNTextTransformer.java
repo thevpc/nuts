86a7616a97ca4596b1dc1556ee85b17e89b029f8
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:05:21 +0100

    Update DefaultNTextTransformer.java : change support to score

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/text/DefaultNTextTransformer.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/text/DefaultNTextTransformer.java
@@ -1,7 +1,7 @@
 package net.thevpc.nuts.runtime.standalone.text;
 
 import net.thevpc.nuts.util.NIllegalArgumentException;
-import net.thevpc.nuts.util.NMsg;
+import net.thevpc.nuts.text.NMsg;
 import net.thevpc.nuts.cmdline.NArg;
 import net.thevpc.nuts.cmdline.NCmdLine;
 import net.thevpc.nuts.io.NPath;
