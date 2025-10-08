a8c66503861229f0cd0d98fad75ebd96ff0971cf
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:05:27 +0100

    Update SyntaxParser.java : change support to score

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/xtra/expr/SyntaxParser.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/xtra/expr/SyntaxParser.java
@@ -2,7 +2,7 @@ package net.thevpc.nuts.runtime.standalone.xtra.expr;
 
 import net.thevpc.nuts.expr.*;
 import net.thevpc.nuts.util.NOptional;
-import net.thevpc.nuts.util.NMsg;
+import net.thevpc.nuts.text.NMsg;
 import net.thevpc.nuts.expr.NToken;
 
 import java.io.StringReader;
