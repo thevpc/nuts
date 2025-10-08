eea06dfb5b02b41ca3bec84b93ab425dfea32247
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:05:11 +0100

    Update DefaultNDefinition.java : change support to score

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/definition/DefaultNDefinition.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/definition/DefaultNDefinition.java
@@ -34,7 +34,7 @@ import net.thevpc.nuts.command.NInstallInformation;
 import net.thevpc.nuts.io.NPath;
 import net.thevpc.nuts.util.NIllegalArgumentException;
 import net.thevpc.nuts.util.NImmutable;
-import net.thevpc.nuts.util.NMsg;
+import net.thevpc.nuts.text.NMsg;
 import net.thevpc.nuts.util.NOptional;
 
 /**
