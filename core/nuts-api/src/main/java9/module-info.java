b93694cc245f9f1360313b6fe5a7687b308f646a
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:05:06 +0100

    Update module-info.java : change support to score

--- core/nuts-api/src/main/java9/module-info.java
+++ core/nuts-api/src/main/java9/module-info.java
@@ -9,18 +9,23 @@ module net.thevpc.nuts {
     requires net.thevpc.nuts.boot;
 
     exports net.thevpc.nuts;
+    exports net.thevpc.nuts.app;
+    exports net.thevpc.nuts.artifact;
+    exports net.thevpc.nuts.core;
+    exports net.thevpc.nuts.platform;
     exports net.thevpc.nuts.cmdline;
+    exports net.thevpc.nuts.command;
     exports net.thevpc.nuts.concurrent;
     exports net.thevpc.nuts.elem;
     exports net.thevpc.nuts.expr;
     exports net.thevpc.nuts.ext;
-//    exports net.thevpc.nuts.format;
     exports net.thevpc.nuts.io;
     exports net.thevpc.nuts.log;
     exports net.thevpc.nuts.math;
     exports net.thevpc.nuts.reflect;
     exports net.thevpc.nuts.security;
     exports net.thevpc.nuts.spi;
+    exports net.thevpc.nuts.spi.base;
     exports net.thevpc.nuts.text;
     exports net.thevpc.nuts.time;
     exports net.thevpc.nuts.util;
