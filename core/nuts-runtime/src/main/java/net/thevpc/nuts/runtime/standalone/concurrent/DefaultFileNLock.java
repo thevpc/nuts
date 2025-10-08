73e40b7fef257c55f2764d04aec6815092afdc64
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:05:10 +0100

    Update DefaultFileNLock.java : change support to score

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/concurrent/DefaultFileNLock.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/concurrent/DefaultFileNLock.java
@@ -11,6 +11,7 @@ import net.thevpc.nuts.io.NPath;
 import net.thevpc.nuts.log.NLog;
 import net.thevpc.nuts.runtime.standalone.NWorkspaceProfilerImpl;
 import net.thevpc.nuts.runtime.standalone.util.TimePeriod;
+import net.thevpc.nuts.text.NMsg;
 import net.thevpc.nuts.time.NDuration;
 import net.thevpc.nuts.util.*;
 
