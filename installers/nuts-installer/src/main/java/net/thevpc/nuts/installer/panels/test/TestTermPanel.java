3da22a50322ea4a1470229e0a22d9853d8e11f91
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:05:28 +0100

    Update TestTermPanel.java : change support to score

--- installers/nuts-installer/src/main/java/net/thevpc/nuts/installer/panels/test/TestTermPanel.java
+++ installers/nuts-installer/src/main/java/net/thevpc/nuts/installer/panels/test/TestTermPanel.java
@@ -1,9 +1,9 @@
 package net.thevpc.nuts.installer.panels.test;
 
 import net.thevpc.nuts.installer.model.InstallData;
-import net.thevpc.nuts.nswing.WizardPageBase;
-import net.thevpc.nuts.nswing.UIHelper;
-import net.thevpc.nuts.nswing.AnsiTermPane;
+import net.thevpc.nuts.installer.util.swing.AnsiTermPane;
+import net.thevpc.nuts.installer.util.swing.UIHelper;
+import net.thevpc.nuts.installer.util.swing.WizardPageBase;
 
 import javax.swing.*;
 import java.awt.*;
