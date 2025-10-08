eaab5991525d5742e103a8441e66e1f147229e2b
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:05:18 +0100

    Update DefaultNRepositoryDB.java : change support to score

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/repository/DefaultNRepositoryDB.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/repository/DefaultNRepositoryDB.java
@@ -7,12 +7,12 @@ import net.thevpc.nuts.platform.NStoreType;
 import net.thevpc.nuts.io.NPath;
 import net.thevpc.nuts.core.NAddRepositoryOptions;
 import net.thevpc.nuts.core.NRepositoryConfig;
+import net.thevpc.nuts.spi.NScorableContext;
 import net.thevpc.nuts.util.NMaps;
 import net.thevpc.nuts.runtime.standalone.util.NMapWithAlias;
 import net.thevpc.nuts.spi.NRepositoryDB;
 import net.thevpc.nuts.spi.NRepositoryLocation;
-import net.thevpc.nuts.spi.NSupportLevelContext;
-import net.thevpc.nuts.util.NMsg;
+import net.thevpc.nuts.text.NMsg;
 import net.thevpc.nuts.util.NOptional;
 import net.thevpc.nuts.platform.NPlatformHome;
 
@@ -202,7 +202,7 @@ public class DefaultNRepositoryDB implements NRepositoryDB {
 
 
     @Override
-    public int getSupportLevel(NSupportLevelContext context) {
-        return NConstants.Support.DEFAULT_SUPPORT;
+    public int getScore(NScorableContext context) {
+        return DEFAULT_SCORE;
     }
 }
