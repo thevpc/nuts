7d307ff2018956def2b5c95491190b8f585ba225
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:05:16 +0100

    Update NPrintStreamFormatted.java : change support to score

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/io/printstream/NPrintStreamFormatted.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/io/printstream/NPrintStreamFormatted.java
@@ -5,7 +5,7 @@ import net.thevpc.nuts.io.NTerminalMode;
 import net.thevpc.nuts.text.NTerminalCmd;
 import net.thevpc.nuts.text.NText;
 import net.thevpc.nuts.util.NIllegalArgumentException;
-import net.thevpc.nuts.util.NMsg;
+import net.thevpc.nuts.text.NMsg;
 
 import java.io.OutputStream;
 
