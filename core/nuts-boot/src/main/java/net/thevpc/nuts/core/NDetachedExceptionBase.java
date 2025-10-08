53af8bc872b6eb16ed03dd987a2eef7452f3b760
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:05:07 +0100

    Rename NDetachedExceptionBase.java → NDetachedExceptionBase.java : change support to score

--- core/nuts-boot/src/main/java/net/thevpc/nuts/core/NDetachedExceptionBase.java
+++
@@ -1,8 +0,0 @@
-package net.thevpc.nuts.core;
-
-/**
- * Detached Exceptions are Nuts Exception that
- * are not bound to any NWorkspace/NSession
- */
-public interface NDetachedExceptionBase extends NAnyExceptionBase{
-}
