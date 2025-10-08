dfe4fe48891afda6f49a96c39b2723a2f9ee045a
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:05:07 +0100

    Update NBootRepositorySelector.java : change support to score

--- core/nuts-boot/src/main/java/net/thevpc/nuts/boot/NBootRepositorySelector.java
+++ core/nuts-boot/src/main/java/net/thevpc/nuts/boot/NBootRepositorySelector.java
@@ -24,9 +24,9 @@
  */
 package net.thevpc.nuts.boot;
 
-import net.thevpc.nuts.boot.reserved.util.NBootMsg;
-import net.thevpc.nuts.boot.reserved.util.NBootRepositoryDB;
-import net.thevpc.nuts.boot.reserved.util.NBootUtils;
+import net.thevpc.nuts.boot.internal.util.NBootMsg;
+import net.thevpc.nuts.boot.internal.util.NBootRepositoryDB;
+import net.thevpc.nuts.boot.internal.util.NBootUtils;
 
 /**
  * @author thevpc
