95a03f7083972bf38b4c53dedbf9881a3c69ba1d
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:05:21 +0100

    Update FigletNTextArtTextRenderer.java : change support to score

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/text/art/figlet/FigletNTextArtTextRenderer.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/text/art/figlet/FigletNTextArtTextRenderer.java
@@ -40,7 +40,7 @@ import net.thevpc.nuts.io.NPath;
 import net.thevpc.nuts.text.NText;
 import net.thevpc.nuts.text.NTextArtTextRenderer;
 import net.thevpc.nuts.text.NTextBuilder;
-import net.thevpc.nuts.util.NMsg;
+import net.thevpc.nuts.text.NMsg;
 import net.thevpc.nuts.util.NOptional;
 import net.thevpc.nuts.util.NStringUtils;
 
