87e9630e529e3f45cf3c79ff587e528844aeeb10
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:05:15 +0100

    Update DirectoryScanner.java : change support to score

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/io/path/DirectoryScanner.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/io/path/DirectoryScanner.java
@@ -5,7 +5,7 @@ import net.thevpc.nuts.io.NPath;
 import net.thevpc.nuts.runtime.standalone.xtra.glob.GlobUtils;
 import net.thevpc.nuts.util.NFunction;
 import net.thevpc.nuts.util.NIllegalArgumentException;
-import net.thevpc.nuts.util.NMsg;
+import net.thevpc.nuts.text.NMsg;
 import net.thevpc.nuts.util.NStream;
 
 import java.util.*;
