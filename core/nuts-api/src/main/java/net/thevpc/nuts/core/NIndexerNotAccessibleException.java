aaacae6d5a8fd87b7553d0a7b59df3c5b77f70ac
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:04:59 +0100

    Update NIndexerNotAccessibleException.java : change support to score

--- core/nuts-api/src/main/java/net/thevpc/nuts/core/NIndexerNotAccessibleException.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/core/NIndexerNotAccessibleException.java
@@ -26,7 +26,7 @@
 package net.thevpc.nuts.core;
 
 import net.thevpc.nuts.util.NException;
-import net.thevpc.nuts.util.NMsg;
+import net.thevpc.nuts.text.NMsg;
 
 /**
  * Exception thrown when copy validation fails
