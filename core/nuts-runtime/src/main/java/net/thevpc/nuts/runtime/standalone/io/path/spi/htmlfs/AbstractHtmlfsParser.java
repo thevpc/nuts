99ca36d25f0eb542136063a2f978fc1870b8afc1
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:05:16 +0100

    Update AbstractHtmlfsParser.java : change support to score

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/io/path/spi/htmlfs/AbstractHtmlfsParser.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/io/path/spi/htmlfs/AbstractHtmlfsParser.java
@@ -1,7 +1,7 @@
 package net.thevpc.nuts.runtime.standalone.io.path.spi.htmlfs;
 
-import net.thevpc.nuts.util.NMsg;
-import net.thevpc.nuts.concurrent.NCallableSupport;
+import net.thevpc.nuts.text.NMsg;
+import net.thevpc.nuts.concurrent.NScorableCallable;
 
 import java.util.List;
 import java.util.function.Supplier;
@@ -11,12 +11,12 @@ public abstract class AbstractHtmlfsParser implements HtmlfsParser{
     public AbstractHtmlfsParser() {
     }
 
-    protected NCallableSupport<List<String>> toSupported(int level, List<String> li) {
+    protected NScorableCallable<List<String>> toSupported(int level, List<String> li) {
         Supplier<NMsg> msg = () -> NMsg.ofInvalidValue("htmlfs list");
         if (li == null || li.isEmpty()) {
-            return NCallableSupport.ofInvalid(msg);
+            return NScorableCallable.ofInvalid(msg);
         }
-        return NCallableSupport.of(level, () -> li,msg);
+        return NScorableCallable.of(level, () -> li,msg);
     }
 
 }
