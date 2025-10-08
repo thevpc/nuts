83eb747f3a653fadb80b6d4edc16f877c7db4b1d
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:05:14 +0100

    Update YamlFormat.java : change support to score

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/format/yaml/YamlFormat.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/format/yaml/YamlFormat.java
@@ -4,7 +4,7 @@ import net.thevpc.nuts.util.NIllegalArgumentException;
 import net.thevpc.nuts.elem.*;
 import net.thevpc.nuts.io.NInputSource;
 import net.thevpc.nuts.io.NPrintStream;
-import net.thevpc.nuts.util.NMsg;
+import net.thevpc.nuts.text.NMsg;
 import net.thevpc.nuts.util.NStringUtils;
 
 import java.util.Base64;
