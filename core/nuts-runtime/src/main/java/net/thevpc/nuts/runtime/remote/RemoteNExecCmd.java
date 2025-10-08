a8ab7c13e2c4982876eabb0482719d4ec21c34f2
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:05:09 +0100

    Update RemoteNExecCmd.java : change support to score

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/remote/RemoteNExecCmd.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/remote/RemoteNExecCmd.java
@@ -7,7 +7,7 @@ import net.thevpc.nuts.core.NSession;
 import net.thevpc.nuts.core.NWorkspace;
 import net.thevpc.nuts.elem.NElement;
 import net.thevpc.nuts.runtime.standalone.workspace.cmd.exec.AbstractNExecCmd;
-import net.thevpc.nuts.util.NMsg;
+import net.thevpc.nuts.text.NMsg;
 
 public class RemoteNExecCmd extends AbstractNExecCmd {
 
