2af8ce7179ee93f19d13cc0fb63c993fc05169dd
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:05:16 +0100

    Update AnsiNPrintStreamTerminalBase.java : change support to score

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/io/printstream/AnsiNPrintStreamTerminalBase.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/io/printstream/AnsiNPrintStreamTerminalBase.java
@@ -1,8 +1,7 @@
 package net.thevpc.nuts.runtime.standalone.io.printstream;
 
-import net.thevpc.nuts.core.NConstants;
-import net.thevpc.nuts.spi.NSupportLevelContext;
-import net.thevpc.nuts.util.NMsg;
+import net.thevpc.nuts.spi.NScorableContext;
+import net.thevpc.nuts.text.NMsg;
 import net.thevpc.nuts.cmdline.NCmdLineAutoCompleteResolver;
 import net.thevpc.nuts.cmdline.NCmdLineHistory;
 import net.thevpc.nuts.io.NPrintStream;
@@ -103,8 +102,8 @@ public class AnsiNPrintStreamTerminalBase extends NSystemTerminalBaseImpl {
         }
     }
     @Override
-    public int getSupportLevel(NSupportLevelContext context) {
-        return NConstants.Support.DEFAULT_SUPPORT;
+    public int getScore(NScorableContext context) {
+        return DEFAULT_SCORE;
     }
 
 }
