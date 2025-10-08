739d9278c33296c864ee5a47720178521768512b
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:05:11 +0100

    Update DefaultNDefinitionBuilder.java : change support to score

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/definition/DefaultNDefinitionBuilder.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/definition/DefaultNDefinitionBuilder.java
@@ -28,7 +28,7 @@ import net.thevpc.nuts.artifact.*;
 import net.thevpc.nuts.command.NInstallInformation;
 import net.thevpc.nuts.io.NPath;
 import net.thevpc.nuts.util.NIllegalArgumentException;
-import net.thevpc.nuts.util.NMsg;
+import net.thevpc.nuts.text.NMsg;
 import net.thevpc.nuts.util.NOptional;
 
 import java.util.Objects;
