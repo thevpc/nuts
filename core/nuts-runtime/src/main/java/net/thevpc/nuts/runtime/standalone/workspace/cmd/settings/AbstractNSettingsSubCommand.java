8a62b43ed60e647baba4a055465320af278acfbb
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:05:24 +0100

    Update AbstractNSettingsSubCommand.java : change support to score

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/workspace/cmd/settings/AbstractNSettingsSubCommand.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/workspace/cmd/settings/AbstractNSettingsSubCommand.java
@@ -5,8 +5,7 @@
  */
 package net.thevpc.nuts.runtime.standalone.workspace.cmd.settings;
 
-import net.thevpc.nuts.core.NConstants;
-import net.thevpc.nuts.spi.NSupportLevelContext;
+import net.thevpc.nuts.spi.NScorableContext;
 
 /**
  *
@@ -18,7 +17,7 @@ public abstract class AbstractNSettingsSubCommand implements NSettingsSubCommand
     }
 
     @Override
-    public int getSupportLevel(NSupportLevelContext criteria) {
-        return NConstants.Support.DEFAULT_SUPPORT;
+    public int getScore(NScorableContext criteria) {
+        return DEFAULT_SCORE;
     }
 }
