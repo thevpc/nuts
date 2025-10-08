e7c549144f5aa30a4658cb72eadbe5ae4ac396ef
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:05:14 +0100

    Update RollingFileService.java : change support to score

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/format/obj/RollingFileService.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/format/obj/RollingFileService.java
@@ -3,7 +3,7 @@ package net.thevpc.nuts.runtime.standalone.format.obj;
 import net.thevpc.nuts.io.NPath;
 import net.thevpc.nuts.io.NPathOption;
 import net.thevpc.nuts.util.NIllegalArgumentException;
-import net.thevpc.nuts.util.NMsg;
+import net.thevpc.nuts.text.NMsg;
 import net.thevpc.nuts.util.NOptional;
 
 import java.math.BigInteger;
