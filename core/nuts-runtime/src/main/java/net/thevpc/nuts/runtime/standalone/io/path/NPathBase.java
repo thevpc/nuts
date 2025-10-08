0cb0d285765b358de0d7987ab7f04391392ee362
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:05:15 +0100

    Update NPathBase.java : change support to score

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/io/path/NPathBase.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/io/path/NPathBase.java
@@ -1,14 +1,14 @@
 package net.thevpc.nuts.runtime.standalone.io.path;
 
-import net.thevpc.nuts.core.NConstants;
 import net.thevpc.nuts.artifact.NVersion;
 import net.thevpc.nuts.cmdline.NCmdLine;
+import net.thevpc.nuts.spi.NScorableContext;
 import net.thevpc.nuts.text.NFormat;
 import net.thevpc.nuts.io.*;
 import net.thevpc.nuts.runtime.standalone.format.DefaultFormatBase;
 import net.thevpc.nuts.runtime.standalone.io.util.AbstractMultiReadNInputSource;
 import net.thevpc.nuts.spi.NPathSPIAware;
-import net.thevpc.nuts.spi.NSupportLevelContext;
+import net.thevpc.nuts.text.NMsg;
 import net.thevpc.nuts.text.NText;
 import net.thevpc.nuts.text.NTextStyle;
 import net.thevpc.nuts.util.*;
@@ -416,8 +416,8 @@ public abstract class NPathBase extends AbstractMultiReadNInputSource implements
         }
 
         @Override
-        public int getSupportLevel(NSupportLevelContext context) {
-            return NConstants.Support.DEFAULT_SUPPORT;
+        public int getScore(NScorableContext context) {
+            return DEFAULT_SCORE;
         }
     }
 
