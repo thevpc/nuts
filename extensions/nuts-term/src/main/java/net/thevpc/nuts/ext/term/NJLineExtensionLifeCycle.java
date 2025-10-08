77e9bf943351b542439f609d8710093ce88a5802
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:05:28 +0100

    Update NJLineExtensionLifeCycle.java : change support to score

--- extensions/nuts-term/src/main/java/net/thevpc/nuts/ext/term/NJLineExtensionLifeCycle.java
+++ extensions/nuts-term/src/main/java/net/thevpc/nuts/ext/term/NJLineExtensionLifeCycle.java
@@ -1,10 +1,9 @@
 package net.thevpc.nuts.ext.term;
 
-import net.thevpc.nuts.core.NConstants;
 import net.thevpc.nuts.core.NWorkspaceExtension;
 import net.thevpc.nuts.io.NIO;
 import net.thevpc.nuts.spi.NExtensionLifeCycle;
-import net.thevpc.nuts.spi.NSupportLevelContext;
+import net.thevpc.nuts.spi.NScorableContext;
 import net.thevpc.nuts.util.NUnused;
 
 @NUnused
@@ -13,8 +12,8 @@ public class NJLineExtensionLifeCycle implements NExtensionLifeCycle {
     }
 
     @Override
-    public int getSupportLevel(NSupportLevelContext context) {
-        return NConstants.Support.DEFAULT_SUPPORT;
+    public int getScore(NScorableContext context) {
+        return DEFAULT_SCORE;
     }
 
     @Override
