2ca54ef687eb637051250a9d9d0521bf389b1b73
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:05:27 +0100

    Update DefaultNGlob.java : change support to score

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/xtra/glob/DefaultNGlob.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/xtra/glob/DefaultNGlob.java
@@ -1,7 +1,6 @@
 package net.thevpc.nuts.runtime.standalone.xtra.glob;
 
-import net.thevpc.nuts.core.NConstants;
-import net.thevpc.nuts.spi.NSupportLevelContext;
+import net.thevpc.nuts.spi.NScorableContext;
 import net.thevpc.nuts.util.NBlankable;
 import net.thevpc.nuts.expr.NGlob;
 
@@ -62,8 +61,8 @@ public class DefaultNGlob implements NGlob {
     }
 
     @Override
-    public int getSupportLevel(NSupportLevelContext context) {
-        return NConstants.Support.DEFAULT_SUPPORT;
+    public int getScore(NScorableContext context) {
+        return DEFAULT_SCORE;
     }
 
     public String escape(String s) {
