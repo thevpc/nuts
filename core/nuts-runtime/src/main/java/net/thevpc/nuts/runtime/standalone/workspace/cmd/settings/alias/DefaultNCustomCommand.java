d1ca233413f078dfc76fbcd7e1dc25f35a6111e7
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:05:25 +0100

    Update DefaultNCustomCommand.java : change support to score

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/workspace/cmd/settings/alias/DefaultNCustomCommand.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/workspace/cmd/settings/alias/DefaultNCustomCommand.java
@@ -9,7 +9,7 @@ import net.thevpc.nuts.text.NTextStyle;
 import net.thevpc.nuts.log.NLog;
 
 import net.thevpc.nuts.util.NBlankable;
-import net.thevpc.nuts.util.NMsg;
+import net.thevpc.nuts.text.NMsg;
 
 import java.util.ArrayList;
 import java.util.Arrays;
