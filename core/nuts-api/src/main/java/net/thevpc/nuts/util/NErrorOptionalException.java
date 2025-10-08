b69abc7428f2d6c7c1a1c929fb45111ead072939
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:05:05 +0100

    Update NErrorOptionalException.java : change support to score

--- core/nuts-api/src/main/java/net/thevpc/nuts/util/NErrorOptionalException.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/util/NErrorOptionalException.java
@@ -1,8 +1,10 @@
 package net.thevpc.nuts.util;
 
+import net.thevpc.nuts.text.NMsg;
+
 public class NErrorOptionalException extends NException {
 
-    public NErrorOptionalException(NMsg formattedMessage,Throwable cause) {
+    public NErrorOptionalException(NMsg formattedMessage, Throwable cause) {
         super(formattedMessage, cause);
     }
 
