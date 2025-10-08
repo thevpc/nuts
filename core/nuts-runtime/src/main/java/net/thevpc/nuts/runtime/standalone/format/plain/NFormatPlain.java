7e5d170604114f40028eaf5b7d628c920a634f84
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:05:14 +0100

    Update NFormatPlain.java : change support to score

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/format/plain/NFormatPlain.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/format/plain/NFormatPlain.java
@@ -5,18 +5,17 @@
  */
 package net.thevpc.nuts.runtime.standalone.format.plain;
 
-import net.thevpc.nuts.core.NConstants;
 import net.thevpc.nuts.cmdline.NArg;
 import net.thevpc.nuts.cmdline.NCmdLine;
 import net.thevpc.nuts.elem.*;
 import net.thevpc.nuts.io.NPrintStream;
 import net.thevpc.nuts.runtime.standalone.format.DefaultFormatBase;
 import net.thevpc.nuts.runtime.standalone.format.props.DefaultNPropertiesFormat;
-import net.thevpc.nuts.spi.NSupportLevelContext;
+import net.thevpc.nuts.spi.NScorableContext;
 import net.thevpc.nuts.text.NContentType;
 import net.thevpc.nuts.text.NContentTypeFormat;
 import net.thevpc.nuts.text.NText;
-import net.thevpc.nuts.util.NMsg;
+import net.thevpc.nuts.text.NMsg;
 import net.thevpc.nuts.util.NUnsupportedArgumentException;
 
 import java.util.*;
@@ -186,8 +185,8 @@ public class NFormatPlain extends DefaultFormatBase<NContentTypeFormat> implemen
     }
 
     @Override
-    public int getSupportLevel(NSupportLevelContext context) {
-        return NConstants.Support.DEFAULT_SUPPORT;
+    public int getScore(NScorableContext context) {
+        return DEFAULT_SCORE;
     }
 
     @Override
