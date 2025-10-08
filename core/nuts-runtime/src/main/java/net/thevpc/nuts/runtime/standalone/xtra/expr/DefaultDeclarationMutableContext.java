39587da5d79fdf920c0cfe5d74c70051139107b1
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:05:26 +0100

    Update DefaultDeclarationMutableContext.java : change support to score

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/xtra/expr/DefaultDeclarationMutableContext.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/xtra/expr/DefaultDeclarationMutableContext.java
@@ -1,7 +1,7 @@
 package net.thevpc.nuts.runtime.standalone.xtra.expr;
 
 import net.thevpc.nuts.util.NBlankable;
-import net.thevpc.nuts.util.NMsg;
+import net.thevpc.nuts.text.NMsg;
 import net.thevpc.nuts.util.NOptional;
 import net.thevpc.nuts.expr.*;
 
