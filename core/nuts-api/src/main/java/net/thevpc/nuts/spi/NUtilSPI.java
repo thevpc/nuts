3517bf55e52758ee49dca11ff712ab35240acabf
thevpc <taha.bensalah@gmail.com>
2025-10-09 12:13:34 +0100

    Update NUtilSPI.java : fixed

--- core/nuts-api/src/main/java/net/thevpc/nuts/spi/NUtilSPI.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/spi/NUtilSPI.java
@@ -1,5 +1,8 @@
 package net.thevpc.nuts.spi;
 
+import net.thevpc.nuts.util.NScorable;
+import net.thevpc.nuts.util.NScorableQuery;
+
 public interface NUtilSPI extends NComponent{
-    <T extends NScorable> NScorable.Query<T> ofScorableQuery();
+    <T extends NScorable> NScorableQuery<T> ofScorableQuery();
 }
