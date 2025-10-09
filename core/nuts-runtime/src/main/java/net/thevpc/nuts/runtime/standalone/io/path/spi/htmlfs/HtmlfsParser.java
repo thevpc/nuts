2f351858c278179687f91561868bd37b59b61917
thevpc <taha.bensalah@gmail.com>
2025-10-09 12:13:36 +0100

    Update HtmlfsParser.java : fixed

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/io/path/spi/htmlfs/HtmlfsParser.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/io/path/spi/htmlfs/HtmlfsParser.java
@@ -1,9 +1,9 @@
 package net.thevpc.nuts.runtime.standalone.io.path.spi.htmlfs;
 
-import net.thevpc.nuts.concurrent.NScorableCallable;
+import net.thevpc.nuts.concurrent.NScoredCallable;
 
 import java.util.List;
 
 public interface HtmlfsParser {
-    NScorableCallable<List<String>> parseHtmlTomcat(byte[] bytes);
+    NScoredCallable<List<String>> parseHtmlTomcat(byte[] bytes);
 }
