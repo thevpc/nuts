653ff2f3dee5f8925554891b32faa32eabfdd384
thevpc <taha.bensalah@gmail.com>
2025-10-09 01:07:14 +0100

    Update NUtilSPIImpl.java : FIXED

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/util/NUtilSPIImpl.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/util/NUtilSPIImpl.java
@@ -1,9 +1,8 @@
 package net.thevpc.nuts.runtime.standalone.util;
 
-import net.thevpc.nuts.spi.NScorable;
-import net.thevpc.nuts.spi.NScorableContext;
-import net.thevpc.nuts.spi.NUtilSPI;
+import net.thevpc.nuts.spi.*;
 
+@NComponentScope(NScopeType.WORKSPACE)
 public class NUtilSPIImpl implements NUtilSPI {
     @Override
     public <T extends NScorable> Query<T> ofScorableQuery() {
