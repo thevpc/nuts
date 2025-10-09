8c90c8254b51defb124104a3c0a24e7eb30fc8a7
thevpc <taha.bensalah@gmail.com>
2025-10-09 12:13:35 +0100

    Update DefaultNDescriptorFormat.java : fixed

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/descriptor/format/DefaultNDescriptorFormat.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/descriptor/format/DefaultNDescriptorFormat.java
@@ -7,7 +7,7 @@ import net.thevpc.nuts.elem.NElementWriter;
 import net.thevpc.nuts.text.NDescriptorFormat;
 import net.thevpc.nuts.io.NPrintStream;
 import net.thevpc.nuts.runtime.standalone.format.DefaultFormatBase;
-import net.thevpc.nuts.spi.NScorableContext;
+import net.thevpc.nuts.util.NScorableContext;
 import net.thevpc.nuts.text.NText;
 import net.thevpc.nuts.text.NTextCode;
 import net.thevpc.nuts.text.NMsg;
