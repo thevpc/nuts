cd8048e3688809886d88b80ec886119f21adcf62
thevpc <taha.bensalah@gmail.com>
2025-10-09 12:13:38 +0100

    Update DefaultNWorkspaceList.java : fixed

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/workspace/list/DefaultNWorkspaceList.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/workspace/list/DefaultNWorkspaceList.java
@@ -10,7 +10,7 @@ import net.thevpc.nuts.elem.NElementWriter;
 
 import net.thevpc.nuts.platform.NStoreType;
 import net.thevpc.nuts.io.NPath;
-import net.thevpc.nuts.spi.NScorableContext;
+import net.thevpc.nuts.util.NScorableContext;
 import net.thevpc.nuts.util.NBlankable;
 
 import java.util.*;
