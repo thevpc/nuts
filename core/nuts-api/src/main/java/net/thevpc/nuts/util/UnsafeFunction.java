a8f4808dbbd0354284b7c41802d96b8c8a55dcba
thevpc <taha.bensalah@gmail.com>
2025-10-09 12:13:34 +0100

    Update UnsafeFunction.java : fixed

--- core/nuts-api/src/main/java/net/thevpc/nuts/util/UnsafeFunction.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/util/UnsafeFunction.java
@@ -8,12 +8,12 @@ public interface UnsafeFunction<T, V> {
     V apply(T t) throws Exception;
 
     default <V2> UnsafeFunction<V2, V> compose(Function<? super V2, ? extends T> before) {
-        Objects.requireNonNull(before);
+        NAssert.requireNonNull(before);
         return (V2 v) -> apply(before.apply(v));
     }
 
     default <V2> UnsafeFunction<T, V2> andThen(Function<? super V, ? extends V2> after) {
-        Objects.requireNonNull(after);
+        NAssert.requireNonNull(after);
         return (T t) -> after.apply(apply(t));
     }
 
