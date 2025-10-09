df729d2b11676c43ac2db934cc466bb4b508b129
thevpc <taha.bensalah@gmail.com>
2025-10-09 12:13:41 +0100

    Update NScoredRunnable.java : fixed

---
+++ core/nuts-api/src/main/java/net/thevpc/nuts/concurrent/NScoredRunnable.java
@@ -0,0 +1,55 @@
+/**
+ * ====================================================================
+ * Nuts : Network Updatable Things Service
+ * (universal package manager)
+ * <br>
+ * is a new Open Source Package Manager to help install packages
+ * and libraries for runtime execution. Nuts is the ultimate companion for
+ * maven (and other build managers) as it helps installing all package
+ * dependencies at runtime. Nuts is not tied to java and is a good choice
+ * to share shell scripts and other 'things' . Its based on an extensible
+ * architecture to help supporting a large range of sub managers / repositories.
+ *
+ * <br>
+ * <p>
+ * Copyright [2020] [thevpc]
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE Version 3 (the "License");
+ * you may  not use this file except in compliance with the License. You may obtain
+ * a copy of the License at https://www.gnu.org/licenses/lgpl-3.0.en.html
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ * <br>
+ * ====================================================================
+ */
+package net.thevpc.nuts.concurrent;
+
+import net.thevpc.nuts.util.NScorable;
+import net.thevpc.nuts.util.NScorableContext;
+import net.thevpc.nuts.text.NMsg;
+
+import java.util.function.Supplier;
+
+public interface NScoredRunnable extends NScorable {
+    static NScoredRunnable of(int score, Runnable supplier) {
+        return of(score, supplier, null);
+    }
+
+    static NScoredRunnable of(int score, Runnable supplier, Supplier<NMsg> emptyMessage) {
+        return (score <= 0 || supplier == null) ? ofInvalid(emptyMessage)
+                : new DefaultNScoredRunnable(supplier, score, emptyMessage)
+                ;
+    }
+
+    @SuppressWarnings("unchecked")
+    static NScoredRunnable ofInvalid(Supplier<NMsg> emptyMessage) {
+        return new DefaultNScoredRunnable(null, UNSUPPORTED_SCORE, emptyMessage);
+    }
+
+    void run();
+
+
+    int getScore(NScorableContext scorableContext);
+}
