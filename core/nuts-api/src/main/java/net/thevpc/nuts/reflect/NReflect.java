66b8839a7441683c3c11e8d7324f335792d5cf02
thevpc <taha.bensalah@gmail.com>
2025-10-14 15:53:55 +0100

    Update NReflect.java : update documentation

--- core/nuts-api/src/main/java/net/thevpc/nuts/reflect/NReflect.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/reflect/NReflect.java
@@ -1,13 +1,15 @@
 package net.thevpc.nuts.reflect;
 
-import net.thevpc.nuts.concurrent.NScopedValue;
+import net.thevpc.nuts.concurrent.NScopedStack;
 import net.thevpc.nuts.ext.NExtensions;
 import net.thevpc.nuts.spi.NComponent;
+import net.thevpc.nuts.util.NOptional;
 
 public interface NReflect extends NComponent {
     static NReflect of() {
         return NExtensions.of(NReflect.class);
     }
 
-    NScopedValue<NBeanContainer> scopedBeanContainer();
+    NScopedStack<NBeanContainer> scopedBeanContainerStack();
+    NBeanContainer scopedBeanContainer();
 }
