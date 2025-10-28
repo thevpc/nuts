b8ddab4a7299c6e2ddbc169e684da2eca4eaf3f4
thevpc <taha.bensalah@gmail.com>
2025-10-28 23:53:45 +0100

    Update DotfilefsPath.java : optimize-install [still WIP]

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/io/path/spi/DotfilefsPath.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/io/path/spi/DotfilefsPath.java
@@ -7,6 +7,7 @@ import net.thevpc.nuts.core.NSession;
 import net.thevpc.nuts.io.*;
 import net.thevpc.nuts.log.NLog;
 import net.thevpc.nuts.runtime.standalone.util.CoreNConstants;
+import net.thevpc.nuts.runtime.standalone.util.NCoreLogUtils;
 import net.thevpc.nuts.runtime.standalone.xtra.expr.StringTokenizerUtils;
 import net.thevpc.nuts.spi.*;
 import net.thevpc.nuts.text.NMsg;
@@ -182,10 +183,10 @@ public class DotfilefsPath extends AbstractPathSPIAdapter {
         NSession session = NSession.of();
         NVersion versionString = NVersion.get("0.5.5").get();
         try (InputStream foldersFileStream = NInputStreamMonitor.of().setSource(NPath.of(dotFilesUrl)).create()) {
-            session.getTerminal().printProgress(NMsg.ofC("%-8s %s", "browse", NPath.of(baseUrl).toCompressedForm()));
+            session.getTerminal().printProgress(NMsg.ofC("%-8s %s", "browse", NCoreLogUtils.forProgress(NPath.of(baseUrl))));
             dotFilesContent=NIOUtils.loadString(foldersFileStream, false);
         } catch (IOException | NIOException | UncheckedIOException e) {
-            session.getTerminal().printProgress(NMsg.ofC("%-8s %s", "not found", NPath.of(baseUrl).toCompressedForm()));
+            session.getTerminal().printProgress(NMsg.ofC("%-8s %s", "not found", NCoreLogUtils.forProgress(NPath.of(baseUrl))));
             // not found
         }
         if (dotFilesContent != null) {
