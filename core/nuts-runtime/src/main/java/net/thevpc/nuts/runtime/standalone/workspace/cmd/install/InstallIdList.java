0df583dbb772840164d0f9c4d07abbdbb9ebacb5
thevpc <taha.bensalah@gmail.com>
2025-10-28 23:53:45 +0100

    Update InstallIdList.java : optimize-install [still WIP]

---
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/workspace/cmd/install/InstallIdList.java
@@ -0,0 +1,113 @@
+package net.thevpc.nuts.runtime.standalone.workspace.cmd.install;
+
+import net.thevpc.nuts.artifact.NId;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class InstallIdList {
+
+    boolean emptyCommand = true;
+    Map<String, InstallIdInfo> visited = new LinkedHashMap<>();
+    InstallCache cache=new InstallCache();
+
+    public InstallIdList() {
+    }
+
+    public boolean isVisited(NId id) {
+        return visited.containsKey(InstallIdCacheItem.normalizeId(id).toString());
+    }
+
+    public List<NId> ids(Predicate<InstallIdInfo> filter) {
+        return infos().stream().filter(filter).map(x -> x.id).collect(Collectors.toList());
+    }
+
+    public List<InstallIdInfo> infos(Predicate<InstallIdInfo> filter) {
+        if (filter == null) {
+            return infos();
+        }
+        return infos().stream().filter(filter).collect(Collectors.toList());
+    }
+
+    public List<InstallIdInfo> infos() {
+        return new ArrayList<>(visited.values());
+    }
+
+    public InstallIdInfo addAsDeployed(NId id,InstallFlags flags) {
+        cache.get(id);
+        String sid = InstallIdCacheItem.normalizeId(id).toString();
+        InstallIdInfo old = visited.get(sid);
+        if(old!=null){
+            old.flags.merge(flags);
+            return old;
+        }
+        InstallIdInfo ii = new InstallIdInfo();
+        ii.id = id;
+        ii.sid = sid;
+        ii.flags = flags;
+        visited.put(ii.sid, ii);
+        return ii;
+    }
+
+    public InstallIdInfo addAsRequired(NId id,NId forId,InstallFlags flags) {
+        cache.get(id);
+        String sid = InstallIdCacheItem.normalizeId(id).toString();
+        InstallIdInfo old = visited.get(sid);
+        if(old!=null){
+            old.flags.merge(flags);
+            old.doRequire=true;
+            old.requiredForIds.add(forId);
+            return old;
+        }
+        InstallIdInfo ii = new InstallIdInfo();
+        ii.id = id;
+        ii.sid = sid;
+        ii.flags = flags;
+        visited.put(ii.sid, ii);
+        return ii;
+    }
+
+    public InstallIdInfo addAsInstalled(NId id, InstallFlags flags) {
+        String sid = InstallIdCacheItem.normalizeId(id).toString();
+        InstallIdInfo old = visited.get(sid);
+        if(old!=null){
+            old.flags.merge(flags);
+            return old;
+        }
+        cache.get(id);
+        emptyCommand = false;
+        InstallIdInfo ii = new InstallIdInfo();
+        ii.id = id;
+        ii.sid = sid;
+
+        ii.flags = flags;
+        visited.put(ii.sid, ii);
+        return ii;
+    }
+
+    public InstallIdInfo addAsUninstalled(NId id, InstallFlags flags) {
+        String sid = InstallIdCacheItem.normalizeId(id).toString();
+        InstallIdInfo old = visited.get(sid);
+        if(old!=null){
+            old.flags.merge(flags);
+            return old;
+        }
+        cache.get(id);
+        emptyCommand = false;
+        InstallIdInfo ii = new InstallIdInfo();
+        ii.id = id;
+        ii.sid = sid;
+
+        ii.flags = flags;
+        visited.put(ii.sid, ii);
+        return ii;
+    }
+
+    public InstallIdInfo get(NId id) {
+        return visited.get(InstallIdCacheItem.normalizeId(id).toString());
+    }
+}
