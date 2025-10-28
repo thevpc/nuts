5ae8572588f797cfcbd2586e2b02db41602e97a4
thevpc <taha.bensalah@gmail.com>
2025-10-28 23:53:45 +0100

    Update SafeRecommendationConnector.java : optimize-install [still WIP]

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/workspace/cmd/recom/SafeRecommendationConnector.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/workspace/cmd/recom/SafeRecommendationConnector.java
@@ -1,5 +1,8 @@
 package net.thevpc.nuts.runtime.standalone.workspace.cmd.recom;
 
+import net.thevpc.nuts.core.NWorkspace;
+import net.thevpc.nuts.runtime.standalone.workspace.NWorkspaceExt;
+
 import java.util.Map;
 
 public class SafeRecommendationConnector implements RecommendationConnector {
@@ -9,6 +12,18 @@ public class SafeRecommendationConnector implements RecommendationConnector {
         this.c = c;
     }
 
+    public void trackRecommendationsAsync(RequestQueryInfo ri, NRecommendationPhase phase, boolean failure) {
+        NWorkspace.of().runWith(() -> {
+            new Thread(() -> {
+                try {
+                    Map rec = null;
+                    rec = NWorkspaceExt.of().getModel().recomm.getRecommendations(ri, phase, failure);
+                } catch (Exception ex3) {
+                    //just ignore
+                }
+            }).start();
+        });
+    }
     @Override
     public Map getRecommendations(RequestQueryInfo ri, NRecommendationPhase phase, boolean failure) {
         try {
