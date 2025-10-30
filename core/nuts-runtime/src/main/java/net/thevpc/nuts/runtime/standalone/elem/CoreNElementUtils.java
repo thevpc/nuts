141d2fec9b93aadba54f2d05461fbb08f27afe01
thevpc <taha.bensalah@gmail.com>
2025-10-30 08:29:31 +0100

    Update CoreNElementUtils.java : work_on_destructables

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/elem/CoreNElementUtils.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/elem/CoreNElementUtils.java
@@ -4,9 +4,14 @@ import net.thevpc.nuts.elem.NElement;
 import net.thevpc.nuts.text.NFormattable;
 import net.thevpc.nuts.text.NText;
 import net.thevpc.nuts.text.NMsg;
+import net.thevpc.nuts.util.NTruth;
+import net.thevpc.nuts.util.NTruthPredicate;
 
 import java.lang.reflect.*;
 import java.time.temporal.Temporal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Predicate;
 
 public class CoreNElementUtils {
@@ -91,6 +96,89 @@ public class CoreNElementUtils {
             }
         }
     };
+//    private List<NTruthPredicate<Type>> undestructubles=new ArrayList<>(
+//            Arrays.asList(
+//                    new NTruthPredicate[]{
+//                            new NTruthPredicate<Type>() {
+//                                @Override
+//                                public NTruth test(Type type) {
+//                                    if (type == null) {
+//                                        return NTruth.TRUE;
+//                                    }
+//                                    if (type instanceof Class<?>) {
+//                                        Class cls = (Class) type;
+//                                        switch (cls.getName()) {
+//                                            case "boolean":
+//                                            case "byte":
+//                                            case "char":
+//                                            case "short":
+//                                            case "int":
+//                                            case "long":
+//                                            case "float":
+//                                            case "double":
+//                                            case "java.lang.Character":
+//                                            case "java.lang.String":
+//                                            case "java.lang.StringBuilder":
+//                                            case "java.lang.Boolean":
+//                                            case "java.lang.Byte":
+//                                            case "java.lang.Short":
+//                                            case "java.lang.Integer":
+//                                            case "java.lang.Long":
+//                                            case "java.lang.Float":
+//                                            case "java.lang.Double":
+//                                            case "java.math.BigDecimal":
+//                                            case "java.math.BigInteger":
+//                                            case "java.util.Date":
+//                                            case "java.sql.Time":
+//                                            case "net.thevpc.nuts.math.NDoubleComplex":
+//                                            case "net.thevpc.nuts.math.NFloatComplex":
+//                                            case "net.thevpc.nuts.math.NBigComplex":
+//                                                return NTruth.TRUE;
+//                                        }
+//                                        if (Temporal.class.isAssignableFrom(cls)) {
+//                                            return true;
+//                                        }
+//                                        if (java.util.Date.class.isAssignableFrom(cls)) {
+//                                            return true;
+//                                        }
+//                                        if (NElement.class.isAssignableFrom(cls)) {
+//                                            return true;
+//                                        }
+//                                        return (
+//                                                NText.class.isAssignableFrom(cls)
+//                                                        || NElement.class.isAssignableFrom(cls)
+//                                                        || NFormattable.class.isAssignableFrom(cls)
+//                                                        || NMsg.class.isAssignableFrom(cls)
+//                                        );
+//                                    } else if (type instanceof ParameterizedType) {
+//                                        // e.g. List<String> -> get raw type List
+//                                        Type rawType = ((ParameterizedType) type).getRawType();
+//                                        if (rawType instanceof Class<?>) {
+//                                            return test((Class<?>) rawType);
+//                                        } else {
+//                                            throw new IllegalArgumentException("Unexpected raw type: " + rawType);
+//                                        }
+//                                    } else if (type instanceof GenericArrayType) {
+//                                        return false;
+//                                    } else if (type instanceof TypeVariable) {
+//                                        return false;
+//                                    } else if (type instanceof WildcardType) {
+//                                        // e.g. ? extends Number or ? super Integer
+//                                        WildcardType wildcardType = (WildcardType) type;
+//                                        Type[] upperBounds = wildcardType.getUpperBounds();
+//                                        // Usually use upper bounds, fallback to Object
+//                                        if (upperBounds.length == 0) {
+//                                            return test(Object.class);
+//                                        }
+//                                        return test(upperBounds[0]);
+//                                    } else {
+//                                        throw new IllegalArgumentException("Unknown Type: " + type);
+//                                    }
+//                                }
+//                            }
+//                    }
+//            )
+//    );
     public static Predicate<Type> DEFAULT_INDESTRUCTIBLE = new Predicate<Type>() {
         @Override
         public boolean test(Type type) {
