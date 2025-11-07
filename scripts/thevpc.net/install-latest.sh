080068e045667e6c99502959766f2429052e0671
thevpc <taha.bensalah@gmail.com>
2025-11-07 10:42:45 +0100

    Update install-latest.sh : prepare-final-0.8.7

---
+++ scripts/thevpc.net/install-latest.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+NUTS_VERSION=0.8.7
+## using -N option (newer) to force existing nuts installations to redownload packages
+curl -sL https://maven.thevpc.net/net/thevpc/nuts/nuts-app/${NUTS_VERSION}/nuts-app-${NUTS_VERSION}.jar -o nuts.jar && java -jar nuts.jar  -Ny "$@"
