c7f07424abbbfd7bcdbb183027a33c77e75422f3
thevpc <taha.bensalah@gmail.com>
2025-11-09 22:36:57 +0100

    Update bootstrap-container-latest.sh : bumped-nuts

--- scripts/thevpc.net/bootstrap-container-latest.sh
+++ scripts/thevpc.net/bootstrap-container-latest.sh
@@ -2,7 +2,7 @@
 set -e
 
 # --- Environment variables with defaults ---
-NUTS_VERSION=${NUTS_VERSION:-0.8.7}
+NUTS_VERSION=${NUTS_VERSION:-0.8.8}
 NUTS_ARGS=("$@")
 
 if [ "${NUTS_CONTAINER_VERBOSE:-0}" = "1" ]; then
