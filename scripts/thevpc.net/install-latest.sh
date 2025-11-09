48afd0e29c78c662586225a14ec0884ec6a5eae2
thevpc <taha.bensalah@gmail.com>
2025-11-09 22:36:57 +0100

    Update install-latest.sh : bumped-nuts

--- scripts/thevpc.net/install-latest.sh
+++ scripts/thevpc.net/install-latest.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-NUTS_VERSION=0.8.7
+NUTS_VERSION=0.8.8
 ## using -N option (newer) to force existing nuts installations to redownload packages
 curl -sL https://maven.thevpc.net/net/thevpc/nuts/nuts-app/${NUTS_VERSION}/nuts-app-${NUTS_VERSION}.jar -o nuts.jar && java -jar nuts.jar  -Ny "$@"
