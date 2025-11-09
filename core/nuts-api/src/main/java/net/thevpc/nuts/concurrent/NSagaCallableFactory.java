a5514c6a83aee56ab29c8b58e157ed0d2f0180b1
thevpc <taha.bensalah@gmail.com>
2025-11-09 22:33:44 +0100

    [REMOVED] removed beanContainer in concurrent

--- core/nuts-api/src/main/java/net/thevpc/nuts/concurrent/NSagaCallableFactory.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/concurrent/NSagaCallableFactory.java
@@ -1,15 +1,9 @@
 package net.thevpc.nuts.concurrent;
 
-import net.thevpc.nuts.reflect.NBeanContainer;
-
 /**
  * @since 0.8.7
  */
 public interface NSagaCallableFactory {
-    NBeanContainer getBeanContainer();
-
-    NSagaCallableFactory withBeanContainer(NBeanContainer container);
-
     NSagaStore getStore();
 
     NSagaCallableFactory withStore(NSagaStore store);
