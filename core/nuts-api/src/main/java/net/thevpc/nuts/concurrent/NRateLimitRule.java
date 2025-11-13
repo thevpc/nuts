78ed5e1ecc630c2ca41ddf54ad6d55636915192d
thevpc <taha.bensalah@gmail.com>
2025-11-13 15:44:14 +0100

    Update NRateLimitRule.java : updated-documentation

--- core/nuts-api/src/main/java/net/thevpc/nuts/concurrent/NRateLimitRule.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/concurrent/NRateLimitRule.java
@@ -1,5 +1,16 @@
 package net.thevpc.nuts.concurrent;
 
+/**
+ * Represents a rate limiting rule that controls how frequently actions
+ * can be performed or resources can be consumed within a given time window.
+ * <p>
+ * Implementations typically use token-bucket, leaky-bucket, or fixed-window
+ * algorithms to track and limit the number of permitted operations.
+ * Each call to {@link #tryConsume(int)} attempts to consume one or more tokens,
+ * representing permission to proceed with an operation.
+ *
+ * @since 0.8.8
+ */
 public interface NRateLimitRule {
     /**
      * Try to consume tokens.
