3a340ff6b5cb99f7a9d65d57bb2bd18141ff1765
thevpc <taha.bensalah@gmail.com>
2025-11-19 12:12:01 +0100

    Update DefaultNPs.java : start-v0.8.9

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/xtra/ps/DefaultNPs.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/xtra/ps/DefaultNPs.java
@@ -226,28 +226,15 @@ public class DefaultNPs implements NPs {
                 return new UnixPsParser().parse(new StringReader(u.getGrabbedOutString()));
             }
             case WINDOWS: {
-                NPath tempPath = null;
-                try {
-                    tempPath = NPath.ofTempFile();
-                    String cmd = "Get-WmiObject Win32_Process | ForEach-Object { $o=$_.GetOwner(); $user=if($o){$o.User}else{'N/A'}; $mem=Get-WmiObject Win32_ComputerSystem; $state=if ($_.ExecutionState -eq 0) {'Running'} elseif ($_.ExecutionState -eq 2) {'Sleeping'} else {'Suspended'}; $start=if ($_.CreationDate) {$_.CreationDate.Substring(0,12)} else {'N/A'}; New-Object PSObject -Property @{ USER=$user; PID=$_.ProcessId; CPU=([math]::Round(($_.KernelModeTime+$_.UserModeTime)/1e7,2)); MEM=([math]::Round($_.WorkingSetSize/$mem.TotalPhysicalMemory*100,2)); VSZ=[long]($_.VirtualSize/1KB); RSS=[long]($_.WorkingSetSize/1KB); TTY='N/A'; STAT=$state; START=$start; TIME=([math]::Round(($_.KernelModeTime+$_.UserModeTime)/1e7,2)); COMMAND=$_.CommandLine } } | Export-Csv -NoTypeInformation -Encoding UTF8 \"" + tempPath.toString() + "\"";
-                    NExecCmd u = NExecCmd.of()
-                            .setIn(NExecInput.ofNull())
-                            .grabErr()
-                            .grabOut()
-                            .addCommand(
-                                    "powershell.exe", "-Command",
-                                    //"Get-WmiObject Win32_Process | ForEach-Object { $o = $_.GetOwner(); $user = if ($o) { $o.User } else { 'N/A' }; $mem = Get-WmiObject Win32_ComputerSystem; $state = if ($_.ExecutionState -eq 0) { 'Running' } elseif ($_.ExecutionState -eq 2) { 'Sleeping' } else { 'Suspended' }; $start = if ($_.CreationDate) { $_.CreationDate.Substring(0, 12) } else { 'N/A' }; New-Object PSObject -Property @{ USER=$user; PID=$_.ProcessId; CPU=([math]::Round(($_.KernelModeTime + $_.UserModeTime)/1e7, 2)); MEM=([math]::Round($_.WorkingSetSize / $mem.TotalPhysicalMemory * 100, 2)); VSZ=[long]($_.VirtualSize / 1KB); RSS=[long]($_.WorkingSetSize / 1KB); TTY='N/A'; STAT=$state; START=$start; TIME=([math]::Round(($_.KernelModeTime + $_.UserModeTime)/1e7, 2)); COMMAND=$_.CommandLine } }"
-                                    cmd
-                                    //                                        "$mem=(Get-WmiObject Win32_ComputerSystem).TotalPhysicalMemory; Get-WmiObject Win32_Process|ForEach-Object{ $o=$_.GetOwner();$user=if($o){$o.User}else{'N/A'};$state=if($_.ExecutionState -eq 0){'Running'}elseif($_.ExecutionState -eq 2){'Sleeping'}else{'Suspended'};$start=if($_.CreationDate){$_.CreationDate.Substring(0,12)}else{'N/A'};New-Object PSObject -Property @{USER=$user;PID=$_.ProcessId;CPU=[math]::Round(($_.KernelModeTime+$_.UserModeTime)/1e7,2);MEM=[math]::Round($_.WorkingSetSize/$mem*100,2);VSZ=[long]($_.VirtualSize/1KB);RSS=[long]($_.WorkingSetSize/1KB);TTY='N/A';STAT=$state;START=$start;TIME=[math]::Round(($_.KernelModeTime+$_.UserModeTime)/1e7,2);COMMAND=$_.CommandLine}}|ConvertTo-Csv -NoTypeInformation  | Out-String -Width 1000"
-                            )
-                            .setFailFast(isFailFast());
-                    String grabbedOutString = u.getGrabbedOutString();
-                    String tempValue = tempPath.isRegularFile() ? tempPath.readString() : "";
-//                    NPath.ofTempIdFile("ps-result.txt", NId.API_ID).writeString(connexionString);
-                    return new WindowsPsCsvParser().parse(new StringReader(tempValue));
-                } finally {
-                    if (tempPath != null && tempPath.isRegularFile()) {
-                        tempPath.delete();
+                final int IMPL_WmiObject_Win32_Process_Csv=1;
+                final int IMPL_WmiObject_Win32_Process_NoFile=2;
+                int mode=IMPL_WmiObject_Win32_Process_Csv;
+                switch (mode){
+                    case IMPL_WmiObject_Win32_Process_Csv:{
+                        return new WindowsPsCsvCaller().call(isFailFast());
+                    }
+                    case IMPL_WmiObject_Win32_Process_NoFile:{
+                        return new WindowsPs1Caller().call(isFailFast());
                     }
                 }
             }
