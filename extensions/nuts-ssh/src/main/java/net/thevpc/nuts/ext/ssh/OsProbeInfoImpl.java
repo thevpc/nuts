17c9563bd2cfabdc8c6ba3e7960783abb7703c43
thevpc <taha.bensalah@gmail.com>
2025-11-24 06:05:41 +0100

    Update OsProbeInfoImpl.java : fixing-ssh

--- extensions/nuts-ssh/src/main/java/net/thevpc/nuts/ext/ssh/OsProbeInfoImpl.java
+++ extensions/nuts-ssh/src/main/java/net/thevpc/nuts/ext/ssh/OsProbeInfoImpl.java
@@ -5,6 +5,8 @@ import net.thevpc.nuts.command.NExecCmd;
 import net.thevpc.nuts.command.NExecCmdExtension;
 import net.thevpc.nuts.command.NExecutionException;
 import net.thevpc.nuts.core.NSession;
+import net.thevpc.nuts.io.NIO;
+import net.thevpc.nuts.io.NOut;
 import net.thevpc.nuts.platform.NOsFamily;
 import net.thevpc.nuts.platform.NShellFamily;
 import net.thevpc.nuts.text.NMsg;
@@ -14,6 +16,8 @@ import net.thevpc.nuts.util.NStringUtils;
 import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class OsProbeInfoImpl implements OsProbeInfo {
     private String target;
@@ -71,150 +75,166 @@ public class OsProbeInfoImpl implements OsProbeInfo {
         return userHome;
     }
 
-    private synchronized void tryUpdate(){
-        if(osFamily==null){
+    public synchronized boolean tryUpdate() {
+        if (osFamily == null) {
             update();
+            return true;
         }
+        return false;
     }
-    private synchronized void update(){
+
+    private synchronized void update() {
         //test for posix
-        boolean ok=false;
-        try{
+
+        NSession session = NSession.of();
+        boolean ok = false;
+        try {
             String cmd =
                     "sh -c 'echo -n \"$(uname -s)|$(uname -r)|$(whoami)|${HOME}|$SHELL|\"; " +
                             "v=$($SHELL --version 2>/dev/null | head -n1 || " +
                             "$SHELL -version 2>/dev/null | head -n1 || " +
                             "$SHELL version 2>/dev/null | head -n1 || echo unknown); " +
                             "echo \"$v\"'";
-            String result=runOnceSystemGrab(target,cmd);
-            if(NBlankable.isBlank(result)){
+            String result = runOnceSystemGrab(cmd);
+            if (!NBlankable.isBlank(result)) {
                 List<String> cols = NStringUtils.split(result, "|", false, false);
-                if(cols.size()>=6){
+                if (cols.size() >= 6) {
                     String luname = cols.get(0).toLowerCase();
-                    osId =NId.of(null,cols.get(0),cols.get(1));
-                    if(luname.startsWith("linux")){
-                        osFamily =NOsFamily.LINUX;
-                    }else if(luname.startsWith("darwin")){
-                        osFamily =NOsFamily.MACOS;
-                    }else if(luname.startsWith("sunos")){
-                        osFamily =NOsFamily.UNIX;
-                    }else if(
+                    osId = NId.of(null, cols.get(0), cols.get(1));
+                    if (luname.startsWith("linux")) {
+                        osFamily = NOsFamily.LINUX;
+                    } else if (luname.startsWith("darwin")) {
+                        osFamily = NOsFamily.MACOS;
+                    } else if (luname.startsWith("sunos")) {
+                        osFamily = NOsFamily.UNIX;
+                    } else if (
                             luname.startsWith("freebsd")
-                            ||luname.startsWith("openbsd")
-                            ||luname.startsWith("netbsd")
-                    ){
-                        osFamily =NOsFamily.UNIX;
+                                    || luname.startsWith("openbsd")
+                                    || luname.startsWith("netbsd")
+                    ) {
+                        osFamily = NOsFamily.UNIX;
                     }
-                    userName=cols.get(2);
-                    userHome=cols.get(3);
-                    shellId =NId.of(null,cols.get(4),cols.get(5));
+                    userName = cols.get(2);
+                    userHome = cols.get(3);
+                    shellId = NId.of(null, cols.get(4), cols.get(5));
                     shellFamily = NShellFamily.parse(cols.get(4)).orElse(NShellFamily.SH);
                     rootUserName = "root";
-                    ok=true;
+                    ok = true;
                 }
             }
-        }catch (Exception ex){
+        } catch (Exception ex) {
             // not posix
         }
-        if(!ok){
-            try{
+        if (!ok) {
+            try {
                 String cmd =
-                        "powershell -NoProfile -Command $os='Windows';$osver=[System.Environment]::OSVersion.Version.ToString();$user=$env:USERNAME;$home=$env:USERPROFILE;$shell=(Get-Command pwsh -ErrorAction SilentlyContinue).Name;$shellver=$PSVersionTable.PSVersion;Write-Output $os'|'$osver'|'$user'|'$home'|'$shell'|'$shellver";
-                String result=runOnceSystemGrab(target,cmd);
-                if(NBlankable.isBlank(result)){
+                        "powershell -NoProfile -Command \"& {$os='Windows'; $osver=[System.Environment]::OSVersion.Version.ToString(); $user=$env:USERNAME; $homedir=$env:USERPROFILE; $shell=(Get-Command pwsh -ErrorAction SilentlyContinue).Name; $shellver=$PSVersionTable.PSVersion.ToString(); Write-Output ($os+'|'+$osver+'|'+$user+'|'+$homedir+'|'+$shell+'|'+$shellver)}\"";
+                String result = runOnceSystemGrab(cmd);
+                if (!NBlankable.isBlank(result)) {
                     List<String> cols = NStringUtils.split(result, "|", false, false);
-                    if(cols.size()>=6){
+                    if (cols.size() >= 6) {
                         String luname = cols.get(0).toLowerCase();
-                        osId =NId.of(null,cols.get(0),cols.get(1));
-                        if(luname.startsWith("linux")){
-                            osFamily =NOsFamily.LINUX;
-                        }else if(luname.startsWith("darwin")){
-                            osFamily =NOsFamily.MACOS;
-                        }else if(luname.startsWith("sunos")){
-                            osFamily =NOsFamily.UNIX;
-                        }else if(
+                        osId = NId.of(null, cols.get(0), cols.get(1));
+                        if (luname.startsWith("linux")) {
+                            osFamily = NOsFamily.LINUX;
+                        } else if (luname.startsWith("darwin")) {
+                            osFamily = NOsFamily.MACOS;
+                        } else if (luname.startsWith("sunos")) {
+                            osFamily = NOsFamily.UNIX;
+                        } else if (
                                 luname.startsWith("freebsd")
-                                        ||luname.startsWith("openbsd")
-                                        ||luname.startsWith("netbsd")
-                        ){
-                            osFamily =NOsFamily.UNIX;
+                                        || luname.startsWith("openbsd")
+                                        || luname.startsWith("netbsd")
+                        ) {
+                            osFamily = NOsFamily.UNIX;
                         }
-                        userName=cols.get(2);
-                        userHome=cols.get(3);
-                        shellId =NId.of(null,cols.get(4),cols.get(5));
+                        userName = cols.get(2);
+                        userHome = cols.get(3);
+                        shellId = NId.of(null, cols.get(4), cols.get(5));
                         shellFamily = NShellFamily.parse(cols.get(4)).orElse(NShellFamily.SH);
                         rootUserName = "Administrator";
-                        ok=true;
+                        ok = true;
                     }
                 }
-            }catch (Exception ex){
+            } catch (Exception ex) {
                 // not posix
             }
         }
-        if(!ok){
-            try{
+        if (!ok) {
+            try {
                 String cmd = "cmd /c \"echo Windows|%OS%|%USERNAME%|%USERPROFILE%|cmd|unknown\"";
-                String result=runOnceSystemGrab(target,cmd);
-                if(NBlankable.isBlank(result)){
+                String result = runOnceSystemGrab(cmd);
+                if (!NBlankable.isBlank(result)) {
                     List<String> cols = NStringUtils.split(result, "|", false, false);
-                    if(cols.size()>=6){
+                    if (cols.size() >= 6) {
                         String luname = cols.get(0).toLowerCase();
-                        osId =NId.of(null,cols.get(0),cols.get(1));
-                        if(luname.startsWith("linux")){
-                            osFamily =NOsFamily.LINUX;
-                        }else if(luname.startsWith("darwin")){
-                            osFamily =NOsFamily.MACOS;
-                        }else if(luname.startsWith("sunos")){
-                            osFamily =NOsFamily.UNIX;
-                        }else if(
+                        osId = NId.of(null, cols.get(0), cols.get(1));
+                        if (luname.startsWith("linux")) {
+                            osFamily = NOsFamily.LINUX;
+                        } else if (luname.startsWith("darwin")) {
+                            osFamily = NOsFamily.MACOS;
+                        } else if (luname.startsWith("sunos")) {
+                            osFamily = NOsFamily.UNIX;
+                        } else if (
                                 luname.startsWith("freebsd")
-                                        ||luname.startsWith("openbsd")
-                                        ||luname.startsWith("netbsd")
-                        ){
-                            osFamily =NOsFamily.UNIX;
+                                        || luname.startsWith("openbsd")
+                                        || luname.startsWith("netbsd")
+                        ) {
+                            osFamily = NOsFamily.UNIX;
                         }
-                        userName=cols.get(2);
-                        userHome=cols.get(3);
-                        shellId =NId.of(null,cols.get(4),cols.get(5));
+                        userName = cols.get(2);
+                        userHome = cols.get(3);
+                        shellId = NId.of(null, cols.get(4), cols.get(5));
                         shellFamily = NShellFamily.parse(cols.get(4)).orElse(NShellFamily.SH);
                         rootUserName = "Administrator";
-                        ok=true;
+                        ok = true;
                     }
                 }
-            }catch (Exception ex){
+            } catch (Exception ex) {
                 // not posix
             }
         }
-        if(!ok){
-            osFamily=NOsFamily.UNKNOWN;
-            osId =NId.BLANK;
-            shellFamily=NShellFamily.UNKNOWN;
-            shellId =NId.BLANK;
-            userHome=null;
-            userName=null;
+        if (!ok) {
+            osFamily = NOsFamily.UNKNOWN;
+            osId = NId.BLANK;
+            shellFamily = NShellFamily.UNKNOWN;
+            shellId = NId.BLANK;
+            userHome = null;
+            userName = null;
             rootUserName = "root";
         }
+
     }
 
-    public String runOnceSystemGrab(String target, String... cmd) {
+    public String runOnceSystemGrab(String cmd) {
 
-        OutputStream out = new ByteArrayOutputStream();
-        OutputStream err = new ByteArrayOutputStream();
-        int e;
-        NSession session = NSession.of();
-        try (RemoteConnexionStringInfo.MyNExecCmdExtensionContext d = new RemoteConnexionStringInfo.MyNExecCmdExtensionContext(
-                NExecCmd.of().setConnexionString(target).system(),
-                commExec, target, cmd, out, err)) {
-            e = commExec.exec(d);
-        } catch (RuntimeException ex) {
-            throw new NExecutionException(NMsg.ofC("command failed :%s", ex), ex);
+        try (SShConnection sshc = new SShConnection(target
+                , NSession.of().in()
+                , NOut.asOutputStream()
+                , NIO.ofNullRawOutputStream()
+        ).grabOutputString()) {
+            int r = sshc.exec(cmd);
+            return sshc.getOut().toString();
         }
-        if (e != NExecutionException.SUCCESS) {
-            session.err().println(out.toString());
-            session.err().println(err.toString());
-            throw new NExecutionException(NMsg.ofC("command exit with code :%s", e), e);
-        }
-        return out.toString();
+//
+//
+//        OutputStream out = new ByteArrayOutputStream();
+//        OutputStream err = new ByteArrayOutputStream();
+//        int e;
+//        NSession session = NSession.of();
+//        try (RemoteConnexionStringInfo.MyNExecCmdExtensionContext d = new RemoteConnexionStringInfo.MyNExecCmdExtensionContext(
+//                NExecCmd.of().setConnexionString(target).system(),
+//                commExec, target, cmd, out, err)) {
+//            e = commExec.exec(d);
+//        } catch (RuntimeException ex) {
+//            throw new NExecutionException(NMsg.ofC("command failed :%s", ex), ex);
+//        }
+//        if (e != NExecutionException.SUCCESS) {
+//            session.err().println(out.toString());
+//            session.err().println(err.toString());
+//            throw new NExecutionException(NMsg.ofC("command exit with code :%s", e), e);
+//        }
+//        return out.toString();
     }
 
     @Override
