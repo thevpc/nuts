6a076da1cd18747499a769ab121a4b1d4aa02650
thevpc <taha.bensalah@gmail.com>
2025-11-26 01:07:36 +0100

    Update OsProbeInfo.java : wip-windows-ssh

--- extensions/nuts-ssh/src/main/java/net/thevpc/nuts/ext/ssh/OsProbeInfo.java
+++ extensions/nuts-ssh/src/main/java/net/thevpc/nuts/ext/ssh/OsProbeInfo.java
@@ -1,12 +1,8 @@
 package net.thevpc.nuts.ext.ssh;
 
 import net.thevpc.nuts.artifact.NId;
-import net.thevpc.nuts.command.NExecCmdExtension;
 import net.thevpc.nuts.platform.NOsFamily;
 import net.thevpc.nuts.platform.NShellFamily;
-
-import java.util.function.Function;
-import java.util.function.Supplier;
 
 public interface OsProbeInfo {
     NOsFamily osFamily();
