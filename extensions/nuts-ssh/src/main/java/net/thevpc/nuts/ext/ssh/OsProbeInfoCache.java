eed144286e06475d6a2462b42b9c78bfae866a80
thevpc <taha.bensalah@gmail.com>
2025-11-26 01:07:36 +0100

    Update OsProbeInfoCache.java : wip-windows-ssh

--- extensions/nuts-ssh/src/main/java/net/thevpc/nuts/ext/ssh/OsProbeInfoCache.java
+++ extensions/nuts-ssh/src/main/java/net/thevpc/nuts/ext/ssh/OsProbeInfoCache.java
@@ -2,7 +2,6 @@ package net.thevpc.nuts.ext.ssh;
 
 import net.thevpc.nuts.core.NWorkspace;
 import net.thevpc.nuts.net.NConnexionString;
-import net.thevpc.nuts.util.NLiteral;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -11,12 +10,9 @@ public class OsProbeInfoCache {
     static OsProbeInfoCache of(){
         NWorkspace ws = NWorkspace.of();
         synchronized (ws) {
-            NLiteral u = ws.getProperty(OsProbeInfoCache.class.getName()).orNull();
-            if(u!=null &&  !u.isNull()){
-                Object o = u.asObject().orNull();
-                if(o instanceof OsProbeInfoCache){
-                    return (OsProbeInfoCache) o;
-                }
+            OsProbeInfoCache u = ws.getProperty(OsProbeInfoCache.class).orNull();
+            if(u!=null){
+                return u;
             }
             OsProbeInfoCache r=new OsProbeInfoCache();
             ws.setProperty(OsProbeInfoCache.class.getName(),r);
@@ -24,13 +20,13 @@ public class OsProbeInfoCache {
         }
     }
 
-    private Map<String, OsProbeInfo> cache = new ConcurrentHashMap<>();
+    private Map<NConnexionString, OsProbeInfo> cache = new ConcurrentHashMap<>();
 
     public OsProbeInfo get(NConnexionString id) {
-        return get(id.toString());
+        return cache.computeIfAbsent(id, s -> new OsProbeInfoImpl(id));
     }
 
     public OsProbeInfo get(String id) {
-        return cache.computeIfAbsent(id, s -> new OsProbeInfoImpl(id));
+        return get(NConnexionString.of(id));
     }
 }
