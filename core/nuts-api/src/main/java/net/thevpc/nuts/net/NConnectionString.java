fab8234bd8ef69e98ad9539490137bedcb5821bd
thevpc <taha.bensalah@gmail.com>
2025-11-27 01:51:58 +0100

    Update NConnectionString.java : fix-spelling

---
+++ core/nuts-api/src/main/java/net/thevpc/nuts/net/NConnectionString.java
@@ -0,0 +1,48 @@
+package net.thevpc.nuts.net;
+
+import net.thevpc.nuts.ext.NExtensions;
+import net.thevpc.nuts.util.NImmutable;
+import net.thevpc.nuts.util.NOptional;
+
+import java.util.List;
+import java.util.Map;
+
+public interface NConnectionString extends NImmutable {
+    static NOptional<NConnectionString> get(String expression) {
+        return NExtensions.of(NConnectionStringBuilderFactory.class)
+                .create(expression).map(NConnectionStringBuilder::build);
+    }
+
+    static NConnectionString of(String expression) {
+        return get(expression).get();
+    }
+
+    String getUserName();
+
+    String getPassword();
+
+
+    String getHost();
+
+    NConnectionString getRoot();
+
+    NConnectionString getParent();
+
+    String getPort();
+
+    NOptional<Map<String, List<String>>> getQueryMap();
+
+    String getPath();
+
+    String getProtocol();
+
+    String getQueryString();
+
+    List<String> getNames();
+
+    NConnectionString resolve(String child);
+
+    NConnectionStringBuilder builder();
+
+    NConnectionString withPath(String path);
+}
