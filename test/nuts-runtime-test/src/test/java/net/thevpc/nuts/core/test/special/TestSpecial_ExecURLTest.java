2762ea3814fac4badf8356c3bc36ce81b4c42f9e
thevpc <taha.bensalah@gmail.com>
2025-11-27 01:51:57 +0100

    Update TestSpecial_ExecURLTest.java : fix-spelling

--- test/nuts-runtime-test/src/test/java/net/thevpc/nuts/core/test/special/TestSpecial_ExecURLTest.java
+++ test/nuts-runtime-test/src/test/java/net/thevpc/nuts/core/test/special/TestSpecial_ExecURLTest.java
@@ -36,7 +36,7 @@ public class TestSpecial_ExecURLTest {
     public void testNtf2() {
         TestUtils.println(NVersionFormat.of());
         String result = NExecCmd.of()
-                .setConnexionString("ssh://vpc:a@192.168.1.36")
+                .setConnectionString("ssh://vpc:a@192.168.1.36")
                 //.addCommand("ls","-l")
                 .addCommand("nuts","info")
                 .failFast()
