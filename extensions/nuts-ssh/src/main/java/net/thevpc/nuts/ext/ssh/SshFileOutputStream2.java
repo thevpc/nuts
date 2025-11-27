23d020e88a7580a1b6a5df11c7d1b18298dd7a78
thevpc <taha.bensalah@gmail.com>
2025-11-27 01:51:57 +0100

    Update SshFileOutputStream2.java : fix-spelling

--- extensions/nuts-ssh/src/main/java/net/thevpc/nuts/ext/ssh/SshFileOutputStream2.java
+++ extensions/nuts-ssh/src/main/java/net/thevpc/nuts/ext/ssh/SshFileOutputStream2.java
@@ -1,18 +1,18 @@
 package net.thevpc.nuts.ext.ssh;
 
 import net.thevpc.nuts.io.NPath;
-import net.thevpc.nuts.net.NConnexionString;
+import net.thevpc.nuts.net.NConnectionString;
 
 import java.io.IOException;
 import java.io.OutputStream;
 
 public class SshFileOutputStream2 extends OutputStream {
     private NPath temp;
-    private NConnexionString path;
+    private NConnectionString path;
     private boolean mkdirs;
     private OutputStream tempOS;
 
-    public SshFileOutputStream2(NConnexionString path, boolean mkdirs) {
+    public SshFileOutputStream2(NConnectionString path, boolean mkdirs) {
         super();
         this.path = path;
         this.mkdirs = mkdirs;
