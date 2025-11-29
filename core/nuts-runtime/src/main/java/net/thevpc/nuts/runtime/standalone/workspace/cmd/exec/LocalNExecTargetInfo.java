36eb3358d1325782dbeb7e9268a7829761d8ece4
thevpc <taha.bensalah@gmail.com>
2025-11-29 08:29:07 +0100

    Update LocalNExecTargetInfo.java : wip-ssh

---
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/workspace/cmd/exec/LocalNExecTargetInfo.java
@@ -0,0 +1,75 @@
+package net.thevpc.nuts.runtime.standalone.workspace.cmd.exec;
+
+import net.thevpc.nuts.artifact.NId;
+import net.thevpc.nuts.command.NExecTargetInfo;
+import net.thevpc.nuts.core.NWorkspace;
+import net.thevpc.nuts.platform.NOsFamily;
+import net.thevpc.nuts.platform.NShellFamily;
+import net.thevpc.nuts.util.NStringUtils;
+
+class LocalNExecTargetInfo implements NExecTargetInfo {
+    public static final LocalNExecTargetInfo INSTANCE = new LocalNExecTargetInfo();
+
+    @Override
+    public NOsFamily getOsFamily() {
+        return NWorkspace.of().getOsFamily();
+    }
+
+    @Override
+    public NId getOsId() {
+        return NWorkspace.of().getOs();
+    }
+
+    @Override
+    public NId getShellId() {
+        return NId.of(null, NStringUtils.firstNonBlank(NWorkspace.of().getShellFamily().id()));
+    }
+
+    @Override
+    public NShellFamily getShellFamily() {
+        return NWorkspace.of().getShellFamily();
+    }
+
+    @Override
+    public String getUserName() {
+        return System.getProperty("user.name");
+    }
+
+    @Override
+    public String getRootUserName() {
+        switch (getOsFamily()) {
+            case WINDOWS: {
+                switch (NStringUtils.trim(getUserName()).toLowerCase()) {
+                    case "adminitrateur": {
+                        return "Administrateur";
+                    }
+                    case "administrador": {
+                        return "Administrador";
+                    }
+                    case "administratör": {
+                        return "Administratör";
+                    }
+                    case "järjestelmänvalvoja": {
+                        return "Järjestelmänvalvoja";
+                    }
+                    case "rendszergazda": {
+                        return "Rendszergazda";
+                    }
+                    case "администратор": {
+                        return "Администратор";
+                    }
+                }
+                return "Administrateur";
+            }
+            default: {
+                return "root";
+            }
+        }
+    }
+
+    @Override
+    public String getUserHome() {
+        return System.getProperty("user.home");
+    }
+
+}
