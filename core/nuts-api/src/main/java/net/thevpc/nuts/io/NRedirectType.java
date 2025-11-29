998e716fdbd34cad230b4eaedc0f0c2e2cfd414d
thevpc <taha.bensalah@gmail.com>
2025-11-29 08:29:07 +0100

    Update NRedirectType.java : wip-ssh

--- core/nuts-api/src/main/java/net/thevpc/nuts/io/NRedirectType.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/io/NRedirectType.java
@@ -5,10 +5,37 @@ import net.thevpc.nuts.util.NEnumUtils;
 import net.thevpc.nuts.util.NNameFormat;
 import net.thevpc.nuts.util.NOptional;
 
+/**
+ * Defines the type of redirection for command input or output.
+ *
+ * <p>Correspondence with {@link ProcessBuilder.Redirect} is provided for
+ * PIPE, INHERIT, and REDIRECT types.
+ *
+ * <ul>
+ *     <li>STREAM - use a custom stream</li>
+ *     <li>GRAB_STREAM / GRAB_FILE - capture output</li>
+ *     <li>PIPE - redirect to another process</li>
+ *     <li>INHERIT - use the JVM process streams</li>
+ *     <li>PATH - redirect to a file</li>
+ *     <li>NULL - discard output</li>
+ * </ul>
+ */
 public enum NRedirectType implements NEnum {
+    /**
+     * use JDK redirection
+     */
     REDIRECT,
+    /**
+     * use a custom stream
+     */
     STREAM,
+    /**
+     * capture output as a stream
+     */
     GRAB_STREAM,
+    /**
+     * capture output as a file
+     */
     GRAB_FILE,
     /**
      * The type of {@link ProcessBuilder.Redirect#PIPE Redirect.PIPE}.
@@ -19,7 +46,13 @@ public enum NRedirectType implements NEnum {
      * The type of {@link ProcessBuilder.Redirect#INHERIT Redirect.INHERIT}.
      */
     INHERIT,
+    /**
+     * redirect to a file
+     */
     PATH,
+    /**
+     * discard output
+     */
     NULL,
     ;
 
