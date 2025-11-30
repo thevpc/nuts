df37c905e066d568d5c26a6b3cfa15ae7cd51d8f
thevpc <taha.bensalah@gmail.com>
2025-11-30 21:23:00 +0100

    Update SshConnection.java : fix-and-faster-ssh-windows

--- extensions/nuts-ssh/src/main/java/net/thevpc/nuts/ext/ssh/SshConnection.java
+++ extensions/nuts-ssh/src/main/java/net/thevpc/nuts/ext/ssh/SshConnection.java
@@ -1,6 +1,6 @@
 package net.thevpc.nuts.ext.ssh;
 
-import com.jcraft.jsch.Channel;
+import net.thevpc.nuts.io.NPathInfo;
 import net.thevpc.nuts.io.NPathType;
 
 import java.io.InputStream;
@@ -8,6 +8,8 @@ import java.io.OutputStream;
 import java.util.List;
 
 public interface SshConnection extends AutoCloseable {
+    String IDENTITY_FILE = "identity-file";
+
     void reset();
 
     void close();
@@ -44,7 +46,7 @@ public interface SshConnection extends AutoCloseable {
 
     List<String> list(String path);
 
-    long contentLength(String basePath);
+    long getContentLength(String basePath);
 
     String getContentEncoding(String basePath);
 
@@ -56,7 +58,9 @@ public interface SshConnection extends AutoCloseable {
 
     List<String> walk(String path, boolean followLinks, int maxDepth);
 
-    Channel openExecChannel(String cmd);
-
     byte[] getDigestWithCommand(String algo, String path);
+
+    NPathInfo getInfo(String path);
+
+    List<NPathInfo> listInfos(String path);
 }
