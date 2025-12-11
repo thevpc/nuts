65dff1f889e7f34376ae682e3918060fd27dfbee
thevpc <taha.bensalah@gmail.com>
2025-12-11 02:09:52 +0100

    Update NElementComment.java : update-api

--- core/nuts-api/src/main/java/net/thevpc/nuts/elem/NElementComment.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/elem/NElementComment.java
@@ -3,6 +3,37 @@ package net.thevpc.nuts.elem;
 import net.thevpc.nuts.util.NBlankable;
 
 public interface NElementComment extends Comparable<NElementComment>, NBlankable {
+
+    /**
+     * create some comments
+     * @param text comment
+     * @return NElementComment
+     * @since 0.8.9
+     */
+    static NElementComment of(String text) {
+        return ofMultiLine(text);
+    }
+
+    /**
+     * create some multiline comments
+     * @param text comment
+     * @return NElementComment
+     * @since 0.8.9
+     */
+    static NElementComment ofMultiLine(String... text) {
+        return NElement.ofMultiLineComment(text);
+    }
+
+    /**
+     * create some single comments
+     * @param text comment
+     * @return NElementComment
+     * @since 0.8.9
+     */
+    static NElementComment ofSingleLine(String... text) {
+        return NElement.ofSingleLineComment(text);
+    }
+
     NElementCommentType type();
 
     String text() ;
