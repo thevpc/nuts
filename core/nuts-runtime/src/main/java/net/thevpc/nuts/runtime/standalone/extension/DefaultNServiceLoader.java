92432a157a882c0225b5b3faab834c28f79d21d3
thevpc <taha.bensalah@gmail.com>
2025-12-21 23:19:58 +0100

    Update DefaultNServiceLoader.java : faster-bean-factory

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/extension/DefaultNServiceLoader.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/extension/DefaultNServiceLoader.java
@@ -1,48 +1,73 @@
 package net.thevpc.nuts.runtime.standalone.extension;
 
-import net.thevpc.nuts.core.NWorkspace;
-import net.thevpc.nuts.runtime.standalone.util.NScorableNScorableQueryImpl;
+import net.thevpc.nuts.ext.NExtensions;
+import net.thevpc.nuts.log.NLog;
 import net.thevpc.nuts.spi.NComponent;
 import net.thevpc.nuts.io.NServiceLoader;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.ServiceLoader;
 
 import net.thevpc.nuts.util.NScorableContext;
 import net.thevpc.nuts.text.NMsg;
 import net.thevpc.nuts.util.NOptional;
+import net.thevpc.nuts.util.NScoredValue;
 
 public class DefaultNServiceLoader<T extends NComponent, B> implements NServiceLoader<T> {
 
     private final ClassLoader classLoader;
     private final Class<T> serviceType;
     private final Class<B> criteriaType;
-    private final ServiceLoader<T> loader;
-    private final NWorkspace workspace;
 
-    public DefaultNServiceLoader(NWorkspace workspace, Class<T> serviceType, Class<B> criteriaType, ClassLoader classLoader) {
-        this.workspace = workspace;
+    public DefaultNServiceLoader(Class<T> serviceType, Class<B> criteriaType, ClassLoader classLoader) {
         this.classLoader = classLoader;
         this.serviceType = serviceType;
         this.criteriaType = criteriaType;
-        loader = ServiceLoader.load(serviceType,
-                classLoader == null ? Thread.currentThread().getContextClassLoader() : classLoader
-        );
+
     }
 
     @Override
     public List<T> loadAll(Object criteria) {
-        return new NScorableNScorableQueryImpl<T>(NScorableContext.of(criteria))
-                .fromIterable(loader)
-                .withName(NMsg.ofC("component %s",serviceType))
-                .getAll();
+        List<T> all = new ArrayList<>();
+        NScorableContext context = NScorableContext.of(criteria);
+        NExtensions nExtensions = NExtensions.of();
+        for (Class<? extends T> t : ServiceTypeIterator.loadList(serviceType, classLoader)) {
+            NScoredValue<T> s = nExtensions.getTypeScoredValue(t, serviceType, context);
+            if (s.isValid()) {
+                try {
+                    all.add(s.value());
+                }catch (Exception ex) {
+                    NLog.of(DefaultNServiceLoader.class).log(NMsg.ofC("unable to resolve service type %s : %s",serviceType.getName(),ex));
+                }
+            }
+        }
+        return all;
     }
 
     @Override
     public NOptional<T> loadBest(Object criteria) {
-        return new NScorableNScorableQueryImpl<T>(NScorableContext.of(criteria))
-                .fromIterable(loader)
-                .withName(NMsg.ofC("component %s",serviceType))
-                .getBest();
+        NScorableContext context = NScorableContext.of(criteria);
+        NExtensions nExtensions = NExtensions.of();
+        T bestInstance = null;
+        int bestScore = -1;
+        for (Class<? extends T> t : ServiceTypeIterator.loadList(serviceType, classLoader)) {
+            NScoredValue<T> s = nExtensions.getTypeScoredValue(t, serviceType, context);
+            int score = s.score();
+            if (score>0) {
+                if(bestScore<0 || score>bestScore) {
+                    T i2=null;
+                    try {
+                        i2 = s.value();
+                    }catch (Exception ex) {
+                        NLog.of(DefaultNServiceLoader.class).log(NMsg.ofC("unable to resolve service type %s : %s",serviceType.getName(),ex));
+                    }
+                    if(i2!=null) {
+                        bestScore = score;
+                        bestInstance = i2;
+                    }
+                }
+            }
+        }
+        return NOptional.ofNamed(bestInstance,NMsg.ofC("component %s", serviceType));
     }
 }
