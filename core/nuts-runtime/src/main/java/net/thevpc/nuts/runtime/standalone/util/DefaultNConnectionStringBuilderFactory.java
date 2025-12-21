bbf46a31e933cdcbd8a4882d37cb633904a14698
thevpc <taha.bensalah@gmail.com>
2025-12-21 23:20:01 +0100

    Update DefaultNConnectionStringBuilderFactory.java : faster-bean-factory

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/util/DefaultNConnectionStringBuilderFactory.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/util/DefaultNConnectionStringBuilderFactory.java
@@ -1,11 +1,13 @@
 package net.thevpc.nuts.runtime.standalone.util;
 
 import net.thevpc.nuts.net.NConnectionStringBuilderFactory;
-import net.thevpc.nuts.util.NScorableContext;
+import net.thevpc.nuts.util.NScore;
+import net.thevpc.nuts.util.NScorable;
 import net.thevpc.nuts.net.DefaultNConnectionStringBuilder;
 import net.thevpc.nuts.net.NConnectionStringBuilder;
 import net.thevpc.nuts.util.NOptional;
 
+@NScore(fixed = NScorable.DEFAULT_SCORE)
 public class DefaultNConnectionStringBuilderFactory implements NConnectionStringBuilderFactory {
     @Override
     public NConnectionStringBuilder create() {
@@ -15,10 +17,6 @@ public class DefaultNConnectionStringBuilderFactory implements NConnectionString
     @Override
     public NOptional<NConnectionStringBuilder> create(String expression) {
         return DefaultNConnectionStringBuilder.of(expression);
-    }
-    @Override
-    public int getScore(NScorableContext context) {
-        return DEFAULT_SCORE;
     }
 
 }
