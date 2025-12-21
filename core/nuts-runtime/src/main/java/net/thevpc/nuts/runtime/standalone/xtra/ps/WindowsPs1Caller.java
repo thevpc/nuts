abe750b902875e96ab2bacc109bf7fe03cfdb49b
thevpc <taha.bensalah@gmail.com>
2025-12-21 23:20:05 +0100

    Update WindowsPs1Caller.java : faster-bean-factory

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/xtra/ps/WindowsPs1Caller.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/xtra/ps/WindowsPs1Caller.java
@@ -1,10 +1,7 @@
 package net.thevpc.nuts.runtime.standalone.xtra.ps;
 
-import net.thevpc.nuts.command.NExecCmd;
-import net.thevpc.nuts.command.NExecTargetInfo;
+import net.thevpc.nuts.command.NExec;
 import net.thevpc.nuts.io.NExecInput;
-import net.thevpc.nuts.io.NOut;
-import net.thevpc.nuts.io.NPath;
 import net.thevpc.nuts.io.NPsInfo;
 import net.thevpc.nuts.net.NConnectionString;
 import net.thevpc.nuts.util.NStream;
@@ -15,17 +12,15 @@ import java.util.Base64;
 
 public class WindowsPs1Caller {
     private NConnectionString connectionString;
-    private NExecTargetInfo target;
 
-    public WindowsPs1Caller(NConnectionString connectionString, NExecTargetInfo target) {
+    public WindowsPs1Caller(NConnectionString connectionString) {
         this.connectionString = connectionString;
-        this.target = target;
     }
 
     public NStream<NPsInfo> call(boolean failFast) {
         String script = "Get-WmiObject Win32_Process | ForEach-Object { $o=$_.GetOwner(); $user=if($o){$o.User}else{'N/A'}; $mem=Get-WmiObject Win32_ComputerSystem; $state=if ($_.ExecutionState -eq 0) {'Running'} elseif ($_.ExecutionState -eq 2) {'Sleeping'} else {'Suspended'}; $start=if ($_.CreationDate) {$_.CreationDate.Substring(0,12)} else {'N/A'}; New-Object PSObject -Property @{ USER=$user; PID=$_.ProcessId; CPU=([math]::Round(($_.KernelModeTime+$_.UserModeTime)/1e7,2)); MEM=([math]::Round($_.WorkingSetSize/$mem.TotalPhysicalMemory*100,2)); VSZ=[long]($_.VirtualSize/1KB); RSS=[long]($_.WorkingSetSize/1KB); TTY='N/A'; STAT=$state; START=$start; TIME=([math]::Round(($_.KernelModeTime+$_.UserModeTime)/1e7,2)); COMMAND=$_.CommandLine } }";
         String base64 = Base64.getEncoder().encodeToString(script.getBytes(StandardCharsets.UTF_16LE));
-        NExecCmd u = NExecCmd.ofSystem()
+        NExec u = NExec.ofSystem()
                 .setIn(NExecInput.ofNull())
                 .at(connectionString)
                 .grabErr()
