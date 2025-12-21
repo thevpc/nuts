6c4f3af52fd281d7f53971b7ee4444f6214c58d1
thevpc <taha.bensalah@gmail.com>
2025-12-21 23:20:03 +0100

    Update NSettingsJavaSubCommand.java : faster-bean-factory

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/workspace/cmd/settings/java/NSettingsJavaSubCommand.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/workspace/cmd/settings/java/NSettingsJavaSubCommand.java
@@ -9,14 +9,17 @@ import net.thevpc.nuts.cmdline.NCmdLine;
 
 import net.thevpc.nuts.core.NSession;
 import net.thevpc.nuts.core.NWorkspace;
-import net.thevpc.nuts.platform.NPlatformLocation;
+import net.thevpc.nuts.platform.NExecutionEngines;
+import net.thevpc.nuts.platform.NExecutionEngineLocation;
 import net.thevpc.nuts.text.NMutableTableModel;
 import net.thevpc.nuts.io.NPath;
 import net.thevpc.nuts.io.NPrintStream;
 import net.thevpc.nuts.runtime.standalone.workspace.cmd.settings.AbstractNSettingsSubCommand;
-import net.thevpc.nuts.platform.NPlatformFamily;
+import net.thevpc.nuts.platform.NExecutionEngineFamily;
 import net.thevpc.nuts.text.NText;
 import net.thevpc.nuts.text.NTextArt;
+import net.thevpc.nuts.util.NScore;
+import net.thevpc.nuts.util.NScorable;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -26,6 +29,7 @@ import java.util.List;
 /**
  * @author thevpc
  */
+@NScore(fixed = NScorable.DEFAULT_SCORE)
 public class NSettingsJavaSubCommand extends AbstractNSettingsSubCommand {
     public NSettingsJavaSubCommand() {
         super();
@@ -39,6 +43,7 @@ public class NSettingsJavaSubCommand extends AbstractNSettingsSubCommand {
         NSession session= NSession.of();
         NPrintStream out = session.out();
         NWorkspace workspace = NWorkspace.of();
+        NExecutionEngines pinstaller = NExecutionEngines.of();
         if (cmdLine.next("add java","java add").isPresent()) {
             if (cmdLine.next("--search").isPresent()) {
                 List<String> extraLocations = new ArrayList<>();
@@ -46,13 +51,13 @@ public class NSettingsJavaSubCommand extends AbstractNSettingsSubCommand {
                     extraLocations.add(cmdLine.next().get().image());
                 }
                 if (extraLocations.isEmpty()) {
-                    for (NPlatformLocation loc : workspace.searchSystemPlatforms(NPlatformFamily.JAVA)) {
-                        workspace.addPlatform(loc);
+                    for (NExecutionEngineLocation loc : pinstaller.searchHostExecutionEngines(NExecutionEngineFamily.JAVA)) {
+                        pinstaller.addExecutionEngine(loc);
                     }
                 } else {
                     for (String extraLocation : extraLocations) {
-                        for (NPlatformLocation loc : workspace.searchSystemPlatforms(NPlatformFamily.JAVA, NPath.of(extraLocation))) {
-                            workspace.addPlatform(loc);
+                        for (NExecutionEngineLocation loc : pinstaller.searchHostExecutionEngines(NExecutionEngineFamily.JAVA, NPath.of(extraLocation))) {
+                            pinstaller.addExecutionEngine(loc);
                         }
                     }
                 }
@@ -62,10 +67,10 @@ public class NSettingsJavaSubCommand extends AbstractNSettingsSubCommand {
                 }
             } else {
                 while (cmdLine.hasNext()) {
-                    NPlatformLocation loc = workspace.resolvePlatform(NPlatformFamily.JAVA,
+                    NExecutionEngineLocation loc = pinstaller.resolveExecutionEngine(NExecutionEngineFamily.JAVA,
                             NPath.of(cmdLine.next().get().image()), null).orNull();
                     if (loc != null) {
-                        workspace.addPlatform(loc);
+                        pinstaller.addExecutionEngine(loc);
                     }
                 }
                 if (autoSave) {
@@ -76,15 +81,15 @@ public class NSettingsJavaSubCommand extends AbstractNSettingsSubCommand {
         } else if (cmdLine.next("remove java","java remove").isPresent()) {
             while (cmdLine.hasNext()) {
                 String name = cmdLine.next().get().image();
-                NPlatformLocation loc = workspace.findPlatformByName(NPlatformFamily.JAVA, name).orNull();
+                NExecutionEngineLocation loc = pinstaller.findExecutionEngineByName(NExecutionEngineFamily.JAVA, name).orNull();
                 if (loc == null) {
-                    loc = workspace.findPlatformByName(NPlatformFamily.JAVA, name).orNull();
+                    loc = pinstaller.findExecutionEngineByName(NExecutionEngineFamily.JAVA, name).orNull();
                     if (loc == null) {
-                        loc = workspace.findPlatformByVersion(NPlatformFamily.JAVA, name).orNull();
+                        loc = pinstaller.findExecutionEngineByVersion(NExecutionEngineFamily.JAVA, name).orNull();
                     }
                 }
                 if (loc != null) {
-                    workspace.removePlatform(loc);
+                    pinstaller.removeExecutionEngine(loc);
                 }
             }
             if (autoSave) {
@@ -104,10 +109,10 @@ public class NSettingsJavaSubCommand extends AbstractNSettingsSubCommand {
                 //}
             }
             if (cmdLine.isExecMode()) {
-                NPlatformLocation[] sdks = workspace.findPlatforms(NPlatformFamily.JAVA, null).toArray(NPlatformLocation[]::new);
-                Arrays.sort(sdks, new Comparator<NPlatformLocation>() {
+                NExecutionEngineLocation[] sdks = pinstaller.findExecutionEngines(NExecutionEngineFamily.JAVA, null).toArray(NExecutionEngineLocation[]::new);
+                Arrays.sort(sdks, new Comparator<NExecutionEngineLocation>() {
                     @Override
-                    public int compare(NPlatformLocation o1, NPlatformLocation o2) {
+                    public int compare(NExecutionEngineLocation o1, NExecutionEngineLocation o2) {
                         int x = o1.getName().compareTo(o2.getName());
                         if (x != 0) {
                             return x;
@@ -123,7 +128,7 @@ public class NSettingsJavaSubCommand extends AbstractNSettingsSubCommand {
                         return x;
                     }
                 });
-                for (NPlatformLocation jloc : sdks) {
+                for (NExecutionEngineLocation jloc : sdks) {
                     m.addRow(NText.of(jloc.getName()), NText.of(jloc.getVersion()), NText.of(jloc.getPath()));
                 }
                 out.print(NTextArt.of().getTableRenderer().get().render(m));
