a16da9d3b69b2d6727f30c72842645106ae5f29b
thevpc <taha.bensalah@gmail.com>
2025-12-21 23:20:02 +0100

    Update NInternalCommand.java : faster-bean-factory

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/workspace/cmd/exec/local/internal/NInternalCommand.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/workspace/cmd/exec/local/internal/NInternalCommand.java
@@ -1,10 +1,10 @@
 package net.thevpc.nuts.runtime.standalone.workspace.cmd.exec.local.internal;
 
-import net.thevpc.nuts.command.NExecCmd;
+import net.thevpc.nuts.command.NExec;
 import net.thevpc.nuts.spi.NComponent;
 
 public interface NInternalCommand extends NComponent {
     String getName();
 
-    int execute(String[] args, NExecCmd execCommand);
+    int execute(String[] args, NExec execCommand);
 }
