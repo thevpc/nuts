54fffad8340da7e114dfe7b97194cdc031790f53
thevpc <taha.bensalah@gmail.com>
2025-12-21 23:20:20 +0100

    Update DefaultNUpdateStats.java : faster-bean-factory

---
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/workspace/cmd/settings/updatestats/DefaultNUpdateStats.java
@@ -0,0 +1,113 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.thevpc.nuts.runtime.standalone.workspace.cmd.settings.updatestats;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
+import net.thevpc.nuts.command.NUpdateStats;
+import net.thevpc.nuts.core.NSession;
+import net.thevpc.nuts.core.NWorkspace;
+import net.thevpc.nuts.io.NOut;
+import net.thevpc.nuts.io.NPath;
+import net.thevpc.nuts.core.NRepository;
+import net.thevpc.nuts.runtime.standalone.repository.impl.NRepositoryFolderHelper;
+import net.thevpc.nuts.runtime.standalone.repository.impl.maven.util.MavenRepositoryFolderHelper;
+import net.thevpc.nuts.runtime.standalone.workspace.NWorkspaceUtils;
+import net.thevpc.nuts.spi.NRepositorySPI;
+import net.thevpc.nuts.util.NAssert;
+import net.thevpc.nuts.util.NScore;
+import net.thevpc.nuts.util.NIllegalArgumentException;
+import net.thevpc.nuts.text.NMsg;
+import net.thevpc.nuts.util.NScorable;
+
+/**
+ * @author thevpc
+ */
+@NScore(fixed = NScorable.DEFAULT_SCORE)
+public class DefaultNUpdateStats extends AbstractNUpdateStats {
+
+    public DefaultNUpdateStats() {
+        super();
+    }
+
+    @Override
+    public NUpdateStats run() {
+        boolean processed = false;
+        NSession session=NSession.of();
+        for (String repository : getRepositories()) {
+            processed = true;
+            NRepository repo = NWorkspace.of().findRepository(repository).get();
+            NRepositorySPI repoSPI = NWorkspaceUtils.of().toRepositorySPI(repo);
+            repoSPI.updateStatistics()
+                    //                    .setFetchMode(NutsFetchMode.LOCAL)
+                    .run();
+        }
+        for (Path repositoryPath : getPaths()) {
+            processed = true;
+            NAssert.requireNonBlank(repositoryPath, "location");
+            if (!Files.isDirectory(repositoryPath)) {
+                throw new NIllegalArgumentException(NMsg.ofC("expected folder at location %s",repositoryPath));
+            }
+            File[] mavenRepoRootFiles = repositoryPath.toFile().listFiles(x
+                    -> x.getName().equals("index.html")
+                    || x.getName().equals("plugin-management.html")
+                    || x.getName().equals("distribution-management.html")
+                    || x.getName().equals("dependency-info.html")
+                    || x.getName().equals("dependency-convergence.html")
+                    || x.getName().equals("dependencies.html")
+                    || x.getName().equals("plugins.html")
+                    || x.getName().equals("project-info.html")
+                    || x.getName().equals("project-summary.html")
+            );
+            if (mavenRepoRootFiles != null && mavenRepoRootFiles.length > 3) {
+                new MavenRepositoryFolderHelper(null, NPath.of(repositoryPath)).reindexFolder();
+                if (session.isPlainTrace()) {
+                    session.getTerminal().out().resetLine().println(NMsg.ofC("[%s] updated maven index %s", NWorkspace.of().getWorkspaceLocation(), repositoryPath));
+                }
+            } else {
+                File[] nutsRepoRootFiles = repositoryPath.toFile().listFiles(x
+                        -> x.getName().equals("nuts-repository.json")
+                );
+                if (nutsRepoRootFiles != null && nutsRepoRootFiles.length > 0) {
+                    new NRepositoryFolderHelper(null, NPath.of(repositoryPath), false,"stats",null).reindexFolder();
+                } else {
+                    throw new NIllegalArgumentException(NMsg.ofPlain("unsupported repository folder"));
+                }
+                if (session.isPlainTrace()) {
+                    NOut.resetLine().println(NMsg.ofC("[%s] updated stats %s", NWorkspace.of().getWorkspaceLocation(), repositoryPath));
+                }
+            }
+        }
+        if (!processed) {
+            if (session.isPlainTrace()) {
+                NOut.resetLine().println(NMsg.ofC("%s updating workspace stats", NWorkspace.of().getWorkspaceLocation()));
+            }
+            for (NRepository repo : NWorkspace.of().getRepositories()) {
+                if (session.isPlainTrace()) {
+                    NOut.resetLine().println(NMsg.ofC("%s updating stats %s", NWorkspace.of().getWorkspaceLocation(), repo));
+                }
+                NWorkspaceUtils.of().toRepositorySPI(repo).updateStatistics()
+                        //                        .setFetchMode(NutsFetchMode.LOCAL)
+                        .run();
+            }
+        }
+        return this;
+    }
+
+    @Override
+    public void add(String repo) {
+        NAssert.requireNonBlank(repo, "repository or path");
+        if (repo.equals(".") || repo.equals("..") || repo.contains("/") || repo.contains("\\")) {
+            addPath(Paths.get(repo));
+        } else {
+            addRepo(repo);
+        }
+    }
+}
