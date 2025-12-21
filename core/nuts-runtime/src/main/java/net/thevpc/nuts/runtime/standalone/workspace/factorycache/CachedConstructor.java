68174a46f977b2f1ade32c8fb8d7f5d6d90eb4c4
thevpc <taha.bensalah@gmail.com>
2025-12-21 23:20:05 +0100

    Update CachedConstructor.java : faster-bean-factory

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/workspace/factorycache/CachedConstructor.java
+++
@@ -1,7 +0,0 @@
-package net.thevpc.nuts.runtime.standalone.workspace.factorycache;
-
-import net.thevpc.nuts.core.NSession;
-
-public interface CachedConstructor<T> {
-    T newInstance(Object[] args,NSession session);
-}
