ad887d003e49f255c72b7c96b442106f931a0dd4
thevpc <taha.bensalah@gmail.com>
2025-12-21 23:19:58 +0100

    Update MethodBasedNScorable.java : faster-bean-factory

---
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/extension/MethodBasedNScorable.java
@@ -0,0 +1,28 @@
+package net.thevpc.nuts.runtime.standalone.extension;
+
+import net.thevpc.nuts.text.NMsg;
+import net.thevpc.nuts.util.NScorable;
+import net.thevpc.nuts.util.NScorableContext;
+
+import java.lang.reflect.Method;
+
+class MethodBasedNScorable implements NScorable {
+    private final NExtensionTypeInfo nExtensionTypeInfo;
+    private final Method declaredMethod;
+
+    public MethodBasedNScorable(NExtensionTypeInfo nExtensionTypeInfo, Method declaredMethod) {
+        this.nExtensionTypeInfo = nExtensionTypeInfo;
+        this.declaredMethod = declaredMethod;
+        declaredMethod.setAccessible(true);
+    }
+
+    @Override
+    public int getScore(NScorableContext context) {
+        try {
+            return (int) declaredMethod.invoke(null, context);
+        } catch (Exception e) {
+            nExtensionTypeInfo.LOG().log(NMsg.ofC("[%S] [%s] error invoking score method %s ", nExtensionTypeInfo.getImplType(), nExtensionTypeInfo.getApiType(), declaredMethod).asSevere());
+        }
+        return NScorable.UNSUPPORTED_SCORE;
+    }
+}
