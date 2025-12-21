377d0065974d2eb9a981db1d6b77e76755caf9f3
thevpc <taha.bensalah@gmail.com>
2025-12-21 23:20:01 +0100

    Update NUtilSPIImpl.java : faster-bean-factory

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/util/NUtilSPIImpl.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/util/NUtilSPIImpl.java
@@ -1,20 +1,17 @@
 package net.thevpc.nuts.runtime.standalone.util;
 
 import net.thevpc.nuts.spi.*;
+import net.thevpc.nuts.util.NScore;
 import net.thevpc.nuts.util.NScorable;
 import net.thevpc.nuts.util.NScorableContext;
 import net.thevpc.nuts.util.NScorableQuery;
 
 @NComponentScope(NScopeType.WORKSPACE)
+@NScore(fixed = NScorable.DEFAULT_SCORE)
 public class NUtilSPIImpl implements NUtilSPI {
     @Override
     public <T extends NScorable> NScorableQuery<T> ofScorableQuery() {
         return new NScorableNScorableQueryImpl<>(NScorableContext.of());
-    }
-
-    @Override
-    public int getScore(NScorableContext context) {
-        return DEFAULT_SCORE;
     }
 
 }
