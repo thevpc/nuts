dc83f3dc3d9ef59ff206e8b326e2cac3f8883d36
thevpc <taha.bensalah@gmail.com>
2025-12-21 23:20:20 +0100

    Update NScoredValue.java : faster-bean-factory

---
+++ core/nuts-api/src/main/java/net/thevpc/nuts/util/NScoredValue.java
@@ -0,0 +1,34 @@
+package net.thevpc.nuts.util;
+
+/**
+ * Represents the result of evaluating a {@link NScorable} instance.
+ * <p>
+ * Each result couples a scorable instance with its computed score and the
+ * context in which the score was calculated.
+ * </p>
+ *
+ * @param <T> the type of the scorable instance
+ */
+public interface NScoredValue<T> {
+    /**
+     * Returns the scorable instance that was evaluated.
+     *
+     * @return the scorable instance
+     */
+    T value();
+
+    /**
+     * Returns the score computed for the scorable instance.
+     * <p>
+     * A valid score is greater than zero. A score less than or equal to zero
+     * indicates that the instance should not be considered.
+     * </p>
+     *
+     * @return the score of the scorable instance
+     */
+    int score();
+
+    default boolean isValid() {
+        return score() > 0;
+    }
+}
