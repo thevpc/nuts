c203d100a7481d61998b58a909ab991e74d74046
thevpc <taha.bensalah@gmail.com>
2025-12-21 23:19:57 +0100

    Update GradleFirstNDependencySolverFactory.java : faster-bean-factory

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/dependency/solver/GradleFirstNDependencySolverFactory.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/dependency/solver/GradleFirstNDependencySolverFactory.java
@@ -1,17 +1,14 @@
 package net.thevpc.nuts.runtime.standalone.dependency.solver;
 
-import net.thevpc.nuts.util.NScorableContext;
+import net.thevpc.nuts.util.NScore;
+import net.thevpc.nuts.util.NScorable;
 import net.thevpc.nuts.spi.NDependencySolver;
 import net.thevpc.nuts.spi.NDependencySolverFactory;
 
+@NScore(fixed = NScorable.DEFAULT_SCORE)
 public class GradleFirstNDependencySolverFactory implements NDependencySolverFactory {
 
     public GradleFirstNDependencySolverFactory() {
-    }
-
-    @Override
-    public int getScore(NScorableContext context) {
-        return 1;
     }
 
     @Override
