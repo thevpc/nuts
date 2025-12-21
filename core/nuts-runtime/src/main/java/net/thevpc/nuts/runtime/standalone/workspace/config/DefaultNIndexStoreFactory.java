bd4ee16bb8cabb39c998fb8e7d49792b8f2af336
thevpc <taha.bensalah@gmail.com>
2025-12-21 23:20:04 +0100

    Update DefaultNIndexStoreFactory.java : faster-bean-factory

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/workspace/config/DefaultNIndexStoreFactory.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/workspace/config/DefaultNIndexStoreFactory.java
@@ -29,8 +29,10 @@ package net.thevpc.nuts.runtime.standalone.workspace.config;
 import net.thevpc.nuts.core.NIndexStore;
 import net.thevpc.nuts.spi.NIndexStoreFactory;
 import net.thevpc.nuts.core.NRepository;
-import net.thevpc.nuts.util.NScorableContext;
+import net.thevpc.nuts.util.NScore;
+import net.thevpc.nuts.util.NScorable;
 
+@NScore(fixed = NScorable.DEFAULT_SCORE)
 public class DefaultNIndexStoreFactory implements NIndexStoreFactory {
 
     @Override
@@ -38,8 +40,4 @@ public class DefaultNIndexStoreFactory implements NIndexStoreFactory {
         return new DefaultNIndexStore(repository);
     }
 
-    @Override
-    public int getScore(NScorableContext criteria) {
-        return DEFAULT_SCORE;
-    }
 }
