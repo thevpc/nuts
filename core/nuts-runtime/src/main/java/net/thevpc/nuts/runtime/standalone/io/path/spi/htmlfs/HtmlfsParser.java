15e3e1b0efa5ec402eb44b699d445b5fe1c5bac1
thevpc <taha.bensalah@gmail.com>
2025-12-21 23:19:59 +0100

    Update HtmlfsParser.java : faster-bean-factory

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/io/path/spi/htmlfs/HtmlfsParser.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/io/path/spi/htmlfs/HtmlfsParser.java
@@ -1,6 +1,7 @@
 package net.thevpc.nuts.runtime.standalone.io.path.spi.htmlfs;
 
 import net.thevpc.nuts.concurrent.NScoredCallable;
+import net.thevpc.nuts.io.NPathInfo;
 
 import java.util.List;
 
