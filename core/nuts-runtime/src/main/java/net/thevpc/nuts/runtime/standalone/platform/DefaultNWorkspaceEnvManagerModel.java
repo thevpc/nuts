cb60173346dfb563b101b680870548e071bf652d
thevpc <taha.bensalah@gmail.com>
2025-12-21 23:20:20 +0100

    Update DefaultNWorkspaceEnvManagerModel.java : faster-bean-factory

---
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/platform/DefaultNWorkspaceEnvManagerModel.java
@@ -0,0 +1,54 @@
+///**
+// * ====================================================================
+// * Nuts : Network Updatable Things Service
+// * (universal package manager)
+// * <br>
+// * is a new Open Source Package Manager to help install packages and libraries
+// * for runtime execution. Nuts is the ultimate companion for maven (and other
+// * build managers) as it helps installing all package dependencies at runtime.
+// * Nuts is not tied to java and is a good choice to share shell scripts and
+// * other 'things' . It's based on an extensible architecture to help supporting a
+// * large range of sub managers / repositories.
+// * <br>
+// * <p>
+// * Copyright [2020] [thevpc]
+// * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE Version 3 (the "License");
+// * you may  not use this file except in compliance with the License. You may obtain
+// * a copy of the License at https://www.gnu.org/licenses/lgpl-3.0.en.html
+// * Unless required by applicable law or agreed to in writing, software
+// * distributed under the License is distributed on an
+// * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// * either express or implied. See the License for the specific language
+// * governing permissions and limitations under the License.
+// * <br> ====================================================================
+// */
+//package net.thevpc.nuts.runtime.standalone.platform;
+//
+//import net.thevpc.nuts.core.NWorkspace;
+//import net.thevpc.nuts.ext.NExtensions;
+//import net.thevpc.nuts.platform.*;
+//import net.thevpc.nuts.util.NDefaultObservableMap;
+//import net.thevpc.nuts.util.NObservableMap;
+//import net.thevpc.nuts.util.*;
+//
+//import java.lang.management.ManagementFactory;
+//import java.util.*;
+//import java.util.function.Supplier;
+//
+///**
+// * @author thevpc
+// */
+//public class DefaultNWorkspaceEnvManagerModel {
+//
+//    private final Map<NExecutionEngineFamily, List<NExecutionEngineLocation>> configPlatforms = new LinkedHashMap<>();
+//    //    private Map<String, String> options = new LinkedHashMap<>();
+//    private final NWorkspace workspace;
+//
+//    public DefaultNWorkspaceEnvManagerModel(NWorkspace ws) {
+//        this.workspace = ws;
+//    }
+//
+//
+//
+//
+//}
