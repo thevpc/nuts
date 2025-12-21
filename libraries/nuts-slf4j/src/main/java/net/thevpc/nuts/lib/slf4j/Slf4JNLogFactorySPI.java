87dd4f2e968e753e03ec8912981243f4b464738a
thevpc <taha.bensalah@gmail.com>
2025-12-21 23:20:06 +0100

    Update Slf4JNLogFactorySPI.java : faster-bean-factory

--- libraries/nuts-slf4j/src/main/java/net/thevpc/nuts/lib/slf4j/Slf4JNLogFactorySPI.java
+++ libraries/nuts-slf4j/src/main/java/net/thevpc/nuts/lib/slf4j/Slf4JNLogFactorySPI.java
@@ -2,16 +2,14 @@ package net.thevpc.nuts.lib.slf4j;
 
 import net.thevpc.nuts.log.NLogFactorySPI;
 import net.thevpc.nuts.log.NLogSPI;
-import net.thevpc.nuts.util.NScorableContext;
+import net.thevpc.nuts.util.NScore;
+import net.thevpc.nuts.util.NScorable;
 
+@NScore(fixed = NScorable.CUSTOM_SCORE)
 public class Slf4JNLogFactorySPI implements NLogFactorySPI {
     @Override
     public NLogSPI getLogSPI(String name) {
         return new Slf4JNLogSPI(name);
     }
 
-    @Override
-    public int getScore(NScorableContext context) {
-        return CUSTOM_SCORE;
-    }
 }
