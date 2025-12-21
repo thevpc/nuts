d376dec1469afc7499890b68fffb52f6402ad7fc
thevpc <taha.bensalah@gmail.com>
2025-12-21 23:19:56 +0100

    Update NExecTargetSPI.java : faster-bean-factory

--- core/nuts-api/src/main/java/net/thevpc/nuts/spi/NExecTargetSPI.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/spi/NExecTargetSPI.java
@@ -1,9 +1,5 @@
 package net.thevpc.nuts.spi;
 
-import net.thevpc.nuts.command.NExecTargetInfo;
-
 public interface NExecTargetSPI extends NComponent {
     int exec(NExecTargetCommandContext context);
-
-    NExecTargetInfo getTargetInfo(NExecTargetInfoContext context);
 }
