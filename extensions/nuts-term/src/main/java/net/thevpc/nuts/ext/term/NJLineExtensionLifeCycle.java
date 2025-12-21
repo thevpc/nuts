a635a016d88310b78a7c876b8ce13013f153b474
thevpc <taha.bensalah@gmail.com>
2025-12-21 23:20:06 +0100

    Update NJLineExtensionLifeCycle.java : faster-bean-factory

--- extensions/nuts-term/src/main/java/net/thevpc/nuts/ext/term/NJLineExtensionLifeCycle.java
+++ extensions/nuts-term/src/main/java/net/thevpc/nuts/ext/term/NJLineExtensionLifeCycle.java
@@ -3,17 +3,14 @@ package net.thevpc.nuts.ext.term;
 import net.thevpc.nuts.core.NWorkspaceExtension;
 import net.thevpc.nuts.io.NIO;
 import net.thevpc.nuts.spi.NExtensionLifeCycle;
-import net.thevpc.nuts.util.NScorableContext;
+import net.thevpc.nuts.util.NScore;
+import net.thevpc.nuts.util.NScorable;
 import net.thevpc.nuts.util.NUnused;
 
 @NUnused
+@NScore(fixed = NScorable.DEFAULT_SCORE)
 public class NJLineExtensionLifeCycle implements NExtensionLifeCycle {
     public NJLineExtensionLifeCycle() {
-    }
-
-    @Override
-    public int getScore(NScorableContext context) {
-        return DEFAULT_SCORE;
     }
 
     @Override
