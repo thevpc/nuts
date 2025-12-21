4b42c7d6842c9ff4286977d85ce62b6dce4ae0ee
thevpc <taha.bensalah@gmail.com>
2025-12-21 23:20:19 +0100

    Update NExecutionEngineLocation.java : faster-bean-factory

---
+++ core/nuts-api/src/main/java/net/thevpc/nuts/platform/NExecutionEngineLocation.java
@@ -0,0 +1,195 @@
+/**
+ * ====================================================================
+ * Nuts : Network Updatable Things Service
+ * (universal package manager)
+ * <br>
+ * is a new Open Source Package Manager to help install packages
+ * and libraries for runtime execution. Nuts is the ultimate companion for
+ * maven (and other build managers) as it helps installing all package
+ * dependencies at runtime. Nuts is not tied to java and is a good choice
+ * to share shell scripts and other 'things' . Its based on an extensible
+ * architecture to help supporting a large range of sub managers / repositories.
+ * <br>
+ * <p>
+ * Copyright [2020] [thevpc]
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE Version 3 (the "License");
+ * you may  not use this file except in compliance with the License. You may obtain
+ * a copy of the License at https://www.gnu.org/licenses/lgpl-3.0.en.html
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ * <br>
+ * ====================================================================
+ */
+package net.thevpc.nuts.platform;
+
+import net.thevpc.nuts.core.NConfigItem;
+import net.thevpc.nuts.artifact.NId;
+import net.thevpc.nuts.util.NBlankable;
+
+import java.util.Objects;
+
+/**
+ * SDK location
+ *
+ * @author thevpc
+ * @app.category Config
+ * @since 0.5.4
+ */
+public class NExecutionEngineLocation extends NConfigItem implements Cloneable {
+    public static final String JAVA_PRODUCT_JDK="jdk";
+    public static final String JAVA_PRODUCT_JRE="jre";
+    public static final long serialVersionUID = 3;
+    private final NId id;
+    private final NExecutionEngineFamily executionEngineFamily;
+    private final String name;
+    private final String variant;
+    private final String vendor;
+    private final String packaging;
+    private final String product;
+    private final String path;
+    private final String version;
+    private final int priority;
+
+    /**
+     * default constructor
+     *
+     * @param id        id
+     * @param vendor    SDK oracle/openjdk
+     * @param product   SDK product. In java this is Oracle JDK or OpenJDK.
+     * @param variant   hotspot/graalvm
+     * @param packaging SDK packaging. for Java SDK this is room to set JRE or JDK.
+     * @param path      SDK path
+     * @param name      SDK name
+     * @param version   SDK version
+     * @param priority  SDK priority
+     */
+    public NExecutionEngineLocation(NId id, String vendor, String product, String variant, String name, String path, String version, String packaging, int priority) {
+        this.id = id;
+        this.executionEngineFamily = (id == null || NBlankable.isBlank(id.getArtifactId())) ? NExecutionEngineFamily.JAVA :
+                NExecutionEngineFamily.parse(id.getArtifactId()).orElse(NExecutionEngineFamily.UNKNOWN);
+        this.product = product;
+        this.variant = variant;
+        this.vendor = vendor;
+        this.name = name;
+        this.path = path;
+        this.version = version;
+        this.packaging = packaging;
+        this.priority = priority;
+    }
+
+    public String getVariant() {
+        return variant;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public NExecutionEngineLocation setPriority(int priority) {
+        return new NExecutionEngineLocation(id, vendor,product, variant,name, path, version, packaging, priority);
+    }
+
+    public NId getId() {
+        return id;
+    }
+
+    public NExecutionEngineFamily getExecutionEngineFamily() {
+        return executionEngineFamily;
+    }
+
+    /**
+     * SDK product. In java this is
+     * Oracle JDK or OpenJDK.
+     *
+     * @return product name
+     */
+    public String getProduct() {
+        return product;
+    }
+
+    /**
+     * SDK version
+     *
+     * @return SDK version
+     */
+    public String getVersion() {
+        return version;
+    }
+
+    /**
+     * sdk name
+     *
+     * @return sdk name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * sdk path
+     *
+     * @return sdk path
+     */
+    public String getPath() {
+        return path;
+    }
+
+    /**
+     * sdk packaging. for Java SDK this
+     * is room to set JRE or JDK.
+     *
+     * @return packaging name
+     */
+    public String getPackaging() {
+        return packaging;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, packaging, product, path, version, priority);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        NExecutionEngineLocation that = (NExecutionEngineLocation) o;
+        return Objects.equals(id, that.id) &&
+                Objects.equals(name, that.name) &&
+                Objects.equals(packaging, that.packaging) &&
+                Objects.equals(product, that.product) &&
+                Objects.equals(path, that.path) &&
+                Objects.equals(version, that.version) &&
+                Objects.equals(priority, that.priority)
+                ;
+    }
+
+    @Override
+    public String toString() {
+        return "NutsPlatformLocation{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", packaging='" + packaging + '\'' +
+                ", product='" + product + '\'' +
+                ", path='" + path + '\'' +
+                ", version='" + version + '\'' +
+                ", priority=" + priority +
+                '}';
+    }
+
+    public NExecutionEngineLocation copy() {
+        try {
+            NExecutionEngineLocation cloned = (NExecutionEngineLocation) clone();
+            return cloned;
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
