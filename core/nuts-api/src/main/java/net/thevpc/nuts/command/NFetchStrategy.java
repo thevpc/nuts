93f018f56bb19564265ee7c04dc29889d63698d0
thevpc <taha.bensalah@gmail.com>
2025-12-29 00:20:40 +0100

    [ADDED] added accept

--- core/nuts-api/src/main/java/net/thevpc/nuts/command/NFetchStrategy.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/command/NFetchStrategy.java
@@ -13,13 +13,13 @@
  * <br>
  * <p>
  * Copyright [2020] [thevpc]
- * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE Version 3 (the "License"); 
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE Version 3 (the "License");
  * you may  not use this file except in compliance with the License. You may obtain
  * a copy of the License at https://www.gnu.org/licenses/lgpl-3.0.en.html
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an 
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
- * either express or implied. See the License for the specific language 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  * <br>
  * ====================================================================
@@ -134,5 +134,9 @@ public enum NFetchStrategy implements Iterable<NFetchMode>, NEnum {
     @Override
     public Iterator<NFetchMode> iterator() {
         return Arrays.asList(all).iterator();
+    }
+
+    public boolean accept(NFetchMode mode) {
+        return Arrays.asList(all).contains(mode);
     }
 }
