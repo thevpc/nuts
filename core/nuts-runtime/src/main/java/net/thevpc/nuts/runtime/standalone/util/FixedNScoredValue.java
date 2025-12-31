6c458e3d9ad305cd554376c282854e180934163a
thevpc <taha.bensalah@gmail.com>
2025-12-31 20:30:56 +0100

    Update FixedNScoredValue.java : refactor-nops

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/util/FixedNScoredValue.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/util/FixedNScoredValue.java
@@ -1,13 +1,18 @@
 package net.thevpc.nuts.runtime.standalone.util;
 
+import net.thevpc.nuts.text.NMsg;
+import net.thevpc.nuts.util.NIllegalStateException;
 import net.thevpc.nuts.util.NScorable;
 import net.thevpc.nuts.util.NScoredValue;
 
 public class FixedNScoredValue<T> implements NScoredValue<T> {
     T value;
     int score;
-    public static final NScoredValue UNSUPPORTED=new FixedNScoredValue(null, NScorable.UNSUPPORTED_SCORE);
-    public static <T> NScoredValue<T> UNSUPPORTED(){return UNSUPPORTED;};
+    public static final NScoredValue UNSUPPORTED = new FixedNScoredValue(null, NScorable.UNSUPPORTED_SCORE);
+
+    public static <T> NScoredValue<T> ofUnsupported(Class<? extends T> implType, Class<T> apiType) {
+        return UNSUPPORTED;
+    }
 
     public FixedNScoredValue(T value, int score) {
         this.value = value;
@@ -15,7 +20,20 @@ public class FixedNScoredValue<T> implements NScoredValue<T> {
     }
 
     @Override
+    public Class<T> getApiType() {
+        return null;
+    }
+
+    @Override
+    public Class<? extends T> getImplType() {
+        return null;
+    }
+
+    @Override
     public T value() {
+        if (score <= 0) {
+            throw new NIllegalStateException(NMsg.ofC("an unsupported value could not be instantiated"));
+        }
         return value;
     }
 
