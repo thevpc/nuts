2c6e4dc5b178cea71f9ad0ce329d11a5d5076f89
thevpc <taha.bensalah@gmail.com>
2025-12-31 20:30:56 +0100

    Update NExtensions.java : refactor-nops

--- core/nuts-api/src/main/java/net/thevpc/nuts/ext/NExtensions.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/ext/NExtensions.java
@@ -43,11 +43,11 @@ import java.util.Set;
  * @since 0.5.4
  */
 public interface NExtensions extends NComponent {
-    static <T extends NComponent> T of(Class<T> type) {
+    static <T> T of(Class<T> type) {
         return of().createComponent(type).get();
     }
 
-    static <T extends NComponent> NOptional<T> get(Class<T> type) {
+    static <T> NOptional<T> get(Class<T> type) {
         return get().flatMap(x -> x.createComponent(type));
     }
 
@@ -65,20 +65,20 @@ public interface NExtensions extends NComponent {
 
     Set<Class<?>> discoverTypes(NId id, ClassLoader classLoader);
 
-    <T extends NComponent, B> NServiceLoader<T> createServiceLoader(Class<T> serviceType, Class<B> criteriaType);
+    <T, B> NServiceLoader<T> createServiceLoader(Class<T> serviceType, Class<B> criteriaType);
 
-    <T extends NComponent, B> NServiceLoader<T> createServiceLoader(Class<T> serviceType, Class<B> criteriaType, ClassLoader classLoader);
+    <T, B> NServiceLoader<T> createServiceLoader(Class<T> serviceType, Class<B> criteriaType, ClassLoader classLoader);
 
 
     NMutableClassLoader createMutableClassLoader(ClassLoader parentClassLoader);
 
-    <T extends NComponent> NScoredValue<T> getTypeScoredValue(Class<? extends T> implType, Class<T> apiType, NScorableContext scorableContext);
+    <T> NScoredValue<T> getTypeScoredValue(Class<? extends T> implType, Class<T> apiType, NScorableContext scorableContext);
 
-    <T extends NComponent> NScoredValue<T> getInstanceScoredValue(T instance, Class<T> apiType, NScorableContext scorableContext);
+    <T> NScoredValue<T> getInstanceScoredValue(T instance, Class<T> apiType, NScorableContext scorableContext);
 
-    <T extends NComponent> NOptional<NScorable> getTypeScorable(Class<? extends T> implType, Class<T> apiType);
+    <T> NOptional<NScorable> getTypeScorable(Class<? extends T> implType, Class<T> apiType);
 
-    <T extends NComponent> NOptional<NScorable> getInstanceScorable(T instance, Class<T> apiType);
+    <T> NOptional<NScorable> getInstanceScorable(T instance, Class<T> apiType);
 
     /**
      * create supported extension implementation or return null.
@@ -87,7 +87,7 @@ public interface NExtensions extends NComponent {
      * @param type extension type
      * @return valid instance or null if no extension implementation was found
      */
-    <T extends NComponent> NOptional<T> createComponent(Class<T> type);
+    <T> NOptional<T> createComponent(Class<T> type);
 
     /**
      * create supported extension implementation or return null.
@@ -98,28 +98,28 @@ public interface NExtensions extends NComponent {
      * @param supportCriteria context
      * @return valid instance or null if no extension implementation was found
      */
-    <T extends NComponent, V> NOptional<T> createSupported(Class<T> type, V supportCriteria);
+    <T, V> NOptional<T> createSupported(Class<T> type, V supportCriteria);
 
-    <T extends NComponent, V> List<T> createAllSupported(Class<T> type, V supportCriteria);
+    <T, V> List<T> createAllSupported(Class<T> type, V supportCriteria);
 
-    <T extends NComponent> List<T> createAll(Class<T> type);
+    <T> List<T> createAll(Class<T> type);
 
 //    Set<Class> getExtensionPoints(NSession session);
 
 
-    <T extends NComponent> Set<Class<? extends T>> getExtensionTypes(Class<T> extensionPoint);
+    <T> Set<Class<? extends T>> getExtensionTypes(Class<T> extensionPoint);
 
-    <T extends NComponent> List<T> getExtensionObjects(Class<T> extensionPoint);
+    <T> List<T> getExtensionObjects(Class<T> extensionPoint);
 
-    <T extends NComponent> boolean isRegisteredType(Class<T> extensionPointType, String name);
+    <T> boolean isRegisteredType(Class<T> extensionPointType, String name);
 
-    <T extends NComponent> boolean isRegisteredInstance(Class<T> extensionPointType, T extensionImpl);
+    <T> boolean isRegisteredInstance(Class<T> extensionPointType, T extensionImpl);
 
-    <T extends NComponent> boolean registerInstance(Class<T> extensionPoint, T implementation);
+    <T> boolean registerInstance(Class<T> extensionPoint, T implementation);
 
-    <T extends NComponent> boolean registerType(Class<T> extensionPointType, Class<? extends T> implementation, NId source);
+    <T> boolean registerType(Class<T> extensionPointType, Class<? extends T> implementation, NId source);
 
-    <T extends NComponent> boolean isRegisteredType(Class<T> extensionPointType, Class<? extends T> implementation);
+    <T> boolean isRegisteredType(Class<T> extensionPointType, Class<? extends T> implementation);
 
     boolean isLoadedId(NId id);
 
