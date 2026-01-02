bb26e029c8fc3d7db8bc338bdd2c1bfa8700abac
thevpc <taha.bensalah@gmail.com>
2026-01-02 16:24:09 +0100

    Update NDefaultsTo.java : refactor-NFormat

---
+++ core/nuts-api/src/main/java/net/thevpc/nuts/reflect/NDefaultsTo.java
@@ -0,0 +1,12 @@
+package net.thevpc.nuts.reflect;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface NDefaultsTo {
+    String value();
+}
