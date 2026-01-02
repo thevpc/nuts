57af141e370e9be7ddbf47976a915df42ce265f1
thevpc <taha.bensalah@gmail.com>
2026-01-02 16:24:09 +0100

    Update NUtils.java : refactor-NFormat

--- core/nuts-api/src/main/java/net/thevpc/nuts/util/NUtils.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/util/NUtils.java
@@ -2,6 +2,8 @@ package net.thevpc.nuts.util;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class NUtils {
 
@@ -11,6 +13,46 @@ public class NUtils {
         }
         if (b != null) {
             return b;
+        }
+        return null;
+    }
+
+    public static <T> T firstNonNullLazy(T first, Supplier<T>... suppliers) {
+        if (first != null) {
+            return first;
+        }
+        if (suppliers != null) {
+            for (Supplier<T> supplier : suppliers) {
+                if (supplier != null) {
+                    T v = supplier.get();
+                    if (v != null) {
+                        return v;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    public static <T> T firstMatchingLazy(Predicate<? super T> filter, T first, Supplier<? extends T>... suppliers) {
+        NAssert.requireNonNull(filter, "filter");
+        if (filter.test(first)) {
+            return first;
+        }
+        return firstMatchingLazy(filter,suppliers);
+    }
+
+    public static <T> T firstMatchingLazy(Predicate<? super T> filter, Supplier<? extends T>... suppliers) {
+        NAssert.requireNonNull(filter, "filter");
+        if (suppliers != null) {
+            for (Supplier<? extends T> supplier : suppliers) {
+                if (supplier != null) {
+                    T v = supplier.get();
+                    if (filter.test(v)) {
+                        return v;
+                    }
+                }
+            }
         }
         return null;
     }
@@ -30,18 +72,18 @@ public class NUtils {
         return null;
     }
 
-    public static int compareObjects(Object a,Object b){
+    public static int compareObjects(Object a, Object b) {
         if (a == null && b == null) {
             return 0;
         }
-        if (a == null ) {
+        if (a == null) {
             return -1;
         }
-        if (b == null ) {
+        if (b == null) {
             return 1;
         }
-        if(a instanceof  Number && b instanceof Number){
-            return NNumberUtils.compareNumbers((Number)a,(Number)b);
+        if (a instanceof Number && b instanceof Number) {
+            return NNumberUtils.compareNumbers((Number) a, (Number) b);
         }
         if (a instanceof Comparable && b instanceof Comparable) {
             try {
