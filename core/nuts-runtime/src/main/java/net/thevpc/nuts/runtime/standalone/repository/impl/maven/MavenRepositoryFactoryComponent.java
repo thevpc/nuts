5ec66833bd7e3366b96d66462e4cd4f737f8c0c8
thevpc <taha.bensalah@gmail.com>
2026-01-02 18:51:09 +0100

    Update MavenRepositoryFactoryComponent.java : refactor-element-reader-writer

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/repository/impl/maven/MavenRepositoryFactoryComponent.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/repository/impl/maven/MavenRepositoryFactoryComponent.java
@@ -26,7 +26,7 @@ package net.thevpc.nuts.runtime.standalone.repository.impl.maven;
 
 import net.thevpc.nuts.core.NConstants;
 import net.thevpc.nuts.elem.NElement;
-import net.thevpc.nuts.elem.NElementParser;
+import net.thevpc.nuts.elem.NElementReader;
 import net.thevpc.nuts.elem.NObjectElement;
 import net.thevpc.nuts.io.NPath;
 import net.thevpc.nuts.core.NAddRepositoryOptions;
@@ -83,7 +83,7 @@ public class MavenRepositoryFactoryComponent implements NRepositoryFactoryCompon
                     String repositoryName = null;
                     String repositoryLayout = null;
                     try {
-                        e = NElementParser.ofJson().parse(nr);
+                        e = NElementReader.ofJson().read(nr);
                     } catch (Exception ex) {
                         // just ignore
                     }
