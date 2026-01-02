def2ee262021387b3cbbfcf1b864d139b5941627
thevpc <taha.bensalah@gmail.com>
2026-01-02 16:24:09 +0100

    Update NFormatAndValue.java : refactor-NFormat

---
+++ core/nuts-api/src/main/java/net/thevpc/nuts/text/NFormatAndValue.java
@@ -0,0 +1,19 @@
+package net.thevpc.nuts.text;
+
+public class NFormatAndValue<V,F extends NFormat> {
+    private V value;
+    private F format;
+
+    public NFormatAndValue(V value, F format) {
+        this.value = value;
+        this.format = format;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public F getFormat() {
+        return format;
+    }
+}
