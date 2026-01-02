b0186f83334bd98f5b268a95ec9b9a43599422a5
thevpc <taha.bensalah@gmail.com>
2026-01-02 16:24:22 +0100

    Update test-commands.sh : refactor-NFormat

--- test-commands.sh
+++ test-commands.sh
@@ -18,3 +18,7 @@ java -jar nuts-0.8.7-bundle.jar
 #nuts-dev bundle -y net.thevpc.nops:nops --lib=com.cts.ops:cts-ops#1.0,com.cts.ops:cts-protos#1.0,com.cts.ops:icon-misa-ops#1.0,com.cts.ops:icon-veoni-ops#1.0,nsh,nuts-ssh,net.thevpc.nops:nops-desktop-lib,com.ops:my-ops
 #-ZyS --verbose -y net.thevpc.nuts-spring-boot-admin:nuts-spring-boot-admin
 #
+nuts -ZySb --stacktrace --color=formatted nsh -c ls -l ssh://vpc@thevpc.net/
+
+cd /home/vpc/xprojects/nuts-world/nuts/
+nuts -ZySb --stacktrace --color=formatted nuts-release-tool
