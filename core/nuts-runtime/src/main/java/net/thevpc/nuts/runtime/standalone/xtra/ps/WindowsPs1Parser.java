cf96dff8b6105da9d73ff0a171bb9f61153c2d98
thevpc <taha.bensalah@gmail.com>
2026-01-03 20:43:22 +0100

    Update WindowsPs1Parser.java : normalize-properties

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/xtra/ps/WindowsPs1Parser.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/xtra/ps/WindowsPs1Parser.java
@@ -225,7 +225,7 @@ public class WindowsPs1Parser {
                 try {
                     v.setCmdLineArgs(nCmdLines.parseCmdLine(line).map(NCmdLine::toStringArray).orElse(null));
                 } catch (Exception ex2) {
-                    //System.err.println("really??");
+                    // just ignore
                 }
             }
         }
