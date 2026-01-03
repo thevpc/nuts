cc7d6a26b31b1565a46160de119f50401b8fc6bc
thevpc <taha.bensalah@gmail.com>
2026-01-03 20:43:21 +0100

    Update DefaultNTextCode.java : normalize-properties

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/text/parser/DefaultNTextCode.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/text/parser/DefaultNTextCode.java
@@ -30,6 +30,7 @@ package net.thevpc.nuts.runtime.standalone.text.parser;
 import net.thevpc.nuts.runtime.standalone.text.DefaultNTexts;
 import net.thevpc.nuts.spi.NCodeHighlighter;
 import net.thevpc.nuts.text.*;
+import net.thevpc.nuts.util.NImmutable;
 import net.thevpc.nuts.util.NStream;
 import net.thevpc.nuts.util.NStringUtils;
 
@@ -40,6 +41,7 @@ import java.util.stream.Collectors;
 /**
  * Created by vpc on 5/23/17.
  */
+@NImmutable
 public class DefaultNTextCode extends NTextSpecialBase implements NTextCode {
 
     private final String text;
