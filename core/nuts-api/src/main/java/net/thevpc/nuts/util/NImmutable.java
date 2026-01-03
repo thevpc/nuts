501c6a2a538db12ab37c4ac126ef6f3eae72bb82
thevpc <taha.bensalah@gmail.com>
2026-01-03 20:43:18 +0100

    Update NImmutable.java : normalize-properties

--- core/nuts-api/src/main/java/net/thevpc/nuts/util/NImmutable.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/util/NImmutable.java
@@ -1,8 +1,15 @@
 package net.thevpc.nuts.util;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * simple interface that mean that the object is a fixed state
  * and is not
  */
-public interface NImmutable {
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface NImmutable {
 }
