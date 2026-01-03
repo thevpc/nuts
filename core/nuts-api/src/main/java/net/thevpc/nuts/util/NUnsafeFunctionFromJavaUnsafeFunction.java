b0cf00baa35eb83077eae62b2f51fdf5be3df535
thevpc <taha.bensalah@gmail.com>
2026-01-03 20:43:19 +0100

    Update NUnsafeFunctionFromJavaUnsafeFunction.java : normalize-properties

--- core/nuts-api/src/main/java/net/thevpc/nuts/util/NUnsafeFunctionFromJavaUnsafeFunction.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/util/NUnsafeFunctionFromJavaUnsafeFunction.java
@@ -5,7 +5,8 @@ import net.thevpc.nuts.elem.NElement;
 
 import java.util.function.Supplier;
 
-public class NUnsafeFunctionFromJavaUnsafeFunction<T, V> implements NUnsafeFunction<T, V>, NImmutable {
+@NImmutable
+public class NUnsafeFunctionFromJavaUnsafeFunction<T, V> implements NUnsafeFunction<T, V>{
     private final UnsafeFunction<T, V> base;
     private final Supplier<NElement> description;
 
