0f80d5c6f2edf595e2ad0dc4e00e0d22f9aa7e14
thevpc <taha.bensalah@gmail.com>
2026-01-17 00:30:34 +0100

    Update TsonInstant.java : better-tson

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/format/tson/bundled/TsonInstant.java
+++
@@ -1,9 +0,0 @@
-package net.thevpc.nuts.runtime.standalone.format.tson.bundled;
-
-import java.time.Instant;
-
-public interface TsonInstant extends TsonTemporal {
-    Instant value();
-
-    TsonPrimitiveBuilder builder();
-}
