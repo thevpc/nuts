03409d58d051e331e66cb8c3e1277f12585982b8
thevpc <taha.bensalah@gmail.com>
2026-01-17 00:30:33 +0100

    Update TsonBigDecimal.java : better-tson

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/format/tson/bundled/TsonBigDecimal.java
+++
@@ -1,9 +0,0 @@
-package net.thevpc.nuts.runtime.standalone.format.tson.bundled;
-
-import java.math.BigDecimal;
-
-public interface TsonBigDecimal extends TsonNumber {
-    BigDecimal value();
-
-    TsonPrimitiveBuilder builder();
-}
