8e8d38a867adb8a00fa5f131017c52be54c1f205
thevpc <taha.bensalah@gmail.com>
2026-01-17 00:30:33 +0100

    Update TsonBigInt.java : better-tson

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/format/tson/bundled/TsonBigInt.java
+++
@@ -1,9 +0,0 @@
-package net.thevpc.nuts.runtime.standalone.format.tson.bundled;
-
-import java.math.BigInteger;
-
-public interface TsonBigInt extends TsonNumber {
-    BigInteger value();
-
-    TsonPrimitiveBuilder builder();
-}
