782fdcaea588df17e44ddccc9a4cc08c9562bef2
thevpc <taha.bensalah@gmail.com>
2026-01-17 00:30:30 +0100

    Update NExprElementOrOperator.java : better-tson

---
+++ core/nuts-api/src/main/java/net/thevpc/nuts/elem/NExprElementOrOperator.java
@@ -0,0 +1,20 @@
+//package net.thevpc.nuts.elem;
+//
+//import net.thevpc.nuts.util.NBlankable;
+//import net.thevpc.nuts.util.NOptional;
+//
+///**
+// * @since 0.8.9
+// */
+//public interface NExprElementOrOperator extends NBlankable {
+//    boolean isOperator();
+//    boolean isElement();
+//
+//    NOptional<NElement> element();
+//
+//    NElementType elementType();
+//
+//    NOptional<NElementType> operator();
+//
+//    String toString(boolean compact);
+//}
