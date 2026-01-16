807bc58becc2a6baafe1cf484ab7cd33f108bb0a
thevpc <taha.bensalah@gmail.com>
2026-01-17 00:30:35 +0100

    Update TsonRegex.java : better-tson

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/format/tson/bundled/TsonRegex.java
+++
@@ -1,9 +0,0 @@
-package net.thevpc.nuts.runtime.standalone.format.tson.bundled;
-
-import java.util.regex.Pattern;
-
-public interface TsonRegex extends TsonElement {
-    Pattern value();
-
-    TsonPrimitiveBuilder builder();
-}
