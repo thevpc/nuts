fa1704ac86f431ecdb3d603896009d36ee5cfd25
thevpc <taha.bensalah@gmail.com>
2026-01-17 00:30:35 +0100

    Update TsonParametrizedElement.java : better-tson

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/format/tson/bundled/TsonParametrizedElement.java
+++
@@ -1,9 +0,0 @@
-package net.thevpc.nuts.runtime.standalone.format.tson.bundled;
-
-public interface TsonParametrizedElement extends TsonElement {
-    boolean isParametrized();
-
-    TsonElementList params();
-
-    int paramsCount();
-}
