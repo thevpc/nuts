0bd2be7cd88278d774a6f9cdf1de12d5b369577e
thevpc <taha.bensalah@gmail.com>
2026-01-17 00:30:35 +0100

    Update TsonProcessor.java : better-tson

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/format/tson/bundled/TsonProcessor.java
+++
@@ -1,7 +0,0 @@
-package net.thevpc.nuts.runtime.standalone.format.tson.bundled;
-
-public interface TsonProcessor {
-    TsonElement removeComments(TsonElement element);
-
-    TsonElement resolveAliases(TsonElement element);
-}
