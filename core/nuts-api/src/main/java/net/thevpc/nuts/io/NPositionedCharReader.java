bb99baf1737b56b8d6efd3b44c53d2a7d3989174
thevpc <taha.bensalah@gmail.com>
2026-01-17 00:30:31 +0100

    Update NPositionedCharReader.java : better-tson

---
+++ core/nuts-api/src/main/java/net/thevpc/nuts/io/NPositionedCharReader.java
@@ -0,0 +1,68 @@
+package net.thevpc.nuts.io;
+
+import java.io.Reader;
+
+public class NPositionedCharReader extends NCharReader {
+    private int line = 1;
+    private int column = 1;
+    private long pos = 1;
+    private boolean lastWasCR = false;
+
+    public NPositionedCharReader(Reader reader) {
+        super(reader);
+    }
+
+    public long pos() {
+        return pos;
+    }
+
+    public int line() {
+        return line;
+    }
+
+    public int column() {
+        return column;
+    }
+
+    @Override
+    public int read() {
+        int x = super.read();
+        if (x < 0) {
+            return x;
+        }
+        advance((char) x);
+        return x;
+    }
+
+    @Override
+    public char readChar() {
+        char c = super.readChar();
+        advance(c);
+        return c;
+    }
+
+    @Override
+    public int read(char[] dst, int offset, int count) {
+        int n = super.read(dst, offset, count);
+        for (int i = 0; i < n; i++) {
+            advance(dst[offset + i]);
+        }
+        return n;
+    }
+
+    private void advance(char c) {
+        if (c == '\n') {
+            if (!lastWasCR) line++;
+            column = 1;
+            lastWasCR = false;
+        } else if (c == '\r') {
+            line++;
+            column = 1;
+            lastWasCR = true;
+        } else {
+            column++;
+            lastWasCR = false;
+        }
+        pos++;
+    }
+}
