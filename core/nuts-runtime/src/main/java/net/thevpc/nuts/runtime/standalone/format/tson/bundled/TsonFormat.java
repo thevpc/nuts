8dbc5037299551ae188703446dfb3a976464d13b
thevpc <taha.bensalah@gmail.com>
2026-01-17 00:30:38 +0100

    Rename TsonFormat.java → TsonFormat.java : better-tson

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/format/tson/bundled/TsonFormat.java
+++
@@ -1,9 +0,0 @@
-package net.thevpc.nuts.runtime.standalone.format.tson.bundled;
-
-public interface TsonFormat {
-    String format(TsonElement element);
-
-    String format(TsonDocument element);
-
-    TsonFormatBuilder builder();
-}
