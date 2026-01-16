16cf108270feae1cbb778c01744496ed94dcdd18
thevpc <taha.bensalah@gmail.com>
2026-01-17 00:30:34 +0100

    Update TsonElementToObject.java : better-tson

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/format/tson/bundled/TsonElementToObject.java
+++
@@ -1,5 +0,0 @@
-package net.thevpc.nuts.runtime.standalone.format.tson.bundled;
-
-public interface TsonElementToObject<T> {
-    T toObject(TsonElement element,Class<T> to, TsonObjectContext context);
-}
