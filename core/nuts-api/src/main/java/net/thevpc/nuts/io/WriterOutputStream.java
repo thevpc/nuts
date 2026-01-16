e223a3ea78d3749650da29d06b930f0f6285b2b5
thevpc <taha.bensalah@gmail.com>
2026-01-17 00:30:31 +0100

    Update WriterOutputStream.java : better-tson

--- core/nuts-api/src/main/java/net/thevpc/nuts/io/WriterOutputStream.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/io/WriterOutputStream.java
@@ -5,14 +5,18 @@ import net.thevpc.nuts.util.NAssert;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.Writer;
+import java.nio.charset.Charset;
 
 public class WriterOutputStream extends OutputStream {
     private final Writer writer;
+    private final Charset charset;
 
     // Constructor accepting a Writer
-    public WriterOutputStream(Writer writer) {
+    public WriterOutputStream(Writer writer, Charset charset) {
         NAssert.requireNonNull(writer, "writer");
+        NAssert.requireNonNull(charset, "charset");
         this.writer = writer;
+        this.charset = charset;
     }
 
     @Override
@@ -22,12 +26,12 @@ public class WriterOutputStream extends OutputStream {
 
     @Override
     public void write(byte[] b, int off, int len) throws IOException {
-        writer.write(new String(b, off, len));
+        writer.write(new String(b, off, len, charset));
     }
 
     @Override
     public void close() throws IOException {
-        writer.close();
+        writer.flush();
     }
 
     @Override
