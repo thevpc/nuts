bcc7e16694da4c455be3619afed5faf22a5ef2ed
thevpc <taha.bensalah@gmail.com>
2026-01-17 00:30:49 +0100

    Update TsonParserVisitor.java : better-tson

---
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/format/tson/parser/TsonParserVisitor.java
@@ -0,0 +1,154 @@
+//package net.thevpc.nuts.runtime.standalone.format.tson.parser;
+//
+//import net.thevpc.nuts.elem.NCustomElement;
+//import net.thevpc.nuts.elem.NElement;
+//import net.thevpc.nuts.elem.NElementComment;
+//
+//public interface TsonParserVisitor {
+//    default void visitComments(NElementComment comments) {
+//    }
+//
+//    default void visitAnnotationStart(String annotationName) {
+//    }
+//
+//    default void visitPrimitiveEnd(NElement primitiveElement) {
+//    }
+//
+//    default void visitObjectStart() {
+//    }
+//
+//    default void visitArrayStart() {
+//    }
+//
+//    default void visitNamedStart(String id) {
+//    }
+//
+//    default void visitParamsStart() {
+//    }
+//
+//    default void visitUpletEnd() {
+//    }
+//
+////    default void visitFunctionEnd() {
+////    }
+//
+//    default void visitParamsEnd() {
+//    }
+//
+//    default void visitObjectEnd() {
+//
+//    }
+//
+////    default void visitBinOpEnd(String op) {
+////
+////    }
+//
+////    default void visitKeyValueEnd() {
+////
+////    }
+//
+//    default void visitElementEnd() {
+//
+//    }
+//    default void visitElementStart() {
+//
+//    }
+//
+//    default void visitDocumentEnd() {
+//
+//    }
+//
+//    default void visitAnnotationEnd() {
+//
+//    }
+//
+//    default void visitNamedObjectEnd() {
+//
+//    }
+//
+//    default void visitNamedArrayEnd() {
+//
+//    }
+//
+//
+//    default void visitAnnotationParamsStart() {
+//    }
+//
+//    default void visitAnnotationParamsEnd() {
+//    }
+//
+//    default void visitAnnotationParamStart() {
+//    }
+//
+//    default void visitAnnotationParamEnd() {
+//    }
+//
+//    default void visitParamElementStart() {
+//    }
+//
+//    default void visitParamElementEnd() {
+//    }
+//
+//    default void visitObjectElementStart() {
+//    }
+//
+//    default void visitObjectElementEnd() {
+//    }
+//
+//    default void visitArrayElementStart() {
+//    }
+//
+//    default void visitArrayElementEnd() {
+//    }
+//
+//    default void visitInstructionStart() {
+//    }
+//
+//    default void visitSimpleEnd() {
+//    }
+//
+//    default void visitArrayEnd() {
+//
+//    }
+//
+//    default void visitNamedObjectStart() {
+//
+//    }
+//
+//    default void visitNamedArrayStart() {
+//
+//    }
+//
+////    default void listSeparator() {
+////
+////    }
+////
+//    default void matrixRowSeparator() {
+//
+//    }
+//
+//    default void visitCustomEnd(NCustomElement custom) {
+//    }
+//
+//    default void visitPair(String image) {
+//    }
+//
+//
+//    default void visitUpletItemSeparator(String image) {
+//    }
+//
+//    default void visitObjectItemSeparator(String image) {
+//    }
+//
+//    default void visitArrayItemSeparator(String image) {
+//    }
+//
+//    default void visitExprStart(){
+//
+//    }
+//
+//    default void visitExprOp(String image) {
+//    }
+//    default void visitExprNextElement(){}
+//    default void visitExprEnd(){}
+//}
