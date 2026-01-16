5eb78aa2bb395ddd6af64bb2696fb8bcab1812fd
thevpc <taha.bensalah@gmail.com>
2026-01-17 00:30:30 +0100

    Update NElementAnnotationBuilder.java : better-tson

---
+++ core/nuts-api/src/main/java/net/thevpc/nuts/elem/NElementAnnotationBuilder.java
@@ -0,0 +1,26 @@
+package net.thevpc.nuts.elem;
+
+import net.thevpc.nuts.util.NBlankable;
+
+import java.util.List;
+
+public interface NElementAnnotationBuilder extends NBlankable {
+    String name();
+
+    boolean isParametrized();
+    NElementAnnotationBuilder setUnparameterized();
+
+    NElement param(int index);
+
+    NElementAnnotationBuilder setName(String name);
+
+    String toString(boolean compact);
+
+    NElementAnnotation build();
+
+    List<NElement> params();
+
+    NElementAnnotationBuilder removeAt(int index);
+
+    int size();
+}
