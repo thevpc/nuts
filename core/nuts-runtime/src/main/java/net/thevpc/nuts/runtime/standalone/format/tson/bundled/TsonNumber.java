4d0d3b3cb3b4245f3dd72b7c9d59660b52ccfba6
thevpc <taha.bensalah@gmail.com>
2026-01-17 00:30:35 +0100

    Update TsonNumber.java : better-tson

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/format/tson/bundled/TsonNumber.java
+++
@@ -1,6 +0,0 @@
-package net.thevpc.nuts.runtime.standalone.format.tson.bundled;
-
-public interface TsonNumber extends TsonElement{
-    TsonNumberLayout numberLayout();
-    String numberSuffix();
-}
