afc92dcfd20997fad911de262ccea4fda3b84d44
thevpc <taha.bensalah@gmail.com>
2026-01-17 00:30:31 +0100

    Rename NReflectMappingStrategy.java → NReflectTypeMapper.java : better-tson

--- core/nuts-api/src/main/java/net/thevpc/nuts/reflect/NReflectTypeMapper.java
+++
@@ -1,7 +0,0 @@
-package net.thevpc.nuts.reflect;
-
-
-public interface NReflectTypeMapper {
-    boolean copy(Object a, Object b, NReflectMapperContext context);
-    Object mapToType(Object a, NReflectType fromType, NReflectType toType, NReflectMapperContext context);
-}
