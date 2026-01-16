7a71f4b59001477e18bd0529ea459c23bb48a123
thevpc <taha.bensalah@gmail.com>
2026-01-17 00:30:35 +0100

    Update TsonParserFactory.java : better-tson

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/format/tson/bundled/TsonParserFactory.java
+++
@@ -1,8 +0,0 @@
-package net.thevpc.nuts.runtime.standalone.format.tson.bundled;
-
-import java.io.Reader;
-
-public interface TsonParserFactory {
-    String id();
-    TsonStreamParser fromReader(Reader reader, String parser, Object source);
-}
