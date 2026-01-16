74d89358a8cac6dd74ddd9624770f32dc9da0c31
thevpc <taha.bensalah@gmail.com>
2026-01-17 00:30:35 +0100

    Update TsonListContainer.java : better-tson

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/format/tson/bundled/TsonListContainer.java
+++
@@ -1,6 +0,0 @@
-package net.thevpc.nuts.runtime.standalone.format.tson.bundled;
-
-public interface TsonListContainer extends TsonElement, TsonParametrizedElement, TsonNamedElement {
-    TsonElementList body();
-
-}
