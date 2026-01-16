8846e32695314fec2536f6c3ae686b4f327bf360
thevpc <taha.bensalah@gmail.com>
2026-01-17 00:30:35 +0100

    Update TsonLocalDateTime.java : better-tson

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/format/tson/bundled/TsonLocalDateTime.java
+++
@@ -1,9 +0,0 @@
-package net.thevpc.nuts.runtime.standalone.format.tson.bundled;
-
-import java.time.LocalDateTime;
-
-public interface TsonLocalDateTime extends TsonTemporal {
-    LocalDateTime value();
-
-    TsonPrimitiveBuilder builder();
-}
