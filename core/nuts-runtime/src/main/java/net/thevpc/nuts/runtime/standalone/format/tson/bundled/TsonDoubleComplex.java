65996c3dcb6be595131629acdf0132f03d0c7102
thevpc <taha.bensalah@gmail.com>
2026-01-17 00:30:34 +0100

    Update TsonDoubleComplex.java : better-tson

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/format/tson/bundled/TsonDoubleComplex.java
+++
@@ -1,9 +0,0 @@
-package net.thevpc.nuts.runtime.standalone.format.tson.bundled;
-
-public interface TsonDoubleComplex extends TsonNumber {
-    double real();
-
-    double imag();
-
-    TsonPrimitiveBuilder builder();
-}
