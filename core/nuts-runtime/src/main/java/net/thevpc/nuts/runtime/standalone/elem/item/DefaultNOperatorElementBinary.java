fbba479319ddf21d240775dff9df84018ae16c65
thevpc <taha.bensalah@gmail.com>
2026-01-18 17:03:08 +0100

    Update DefaultNOperatorElementBinary.java : wip

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/elem/item/DefaultNOperatorElementBinary.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/elem/item/DefaultNOperatorElementBinary.java
@@ -2,8 +2,26 @@ package net.thevpc.nuts.runtime.standalone.elem.item;
 
 import net.thevpc.nuts.elem.*;
 
-public class DefaultNOperatorElementBinary extends AbstractNOperatorElement implements NBinaryOperatorElement{
+public class DefaultNOperatorElementBinary extends AbstractNOperatorElement implements NBinaryOperatorElement {
     public DefaultNOperatorElementBinary(NOperatorSymbol symbol, NOperatorPosition position, NElement first, NElement second, NElementAnnotation[] annotations, NElementComments comments) {
-        super(symbol, position, first, second, annotations, comments);
+        super(new NOperatorSymbol[]{symbol}, position
+                , new NElement[]{first, second}
+                , annotations, comments
+        );
+    }
+
+    @Override
+    public NOperatorSymbol operatorSymbol() {
+        return operatorSymbols().get(0);
+    }
+
+    @Override
+    public NElement firstOperand() {
+        return operands().get(0);
+    }
+
+    @Override
+    public NElement secondOperand() {
+        return operands().get(1);
     }
 }
