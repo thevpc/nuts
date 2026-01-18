1794175562338f9aa2f8371419a13d9d41baf2f0
thevpc <taha.bensalah@gmail.com>
2026-01-18 17:03:08 +0100

    Update DefaultNOperatorSymbolElement.java : wip

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/elem/item/DefaultNOperatorSymbolElement.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/elem/item/DefaultNOperatorSymbolElement.java
@@ -3,20 +3,20 @@ package net.thevpc.nuts.runtime.standalone.elem.item;
 import net.thevpc.nuts.elem.*;
 
 public class DefaultNOperatorSymbolElement extends AbstractNElement implements NOperatorSymbolElement {
-    private NOperatorSymbol kind;
+    private NOperatorSymbol symbol;
 
     public DefaultNOperatorSymbolElement(NOperatorSymbol op, NElementAnnotation[] annotations, NElementComments comments) {
         super(NElementType.OPERATOR_SYMBOL, annotations, comments);
-        this.kind = op;
+        this.symbol = op;
     }
 
     @Override
-    public NOperatorSymbol kind() {
-        return kind;
+    public NOperatorSymbol symbol() {
+        return symbol;
     }
 
     @Override
     public String toString(boolean compact) {
-        return kind.lexeme();
+        return symbol.lexeme();
     }
 }
