378381cd3eb364bda34b7da2b0fb9e16f6ceb67f
thevpc <taha.bensalah@gmail.com>
2026-01-18 17:03:09 +0100

    Update AbstractOp.java : wip

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/xtra/expr/AbstractOp.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/xtra/expr/AbstractOp.java
@@ -1,16 +1,16 @@
 package net.thevpc.nuts.runtime.standalone.xtra.expr;
 
 import net.thevpc.nuts.expr.NExprOp;
-import net.thevpc.nuts.expr.NExprOpAssociativity;
+import net.thevpc.nuts.elem.NOperatorAssociativity;
 import net.thevpc.nuts.expr.NExprOpType;
 
 public abstract class AbstractOp implements NExprOp {
     private final NExprOpType type;
     private final String name;
     private final int precedence;
-    private final NExprOpAssociativity associativity;
+    private final NOperatorAssociativity associativity;
 
-    public AbstractOp(String name, int precedence, NExprOpAssociativity associativity, NExprOpType type) {
+    public AbstractOp(String name, int precedence, NOperatorAssociativity associativity, NExprOpType type) {
         this.name = name;
         this.type = type;
         this.precedence = precedence;
@@ -33,7 +33,7 @@ public abstract class AbstractOp implements NExprOp {
         return name;
     }
 
-    public NExprOpAssociativity getAssociativity() {
+    public NOperatorAssociativity getAssociativity() {
         return associativity;
     }
 }
