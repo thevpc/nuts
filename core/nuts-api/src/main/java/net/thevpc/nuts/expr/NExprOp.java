d87b1868c5d5de3dd2a2d913018deb391c599c84
thevpc <taha.bensalah@gmail.com>
2026-01-18 17:03:08 +0100

    Update NExprOp.java : wip

--- core/nuts-api/src/main/java/net/thevpc/nuts/expr/NExprOp.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/expr/NExprOp.java
@@ -1,9 +1,11 @@
 package net.thevpc.nuts.expr;
 
+import net.thevpc.nuts.elem.NOperatorAssociativity;
+
 import java.util.List;
 
 public interface NExprOp {
-    NExprOpAssociativity getAssociativity();
+    NOperatorAssociativity getAssociativity();
 
     NExprOpType getType();
 
