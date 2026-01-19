ebe9fd0d352f715bf43c82657690e4905e3abb1a
thevpc <taha.bensalah@gmail.com>
2026-01-19 01:07:18 +0100

    Update DefaultNOperatorElementUnary.java : fix-tson

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/elem/item/DefaultNOperatorElementUnary.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/elem/item/DefaultNOperatorElementUnary.java
@@ -2,9 +2,12 @@ package net.thevpc.nuts.runtime.standalone.elem.item;
 
 import net.thevpc.nuts.elem.*;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class DefaultNOperatorElementUnary extends AbstractNOperatorElement implements NUnaryOperatorElement {
-    public DefaultNOperatorElementUnary(NOperatorSymbol symbol, NOperatorPosition position, NElement first, NElementAnnotation[] annotations, NElementComments comments) {
-        super(new NOperatorSymbol[]{symbol}, position, new NElement[]{first}, annotations, comments);
+    public DefaultNOperatorElementUnary(NOperatorSymbol symbol, NOperatorPosition position, NElement first, List<NElementAnnotation> annotations, NElementComments comments, List<NElementDiagnostic> diagnostics) {
+        super(Arrays.asList(symbol), position, Arrays.asList(first), annotations, comments,diagnostics);
     }
 
     @Override
