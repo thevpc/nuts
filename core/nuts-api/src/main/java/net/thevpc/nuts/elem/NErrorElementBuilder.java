4f2390ab78f20dd57f1141de967bbafd7374a705
thevpc <taha.bensalah@gmail.com>
2026-01-19 01:07:18 +0100

    Rename NEmptyElementBuilder.java → NErrorElementBuilder.java : fix-tson

--- core/nuts-api/src/main/java/net/thevpc/nuts/elem/NErrorElementBuilder.java
+++
@@ -1,9 +0,0 @@
-package net.thevpc.nuts.elem;
-
-import net.thevpc.nuts.text.NMsg;
-
-public interface NErrorElementBuilder extends NElementBuilder {
-    NMsg getMessage();
-    NErrorElementBuilder setMessage(NMsg msg);
-    NErrorElement build();
-}
