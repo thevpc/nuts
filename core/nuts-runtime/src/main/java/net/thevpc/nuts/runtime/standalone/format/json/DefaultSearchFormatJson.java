56f3160159962e8abad6fb0bb6df9c1f25486733
thevpc <taha.bensalah@gmail.com>
2026-01-24 16:26:21 +0100

    Update DefaultSearchFormatJson.java : TSON2

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/format/json/DefaultSearchFormatJson.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/format/json/DefaultSearchFormatJson.java
@@ -7,6 +7,7 @@ package net.thevpc.nuts.runtime.standalone.format.json;
 
 import net.thevpc.nuts.cmdline.NArg;
 import net.thevpc.nuts.cmdline.NCmdLine;
+import net.thevpc.nuts.elem.NElementFormatter;
 import net.thevpc.nuts.elem.NElementWriter;
 import net.thevpc.nuts.text.NContentType;
 import net.thevpc.nuts.io.NPrintStream;
@@ -70,7 +71,7 @@ public class DefaultSearchFormatJson extends DefaultSearchFormatBase {
             getWriter().print("  ");
         }
         String json = NElementWriter.ofJson()
-                .setCompact(isCompact())
+                .setFormatter(isCompact()? NElementFormatter.ofJsonCompact() : NElementFormatter.ofJsonPretty())
                 .formatPlain(object);
         NText ee = codeFormat.stringToText(json, txt);
         getWriter().println(ee);
