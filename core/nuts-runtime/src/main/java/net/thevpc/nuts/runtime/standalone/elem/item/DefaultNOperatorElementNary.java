078d536f93feb7af457bde2dbe7da3eee186cfbd
thevpc <taha.bensalah@gmail.com>
2026-01-24 16:26:20 +0100

    Update DefaultNOperatorElementNary.java : TSON2

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/elem/item/DefaultNOperatorElementNary.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/elem/item/DefaultNOperatorElementNary.java
@@ -5,7 +5,7 @@ import net.thevpc.nuts.elem.*;
 import java.util.List;
 
 public class DefaultNOperatorElementNary extends AbstractNOperatorElement implements NAryOperatorElement{
-    public DefaultNOperatorElementNary(List<NElement> operands, List<NOperatorSymbol> symbols, NOperatorPosition position, List<NElementAnnotation> annotations, NElementComments comments, List<NElementDiagnostic> diagnostics) {
-        super(symbols, position, operands, annotations, comments, diagnostics);
+    public DefaultNOperatorElementNary(List<NElement> operands, List<NOperatorSymbol> symbols, NOperatorPosition position, List<NBoundAffix> affixes, List<NElementDiagnostic> diagnostics) {
+        super(symbols, position, operands, affixes, diagnostics);
     }
 }
