6716c27440b86ae94044f53d27eca4d40f007e45
thevpc <taha.bensalah@gmail.com>
2026-01-24 16:26:20 +0100

    Update DefaultNListItemElement.java : TSON2

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/elem/item/DefaultNListItemElement.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/elem/item/DefaultNListItemElement.java
@@ -1,26 +1,52 @@
 package net.thevpc.nuts.runtime.standalone.elem.item;
 
-import net.thevpc.nuts.elem.NElement;
-import net.thevpc.nuts.elem.NListElement;
-import net.thevpc.nuts.elem.NListItemElement;
+import net.thevpc.nuts.elem.*;
+import net.thevpc.nuts.runtime.standalone.util.CoreNUtils;
 import net.thevpc.nuts.util.NOptional;
 
+import java.util.List;
 import java.util.Objects;
 
 public class DefaultNListItemElement implements NListItemElement {
+    private String marker;
+    private String markerVariant;
     private int depth;
     private NElement value;
     private NListElement subList;
+    private NElementType listType;
+    private List<NBoundAffix> affixes;
 
-    public DefaultNListItemElement(int depth, NElement value, NListElement subList) {
+    public DefaultNListItemElement(NElementType listType, String marker, String markerVariant, int depth, NElement value, NListElement subList, List<NBoundAffix> affixes) {
+        this.marker = marker;
+        this.markerVariant = markerVariant;
         this.depth = depth;
         this.value = value;
         this.subList = subList;
+        this.listType = listType;
+        this.affixes = CoreNUtils.copyNonNullUnmodifiableList(affixes);
     }
 
     @Override
     public int depth() {
         return depth;
+    }
+
+    public List<NBoundAffix> affixes() {
+        return affixes;
+    }
+
+    public NElementType listType() {
+        return listType;
+    }
+
+    @Override
+    public String marker() {
+        return marker;
+    }
+
+    @Override
+    public String markerVariant() {
+        return markerVariant;
     }
 
     @Override
@@ -52,7 +78,7 @@ public class DefaultNListItemElement implements NListItemElement {
             sb.append(value);
             sb.append("\n");
             sb.append(subList);
-        }else {
+        } else {
             if (value != null) {
                 sb.append(value);
             }
