d69c2bfda5bb40b71928b2863d65bfa947800142
thevpc <taha.bensalah@gmail.com>
2026-01-24 16:26:20 +0100

    Update DefaultNElementFormatContext.java : TSON2

---
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/elem/item/DefaultNElementFormatContext.java
@@ -0,0 +1,76 @@
+package net.thevpc.nuts.runtime.standalone.elem.item;
+
+import net.thevpc.nuts.elem.*;
+import net.thevpc.nuts.util.NStringUtils;
+
+import java.util.Map;
+import java.util.Objects;
+
+public class DefaultNElementFormatContext extends DefaultNElementTransformContext implements NElementFormatContext {
+    private String indent = "";
+    private NElementFormatOptions options;
+    private NElementBuilder builder;
+
+    public DefaultNElementFormatContext(NElement element) {
+        super(element);
+    }
+
+    public DefaultNElementFormatContext(NElement element, NElementPath path, Map<String, Object> properties, Map<String, Object> sharedConfig, String indent, NElementFormatOptions options, NElementBuilder builder) {
+        super(element, path, properties, sharedConfig);
+        this.indent = NStringUtils.firstNonNull(indent, "");
+        this.options = options;
+        this.builder = builder;
+    }
+
+    @Override
+    public String indent() {
+        return indent;
+    }
+
+    @Override
+    public NElementFormatOptions options() {
+        return options;
+    }
+
+    @Override
+    public NElementBuilder builder() {
+        return builder;
+    }
+
+    @Override
+    public NElementFormatContext withBuilder(NElementBuilder builder) {
+        return newInstance(element(), path(), properties(), sharedConfig(), indent, options, builder);
+    }
+
+    @Override
+    public NElementFormatContext withOptions(NElementFormatOptions options) {
+        if (Objects.equals(options, this.options)) {
+            return this;
+        }
+        return newInstance(element(), path(), properties(), sharedConfig(), indent, options, builder);
+    }
+
+    @Override
+    public NElementFormatContext withPath(NElementPath path) {
+        return (NElementFormatContext) super.withPath(path);
+    }
+
+    @Override
+    public NElementFormatContext withElement(NElement element) {
+        return (NElementFormatContext) super.withElement(element);
+    }
+
+
+    public NElementFormatContext withIndent(String indent) {
+        return newInstance(element(), path(), properties(), sharedConfig(), indent, options, builder);
+    }
+
+    @Override
+    protected NElementFormatContext newInstance(NElement element, NElementPath path, Map<String, Object> properties, Map<String, Object> sharedConfig) {
+        return newInstance(element, path, properties, sharedConfig, indent, options, builder);
+    }
+
+    protected NElementFormatContext newInstance(NElement element, NElementPath path, Map<String, Object> properties, Map<String, Object> sharedConfig, String indent, NElementFormatOptions options, NElementBuilder builder) {
+        return new DefaultNElementFormatContext(element, path, properties, sharedConfig, indent, options, builder);
+    }
+}
