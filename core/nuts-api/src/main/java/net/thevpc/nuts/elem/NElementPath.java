cfd22ea17dd721790f22cd46137b86d313e4e0ae
thevpc <taha.bensalah@gmail.com>
2026-01-24 16:26:15 +0100

    Update NElementPath.java : TSON2

--- core/nuts-api/src/main/java/net/thevpc/nuts/elem/NElementPath.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/elem/NElementPath.java
@@ -1,4 +1,19 @@
 package net.thevpc.nuts.elem;
 
 public interface NElementPath {
+    static NElementPath ofRoot() {
+        return NElements.of().createRootPath();
+    }
+
+    int size();
+
+    NElementPath parent();
+
+    boolean isRoot();
+
+    NElementPath child(int i);
+
+    NElementPath param(int i);
+
+    NElementPath ann(int i);
 }
