47bb9c7b02f6eae4fa36bc6201866a5c565e1546
thevpc <taha.bensalah@gmail.com>
2026-01-24 16:26:22 +0100

    Update TsonParserVisitorEmpty.java : TSON2

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/format/tson/parser/TsonParserVisitorEmpty.java
+++
@@ -1,5 +0,0 @@
-//package net.thevpc.nuts.runtime.standalone.format.tson.parser;
-//
-//public class TsonParserVisitorEmpty implements TsonParserVisitor {
-//    public static final TsonParserVisitor INSTANCE=new TsonParserVisitorEmpty();
-//}
