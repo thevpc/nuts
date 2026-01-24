d366dd3bf8dd4998c932a7caaac76e4a2f93a532
thevpc <taha.bensalah@gmail.com>
2026-01-24 16:26:15 +0100

    Update NElementComments.java : TSON2

--- core/nuts-api/src/main/java/net/thevpc/nuts/elem/NElementComments.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/elem/NElementComments.java
@@ -1,13 +1,13 @@
-package net.thevpc.nuts.elem;
-
-import net.thevpc.nuts.util.NBlankable;
-
-import java.util.List;
-
-public interface NElementComments extends Comparable<NElementComments>, NBlankable {
-    List<NElementComment> trailingComments();
-
-    List<NElementComment> leadingComments();
-
-    boolean isEmpty();
-}
+//package net.thevpc.nuts.elem;
+//
+//import net.thevpc.nuts.util.NBlankable;
+//
+//import java.util.List;
+//
+//public interface NElementComments extends Comparable<NElementComments>, NBlankable {
+//    List<NElementComment> trailingComments();
+//
+//    List<NElementComment> leadingComments();
+//
+//    boolean isEmpty();
+//}
