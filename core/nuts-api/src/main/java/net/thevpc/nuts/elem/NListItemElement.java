54c45c4f2f9da925aa2588dc8b5750cec9ece9bc
thevpc <taha.bensalah@gmail.com>
2026-01-24 16:26:16 +0100

    Update NListItemElement.java : TSON2

--- core/nuts-api/src/main/java/net/thevpc/nuts/elem/NListItemElement.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/elem/NListItemElement.java
@@ -2,8 +2,20 @@ package net.thevpc.nuts.elem;
 
 import net.thevpc.nuts.util.NOptional;
 
+import java.util.List;
+
 public interface NListItemElement {
+    String markerVariant();
+
+    String marker();
+
     int depth();
+
+    List<NBoundAffix> affixes();
+
+    NElementType listType();
+
     NOptional<NElement> value();
+
     NOptional<NListElement> subList();
 }
