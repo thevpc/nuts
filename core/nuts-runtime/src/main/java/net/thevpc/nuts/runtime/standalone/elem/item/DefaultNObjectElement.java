64df36a3430d93033225b1203bf4d0f81ed8a810
thevpc <taha.bensalah@gmail.com>
2026-01-26 18:23:38 +0100

    Update DefaultNObjectElement.java : TSONv2

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/elem/item/DefaultNObjectElement.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/elem/item/DefaultNObjectElement.java
@@ -23,9 +23,9 @@ public class DefaultNObjectElement extends AbstractNListContainerElement impleme
     public DefaultNObjectElement(String name, List<NElement> params, List<NElement> values, List<NBoundAffix> affixes, List<NElementDiagnostic> diagnostics) {
         super(
                 name == null && params == null ? NElementType.OBJECT
-                        : name == null && params != null ? NElementType.PARAMETRIZED_OBJECT
+                        : name == null && params != null ? NElementType.PARAM_OBJECT
                         : name != null && params == null ? NElementType.NAMED_OBJECT
-                        : NElementType.NAMED_PARAMETRIZED_OBJECT,
+                        : NElementType.FULL_OBJECT,
                 affixes,diagnostics);
         if (name != null) {
             NAssert.requireTrue(NElementUtils.isValidElementName(name), "valid name : " + name);
