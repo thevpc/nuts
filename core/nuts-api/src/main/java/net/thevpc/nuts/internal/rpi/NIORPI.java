7b735c66fa452fa26eeccfa9df53a1737747a3b7
thevpc <taha.bensalah@gmail.com>
2026-01-27 17:53:04 +0100

    Update NIORPI.java : TSONv2

--- core/nuts-api/src/main/java/net/thevpc/nuts/internal/rpi/NIORPI.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/internal/rpi/NIORPI.java
@@ -1,16 +1,18 @@
 package net.thevpc.nuts.internal.rpi;
 
+import net.thevpc.nuts.artifact.NId;
 import net.thevpc.nuts.command.NExecutionEntry;
+import net.thevpc.nuts.core.NRepository;
 import net.thevpc.nuts.ext.NExtensions;
 import net.thevpc.nuts.io.*;
 import net.thevpc.nuts.spi.NComponent;
+import net.thevpc.nuts.spi.NPathSPI;
 import net.thevpc.nuts.spi.NSystemTerminalBase;
 import net.thevpc.nuts.io.NAsk;
 
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.Reader;
-import java.io.Writer;
+import java.io.*;
+import java.net.URL;
+import java.nio.file.Path;
 import java.util.List;
 
 /**
@@ -157,5 +159,95 @@ public interface NIORPI extends NComponent {
      * @return execution entries (class names with main method)
      */
     List<NExecutionEntry> parseExecutionEntries(InputStream inputStream, String type, String sourceName);
+
+    NTextCursorTracker createTextCursorTracker();
+    NTextCursorTracker createTextCursorTracker(int tabSize, int maxRewindDepth);
+
+    /**
+     * create temp file in the repositoryId's temp folder
+     *
+     * @param name file name
+     * @return newly created file path
+     */
+    NPath ofTempFile(String name);
+
+    /**
+     * create temp file in the repositoryId's temp folder
+     *
+     * @return newly created file path
+     */
+    NPath ofTempFile();
+
+    /**
+     * create temp folder in the workspace's temp folder
+     *
+     * @param name folder name
+     * @return newly created temp folder
+     */
+    NPath ofTempFolder(String name);
+
+    /**
+     * create temp folder in the workspace's temp folder
+     *
+     * @return newly created temp folder
+     */
+    NPath ofTempFolder();
+
+    /**
+     * create temp file in the repositoryId's temp folder
+     *
+     * @param name file name
+     * @return newly created file path
+     */
+    NPath ofTempRepositoryFile(String name, NRepository repository);
+
+    /**
+     * create temp file in the repositoryId's temp folder
+     *
+     * @return newly created file path
+     */
+    NPath ofTempRepositoryFile(NRepository repository);
+
+    /**
+     * create temp folder in the repository's temp folder
+     *
+     * @param name folder name
+     * @return newly created temp folder
+     */
+    NPath ofTempRepositoryFolder(String name, NRepository repository);
+
+    /**
+     * create temp folder in the repository's temp folder
+     *
+     * @return newly created temp folder
+     */
+    NPath ofTempRepositoryFolder(NRepository repository);
+
+
+    NPath ofTempIdFile(String name, NId repository);
+
+    NPath ofTempIdFolder(String name, NId repository);
+
+    NPath ofTempIdFile(NId repository);
+
+    NPath ofTempIdFolder(NId repository);
+
+    /**
+     * expand path to Workspace Location
+     *
+     * @param path path to expand
+     * @return expanded path
+     */
+    NPath createPath(String path);
+
+    NPath createPath(File path);
+
+    NPath createPath(Path path);
+
+    NPath createPath(URL path);
+
+    NPath createPath(String path, ClassLoader classLoader);
+
+    NPath createPath(NPathSPI path);
 
 }
