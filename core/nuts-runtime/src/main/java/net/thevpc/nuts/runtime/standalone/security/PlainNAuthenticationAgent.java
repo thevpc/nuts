e2f150b01e6443f05617964606b65b7cff9d8c5a
thevpc <taha.bensalah@gmail.com>
2026-02-02 02:40:27 +0100

    Update PlainNAuthenticationAgent.java : fixed-auth

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/security/PlainNAuthenticationAgent.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/security/PlainNAuthenticationAgent.java
@@ -1,84 +1,33 @@
 package net.thevpc.nuts.runtime.standalone.security;
 
-import net.thevpc.nuts.security.NAuthenticationAgent;
-import net.thevpc.nuts.security.NSecurityException;
+import net.thevpc.nuts.artifact.NVersion;
 import net.thevpc.nuts.spi.*;
-import net.thevpc.nuts.util.NBlankable;
-import net.thevpc.nuts.text.NMsg;
 import net.thevpc.nuts.util.NScore;
 import net.thevpc.nuts.util.NScorable;
 
 import java.util.Arrays;
-import java.util.Map;
+import java.util.function.Function;
 
 @NComponentScope(NScopeType.WORKSPACE)
-@NScore(fixed = NScorable.DEFAULT_SCORE -1)
-public class PlainNAuthenticationAgent implements NAuthenticationAgent {
+@NScore(fixed = NScorable.DEFAULT_SCORE -2)
+public class PlainNAuthenticationAgent extends AbstractNAuthenticationAgent {
 
-    @Override
-    public String getId() {
-        return "plain";
+    public PlainNAuthenticationAgent() {
+        super("plain", NVersion.of("1"));
     }
 
     @Override
-    public void checkCredentials(char[] credentialsId, char[] password, Map<String, String> envProvider) {
-        if (password == null || NBlankable.isBlank(new String(password))) {
-            throw new NSecurityException(NMsg.ofPlain("missing old password"));
-        }
-        char[] iid = extractId(credentialsId);
-        if (Arrays.equals(iid, password)) {
-            return;
-        }
-        throw new NSecurityException(NMsg.ofPlain("invalid login or password"));
+    protected char[] oneWayChars(char[] data, Function<String, String> env) {
+        return Arrays.copyOf(data, data.length);
     }
 
     @Override
-    public char[] getCredentials(char[] credentialsId, Map<String, String> envProvider) {
-        return extractId(credentialsId);
+    protected char[] encryptChars(char[] data, Function<String, String> env) {
+        return Arrays.copyOf(data, data.length);
     }
 
     @Override
-    public boolean removeCredentials(char[] credentialsId, Map<String, String> envProvider) {
-        extractId(credentialsId);
-        return true;
-    }
-
-    @Override
-    public char[] createCredentials(
-            char[] credentials,
-            boolean allowRetrieve,
-            char[] credentialId,
-            Map<String, String> envProvider) {
-        if (credentials == null || NBlankable.isBlank(new String(credentials))) {
-            return null;
-        } else {
-            char[] val = credentials;
-            String id = getId();
-            char[] r = new char[id.length() + 1 + val.length];
-            System.arraycopy(id.toCharArray(), 0, r, 0, id.length());
-            r[id.length()] = ':';
-            System.arraycopy(val, 0, r, id.length() + 1, val.length);
-            return r;
-        }
-    }
-
-
-    private char[] extractId(char[] a) {
-        if (!(a == null || NBlankable.isBlank(new String(a)))) {
-            char[] idc = (getId() + ":").toCharArray();
-            if (a.length > idc.length + 1) {
-                boolean ok = true;
-                for (int i = 0; i < idc.length; i++) {
-                    if (a[i] != idc[i]) {
-                        ok = false;
-                        break;
-                    }
-                }
-                if (ok) {
-                    return Arrays.copyOfRange(a, idc.length, a.length);
-                }
-            }
-        }
-        throw new NSecurityException(NMsg.ofC("credential id must start with '%s:'", getId()));
+    protected char[] decryptChars(char[] data, Function<String, String> env) {
+        return Arrays.copyOf(data, data.length);
     }
 }
