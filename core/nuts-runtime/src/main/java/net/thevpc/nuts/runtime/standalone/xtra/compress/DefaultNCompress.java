d6a5a67138474c89e419fef7e8060e54677d8204
thevpc <taha.bensalah@gmail.com>
2026-02-02 02:40:28 +0100

    Update DefaultNCompress.java : fixed-auth

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/xtra/compress/DefaultNCompress.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/xtra/compress/DefaultNCompress.java
@@ -6,7 +6,7 @@
 package net.thevpc.nuts.runtime.standalone.xtra.compress;
 
 import net.thevpc.nuts.core.NWorkspace;
-import net.thevpc.nuts.elem.NElementDescribables;
+import net.thevpc.nuts.elem.NDescribables;
 import net.thevpc.nuts.ext.NExtensions;
 import net.thevpc.nuts.io.*;
 import net.thevpc.nuts.runtime.standalone.xtra.time.SingletonNInputStreamProgressFactory;
@@ -319,7 +319,7 @@ public class DefaultNCompress implements NCompress {
                 return p.stream().map(
                                 NFunction.of(
                                         (NPath x) -> new Item(x, c)
-                                ).redescribe(NElementDescribables.ofDesc("NutsStreamOrPath::of"))
+                                ).withDescription(NDescribables.ofDesc("NutsStreamOrPath::of"))
                         )
                         .toArray(Item[]::new);
             }
