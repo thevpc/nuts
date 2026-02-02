fb833d43a65510b6cdb6c013c4771fb477333ad9
thevpc <taha.bensalah@gmail.com>
2026-02-02 02:40:24 +0100

    Update NIteratorBase.java : fixed-auth

--- core/nuts-api/src/main/java/net/thevpc/nuts/util/NIteratorBase.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/util/NIteratorBase.java
@@ -2,15 +2,33 @@ package net.thevpc.nuts.util;
 
 import net.thevpc.nuts.elem.NElement;
 
+import java.util.Iterator;
 import java.util.function.Supplier;
 
 public abstract class NIteratorBase<T> implements NIterator<T> {
+    @Override
+    public NIterator<T> onClose(Runnable closeHandler) {
+        if (closeHandler == null) {
+            return this;
+        }
+        return new NIteratorWithDescription<>(this, null, closeHandler);
+    }
 
-    public NIterator<T> redescribe(Supplier<NElement> description) {
+    public NIterator<T> withDescription(Supplier<NElement> description) {
         if (description == null) {
             return this;
         }
-        return new NIteratorWithDescription<>(this, description);
+        return new NIteratorWithDescription<>(this, description, null);
     }
+    @Override
+    public final boolean hasNext() {
+        boolean b = hasNextImpl();
+        if(!b){
+            close();
+        }
+        return b;
+    }
+
+    protected abstract boolean hasNextImpl();
 
 }
