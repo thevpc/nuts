b5994f13bfac026f7fc254682f02fd6780258c06
thevpc <taha.bensalah@gmail.com>
2026-02-02 02:40:23 +0100

    Update NDoubleFunctionWithDescription.java : fixed-auth

--- core/nuts-api/src/main/java/net/thevpc/nuts/util/NDoubleFunctionWithDescription.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/util/NDoubleFunctionWithDescription.java
@@ -1,6 +1,6 @@
 package net.thevpc.nuts.util;
 
-import net.thevpc.nuts.elem.NElementDescribables;
+import net.thevpc.nuts.elem.NDescribables;
 import net.thevpc.nuts.elem.NElement;
 
 import java.util.function.Supplier;
@@ -21,7 +21,7 @@ public class NDoubleFunctionWithDescription implements NDoubleFunction{
     }
 
     @Override
-    public NDoubleFunction redescribe(Supplier<NElement> description) {
+    public NDoubleFunction withDescription(Supplier<NElement> description) {
         if(description==null){
             return base;
         }
@@ -35,10 +35,10 @@ public class NDoubleFunctionWithDescription implements NDoubleFunction{
 
     @Override
     public NElement describe() {
-        return NElementDescribables.safeDescribe(
+        return NDescribables.safeDescribe(
                 description,
-                NElementDescribables.ofDesc(base),
-                NElementDescribables.ofLateToString(this)
+                NDescribables.ofDesc(base),
+                NDescribables.ofLateToString(this)
         );
     }
 }
