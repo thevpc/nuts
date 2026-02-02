02ff2a73194cf1e15907a8e133301d12930384bf
thevpc <taha.bensalah@gmail.com>
2026-02-02 02:40:27 +0100

    Update DefaultNRemoveUserCmd.java : fixed-auth

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/workspace/cmd/settings/user/DefaultNRemoveUserCmd.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/workspace/cmd/settings/user/DefaultNRemoveUserCmd.java
@@ -1,35 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package net.thevpc.nuts.runtime.standalone.workspace.cmd.settings.user;
-
-import net.thevpc.nuts.security.NRemoveUserCmd;
-import net.thevpc.nuts.runtime.standalone.workspace.NWorkspaceExt;
-import net.thevpc.nuts.runtime.standalone.workspace.config.NRepositoryConfigManagerExt;
-import net.thevpc.nuts.util.NScore;
-import net.thevpc.nuts.util.NScorable;
-
-/**
- *
- * @author thevpc
- */
-@NScore(fixed = NScorable.DEFAULT_SCORE)
-public class DefaultNRemoveUserCmd extends AbstractNRemoveUserCmd {
-
-    public DefaultNRemoveUserCmd() {
-        super();
-    }
-
-    @Override
-    public NRemoveUserCmd run() {
-        if (repository != null) {
-            NRepositoryConfigManagerExt.of(repository.config()).getModel().removeUser(login);
-        } else {
-            NWorkspaceExt.of().getConfigModel().removeUser(login);
-        }
-        return this;
-    }
-
-}
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package net.thevpc.nuts.runtime.standalone.workspace.cmd.settings.user;
+//
+//import net.thevpc.nuts.security.NRemoveUserCmd;
+//import net.thevpc.nuts.runtime.standalone.workspace.NWorkspaceExt;
+//import net.thevpc.nuts.runtime.standalone.workspace.config.NRepositoryConfigManagerExt;
+//import net.thevpc.nuts.util.NScore;
+//import net.thevpc.nuts.util.NScorable;
+//
+///**
+// *
+// * @author thevpc
+// */
+//@NScore(fixed = NScorable.DEFAULT_SCORE)
+//public class DefaultNRemoveUserCmd extends AbstractNRemoveUserCmd {
+//
+//    public DefaultNRemoveUserCmd() {
+//        super();
+//    }
+//
+//    @Override
+//    public NRemoveUserCmd run() {
+//        if (repository != null) {
+//            NRepositoryConfigManagerExt.of(repository.config()).getModel().removeUser(login);
+//        } else {
+//            NWorkspaceExt.of().getConfigModel().removeUser(login);
+//        }
+//        return this;
+//    }
+//
+//}
