81c2546e50134a09ddce35de069e9379528fa700
thevpc <taha.bensalah@gmail.com>
2026-02-02 02:40:24 +0100

    Update NFunction.java : fixed-auth

--- core/nuts-api/src/main/java/net/thevpc/nuts/util/NFunction.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/util/NFunction.java
@@ -5,15 +5,19 @@ import net.thevpc.nuts.elem.*;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
-public interface NFunction<T, V> extends Function<T, V>, NElementRedescribable<NFunction<T, V>> {
+public interface NFunction<T, V> extends Function<T, V>, NRedescribable<NFunction<T, V>> {
     static <T, V> NFunction<T, V> of(Function<T, V> o) {
+        return of(o, null);
+    }
+
+    static <T, V> NFunction<T, V> of(Function<T, V> o, NElement defaultDescription) {
         if (o == null) {
             return null;
         }
         if (o instanceof NFunction) {
             return (NFunction<T, V>) o;
         }
-        return new NFunctionFromJavaFunction<>(o);
+        return new NFunctionFromJavaFunction<>(o, defaultDescription);
     }
 
     static <T, V> NFunction<T, V> ofUnsafe(UnsafeFunction<T, V> o, Function<Exception, ? extends V> onError) {
@@ -34,7 +38,7 @@ public interface NFunction<T, V> extends Function<T, V>, NElementRedescribable<N
                     return onError.apply(e);
                 }
             }
-        });
+        }, NElement.ofNamedUplet("unsafe", NDescribables.describeResolveOrDestructAsObject(o)));
     }
 
     default NElement describe() {
@@ -42,10 +46,10 @@ public interface NFunction<T, V> extends Function<T, V>, NElementRedescribable<N
     }
 
     @Override
-    default NFunction<T, V> redescribe(Supplier<NElement> description) {
-        if(description==null){
+    default NFunction<T, V> withDescription(Supplier<NElement> description) {
+        if (description == null) {
             return this;
         }
-        return new NFunctionWithDescription<>(this,description);
+        return new NFunctionWithDescription<>(this, description);
     }
 }
