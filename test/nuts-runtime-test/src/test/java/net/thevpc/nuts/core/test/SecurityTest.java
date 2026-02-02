f8b13221410bf61446c5dc90dc823897eb50eea2
thevpc <taha.bensalah@gmail.com>
2026-02-02 02:40:28 +0100

    Update SecurityTest.java : fixed-auth

--- test/nuts-runtime-test/src/test/java/net/thevpc/nuts/core/test/SecurityTest.java
+++ test/nuts-runtime-test/src/test/java/net/thevpc/nuts/core/test/SecurityTest.java
@@ -1,16 +1,20 @@
 package net.thevpc.nuts.core.test;
 
 import net.thevpc.nuts.core.test.utils.TestUtils;
-import net.thevpc.nuts.runtime.standalone.io.util.CoreSecurityUtils;
-import net.thevpc.nuts.runtime.standalone.security.DefaultNAuthenticationAgent;
+import net.thevpc.nuts.runtime.standalone.io.util.CoreSecurityUtilsV1;
+import net.thevpc.nuts.runtime.standalone.security.DefaultNAuthenticationAgentV1;
+import net.thevpc.nuts.runtime.standalone.security.DefaultNAuthenticationAgentV2;
 import net.thevpc.nuts.runtime.standalone.security.PlainNAuthenticationAgent;
 import net.thevpc.nuts.security.NAuthenticationAgent;
+import net.thevpc.nuts.security.NCredentialId;
+import net.thevpc.nuts.security.NSecretCaller;
+import net.thevpc.nuts.security.NSecretRunner;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.Objects;
+import java.util.function.Function;
 
 public class SecurityTest {
     @BeforeAll
@@ -19,49 +23,39 @@ public class SecurityTest {
     }
 
     @Test
-    public void testEncrypt(){
-        char[] r = CoreSecurityUtils.INSTANCE.defaultEncryptChars("Hello".toCharArray(), "okkay");
+    public void testEncrypt() {
+        char[] r = CoreSecurityUtilsV1.INSTANCE.defaultEncryptChars("Hello".toCharArray(), "okkay");
         TestUtils.println(new String(r));
-        char[] i = CoreSecurityUtils.INSTANCE.defaultDecryptChars(r, "okkay");
+        char[] i = CoreSecurityUtilsV1.INSTANCE.defaultDecryptChars(r, "okkay");
         TestUtils.println(new String(i));
     }
 
     private void testHelperRetrievable(NAuthenticationAgent a) {
         String mySecret = "my-secret";
-        Map<String,String> envProvider = new LinkedHashMap<>();
-        String withAllowRetreiveId = new String(a.createCredentials(mySecret.toCharArray(), true, null, envProvider));
+        Function<String, String> envProvider = s -> null;
+        NCredentialId withAllowRetreiveId = a.addSecret(mySecret.toCharArray(), envProvider);
         TestUtils.println(withAllowRetreiveId);
-        Assertions.assertTrue(withAllowRetreiveId.startsWith(a.getId() + ":"));
-        a.checkCredentials(withAllowRetreiveId.toCharArray(), "my-secret".toCharArray(), envProvider);
-        try {
-            a.checkCredentials(withAllowRetreiveId.toCharArray(), "my-bad-secret".toCharArray(), envProvider);
-            Assertions.assertTrue(false);
-        } catch (SecurityException ex) {
-            Assertions.assertTrue(true);
-        }
-        Assertions.assertEquals(mySecret, new String(a.getCredentials(withAllowRetreiveId.toCharArray(), envProvider)));
-
-        String withoutAllowRetreiveId = new String(a.createCredentials(mySecret.toCharArray(), false, null, envProvider));
+        Assertions.assertTrue(a.verify(withAllowRetreiveId, "my-secret".toCharArray(), envProvider));
+        Assertions.assertFalse(a.verify(withAllowRetreiveId, "my-bad-secret".toCharArray(), envProvider));
+        NCredentialId withoutAllowRetreiveId = a.addOneWayCredential(mySecret.toCharArray(), envProvider);
         TestUtils.println(withoutAllowRetreiveId);
-        Assertions.assertTrue(withoutAllowRetreiveId.startsWith(a.getId() + ":"));
     }
 
     private void testHelperHashed(NAuthenticationAgent a, boolean alwaysRetrievable) {
         String mySecret = "my-secret";
-        Map<String,String> envProvider = new LinkedHashMap<>();
-        String withoutAllowRetreiveId = new String(a.createCredentials(mySecret.toCharArray(), false, null, envProvider));
+        Function<String, String> envProvider = s -> null;
+        NCredentialId withoutAllowRetreiveId = a.addOneWayCredential(mySecret.toCharArray(), envProvider);
         TestUtils.println(withoutAllowRetreiveId);
-        Assertions.assertTrue(withoutAllowRetreiveId.startsWith(a.getId() + ":"));
-        a.checkCredentials(withoutAllowRetreiveId.toCharArray(), "my-secret".toCharArray(), envProvider);
-        try {
-            a.checkCredentials(withoutAllowRetreiveId.toCharArray(), "my-bad-secret".toCharArray(), envProvider);
-            Assertions.assertTrue(false);
-        } catch (SecurityException ex) {
-            Assertions.assertTrue(true);
-        }
+        Assertions.assertTrue(a.verify(withoutAllowRetreiveId, "my-secret".toCharArray(), envProvider));
+        Assertions.assertFalse(a.verify(withoutAllowRetreiveId, "my-bad-secret".toCharArray(), envProvider));
         if (alwaysRetrievable) {
             try {
-                a.getCredentials(withoutAllowRetreiveId.toCharArray(), envProvider);
+                a.withSecret(withoutAllowRetreiveId, new NSecretCaller<Object>() {
+                    @Override
+                    public Object call(NCredentialId id, char[] secretm, Function<String, String> env) {
+                        return null;
+                    }
+                }, envProvider);
                 Assertions.assertTrue(true);
             } catch (SecurityException ex) {
                 Assertions.assertTrue(false);
@@ -69,7 +63,12 @@ public class SecurityTest {
 
         } else {
             try {
-                a.getCredentials(withoutAllowRetreiveId.toCharArray(), envProvider);
+                a.withSecret(withoutAllowRetreiveId, new NSecretCaller<Object>() {
+                    @Override
+                    public Object call(NCredentialId id, char[] secretm, Function<String, String> env) {
+                        return null;
+                    }
+                }, envProvider);
                 Assertions.assertTrue(false);
             } catch (SecurityException ex) {
                 Assertions.assertTrue(true);
@@ -80,21 +79,33 @@ public class SecurityTest {
     }
 
     @Test
-    public void testCredentialsRetrievableDefault() {
-        testHelperRetrievable(new DefaultNAuthenticationAgent());
-        testHelperHashed(new DefaultNAuthenticationAgent(),false);
+    public void testCredentialsRetrievableDefaultV1() {
+        testHelperRetrievable(new DefaultNAuthenticationAgentV1());
+        testHelperHashed(new DefaultNAuthenticationAgentV1(), false);
+    }
+
+    @Test
+    public void testCredentialsRetrievableDefaultV2() {
+        testHelperRetrievable(new DefaultNAuthenticationAgentV2());
+        testHelperHashed(new DefaultNAuthenticationAgentV2(), false);
     }
 
     @Test
     public void testCredentialsRetrievablePlain() {
         testHelperRetrievable(new PlainNAuthenticationAgent());
-        testHelperHashed(new PlainNAuthenticationAgent(),true);
+        testHelperHashed(new PlainNAuthenticationAgent(), false);
     }
 
     @Test
-    public void testCredentialsHashedDefault() {
-        testHelperRetrievable(new DefaultNAuthenticationAgent());
-        testHelperHashed(new DefaultNAuthenticationAgent(),false);
+    public void testCredentialsHashedDefaultV1() {
+        testHelperRetrievable(new DefaultNAuthenticationAgentV1());
+        testHelperHashed(new DefaultNAuthenticationAgentV1(), false);
+    }
+
+    @Test
+    public void testCredentialsHashedDefaultV2() {
+        testHelperRetrievable(new DefaultNAuthenticationAgentV2());
+        testHelperHashed(new DefaultNAuthenticationAgentV2(), false);
     }
 
 }
