1d8453ebb6bc4ef351567a7c8ebddc529c7b3436
thevpc <taha.bensalah@gmail.com>
2026-02-02 02:40:24 +0100

    Update NFunction3.java : fixed-auth

--- core/nuts-api/src/main/java/net/thevpc/nuts/util/NFunction3.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/util/NFunction3.java
@@ -4,10 +4,10 @@ import net.thevpc.nuts.elem.*;
 
 import java.util.function.Supplier;
 
-public interface NFunction3<A, B, C, V> extends NElementRedescribable<NFunction3<A, B, C, V>> {
+public interface NFunction3<A, B, C, V> extends NRedescribable<NFunction3<A, B, C, V>> {
     V apply(A a, B b, C c);
 
-    default NFunction3<A, B, C, V> redescribe(Supplier<NElement> description) {
+    default NFunction3<A, B, C, V> withDescription(Supplier<NElement> description) {
         return description == null ? this : new NFunction3WithDescription<>(this, description);
     }
 
