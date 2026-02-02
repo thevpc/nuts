d864ff5cac44db04a4f3b77f9fa5fee7aab218d7
thevpc <taha.bensalah@gmail.com>
2026-02-02 02:40:27 +0100

    Update NStreamFromList.java : fixed-auth

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/util/stream/NStreamFromList.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/util/stream/NStreamFromList.java
@@ -1,84 +1,84 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package net.thevpc.nuts.runtime.standalone.util.stream;
-
-import net.thevpc.nuts.elem.NElementDescribables;
-import net.thevpc.nuts.elem.NElement;
-import net.thevpc.nuts.elem.NElementDescribable;
-import net.thevpc.nuts.util.NIterator;
-
-import java.util.*;
-import java.util.stream.Stream;
-
-/**
- * @param <T> collection element type
- * @author thevpc
- */
-public class NStreamFromList<T> extends NStreamBase<T> {
-
-    private final List<?> o;
-
-    public NStreamFromList(String nutsBase, List<T> o) {
-        super(nutsBase);
-        this.o = o;
-    }
-
-    @Override
-    public NElement describe() {
-        if(o instanceof NElementDescribable) {
-            return ((NElementDescribable) o).describe();
-        }
-        return NElementDescribables.describeResolveOrDestruct(o);
-    }
-
-    @Override
-    public List<T> toList() {
-        return (List<T>) o;
-    }
-
-    @Override
-    public Stream<T> stream() {
-        return ((Collection<T>) o).stream();
-    }
-
-    @Override
-    public NIterator<T> iterator() {
-        return NIterator.of(
-                ((Collection<T>) o).iterator()
-        );
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = 47 * hash + Objects.hashCode(this.o);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final NStreamFromList<?> other = (NStreamFromList<?>) obj;
-        if (!Objects.equals(this.o, other.o)) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "ListBasedResult" + "@" + Integer.toHexString(hashCode());
-    }
-
-}
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package net.thevpc.nuts.runtime.standalone.util.stream;
+//
+//import net.thevpc.nuts.elem.NElementDescribables;
+//import net.thevpc.nuts.elem.NElement;
+//import net.thevpc.nuts.elem.NElementDescribable;
+//import net.thevpc.nuts.util.NIterator;
+//
+//import java.util.*;
+//import java.util.stream.Stream;
+//
+///**
+// * @param <T> collection element type
+// * @author thevpc
+// */
+//public class NStreamFromList<T> extends NStreamBase<T> {
+//
+//    private final List<?> o;
+//
+//    public NStreamFromList(String nutsBase, List<T> o) {
+//        super(nutsBase);
+//        this.o = o;
+//    }
+//
+//    @Override
+//    public NElement describe() {
+//        if(o instanceof NElementDescribable) {
+//            return ((NElementDescribable) o).describe();
+//        }
+//        return NElementDescribables.describeResolveOrDestruct(o);
+//    }
+//
+//    @Override
+//    public List<T> toList() {
+//        return (List<T>) o;
+//    }
+//
+//    @Override
+//    public Stream<T> stream() {
+//        return ((Collection<T>) o).stream();
+//    }
+//
+//    @Override
+//    public NIterator<T> iteratorImpl() {
+//        return NIterator.of(
+//                ((Collection<T>) o).iterator()
+//        );
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        int hash = 7;
+//        hash = 47 * hash + Objects.hashCode(this.o);
+//        return hash;
+//    }
+//
+//    @Override
+//    public boolean equals(Object obj) {
+//        if (this == obj) {
+//            return true;
+//        }
+//        if (obj == null) {
+//            return false;
+//        }
+//        if (getClass() != obj.getClass()) {
+//            return false;
+//        }
+//        final NStreamFromList<?> other = (NStreamFromList<?>) obj;
+//        if (!Objects.equals(this.o, other.o)) {
+//            return false;
+//        }
+//        return true;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "ListBasedResult" + "@" + Integer.toHexString(hashCode());
+//    }
+//
+//}
