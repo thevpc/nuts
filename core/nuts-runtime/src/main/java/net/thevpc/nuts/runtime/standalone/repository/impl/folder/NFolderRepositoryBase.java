c82e4905e2444642d0be3c7a55eafeda104bccd8
thevpc <taha.bensalah@gmail.com>
2026-02-02 02:40:26 +0100

    Update NFolderRepositoryBase.java : fixed-auth

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/repository/impl/folder/NFolderRepositoryBase.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/repository/impl/folder/NFolderRepositoryBase.java
@@ -5,7 +5,7 @@ import net.thevpc.nuts.artifact.*;
 import net.thevpc.nuts.command.NFetchMode;
 import net.thevpc.nuts.command.NFetchModeNotSupportedException;
 import net.thevpc.nuts.core.NSession;
-import net.thevpc.nuts.elem.NElementDescribables;
+import net.thevpc.nuts.elem.NDescribables;
 import net.thevpc.nuts.elem.NElement;
 import net.thevpc.nuts.core.NSpeedQualifier;
 import net.thevpc.nuts.core.NStoreStrategy;
@@ -208,7 +208,7 @@ public abstract class NFolderRepositoryBase extends NCachedRepository {
                     try {
                         return NIterator.of(
                                 foldersFileUrl.stream()
-                                        .filter(NPath::isDirectory).redescribe(NElementDescribables.ofDesc("isDirectory"))
+                                        .filter(NPath::isDirectory).withDescription(NDescribables.ofDesc("isDirectory"))
                                         .map(versionFolder -> {
                                             String versionName = versionFolder.getName();
                                             NId expectedId = NIdBuilder.of(groupId, artifactId).setVersion(versionName).build();
@@ -223,8 +223,8 @@ public abstract class NFolderRepositoryBase extends NCachedRepository {
                                             }
                                             return null;
                                         })
-                                        .filterNonNull().iterator()
-                        ).redescribe(NElementDescribables.ofDesc("findNonSingleVersion"));
+                                        .nonNull().iterator()
+                        ).withDescription(NDescribables.ofDesc("findNonSingleVersion"));
                     } catch (UncheckedIOException | NIOException ex) {
                         return NIterator.ofEmpty();
                     }
