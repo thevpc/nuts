eb32c30000b809f8eab789ea68b948c49bec49f3
thevpc <taha.bensalah@gmail.com>
2026-02-08 11:54:00 +0100

    Update NSecretRunner.java : added-metadata

--- core/nuts-api/src/main/java/net/thevpc/nuts/security/NSecretRunner.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/security/NSecretRunner.java
@@ -3,5 +3,5 @@ package net.thevpc.nuts.security;
 import java.util.function.Function;
 
 public interface NSecretRunner {
-    void run(NCredentialId id, char[] secretm, Function<String, String> env);
+    void run(NSecureToken id, NSecureString secretm, Function<String, String> env);
 }
