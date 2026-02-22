14731886c629b6bd932e76704f75f75ba92903fe
thevpc <taha.bensalah@gmail.com>
2026-02-22 16:23:15 +0100

    Update NElementSeparator.java : first-ajor-fix-ntexup

--- core/nuts-api/src/main/java/net/thevpc/nuts/elem/NElementSeparator.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/elem/NElementSeparator.java
@@ -1,5 +1,17 @@
 package net.thevpc.nuts.elem;
 
 public interface NElementSeparator extends NAffix {
+    static NElementSeparator of(String value) {
+        return NElementFactory.of().ofSeparator(value);
+    }
+
+    static NElementSeparator of() {
+        return NElementFactory.of().ofSeparator(',');
+    }
+
+    static NElementSeparator of(char value) {
+        return NElementFactory.of().ofSeparator(value);
+    }
+
     String value();
 }
