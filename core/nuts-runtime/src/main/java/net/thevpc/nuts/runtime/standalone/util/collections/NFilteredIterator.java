5d669f2e47edc91926c3a3699fd004a2ea4a96e6
thevpc <taha.bensalah@gmail.com>
2026-03-06 07:48:39 +0100

    Update NFilteredIterator.java : fixed-v9

---
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/util/collections/NFilteredIterator.java
@@ -0,0 +1,97 @@
+/**
+ * ====================================================================
+ * Nuts : Network Updatable Things Service
+ * (universal package manager)
+ * <br>
+ * is a new Open Source Package Manager to help install packages
+ * and libraries for runtime execution. Nuts is the ultimate companion for
+ * maven (and other build managers) as it helps installing all package
+ * dependencies at runtime. Nuts is not tied to java and is a good choice
+ * to share shell scripts and other 'things' . Its based on an extensible
+ * architecture to help supporting a large range of sub managers / repositories.
+ *
+ * <br>
+ * <p>
+ * Copyright [2020] [thevpc]
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE Version 3 (the "License");
+ * you may  not use this file except in compliance with the License. You may obtain
+ * a copy of the License at https://www.gnu.org/licenses/lgpl-3.0.en.html
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ * <br>
+ * ====================================================================
+ */
+package net.thevpc.nuts.runtime.standalone.util.collections;
+
+import net.thevpc.nuts.elem.NDescribables;
+import net.thevpc.nuts.elem.NElement;
+import net.thevpc.nuts.util.NIterator;
+import net.thevpc.nuts.util.NIteratorBase;
+
+import java.util.Iterator;
+import java.util.function.Predicate;
+
+/**
+ * Created by vpc on 1/9/17.
+ */
+public class NFilteredIterator<T> extends NIteratorBase<T> {
+
+    private final Iterator<T> base;
+    private final Predicate<? super T> filter;
+    private T last;
+
+    public NFilteredIterator(Iterator<T> base, Predicate<? super T> filter) {
+        if (base == null) {
+            this.base = NIterator.ofEmpty();
+        } else {
+            this.base = base;
+        }
+        //Predicate<? super T>
+//        NElementDescribables.cast(filter);
+        this.filter = filter;
+    }
+
+    @Override
+    public NElement describe() {
+        return NElement.ofObjectBuilder()
+                .name("Filter")
+                .set("base", NDescribables.describeResolveOrDestruct(base))
+                .set("accept", NDescribables.describeResolveOrToString(filter))
+                .build()
+                ;
+    }
+
+    @Override
+    public boolean hasNextImpl() {
+        while (true) {
+            if (base.hasNext()) {
+                last = base.next();
+                if (filter.test(last)) {
+                    return true;
+                }
+            } else {
+                return false;
+            }
+        }
+    }
+
+    @Override
+    public T next() {
+        return last;
+    }
+
+    @Override
+    public void remove() {
+        base.remove();
+    }
+
+    @Override
+    public String toString() {
+        return filter.toString() + "(" +
+                base +
+                ')';
+    }
+}
