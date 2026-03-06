576aa158501256990b50f18e55e3b2a691f52882
thevpc <taha.bensalah@gmail.com>
2026-03-06 07:48:39 +0100

    Update NSupplierIteratorJ.java : fixed-v9

---
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/util/collections/NSupplierIteratorJ.java
@@ -0,0 +1,68 @@
+package net.thevpc.nuts.runtime.standalone.util.collections;
+
+import net.thevpc.nuts.elem.NDescribables;
+import net.thevpc.nuts.elem.NElement;
+import net.thevpc.nuts.util.NIterator;
+import net.thevpc.nuts.util.NIteratorBase;
+
+import java.util.Iterator;
+import java.util.function.Supplier;
+
+public class NSupplierIteratorJ<T> extends NIteratorBase<T> {
+
+    private final Supplier<Iterator<T>> from;
+    private NIterator<T> iterator;
+    private Supplier<NElement> name;
+
+    public NSupplierIteratorJ(Supplier<Iterator<T>> from, Supplier<NElement> name) {
+        this.from = from;
+        this.name = name;
+    }
+
+    @Override
+    public NElement describe() {
+        return NElement.ofObjectBuilder()
+                .name("Supplier")
+                .set("template",
+                        NDescribables.describeResolveOr(from, () -> {
+                            NElement t = name.get();
+                            return NElement.ofObjectBuilder().name("Compiled")
+                                    .addAll(t == null ? null : t.asObject().orNull())
+                                    .build();
+                        })
+                )
+                .build();
+    }
+
+    @Override
+    public boolean hasNextImpl() {
+        if (iterator == null) {
+            Iterator<T> it = from.get();
+            iterator = it == null ? null : NIterator.of(it).withDescription(name);
+            if (iterator == null) {
+                return false;
+            }
+        }
+        return iterator.hasNext();
+    }
+
+    @Override
+    public T next() {
+        return iterator.next();
+    }
+
+    @Override
+    public void remove() {
+        if (iterator != null) {
+            iterator.remove();
+        }
+    }
+
+    @Override
+    public String toString() {
+        if (name == null) {
+            return "supplier(" + from + ")";
+        }
+        return String.valueOf(name);
+    }
+}
