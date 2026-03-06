fadea682353da4ff6d9f2d34446d7e0b1d3788b6
thevpc <taha.bensalah@gmail.com>
2026-03-06 07:48:39 +0100

    Update NQueueIterator.java : fixed-v9

---
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/util/collections/NQueueIterator.java
@@ -0,0 +1,114 @@
+/**
+ * ====================================================================
+ *            Nuts : Network Updatable Things Service
+ *                  (universal package manager)
+ * <br>
+ * is a new Open Source Package Manager to help install packages
+ * and libraries for runtime execution. Nuts is the ultimate companion for
+ * maven (and other build managers) as it helps installing all package
+ * dependencies at runtime. Nuts is not tied to java and is a good choice
+ * to share shell scripts and other 'things' . Its based on an extensible
+ * architecture to help supporting a large range of sub managers / repositories.
+ *
+ * <br>
+ *
+ * Copyright [2020] [thevpc]
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE Version 3 (the "License");
+ * you may  not use this file except in compliance with the License. You may obtain
+ * a copy of the License at https://www.gnu.org/licenses/lgpl-3.0.en.html
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ * <br>
+ * ====================================================================
+*/
+package net.thevpc.nuts.runtime.standalone.util.collections;
+
+import net.thevpc.nuts.elem.NDescribables;
+import net.thevpc.nuts.elem.NElement;
+import net.thevpc.nuts.util.NAssert;
+import net.thevpc.nuts.util.NIterator;
+import net.thevpc.nuts.util.NIteratorBase;
+
+import java.util.*;
+
+/**
+ * Created by vpc on 1/7/17.
+ */
+public class NQueueIterator<T> extends NIteratorBase<T> {
+
+    private Queue<NIterator<? extends T>> children = new LinkedList<NIterator<? extends T>>();
+    private int size;
+
+
+    @Override
+    public NElement describe() {
+        return NElement.ofObjectBuilder()
+                .name("Queue")
+                .addAll(
+                        children
+                                .stream().map(
+                                        x -> NDescribables.describeResolveOrDestruct(x)
+                                ).toArray(NElement[]::new)
+                )
+                .build();
+    }
+
+    public void addNonNull(NIterator<? extends T> child) {
+        if (child != null) {
+            add(child);
+        }
+    }
+
+    public void addNonEmpty(NIterator<? extends T> child) {
+        child = NIterator.ofNullifyIfEmpty(child);
+        if (child != null) {
+            add(child);
+        }
+    }
+
+    public void add(NIterator<? extends T> child) {
+        NAssert.requireNamedNonNull(child, "child");
+        children.add(child);
+        size++;
+    }
+
+    @Override
+    public boolean hasNextImpl() {
+        while (!children.isEmpty()) {
+            if (children.peek().hasNext()) {
+                return true;
+            }
+            children.poll();
+            size--;
+        }
+        return false;
+    }
+
+    @Override
+    public T next() {
+        return children.peek().next();
+    }
+
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void remove() {
+        children.peek().remove();
+    }
+
+    public NIterator<T>[] getChildren() {
+        return children.toArray(new NIterator[0]);
+    }
+
+    @Override
+    public String toString() {
+        return "QueueIterator(" +
+                children +
+                ')';
+    }
+}
