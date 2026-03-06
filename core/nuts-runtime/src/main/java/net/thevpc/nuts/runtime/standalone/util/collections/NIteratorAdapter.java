0ac2145dc8336d51aeae2cd057a059ee4e9c54ec
thevpc <taha.bensalah@gmail.com>
2026-03-06 07:48:39 +0100

    Update NIteratorAdapter.java : fixed-v9

---
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/util/collections/NIteratorAdapter.java
@@ -0,0 +1,48 @@
+package net.thevpc.nuts.runtime.standalone.util.collections;
+
+import net.thevpc.nuts.elem.NDescribables;
+import net.thevpc.nuts.elem.NElement;
+import net.thevpc.nuts.util.NIteratorBase;
+
+import java.util.Iterator;
+import java.util.function.Supplier;
+
+public class NIteratorAdapter<T> extends NIteratorBase<T> {
+    private final Iterator<T> base;
+    private final Supplier<NElement> info;
+
+    public NIteratorAdapter(Iterator<T> base, Supplier<NElement> info) {
+        this.base = base;
+        this.info = info;
+    }
+
+    public Iterator<T> getBase() {
+        return base;
+    }
+
+    @Override
+    public boolean hasNextImpl() {
+        boolean b = base.hasNext();
+        if(!b){
+            close();
+        }
+        return b;
+    }
+
+    @Override
+    public T next() {
+        return base.next();
+    }
+
+    @Override
+    public NElement describe() {
+        NElement a = info.get();
+        if(!a.isObject()){
+            a= NElement.ofObjectBuilder().set("name",a).build();
+        }
+        return NDescribables.describeResolveOrDestructAsObject(base)
+                .builder()
+                        .addAll(a.asObject().get().children().toArray(new NElement[0]))
+                .build();
+    }
+}
