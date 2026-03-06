32e909c432ba1867451ce570613b6ed7990ef91a
thevpc <taha.bensalah@gmail.com>
2026-03-06 07:48:33 +0100

    Update NCollectionsRPI.java : fixed-v9

--- core/nuts-api/src/main/java/net/thevpc/nuts/internal/rpi/NCollectionsRPI.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/internal/rpi/NCollectionsRPI.java
@@ -26,15 +26,17 @@
  */
 package net.thevpc.nuts.internal.rpi;
 
+import net.thevpc.nuts.concurrent.NRunnable;
+import net.thevpc.nuts.elem.NElement;
 import net.thevpc.nuts.ext.NExtensions;
+import net.thevpc.nuts.io.NPath;
 import net.thevpc.nuts.spi.NComponent;
-import net.thevpc.nuts.util.NIterable;
-import net.thevpc.nuts.util.NIterator;
-import net.thevpc.nuts.util.NOptional;
-import net.thevpc.nuts.util.NStream;
+import net.thevpc.nuts.util.*;
 
-import java.util.Iterator;
-import java.util.Optional;
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 /**
@@ -50,6 +52,7 @@ public interface NCollectionsRPI extends NComponent {
     <T> NStream<T> iterableToStream(Iterable<T> str);
 
     <T> NStream<T> iteratorToStream(Iterator<T> str);
+    <T> NStream<T> iteratorToStream(Iterator<T> str,Runnable onClose);
 
     <T> NStream<T> toStream(Stream<T> str);
 
@@ -64,4 +67,98 @@ public interface NCollectionsRPI extends NComponent {
     <T> NStream<T> optionalToStream(Optional<T> str);
 
     <T> NStream<T> optionalToStream(NOptional<T> str);
+
+    <T> NChunkedStoreBuilder<T> chunkedStoreBuilder(NPath folder, NChunkedStoreFactory<T> storeFactory);
+
+    NChunkedStoreFactory<String> lineChunkedStoreFactory();
+
+    /**
+     * Constructor
+     */
+    <K extends Comparable<K>, V> NBPlusTree<K, V> btreePlus(int m, boolean allowDuplicates);
+
+    <K extends Comparable<K>, V> NBPlusTree<K, V> btreePlus(int m);
+    <K extends Comparable<K>, V> NBPlusTree<K, V> btreePlus(NBPlusTreeStore<K, V> store);
+
+    <V> NClassMap<V> classMap(Class<V> valueType);
+
+    <V> NClassMap<V> classMap(Class keyType, Class<V> valueType);
+
+    <V> NClassMap<V> classMap(Class keyType, Class<V> valueType, int initialCapacity);
+
+    NClassMap<Class<?>> classClassMap();
+
+    <T> Map<String, T> caseInsensitiveMap();
+
+    <T> Map<String, T> caseInsensitiveMap(Map<String, T> other);
+
+    <K, V> NMultiKeyMap<K, V> multiKeyMap();
+
+    <V> NStringMap<V> stringMap(Map<String, V> map, char separator);
+
+    <T> NIterator<T> iteratorWithDescription(NIterator<T> base, Supplier<NElement> description, Runnable onClose);
+
+    <T> NIterator<T> iteratorAutoClosable(NIterator<T> t, NRunnable close);
+
+    <T> NIterator<T> iteratorSafe(NIteratorErrorHandlerType type, NIterator<T> t);
+
+    <T> NIterator<T> iteratorSafeIgnore(NIterator<T> t);
+
+    <T> NIterator<T> iteratorSafePostpone(NIterator<T> t);
+
+    <T> boolean iteratorIsNullOrEmpty(Iterator<T> t);
+
+    <T> NIterator<T> iteratorNonNull(NIterator<T> t);
+
+    <T> NIterator<T> iteratorConcat(List<NIterator<? extends T>> all);
+
+    <T> NIterator<T> iteratorCoalesce2(List<NIterator<T>> all);
+
+    <T> NIterator<T> iteratorCoalesce(NIterator<? extends T>... all);
+
+    <T> NIterator<T> iteratorConcat(NIterator<? extends T>... all);
+
+    <T> NIterator<T> iteratorConcatLists(List<NIterator<? extends T>>... all);
+
+    <T> NIterator<T> iteratorCoalesce(List<NIterator<? extends T>> all);
+
+    <F, T> NIterator<T> iteratorConvertNonNull(NIterator<F> from, Function<F, T> converter, String name);
+
+    <T> List<T> iteratorToList(Iterator<T> it);
+
+    <T> Set<T> iteratorToSet(NIterator<T> it);
+
+    <T> Set<T> iteratorToTreeSet(NIterator<T> it, NComparator<T> c);
+
+    <T> NIterator<T> iteratorSort(NIterator<T> it, NComparator<T> c, boolean removeDuplicates);
+
+    <T> NIterator<T> iteratorDistinct(NIterator<T> it);
+
+    <F, T> NIterator<F> iteratorDistinct(NIterator<F> it, Function<F, T> converter);
+
+    <T> NIterator<T> iteratorCollector(Iterator<T> it, Consumer<T> consumer);
+
+    <T> NIterator<T> iteratorNullifyIfEmpty(NIterator<T> other);
+
+    <T> NIteratorBuilder<T> iteratorBuilderOfCoalesce(List<NIterator<? extends T>> t);
+
+    <T> NIteratorBuilder<T> iteratorBuilderOfConcat(List<NIterator<? extends T>> t);
+
+    <T> NIteratorBuilder<T> iteratorBuilder(Iterator<T> t);
+
+    <T> NIteratorBuilder<T> iteratorBuilderOfRunnable(NRunnable t);
+
+    <T> NIteratorBuilder<T> iteratorBuilderOfRunnable(Runnable t, String n);
+
+    <T> NIteratorBuilder<T> iteratorBuilderOfSupplier(Supplier<Iterator<T>> from, Supplier<NElement> name);
+
+    <T> NIteratorBuilder<T> iteratorBuilderOfArrayValues(T[] t, NElement n);
+
+    <T> NIteratorBuilder<T> iteratorBuilderOfArrayValues(T[] t, String n);
+
+    <T> NIteratorBuilder<T> iteratorBuilderOfArrayValues(T[] t, Supplier<NElement> n);
+
+    <T> NIteratorBuilder<T> iteratorEmptyBuilder();
+
+    <T> NIteratorBuilder<T> iteratorBuilderOfFlatMap(NIterator<? extends Collection<T>> from);
 }
