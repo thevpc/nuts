724f5593cd8a85a0286ae59f1e7dd803175ab58b
thevpc <taha.bensalah@gmail.com>
2026-03-06 07:48:33 +0100

    Update NStringMap.java : fixed-v9

--- core/nuts-api/src/main/java/net/thevpc/nuts/util/NStringMap.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/util/NStringMap.java
@@ -28,172 +28,41 @@ package net.thevpc.nuts.util;
 
 import java.util.*;
 
-public class NStringMap<V> {
-    private Map<String, V> map;
-    private char separator;
+public interface NStringMap<V> extends NCopiable{
 
-    public NStringMap(Map<String, V> map, char separator) {
-        NAssert.requireNamedNonNull(map, "map");
-        this.map = map;
-        this.separator = separator;
-    }
+    public NStringMap<V> clear() ;
 
-    public NStringMap<V> clear() {
-        map.clear();
-        return this;
-    }
+    public int size() ;
 
-    public int size() {
-        return map.size();
-    }
+    public char getSeparator() ;
 
-    public char getSeparator() {
-        return separator;
-    }
+    public Map<String, V> toMap(String prefix) ;
 
-    public Map<String, V> toMap(String prefix) {
-        NAssert.requireNamedNonNull(prefix, "prefix");
-        if (prefix.isEmpty()) {
-            return new LinkedHashMap<>(map);
-        }
-        Map<String, V> result = new LinkedHashMap<>();
-        for (Iterator<Map.Entry<String, V>> iterator = map.entrySet().iterator(); iterator.hasNext(); ) {
-            Map.Entry<String, V> e = iterator.next();
-            String k = e.getKey();
-            if (k.startsWith(prefix)) {
-                if (k.length() == prefix.length()) {
-                    result.put("", e.getValue());
-                } else if (k.charAt(prefix.length()) == separator) {
-                    result.put(k.substring(prefix.length() + 1), e.getValue());
-                }
-            } else {
-                //just ignore!
-            }
-        }
-        return result;
-    }
+    public Map<String, V> toMap() ;
 
-    public Map<String, V> toMap() {
-        return new LinkedHashMap<>(map);
-    }
+    public NStringMap<V> removeAll(String prefix) ;
 
-    public NStringMap<V> removeAll(String prefix) {
-        NAssert.requireNamedNonNull(prefix, "prefix");
-        if (prefix.isEmpty()) {
-            return this;
-        }
-        for (Iterator<Map.Entry<String, V>> iterator = map.entrySet().iterator(); iterator.hasNext(); ) {
-            Map.Entry<String, V> e = iterator.next();
-            String k = e.getKey();
-            if (k.startsWith(prefix)) {
-                if (k.length() == prefix.length()) {
-                    iterator.remove();
-                } else if (k.charAt(prefix.length()) == separator) {
-                    iterator.remove();
-                }
-            } else {
-                //just ignore!
-            }
-        }
-        return this;
-    }
+    public NStringMap<V> putAll(Map<String, V> values) ;
 
-    public NStringMap<V> putAll(Map<String, V> values) {
-        NAssert.requireNamedNonNull(values, "values");
-        map.putAll(values);
-        return this;
-    }
+    public V put(String prefix, String key, V value) ;
 
-    public V put(String prefix, String key, V value) {
-        NAssert.requireNamedNonNull(prefix, "prefix");
-        return map.put(keyOf(prefix, key), value);
-    }
+    public V put(String key, V value) ;
 
-    public V put(String key, V value) {
-        return map.put(key, value);
-    }
+    public V get(String key) ;
 
-    public V get(String key) {
-        return map.get(key);
-    }
+    public NOptional<V> getOptional(String prefix, String key) ;
 
-    public NOptional<V> getOptional(String prefix, String key) {
-        return getOptional(keyOf(prefix, key));
-    }
+    public NOptional<V> getOptional(String key) ;
 
-    public NOptional<V> getOptional(String key) {
-        V v = map.get(key);
-        if (v == null) {
-            if (map.containsKey(key)) {
-                return NOptional.ofNull();
-            }
-            return NOptional.ofNamedEmpty(key);
-        }
-        return NOptional.of(v);
-    }
+    public V set(String prefix, String key, V value) ;
 
-    public V set(String prefix, String key, V value) {
-        NAssert.requireNamedNonNull(prefix, "prefix");
-        if (value == null) {
-            return map.remove(keyOf(prefix, key));
-        } else {
-            return map.put(keyOf(prefix, key), value);
-        }
-    }
+    public V set(String key, V value) ;
 
-    public V set(String key, V value) {
-        if (value == null) {
-            return map.remove(key);
-        } else {
-            return map.put(key, value);
-        }
-    }
+    public V remove(String prefix, String key) ;
 
-    public V remove(String prefix, String key) {
-        NAssert.requireNamedNonNull(prefix, "prefix");
-        return map.remove(keyOf(prefix, key));
-    }
+    public Set<String> nextKeys(String prefix) ;
 
-    public Set<String> nextKeys(String prefix) {
-        Set<String> keys = new LinkedHashSet<>();
-        NAssert.requireNamedNonNull(prefix, "prefix");
-        for (Iterator<Map.Entry<String, V>> iterator = map.entrySet().iterator(); iterator.hasNext(); ) {
-            Map.Entry<String, V> e = iterator.next();
-            String k = e.getKey();
-            if (k.startsWith(prefix)) {
-                if (k.length() == prefix.length()) {
-                    //
-                } else if (k.charAt(prefix.length()) == separator) {
-                    String n = k.substring(prefix.length() + 1);
-                    int d = n.indexOf(separator);
-                    if (d >= 0) {
-                        keys.add(n.substring(0, d));
-                    } else {
-                        keys.add(n);
-                    }
-                }
-            } else {
-                //just ignore!
-            }
-        }
-        return keys;
-    }
+    public NStringMap<V> putAll(String prefix, Map<String, V> values) ;
 
-    public NStringMap<V> putAll(String prefix, Map<String, V> values) {
-        NAssert.requireNamedNonNull(prefix, "prefix");
-        NAssert.requireNamedNonNull(values, "values");
-        for (Map.Entry<String, V> e : values.entrySet()) {
-            String k = e.getKey();
-            map.put(keyOf(prefix, k), e.getValue());
-        }
-        return this;
-    }
-
-    private String keyOf(String prefix, String k) {
-        return k.isEmpty() ? prefix : (prefix + separator + k);
-    }
-
-    public NStringMap<V> copy() {
-        return new NStringMap<>(new LinkedHashMap<>(map), separator);
-    }
+    public NStringMap<V> copy() ;
 }
