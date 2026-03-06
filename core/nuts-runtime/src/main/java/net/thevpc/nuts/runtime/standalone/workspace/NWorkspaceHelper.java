31865160c304dd46586ff21333adb0e98f98186f
thevpc <taha.bensalah@gmail.com>
2026-03-06 07:48:36 +0100

    Update NWorkspaceHelper.java : fixed-v9

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/workspace/NWorkspaceHelper.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/workspace/NWorkspaceHelper.java
@@ -26,11 +26,27 @@
 */
 package net.thevpc.nuts.runtime.standalone.workspace;
 
+import net.thevpc.nuts.app.NApp;
+import net.thevpc.nuts.app.NApplicationHandleMode;
+import net.thevpc.nuts.app.NApplications;
+import net.thevpc.nuts.artifact.NId;
+import net.thevpc.nuts.boot.NBootWorkspaceImpl;
+import net.thevpc.nuts.cmdline.NCmdLine;
+import net.thevpc.nuts.command.NExec;
+import net.thevpc.nuts.command.NExecutionException;
 import net.thevpc.nuts.command.NFetchStrategy;
+import net.thevpc.nuts.core.NBootOptions;
 import net.thevpc.nuts.core.NRepository;
 import net.thevpc.nuts.core.NRepositoryFilter;
+import net.thevpc.nuts.core.NWorkspace;
+import net.thevpc.nuts.log.NLog;
+import net.thevpc.nuts.log.NMsgIntent;
+import net.thevpc.nuts.runtime.standalone.DefaultNBootOptionsBuilder;
+import net.thevpc.nuts.text.NI18n;
+import net.thevpc.nuts.text.NMsg;
 
 import java.util.*;
+import java.util.logging.Level;
 
 /**
  *
@@ -62,5 +78,101 @@ public class NWorkspaceHelper {
             }
         }
         return repos;
+    }
+
+    public static void runBootCommand(NWorkspace workspace) {
+        workspace.runWith(() -> {
+            NBootOptions info2 = new DefaultNBootOptionsBuilder(((NWorkspaceExt)workspace).getCallerBootOptionsInfo()).build();
+            NApp.of().setId(workspace.getApiId());
+            NLog LOG = NLog.of(NBootWorkspaceImpl.class);
+            LOG.log(NMsg.ofC("running workspace in %s mode", getRunModeString(info2))
+                    .withLevel(Level.CONFIG).withIntent(NMsgIntent.SUCCESS)
+            );
+            NExec execCmd = NExec.of()
+                    .setExecutionType(info2.getExecutionType().orNull())
+                    .setRunAs(info2.getRunAs().orNull())
+                    .failFast();
+            List<String> executorOptions = info2.getExecutorOptions().orNull();
+            if (executorOptions != null) {
+                execCmd.configure(true, executorOptions.toArray(new String[0]));
+            }
+            NCmdLine executorOptionsCmdLine = NCmdLine.of(executorOptions).setExpandSimpleOptions(false);
+            while (executorOptionsCmdLine.hasNext()) {
+                execCmd.configureLast(executorOptionsCmdLine);
+            }
+            if (info2.getApplicationArguments().get().isEmpty()) {
+                if (info2.getSkipWelcome().orElse(false)) {
+                    return;
+                }
+                execCmd.addCommand("welcome");
+            } else {
+                execCmd.addCommand(info2.getApplicationArguments().get());
+            }
+            execCmd.run();
+        });
+
+    }
+    public static void runApplication(NWorkspace workspace,NApplicationHandleMode handleMode) {
+        NApplicationHandleMode.runHandled(() ->
+                workspace.runWith(() -> {
+                    boolean inherited = NWorkspace.of().getBootOptions().getInherited().orElse(false);
+                    NApp nApp = NApp.of();
+                    // Resolve the application class name (explicit or fallback)
+                    String appClassName = nApp.getSourceType() == null ? null : nApp.getSourceType().getName();
+                    if (appClassName == null) {
+                        appClassName = nApp.getSource() == null ? null : nApp.getSource().getClass().getName();
+                    }
+                    NId appId = nApp.getId().orNull();
+                    NLog.of(NApplications.class)
+                            .log(
+                                    NMsg.ofC(
+                                            NI18n.of("running application %s: %s (%s) %s"),
+                                            inherited ? ("(" + NI18n.of("inherited") + ")") : "",
+                                            appId == null ? ("<" + NI18n.of("unresolved-id") + ">") : appId,
+                                            appClassName,
+                                            nApp.getCmdLine()
+                                    ).asFine().withIntent(NMsgIntent.START)
+                            );
+                    try {
+                        switch (nApp.getMode()) {
+                            //both RUN and AUTO_COMPLETE execute the run branch. Later
+                            //session.isExecMode()
+                            case RUN:
+                            case AUTO_COMPLETE: {
+                                nApp.getApplication().run();
+                                return;
+                            }
+                            case INSTALL: {
+                                nApp.getApplication().onInstallApplication();
+                                return;
+                            }
+                            case UPDATE: {
+                                nApp.getApplication().onUpdateApplication();
+                                return;
+                            }
+                            case UNINSTALL: {
+                                nApp.getApplication().onUninstallApplication();
+                                return;
+                            }
+                        }
+                    } catch (NExecutionException e) {
+                        if (e.getExitCode() == NExecutionException.SUCCESS) {
+                            return;
+                        }
+                        throw e;
+                    }
+                    throw new NExecutionException(NMsg.ofC(NI18n.of("unsupported execution mode %s"), nApp.getMode()), NExecutionException.ERROR_255);
+                }), handleMode
+        );
+    }
+
+    protected static String getRunModeString(NBootOptions options) {
+        if (options.getReset().orElse(false)) {
+            return "reset";
+        } else if (options.getRecover().orElse(false)) {
+            return "recover";
+        } else {
+            return "exec";
+        }
     }
 }
