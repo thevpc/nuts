69faac31b599ec04eda493da5072b3d89de4b7a6
thevpc <taha.bensalah@gmail.com>
2026-03-06 07:48:38 +0100

    Update NCoalesceIterator.java : fixed-v9

---
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/util/collections/NCoalesceIterator.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.thevpc.nuts.runtime.standalone.util.collections;
+
+import net.thevpc.nuts.elem.NDescribables;
+import net.thevpc.nuts.elem.NElement;
+import net.thevpc.nuts.util.NAssert;
+import net.thevpc.nuts.util.NIterator;
+import net.thevpc.nuts.util.NIteratorBase;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author thevpc
+ */
+public class NCoalesceIterator<T> extends NIteratorBase<T> {
+
+    private Queue<NIterator<? extends T>> children = new LinkedList<NIterator<? extends T>>();
+    private int size = 0;
+
+    @Override
+    public NElement describe() {
+        return NElement.ofObjectBuilder()
+                .name("Coalesce")
+                .addAll(
+                        children
+                                .stream().map(
+                                        x -> NDescribables.describeResolveOrDestruct(x)
+                                ).toArray(NElement[]::new)
+                )
+                .build();
+    }
+
+
+    public void addNonNull(NIterator<T> child) {
+        if (child != null) {
+            add(child);
+        }
+    }
+
+    public void addNonEmpty(NIterator<T> child) {
+        child = NIterator.ofNullifyIfEmpty(child);
+        if (child != null) {
+            add(child);
+        }
+    }
+
+    public void add(NIterator<? extends T> child) {
+        NAssert.requireNamedNonNull(child, "child");
+        if (child instanceof NCoalesceIterator) {
+            children.addAll(((NCoalesceIterator<? extends T>) child).children);
+        } else {
+            children.add(child);
+        }
+        size=children.size();
+    }
+
+    public int size() {
+        return children.size();
+    }
+
+    public NIterator<T>[] getChildren() {
+        return children.toArray(new NIterator[0]);
+    }
+
+    @Override
+    public boolean hasNextImpl() {
+        while (!children.isEmpty()) {
+            if (children.peek().hasNext()) {
+                if (size > 1) {
+                    //should remove all successors;
+                    NIterator<? extends T> h = children.poll();
+                    children.clear();
+                    children.offer(h);
+                    size = 1;
+                }
+                return true;
+            }
+            children.poll();
+            size--;
+        }
+        return false;
+    }
+
+    public T next() {
+        return children.peek().next();
+    }
+
+    public void remove() {
+        children.peek().remove();
+    }
+
+    @Override
+    public String toString() {
+        return "Coalesce(" +
+                children +
+                ')';
+    }
+}
