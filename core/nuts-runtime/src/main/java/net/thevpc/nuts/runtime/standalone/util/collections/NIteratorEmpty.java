2e3af7c44637ef6e43a99001614324869c0324c5
thevpc <taha.bensalah@gmail.com>
2026-03-06 07:48:39 +0100

    Update NIteratorEmpty.java : fixed-v9

---
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/util/collections/NIteratorEmpty.java
@@ -0,0 +1,41 @@
+package net.thevpc.nuts.runtime.standalone.util.collections;
+
+import net.thevpc.nuts.elem.NElement;
+import net.thevpc.nuts.util.NAssert;
+import net.thevpc.nuts.util.NIteratorBase;
+
+import java.util.NoSuchElementException;
+import java.util.function.Consumer;
+
+public class NIteratorEmpty<E> extends NIteratorBase<E> {
+
+    @Override
+    public boolean hasNextImpl() {
+        return false;
+    }
+
+    @Override
+    public E next() {
+        throw new NoSuchElementException();
+    }
+
+    @Override
+    public void remove() {
+        throw new IllegalStateException();
+    }
+
+    @Override
+    public void forEachRemaining(Consumer<? super E> action) {
+        NAssert.requireNamedNonNull(action);
+    }
+
+    @Override
+    public String toString() {
+        return "EmptyIterator";
+    }
+
+    @Override
+    public NElement describe() {
+        return NElement.ofString("empty");
+    }
+}
