ec421c441d9227b1a6b7419779421e76a8bb9a26
thevpc <taha.bensalah@gmail.com>
2026-03-06 07:48:38 +0100

    Update NDistinctPredicate.java : fixed-v9

---
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/util/collections/NDistinctPredicate.java
@@ -0,0 +1,30 @@
+package net.thevpc.nuts.runtime.standalone.util.collections;
+
+import net.thevpc.nuts.elem.NElement;
+import net.thevpc.nuts.spi.base.AbstractNPredicate;
+
+import java.util.HashSet;
+
+class NDistinctPredicate<T> extends AbstractNPredicate<T> {
+    private HashSet<T> visited = new HashSet<>();
+
+    @Override
+    public boolean test(T value) {
+        if (visited.contains(value)) {
+            return false;
+        }
+        visited.add(value);
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Distinct";
+    }
+
+    @Override
+    public NElement describe() {
+        return NElement.ofString("distinct");
+    }
+
+}
