8110c2675329640d6143484d806c1c176b19aa88
thevpc <taha.bensalah@gmail.com>
2026-03-06 07:48:39 +0100

    Update NIteratorFromJavaIterator.java : fixed-v9

---
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/util/collections/NIteratorFromJavaIterator.java
@@ -0,0 +1,54 @@
+package net.thevpc.nuts.runtime.standalone.util.collections;
+
+import net.thevpc.nuts.elem.NDescribables;
+import net.thevpc.nuts.elem.NElement;
+import net.thevpc.nuts.util.NIteratorBase;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class NIteratorFromJavaIterator<T> extends NIteratorBase<T> {
+
+    private String name;
+    private Iterator<T> base;
+    private List<T> collected = new ArrayList<>();
+
+    public NIteratorFromJavaIterator(String name, Iterator<T> base) {
+        this.name = name;
+        this.base = base;
+    }
+
+    @Override
+    public NElement describe() {
+        return NElement.ofObjectBuilder()
+                .name("Collector")
+                .addParam(NElement.ofString(name))
+                .set("base", NDescribables.describeResolveOrDestruct(base))
+                .build();
+    }
+
+    @Override
+    public boolean hasNextImpl() {
+        return base.hasNext();
+    }
+
+    @Override
+    public T next() {
+        T x = base.next();
+        collected.add(x);
+        return x;
+    }
+
+    public List<T> getCollected() {
+        return collected;
+    }
+
+    @Override
+    public String toString() {
+        if (name == null) {
+            return "collector(" + base + ")";
+        }
+        return String.valueOf(name);
+    }
+}
