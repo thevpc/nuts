23c6e582ebdfdda00a735ac96db8779c89f2453b
thevpc <taha.bensalah@gmail.com>
2026-03-06 07:48:39 +0100

    Update NIteratorOnFinish.java : fixed-v9

---
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/util/collections/NIteratorOnFinish.java
@@ -0,0 +1,46 @@
+package net.thevpc.nuts.runtime.standalone.util.collections;
+
+import net.thevpc.nuts.concurrent.NRunnable;
+import net.thevpc.nuts.elem.NDescribables;
+import net.thevpc.nuts.elem.NElement;
+import net.thevpc.nuts.util.NIteratorBase;
+
+import java.util.Iterator;
+
+class NIteratorOnFinish<T> extends NIteratorBase<T> {
+
+    private final Iterator<T> base;
+    private final NRunnable r;
+
+    public NIteratorOnFinish(Iterator<T> base, NRunnable r) {
+        this.base = base;
+        this.r = r;
+    }
+
+    @Override
+    public NElement describe() {
+        return NDescribables.describeResolveOrDestructAsObject(base)
+                .builder()
+                .set("onFinish", NDescribables.describeResolveOrToString(r))
+                .build()
+                ;
+    }
+
+    @Override
+    public boolean hasNextImpl() {
+        boolean n = base.hasNext();
+        if (!n) {
+            r.run();
+        }
+        return n;
+    }
+
+    public NRunnable getCloseRunnable() {
+        return r;
+    }
+
+    @Override
+    public T next() {
+        return base.next();
+    }
+}
