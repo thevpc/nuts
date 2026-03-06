76e9ec4e2bdfc65843b2d8f272ecfe95214e77b7
thevpc <taha.bensalah@gmail.com>
2026-03-06 07:48:33 +0100

    Update NIteratorBase.java : fixed-v9

--- core/nuts-api/src/main/java/net/thevpc/nuts/util/NIteratorBase.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/util/NIteratorBase.java
@@ -2,7 +2,6 @@ package net.thevpc.nuts.util;
 
 import net.thevpc.nuts.elem.NElement;
 
-import java.util.Iterator;
 import java.util.function.Supplier;
 
 public abstract class NIteratorBase<T> implements NIterator<T> {
@@ -11,14 +10,14 @@ public abstract class NIteratorBase<T> implements NIterator<T> {
         if (closeHandler == null) {
             return this;
         }
-        return new NIteratorWithDescription<>(this, null, closeHandler);
+        return NIterator.ofWithDescription(this, null, closeHandler);
     }
 
     public NIterator<T> withDescription(Supplier<NElement> description) {
         if (description == null) {
             return this;
         }
-        return new NIteratorWithDescription<>(this, description, null);
+        return NIterator.ofWithDescription(this, description, null);
     }
     @Override
     public final boolean hasNext() {
