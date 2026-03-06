9a0d0b4d92d9e8d940774844bbd92f37de4d7844
thevpc <taha.bensalah@gmail.com>
2026-03-06 07:48:38 +0100

    Update NErrorHandlerIterator.java : fixed-v9

---
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/util/collections/NErrorHandlerIterator.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.thevpc.nuts.runtime.standalone.util.collections;
+
+import net.thevpc.nuts.elem.NDescribables;
+import net.thevpc.nuts.elem.NElement;
+import net.thevpc.nuts.log.NLog;
+import net.thevpc.nuts.text.NMsg;
+import net.thevpc.nuts.util.NIteratorBase;
+import net.thevpc.nuts.util.NIteratorErrorHandlerType;
+
+import java.util.Iterator;
+
+/**
+ *
+ * @author thevpc
+ */
+public class NErrorHandlerIterator<T> extends NIteratorBase<T> {
+
+    private NIteratorErrorHandlerType type;
+    private Iterator<T> base;
+    private RuntimeException ex;
+
+    public NErrorHandlerIterator(NIteratorErrorHandlerType type, Iterator<T> base) {
+        this.base = base;
+        this.type = type;
+    }
+
+    @Override
+    public NElement describe() {
+        return NDescribables.describeResolveOrDestructAsObject(base)
+                .builder()
+                .set("onError",type.toString().toLowerCase())
+                .build();
+    }
+
+
+    @Override
+    public boolean hasNextImpl() {
+        try {
+            boolean v = base.hasNext();
+            ex = null;
+            return v;
+        } catch (RuntimeException ex) {
+            NLog.of(NIndexFirstIterator.class)
+                    .log(NMsg.ofC("error evaluating Iterator 'hasNext()' : %s", ex).asDebug().withThrowable(ex));
+            switch (type) {
+                case IGNORE: {
+                    // do nothing
+                    return false;
+                }
+                case POSTPONE: {
+                    // do nothing
+                    this.ex = ex;
+                    return true;
+                }
+                case THROW: {
+                    throw ex;
+                }
+            }
+        }
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public T next() {
+        if (ex != null) {
+            throw ex;
+        }
+        return base.next();
+    }
+
+    @Override
+    public void remove() {
+        if (ex != null) {
+            throw ex;
+        }
+        base.remove();
+    }
+
+    @Override
+    public String toString() {
+        switch (type){
+            case THROW:return "ThrowOnError("+ base +")";
+            case POSTPONE:return "PostponeError("+ base +")";
+            case IGNORE:return "IgnoreError("+ base +")";
+        }
+        return "ErrorHandlerIterator(" +
+                "type=" + type +
+                ", base=" + base +
+                ')';
+    }
+}
