6717331c796051c1af5c9c2f9850cef36041eda1
thevpc <taha.bensalah@gmail.com>
2026-03-21 19:34:49 +0100

    [FIXED] add NLogger

--- core/nuts-api/src/main/java/net/thevpc/nuts/log/NLogSPI.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/log/NLogSPI.java
@@ -1,13 +1,9 @@
 package net.thevpc.nuts.log;
 
-import net.thevpc.nuts.text.NMsg;
-
 import java.util.logging.Level;
 
-public interface NLogSPI {
+public interface NLogSPI extends NLogger{
     default boolean isLoggable(Level level){
         return true;
     }
-
-    void log(NMsg message);
 }
