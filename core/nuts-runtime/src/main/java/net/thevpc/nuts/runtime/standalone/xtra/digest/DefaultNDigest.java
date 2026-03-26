51b2ff547e8ff343cf1fcdb8ae4aa6aca5f42e45
thevpc <taha.bensalah@gmail.com>
2026-03-26 13:48:47 +0100

    Update DefaultNDigest.java : update

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/xtra/digest/DefaultNDigest.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/xtra/digest/DefaultNDigest.java
@@ -131,43 +131,43 @@ public class DefaultNDigest implements NDigest {
 
     @Override
     public NDigest addSource(InputStream source) {
-        this.setSource0((source == null ? null : NInputSource.of(source)));
+        this.addSource0((source == null ? null : NInputSource.of(source)));
         return this;
     }
 
     @Override
     public NDigest addSource(File source) {
-        this.setSource0((source == null ? null : NPath.of(source)));
+        this.addSource0((source == null ? null : NPath.of(source)));
         return this;
     }
 
     @Override
     public NDigest addSource(Path source) {
-        this.setSource0(source == null ? null : NPath.of(source));
+        this.addSource0(source == null ? null : NPath.of(source));
         return this;
     }
 
     @Override
     public NDigest addSource(URL url) {
-        this.setSource0(url == null ? null : NPath.of(url));
+        this.addSource0(url == null ? null : NPath.of(url));
         return this;
     }
 
     @Override
     public NDigest addSource(NPath source) {
-        this.setSource0(source);
+        this.addSource0(source);
         return this;
     }
 
     @Override
     public NDigest addSource(byte[] source) {
-        this.setSource0(source == null ? null : NInputSource.of(source));
+        this.addSource0(source == null ? null : NInputSource.of(source));
         return this;
     }
 
     @Override
     public NDigest addSource(NDescriptor source) {
-        this.setSource0(source == null ? null : new NDescriptorInputSource(source));
+        this.addSource0(source == null ? null : new NDescriptorInputSource(source));
         return this;
     }
 
