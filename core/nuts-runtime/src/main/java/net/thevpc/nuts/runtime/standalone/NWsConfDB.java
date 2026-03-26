07640d7cf5cb721449c6bf2c488643fbee7f4043
thevpc <taha.bensalah@gmail.com>
2026-03-26 13:48:45 +0100

    Update NWsConfDB.java : update

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/NWsConfDB.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/NWsConfDB.java
@@ -1,6 +1,6 @@
 package net.thevpc.nuts.runtime.standalone;
 
-import net.thevpc.nuts.core.NLocationKey;
+import net.thevpc.nuts.core.NStoreKey;
 import net.thevpc.nuts.core.NWorkspace;
 
 import net.thevpc.nuts.io.NPath;
@@ -13,13 +13,13 @@ public class NWsConfDB {
     public NWsConfDB() {
     }
 
-    public void storeStringNonBlank(NLocationKey k, String value) {
+    public void storeStringNonBlank(NStoreKey k, String value) {
         storeString(k, value, true);
     }
 
-    public void storeString(NLocationKey k, String value, boolean deleteIfBlank) {
-        NPath path = NWorkspace.of().getStoreLocation(k.getId(), k.getStoreType(), k.getRepoUuid())
-                .resolve(k.getName());
+    public void storeString(NStoreKey k, String value, boolean deleteIfBlank) {
+        NPath path = NPath.of(k)
+                .resolve(k.name());
         if (NBlankable.isBlank(value) && deleteIfBlank) {
             if (path.isRegularFile()) {
                 path.delete();
