6dd6f5f8d25c31246a82f2f4a499c9b7f3202417
thevpc <taha.bensalah@gmail.com>
2026-04-01 02:06:59 +0100

    Update NClassPairMap.java : update

---
+++ core/nuts-api/src/main/java/net/thevpc/nuts/reflect/NClassPairMap.java
@@ -0,0 +1,35 @@
+package net.thevpc.nuts.reflect;
+
+import net.thevpc.nuts.internal.rpi.NCollectionsRPI;
+import net.thevpc.nuts.util.NUplet;
+
+import java.util.List;
+import java.util.Set;
+
+public interface NClassPairMap<A,B,V> {
+
+    static <A,B,V> NClassPairMap<A,B,V> of(Class<V> clazz) {
+        return of(null, null, clazz, false);
+    }
+
+    static <A,B,V> NClassPairMap<A,B,V> of(Class<A> key1Type, Class<B> key2Type, Class<V> valueType, boolean symmetric) {
+        return NCollectionsRPI.of().classPairMap(key1Type, key2Type, valueType, symmetric);
+    }
+
+
+    Set<NUplet<Class>> keySet();
+
+    V put(Class<? extends A> classKey1, Class<? extends B> classKey2, V value);
+
+    V remove(Class<? extends A> classKey1, Class<? extends B> classKey2);
+
+    List<NUplet<Class>> getSearchPath(Class<? extends A> classKey1, Class<? extends B> classKey2);
+
+    V getExact(Class<? extends A> classKey1, Class<? extends B> classKey2);
+
+    V get(Class<? extends A> classKey1, Class<? extends B> classKey2);
+
+    List<V> findMatches(Class<? extends A> classKey1, Class<? extends B> classKey2);
+
+    void clear();
+}
