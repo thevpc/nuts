913085b694441073e89586ed4fae9181de853f09
thevpc <taha.bensalah@gmail.com>
2026-04-01 02:06:59 +0100

    Update NReflect.java : update

--- core/nuts-api/src/main/java/net/thevpc/nuts/reflect/NReflect.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/reflect/NReflect.java
@@ -22,4 +22,41 @@ public interface NReflect extends NComponent {
     boolean isImmutableType(NReflectType type);
 
     NTypeLoader createTypeLoader(String name);
+
+    NPlatformSignature ofPlatformSignature(Type... types);
+
+    NPlatformSignature ofVarArgsPlatformSignature(Type... types);
+
+    NPlatformSignature ofPlatformSignature(String name, Type... types);
+
+    NPlatformSignature ofVarArgsPlatformSignature(String name, Type... types);
+
+    NTypeNameSignature ofTypeNameSignature(NTypeNameDomain domain, NTypeName... types);
+
+    NTypeNameSignature ofVarArgsTypeNameSignature(NTypeNameDomain domain, NTypeName... types);
+
+    NTypeNameSignature ofTypeNameSignature(NTypeNameDomain domain, String name, NTypeName... types);
+
+    NTypeNameSignature ofVarArgsTypeNameSignature(NTypeNameDomain domain, String name, NTypeName... types);
+
+    NReflectSignature ofReflectSignature(NReflectType... types);
+
+    NReflectSignature ofVarArgsReflectSignature(NReflectType... types);
+
+    NReflectSignature ofReflectSignature(String name, NReflectType... types);
+
+    NReflectSignature ofVarArgsReflectSignature(String name, NReflectType... types);
+
+    <S extends NSignature<T, ?>, T, V> NSignatureMap<S, T, V> ofSignatureMap(NSignatureDomain<T> domain);
+
+    <V> NSignatureMap<NPlatformSignature, Type, V> ofPlatformSignatureMap(NSignatureDomain<Type> domain);
+
+    <V> NSignatureMap<NPlatformSignature, Type, V> ofPlatformSignatureMap();
+
+    <V> NSignatureMap<NReflectSignature, NReflectType, V> ofReflectSignatureMap();
+
+    <V> NSignatureMap<NTypeNameSignature, NTypeName<?>, V> ofTYpeNameSignatureMap(NTypeNameDomain domain);
+
+    NTypeNamePlatformDomain platformDomain();
+
 }
