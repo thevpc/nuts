bd52da28861b63c35f7204eec705ea0367f9e423
thevpc <taha.bensalah@gmail.com>
2026-04-01 02:07:00 +0100

    Update CollectionsTest.java : update

--- test/nuts-runtime-test/src/test/java/net/thevpc/nuts/core/test/CollectionsTest.java
+++ test/nuts-runtime-test/src/test/java/net/thevpc/nuts/core/test/CollectionsTest.java
@@ -1,85 +1,89 @@
 package net.thevpc.nuts.core.test;
 
 import net.thevpc.nuts.core.test.utils.TestUtils;
+import net.thevpc.nuts.reflect.NPlatformSignature;
+import net.thevpc.nuts.runtime.standalone.reflect.NPlatformSignatureImpl;
+import net.thevpc.nuts.runtime.standalone.reflect.NSignatureMapImpl;
 import net.thevpc.nuts.util.NEvictingCharQueue;
-import net.thevpc.nuts.reflect.NPlatformArgsSignature;
-import net.thevpc.nuts.reflect.NPlatformSignatureMap;
+import net.thevpc.nuts.reflect.NSignatureMap;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Type;
 
 public class CollectionsTest {
 
     @Test
-    public void testEvictingCharQueue1(){
-        NEvictingCharQueue q=new NEvictingCharQueue(1);
+    public void testEvictingCharQueue1() {
+        NEvictingCharQueue q = new NEvictingCharQueue(1);
         TestUtils.println(q);
 
         q.add('a');
-        Assertions.assertEquals("a",q.toString());
+        Assertions.assertEquals("a", q.toString());
         q.add('b');
-        Assertions.assertEquals("b",q.toString());
+        Assertions.assertEquals("b", q.toString());
         q.add('c');
-        Assertions.assertEquals("c",q.toString());
+        Assertions.assertEquals("c", q.toString());
         q.add('d');
-        Assertions.assertEquals("d",q.toString());
+        Assertions.assertEquals("d", q.toString());
         q.add('e');
-        Assertions.assertEquals("e",q.toString());
+        Assertions.assertEquals("e", q.toString());
         q.add('f');
-        Assertions.assertEquals("f",q.toString());
+        Assertions.assertEquals("f", q.toString());
         q.add('g');
-        Assertions.assertEquals("g",q.toString());
+        Assertions.assertEquals("g", q.toString());
     }
 
     @Test
-    public void testEvictingCharQueue2(){
-        NEvictingCharQueue q=new NEvictingCharQueue(2);
+    public void testEvictingCharQueue2() {
+        NEvictingCharQueue q = new NEvictingCharQueue(2);
         TestUtils.println(q);
 
         q.add('a');
-        Assertions.assertEquals("a",q.toString());
+        Assertions.assertEquals("a", q.toString());
         q.add('b');
-        Assertions.assertEquals("ab",q.toString());
+        Assertions.assertEquals("ab", q.toString());
         q.add('c');
-        Assertions.assertEquals("bc",q.toString());
+        Assertions.assertEquals("bc", q.toString());
         q.add('d');
-        Assertions.assertEquals("cd",q.toString());
+        Assertions.assertEquals("cd", q.toString());
         q.add('e');
-        Assertions.assertEquals("de",q.toString());
+        Assertions.assertEquals("de", q.toString());
         q.add('f');
-        Assertions.assertEquals("ef",q.toString());
+        Assertions.assertEquals("ef", q.toString());
         q.add('g');
-        Assertions.assertEquals("fg",q.toString());
+        Assertions.assertEquals("fg", q.toString());
     }
 
     @Test
-    public void testEvictingCharQueue3(){
-        NEvictingCharQueue q=new NEvictingCharQueue(3);
+    public void testEvictingCharQueue3() {
+        NEvictingCharQueue q = new NEvictingCharQueue(3);
         TestUtils.println(q);
 
         q.add('a');
-        Assertions.assertEquals("a",q.toString());
+        Assertions.assertEquals("a", q.toString());
         q.add('b');
-        Assertions.assertEquals("ab",q.toString());
+        Assertions.assertEquals("ab", q.toString());
         q.add('c');
-        Assertions.assertEquals("abc",q.toString());
+        Assertions.assertEquals("abc", q.toString());
         q.add('d');
-        Assertions.assertEquals("bcd",q.toString());
+        Assertions.assertEquals("bcd", q.toString());
         q.add('e');
-        Assertions.assertEquals("cde",q.toString());
+        Assertions.assertEquals("cde", q.toString());
         q.add('f');
-        Assertions.assertEquals("def",q.toString());
+        Assertions.assertEquals("def", q.toString());
         q.add('g');
-        Assertions.assertEquals("efg",q.toString());
+        Assertions.assertEquals("efg", q.toString());
     }
 
     @Test
     public void test01() {
-        NPlatformSignatureMap<NPlatformArgsSignature> cm=new NPlatformSignatureMap<NPlatformArgsSignature>(NPlatformArgsSignature.class);
-        cm.put(NPlatformArgsSignature.of(CharSequence.class,int.class), NPlatformArgsSignature.of(CharSequence.class,int.class));
-        cm.put(NPlatformArgsSignature.of(String.class,Number.class), NPlatformArgsSignature.of(String.class,Number.class));
+        NSignatureMap<NPlatformSignature, Type, Object> cm = new NSignatureMapImpl<>(NPlatformSignatureImpl.DOMAIN);
+        cm.put(NPlatformSignatureImpl.of(CharSequence.class, int.class), NPlatformSignatureImpl.of(CharSequence.class, int.class));
+        cm.put(NPlatformSignatureImpl.of(String.class, Number.class), NPlatformSignatureImpl.of(String.class, Number.class));
         Assertions.assertEquals(
-                NPlatformArgsSignature.of(CharSequence.class,int.class),
-                cm.get(NPlatformArgsSignature.of(String.class,Integer.class)).get()
+                NPlatformSignatureImpl.of(CharSequence.class, int.class),
+                cm.get(NPlatformSignatureImpl.of(String.class, Integer.class)).get()
         );
     }
 
