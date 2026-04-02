0e7b03871bce4cf533fa9b0b329e9f9ddfd14ea9
thevpc <taha.bensalah@gmail.com>
2026-04-02 03:27:07 +0100

    Update NEvictingIntQueue.java : update

--- core/nuts-api/src/main/java/net/thevpc/nuts/util/NEvictingIntQueue.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/util/NEvictingIntQueue.java
@@ -1,66 +1,16 @@
 package net.thevpc.nuts.util;
 
-import java.util.Arrays;
+import net.thevpc.nuts.internal.rpi.NCollectionsRPI;
 
-public class NEvictingIntQueue {
-    private int from=0;
-    private int len;
-    private int[] values;
-
-    public NEvictingIntQueue(int max) {
-        this.values=new int[max];
+public interface NEvictingIntQueue {
+    static NEvictingIntQueue of(int size) {
+        return NCollectionsRPI.of().createEvictingIntQueue(size);
     }
+    void clear();
 
-    public void clear() {
-        from=0;
-        len=0;
-    }
+    int size();
 
-    public int size() {
-        return len;
-    }
+    int get(int pos);
 
-    public int get(int pos) {
-        if(pos>=0 && pos<len) {
-            int i=(from+pos)%values.length;
-            return values[i];
-        }
-        throw new IllegalArgumentException("invalid index "+pos);
-    }
-
-    public void add(int t) {
-        int pos=(from+len)%values.length;
-        values[pos]=t;
-        if(len<values.length){
-            len++;
-        }else{
-            from=(from+1)%values.length;
-        }
-    }
-
-    public String dump() {
-        StringBuilder sb=new StringBuilder();
-        for (int i = 0; i < size(); i++) {
-            sb.append(get(i));
-        }
-        return "EvictingIntQueue{" +
-                "from=" + from +
-                ", len=" + len +
-                ", raw=" + Arrays.toString(values) +
-                ", values='" + sb +"'"+
-                '}';
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb=new StringBuilder("{");
-        for (int i = 0; i < size(); i++) {
-            if(i>0){
-                sb.append(",");
-            }
-            sb.append(get(i));
-        }
-        sb.append("}");
-        return sb.toString();
-    }
+    void add(int t);
 }
