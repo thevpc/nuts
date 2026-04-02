91daee7431ccabadb98a55d1eea4cd291139d78e
thevpc <taha.bensalah@gmail.com>
2026-04-02 03:27:07 +0100

    Update NEvictingCharQueue.java : update

--- core/nuts-api/src/main/java/net/thevpc/nuts/util/NEvictingCharQueue.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/util/NEvictingCharQueue.java
@@ -1,62 +1,17 @@
 package net.thevpc.nuts.util;
 
-import java.util.Arrays;
+import net.thevpc.nuts.internal.rpi.NCollectionsRPI;
 
-public class NEvictingCharQueue {
-    private int from=0;
-    private int len;
-    private char[] values;
-
-    public NEvictingCharQueue(int max) {
-        this.values=new char[max];
+public interface NEvictingCharQueue {
+    static NEvictingCharQueue of(int size) {
+        return NCollectionsRPI.of().createEvictingCharQueue(size);
     }
 
-    public void clear() {
-        from=0;
-        len=0;
-    }
+    void clear();
 
-    public int size() {
-        return len;
-    }
+    int size();
 
-    public char get(int pos) {
-        if(pos>=0 && pos<len) {
-            int i=(from+pos)%values.length;
-            return values[i];
-        }
-        throw new IllegalArgumentException("invalid index "+pos);
-    }
+    char get(int pos);
 
-    public void add(char t) {
-        int pos=(from+len)%values.length;
-        values[pos]=t;
-        if(len<values.length){
-            len++;
-        }else{
-            from=(from+1)%values.length;
-        }
-    }
-
-    public String dump() {
-        StringBuilder sb=new StringBuilder();
-        for (int i = 0; i < size(); i++) {
-            sb.append(get(i));
-        }
-        return "EvictingCharQueue{" +
-                "from=" + from +
-                ", len=" + len +
-                ", raw=" + Arrays.toString(values) +
-                ", values='" + sb +"'"+
-                '}';
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb=new StringBuilder();
-        for (int i = 0; i < size(); i++) {
-            sb.append(get(i));
-        }
-        return sb.toString();
-    }
+    void add(char t);
 }
