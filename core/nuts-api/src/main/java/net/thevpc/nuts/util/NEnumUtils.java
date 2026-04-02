c4b756f908bbe6cd6c0b4c169f41e5b36186ca28
thevpc <taha.bensalah@gmail.com>
2026-04-02 03:27:07 +0100

    Update NEnumUtils.java : update

--- core/nuts-api/src/main/java/net/thevpc/nuts/util/NEnumUtils.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/util/NEnumUtils.java
@@ -2,6 +2,7 @@ package net.thevpc.nuts.util;
 
 import net.thevpc.nuts.text.NMsg;
 
+import java.util.Collection;
 import java.util.function.Function;
 
 public class NEnumUtils {
@@ -19,7 +20,7 @@ public class NEnumUtils {
         }
     }
 
-    public static <T extends Enum> NOptional<T> parseEnum(String value, Class<T> type, Function<EnumValue, NOptional<T>> mapper) {
+    public static <T extends Enum> NOptional<T> parseEnum(String value, Class<T> type, Function<NEnumCandidate, NOptional<T>> mapper) {
         if (NBlankable.isBlank(value)) {
             return NOptional.ofEmpty(() -> NMsg.ofC("%s is empty", type.getSimpleName()));
         }
@@ -27,7 +28,7 @@ public class NEnumUtils {
         String normalizedValue = NNameFormat.CONST_NAME.format(parsedValue);
         if (mapper != null) {
             try {
-                NOptional<T> o = mapper.apply(new EnumValue(
+                NOptional<T> o = mapper.apply(new NEnumCandidate(
                         value,
                         normalizedValue,
                         parsedValue
@@ -45,19 +46,46 @@ public class NEnumUtils {
             return NOptional.ofError(() -> NMsg.ofC("%s invalid value : %s", type.getSimpleName(), value), notFound);
         }
     }
+    public static <T extends NEnum> NOptional<T> parseEnum(String value, Class<T> type, Collection<T> knownValues, Function<NEnumCandidate, NOptional<T>> mapper) {
+        if (NBlankable.isBlank(value)) {
+            return NOptional.ofEmpty(() -> NMsg.ofC("%s is empty", type.getSimpleName()));
+        }
+        String[] parsedValue = NNameFormat.parse(value);
+        String normalizedValue = NNameFormat.CONST_NAME.format(parsedValue);
+        if (mapper != null) {
+            try {
+                NOptional<T> o = mapper.apply(new NEnumCandidate(
+                        value,
+                        normalizedValue,
+                        parsedValue
+                ));
+                if (o != null) {
+                    return o;
+                }
+            } catch (Exception notFound) {
+                //just ignore
+            }
+        }
+        for (T v : knownValues) {
+            if (NNameFormat.CONST_NAME.format(v.id()).equals(normalizedValue)) {
+                return NOptional.of(v);
+            }
+        }
+        return NOptional.ofError(() -> NMsg.ofC("%s invalid value : %s", type.getSimpleName(), value));
+    }
 
-    public static class EnumValue {
+    public static class NEnumCandidate {
         private String value;
         private String normalizedValue;
-        private String[] parsedValue;
+        private String[] parsedParts;
 
-        public EnumValue(String value, String normalizedValue, String[] parsedValue) {
+        public NEnumCandidate(String value, String normalizedValue, String[] parsedParts) {
             this.value = value;
             this.normalizedValue = normalizedValue;
-            this.parsedValue = parsedValue;
+            this.parsedParts = parsedParts;
         }
 
-        public String getValue() {
+        public String value() {
             return value;
         }
 
@@ -65,12 +93,12 @@ public class NEnumUtils {
          * enum name formatted using NNameFormat.CONST_NAME
          * @return name formatted using NNameFormat.CONST_NAME
          */
-        public String getNormalizedValue() {
+        public String normalizedValue() {
             return normalizedValue;
         }
 
-        public String[] getParsedValue() {
-            return parsedValue;
+        public String[] parsedParts() {
+            return parsedParts;
         }
     }
 }
