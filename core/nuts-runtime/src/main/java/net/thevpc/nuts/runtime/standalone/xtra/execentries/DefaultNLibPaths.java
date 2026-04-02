d533bc844f1941e82fda9c2386c984ec0ae28fd9
thevpc <taha.bensalah@gmail.com>
2026-04-02 03:27:09 +0100

    Update DefaultNLibPaths.java : update

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/xtra/execentries/DefaultNLibPaths.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/xtra/execentries/DefaultNLibPaths.java
@@ -1,152 +1,36 @@
-package net.thevpc.nuts.runtime.standalone.xtra.execentries;
-
-import net.thevpc.nuts.app.NAppDefinition;
-import net.thevpc.nuts.artifact.NId;
-import net.thevpc.nuts.command.NExecutionEntry;
-import net.thevpc.nuts.core.NWorkspace;
-import net.thevpc.nuts.io.NIOException;
-import net.thevpc.nuts.io.NLibPaths;
-import net.thevpc.nuts.io.NPath;
-
-import net.thevpc.nuts.log.NLog;
-import net.thevpc.nuts.log.NMsgIntent;
-import net.thevpc.nuts.runtime.standalone.repository.impl.maven.pom.NPomXmlParser;
-import net.thevpc.nuts.runtime.standalone.repository.impl.maven.pom.api.NPomId;
-import net.thevpc.nuts.runtime.standalone.repository.impl.maven.util.MavenUtils;
-import net.thevpc.nuts.runtime.standalone.util.jclass.JavaClassUtils;
-import net.thevpc.nuts.runtime.standalone.util.jclass.JavaJarUtils;
-import net.thevpc.nuts.runtime.standalone.xtra.idresolver.NMetaInfIdResolver;
-import net.thevpc.nuts.util.*;
-import net.thevpc.nuts.text.NMsg;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.*;
-import java.util.logging.Level;
-import java.util.stream.Collectors;
-
-@NScore(fixed = NScorable.DEFAULT_SCORE)
-public class DefaultNLibPaths implements NLibPaths {
-
-    public DefaultNLibPaths() {
-    }
-
-    @Override
-    public List<NExecutionEntry> parseExecutionEntries(NPath file) {
-        if (file.getName().toLowerCase().endsWith(".jar")) {
-            try {
-                try (InputStream in = file.getInputStream()) {
-                    return parseExecutionEntries(in, "jar", file.toAbsolute().normalize().toString());
-                }
-            } catch (IOException ex) {
-                throw new NIOException(ex);
-            }
-        } else if (file.getName().toLowerCase().endsWith(".class")) {
-            try {
-                try (InputStream in = file.getInputStream()) {
-                    return parseExecutionEntries(in, "class", file.toAbsolute().normalize().toString());
-                }
-            } catch (IOException ex) {
-                throw new NIOException(ex);
-            }
-        } else {
-            return Collections.emptyList();
-        }
-    }
-
-    @Override
-    public List<NExecutionEntry> parseExecutionEntries(InputStream inputStream, String type, String sourceName) {
-        if ("jar".equals(type)) {
-            return JavaJarUtils.parseJarExecutionEntries(inputStream);
-        } else if ("class".equals(type)) {
-            NExecutionEntry u = JavaClassUtils.parseClassExecutionEntry(inputStream, sourceName);
-            return u == null ? Collections.emptyList() : Arrays.asList(u);
-        }
-        return Collections.emptyList();
-    }
-
-    @Override
-    public List<NPath> resolveLibPaths(Class<?> clazz) {
-        return JavaClassUtils.resolveURLs(clazz).stream().map(x->NPath.of(x)).collect(Collectors.toList());
-    }
-
-    @Override
-    public NOptional<NPath> resolveLibPath(Class<?> clazz) {
-        List<NPath> c = resolveLibPaths(clazz);
-        return c.isEmpty() ?
-                NOptional.ofNamedEmpty("LibPath fo "+clazz)
-                : NOptional.of(c.get(0));
-    }
-
-    @Override
-    public NOptional<NId> resolveId(Class<?> clazz) {
-        clazz=JavaClassUtils.unwrapCGLib(clazz);
-        List<NId> pomIds = resolveIds(clazz);
-        NId defaultValue = null;
-        if (pomIds.isEmpty()) {
-            return NOptional.ofNamedEmpty("Id fo "+clazz);
-        }
-        if (pomIds.size() > 1) {
-            NLog.of(NPomXmlParser.class)
-                    .log(NMsg.ofC(
-                            "multiple ids found : %s for class %s and id %s",
-                            Arrays.asList(pomIds), clazz, defaultValue
-                    ).withIntent(NMsgIntent.ALERT)
-                            .withLevel(Level.FINEST));
-        }
-        return NOptional.of(pomIds.get(0));
-    }
-
-    @Override
-    public NOptional<NId> resolveId(NPath path) {
-        List<NId> pomIds = resolveIds(path);
-        NId defaultValue = null;
-        if (pomIds.isEmpty()) {
-            return NOptional.ofNamedEmpty("Id fo "+path);
-        }
-        if (pomIds.size() > 1) {
-            NLog.of(NPomXmlParser.class)
-
-                    .log(NMsg.ofC(
-                            "multiple ids found : %s for path %s and id %s",
-                            Arrays.asList(pomIds), path, defaultValue
-                    ).withIntent(NMsgIntent.ALERT)
-                            .withLevel(Level.FINEST));
-        }
-        return NOptional.of(pomIds.get(0));
-    }
-
-    @Override
-    public List<NId> resolveIds(NPath path) {
-        LinkedHashSet<NId> all = new LinkedHashSet<>();
-        NPomId[] u = MavenUtils.createPomIdResolver(NWorkspace.of()).resolvePomIds(path);
-        all.addAll(
-                Arrays.asList(new NMetaInfIdResolver().resolvePomIds(path))
-        );
-        for (NPomId uu : u) {
-            all.add(NId.get(uu.getGroupId() + ":" + uu.getArtifactId() + "#" + uu.getVersion()).get());
-        }
-        return new ArrayList<>(all);
-    }
-
-    @Override
-    public List<NId> resolveIds(Class<?> clazz) {
-        clazz=JavaClassUtils.unwrapCGLib(clazz);
-        LinkedHashSet<NId> all = new LinkedHashSet<>();
-        NAppDefinition annotation = (NAppDefinition) clazz.getAnnotation(NAppDefinition.class);
-        if (annotation != null) {
-            if (!NBlankable.isBlank(annotation.id())) {
-                all.add(NId.get(annotation.id()).get());
-            }
-        }
-        NPomId[] u = MavenUtils.createPomIdResolver(NWorkspace.of()).resolvePomIds(clazz);
-        all.addAll(
-                Arrays.asList(new NMetaInfIdResolver().resolvePomIds(clazz))
-        );
-        for (NPomId uu : u) {
-            all.add(NId.get(uu.getGroupId() + ":" + uu.getArtifactId() + "#" + uu.getVersion()).get());
-        }
-        return new ArrayList<>(all);
-    }
-
-}
+//package net.thevpc.nuts.runtime.standalone.xtra.execentries;
+//
+//import net.thevpc.nuts.app.NAppDefinition;
+//import net.thevpc.nuts.artifact.NId;
+//import net.thevpc.nuts.command.NExecutionEntry;
+//import net.thevpc.nuts.core.NWorkspace;
+//import net.thevpc.nuts.io.NIOException;
+//import net.thevpc.nuts.io.NLibPaths;
+//import net.thevpc.nuts.io.NPath;
+//
+//import net.thevpc.nuts.log.NLog;
+//import net.thevpc.nuts.log.NMsgIntent;
+//import net.thevpc.nuts.runtime.standalone.repository.impl.maven.pom.NPomXmlParser;
+//import net.thevpc.nuts.runtime.standalone.repository.impl.maven.pom.api.NPomId;
+//import net.thevpc.nuts.runtime.standalone.repository.impl.maven.util.MavenUtils;
+//import net.thevpc.nuts.runtime.standalone.util.jclass.JavaClassUtils;
+//import net.thevpc.nuts.runtime.standalone.util.jclass.JavaJarUtils;
+//import net.thevpc.nuts.runtime.standalone.xtra.idresolver.NMetaInfIdResolver;
+//import net.thevpc.nuts.util.*;
+//import net.thevpc.nuts.text.NMsg;
+//
+//import java.io.IOException;
+//import java.io.InputStream;
+//import java.util.*;
+//import java.util.logging.Level;
+//import java.util.stream.Collectors;
+//
+//@NScore(fixed = NScorable.DEFAULT_SCORE)
+//public class DefaultNLibPaths implements NLibPaths {
+//
+//    public DefaultNLibPaths() {
+//    }
+//
+//
+//
+//}
