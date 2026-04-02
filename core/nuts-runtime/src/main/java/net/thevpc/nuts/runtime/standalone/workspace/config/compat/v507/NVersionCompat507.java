4a52583348dfabd9f016d532ae684e79087603a4
thevpc <taha.bensalah@gmail.com>
2026-04-02 03:27:09 +0100

    Update NVersionCompat507.java : update

--- core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/workspace/config/compat/v507/NVersionCompat507.java
+++ core/nuts-runtime/src/main/java/net/thevpc/nuts/runtime/standalone/workspace/config/compat/v507/NVersionCompat507.java
@@ -8,8 +8,6 @@ import net.thevpc.nuts.core.NWorkspace;
 import net.thevpc.nuts.elem.NElementReader;
 
 
-import net.thevpc.nuts.platform.NStoreScope;
-import net.thevpc.nuts.platform.NStoreType;
 import net.thevpc.nuts.io.NPath;
 import net.thevpc.nuts.runtime.standalone.workspace.config.*;
 import net.thevpc.nuts.runtime.standalone.util.CoreNConstants;
@@ -21,7 +19,7 @@ import java.util.stream.Collectors;
 
 public class NVersionCompat507 extends AbstractNVersionCompat {
 
-    public static final NVersion CONFIG_VERSION_507 = NVersion.get("5.0.7").get();
+    public static final NVersion CONFIG_VERSION_507 = NVersion.getPartAt("5.0.7").get();
 
     public NVersionCompat507(NVersion apiVersion) {
         super(apiVersion, 507);
