d896f80fafcfeab9b5b5ad252c7146dfc78a3527
thevpc <taha.bensalah@gmail.com>
2026-04-14 18:24:50 +0100

    [ADDED] added API builders for Expr

--- core/nuts-api/src/main/java/net/thevpc/nuts/expr/NExprVar.java
+++ core/nuts-api/src/main/java/net/thevpc/nuts/expr/NExprVar.java
@@ -1,30 +1,52 @@
 package net.thevpc.nuts.expr;
 
 import java.util.Map;
+import java.util.function.Function;
 
 public interface NExprVar {
     static NExprVar ofVar(String name) {
         return NExprs.of().newVar(name);
     }
 
-    static NExprVar ofVar(String name,Object value) {
-        return NExprs.of().newVar(name,value);
+    static NExprVar ofVar(String name, Object value) {
+        return NExprs.of().newVar(name, value);
     }
 
     static NExprVar ofConst(String name, Object value) {
         return NExprs.of().newConst(name, value);
     }
 
-    static NExprVar ofMap(Map<String,Object> vars){
+    static NExprVar ofConsts(Function<String, Object> vars) {
         return new NExprVar() {
             @Override
-            public Object get(String s, NExprDeclarations ctx) { return vars.get(s); }
+            public Object get(String s, NExprDeclarations ctx) {
+                return vars == null ? null : vars.apply(s);
+            }
+
             @Override
             public Object set(String s, Object o, NExprDeclarations ctx) {
-                vars.put(s, o); return o;
+                return vars == null ? null : vars.apply(s);
             }
         };
     }
+
+    static NExprVar ofMap(Map<String, Object> vars) {
+        return new NExprVar() {
+            @Override
+            public Object get(String s, NExprDeclarations ctx) {
+                return vars == null ? null : vars.get(s);
+            }
+
+            @Override
+            public Object set(String s, Object o, NExprDeclarations ctx) {
+                if (vars != null) {
+                    vars.put(s, o);
+                }
+                return o;
+            }
+        };
+    }
+
     Object get(String name, NExprDeclarations context);
 
     Object set(String name, Object value, NExprDeclarations context);
