db6d37c33a74f50127f3cde245517ea11b1071e0
thevpc <taha.bensalah@gmail.com>
2026-06-14 17:21:09 +0100

    [FIXED] fixed version

--- core/nuts-boot/src/main/java/net/thevpc/nuts/boot/NBootWorkspace.java
+++ core/nuts-boot/src/main/java/net/thevpc/nuts/boot/NBootWorkspace.java
@@ -7,7 +7,7 @@ import net.thevpc.nuts.boot.internal.util.NBootUtils;
 import java.util.Arrays;
 
 public interface NBootWorkspace {
-    String NUTS_BOOT_VERSION = "0.8.9";
+    String NUTS_BOOT_VERSION = "1.0.0";
 
     static NBootWorkspace of(String[] args) {
         return of(NBootArguments.of(args));
