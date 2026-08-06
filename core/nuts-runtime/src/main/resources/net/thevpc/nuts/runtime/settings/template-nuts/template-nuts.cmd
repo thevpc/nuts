2fdd7111b4f2d1202f105ed0b01f46a2d9593509
thevpc <taha.bensalah@gmail.com>
2026-08-06 14:26:47 +0100

    [ADDED] WIP added shell autocomplete

--- core/nuts-runtime/src/main/resources/net/thevpc/nuts/runtime/settings/template-nuts/template-nuts.cmd
+++ core/nuts-runtime/src/main/resources/net/thevpc/nuts/runtime/settings/template-nuts/template-nuts.cmd
@@ -34,13 +34,6 @@ if not defined _java (
 )
 
 REM "openjdk version "21.0.10" ..." / "java version "1.8.0_202"" -> token 3
-REM --- resolve java version safely ---
-set "_jver="
-for /f "tokens=3" %%V in ('"%_java%" -version 2^>^&1 ^| findstr /i "version"') do (
-    if not defined _jver set "_jver=%%~V"
-)
-
-REM "openjdk version "21.0.10" ..." / "java version "1.8.0_202"" -> token 3
 set "_jver="
 for /f "tokens=3" %%V in ('@ "%_java%" -version 2^>^&1 ^| findstr /i "version"') do if not defined _jver set "_jver=%%~V"
 
