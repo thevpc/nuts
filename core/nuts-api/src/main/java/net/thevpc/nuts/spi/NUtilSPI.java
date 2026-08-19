7864b4809e269afc92e55f03a3b1d86ae5d9cc75
thevpc <taha.bensalah@gmail.com>
2026-08-19 11:01:17 +0100

    [REMOVE] removed unused files

--- core/nuts-api/src/main/java/net/thevpc/nuts/spi/NUtilSPI.java
+++
@@ -1,8 +0,0 @@
-package net.thevpc.nuts.spi;
-
-import net.thevpc.nuts.reflect.NScorable;
-import net.thevpc.nuts.reflect.NScorableQuery;
-
-public interface NUtilSPI extends NComponent{
-    <T extends NScorable> NScorableQuery<T> ofScorableQuery();
-}
