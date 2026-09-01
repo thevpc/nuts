694619c4f738d2edbcab1202fab031a90ecfe389
thevpc <taha.bensalah@gmail.com>
2026-09-01 19:48:59 +0100

    [UPDATED] update documentation

--- docs/assets/js/common.js
+++ docs/assets/js/common.js
@@ -1,11 +1,11 @@
 /* common.js (templated by nsite) */
-var buildTime = "$2026-09-01-182510";
-var latestJarLocation = "$https://maven.thevpc.net/net/thevpc/nuts/nuts-app/1.0.0/nuts-app-1.0.0.jar";
-var apiVersion = "$1.0.0";
-var runtimeVersion = "$1.0.0.0";
+var buildTime = "2026-09-01-194835";
+var latestJarLocation = "https://maven.thevpc.net/net/thevpc/nuts/nuts-app/1.0.0/nuts-app-1.0.0.jar";
+var apiVersion = "1.0.0";
+var runtimeVersion = "1.0.0.0";
 
-var stableJarLocation = "$https://maven.thevpc.net/net/thevpc/nuts/nuts-app/0.8.9/nuts-app-0.8.9.jar";
-var stableApiVersion = "$0.8.9";
-var stableRuntimeVersion = "$0.8.9.0";
+var stableJarLocation = "https://maven.thevpc.net/net/thevpc/nuts/nuts-app/0.8.9/nuts-app-0.8.9.jar";
+var stableApiVersion = "0.8.9";
+var stableRuntimeVersion = "0.8.9.0";
 
 document.getElementById('build-time').textContent = buildTime;
